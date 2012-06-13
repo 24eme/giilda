5de2089c0f5c22f43f46ad60f538417e667763b2
mathurin <mpetit@actualys.com>
2012-06-13 16:11:08 +0200

    Modification de etape_item pour avoir des étapes de meme taille

--- project/plugins/VracPlugin/modules/vrac/templates/_etapeItem.php
+++ project/plugins/VracPlugin/modules/vrac/templates/_etapeItem.php
@@ -1,13 +1,22 @@
-<?php if($num_etape == 0 && $vrac->etape == 0) echo "<a href=".url_for('vrac_nouveau').">";
-      else
-            if($vrac->etape >= $num_etape) echo "<a href=".url_for($url_etape,$vrac).">";
-            echo '<li class="';
-            if($actif == $num_etape+1) echo 'actif';
-                    else if($actif > $num_etape) echo 'passe';
-            echo '">';
-            echo '<span>1. <span>'.$label.'</span></span>';     
-            echo '</li>';
-            if($vrac->etape >= $num_etape) echo '</a>';
-    if($num_etape == 0 && $vrac->etape == 0) echo "</a>";
-            
-?>     
+<?php
+$liClass = '';
+if($actif == $num_etape+1) $liClass = 'actif';
+  else if($actif > $num_etape) $liClass = 'passe';
+
+ $href ='';
+  if($num_etape == 0 && $vrac->etape == 0) $href = 'href="'.url_for('vrac_nouveau').'"';
+  else if($vrac->etape >= $num_etape) $href = 'href="'.url_for($url_etape,$vrac).'"';
+?>
+
+
+<li class="<?php echo $liClass; ?>">
+    <a <?php echo $href; ?>>
+        <?php if($actif == $num_etape+1) echo '<strong>'; ?>
+        <span><?php echo $num_etape;?>. </span>
+        <?php echo $label; ?> 
+        <?php if($actif == $num_etape+1) echo '</strong>'; ?>
+    </a>    
+</li>
+
+
+   
