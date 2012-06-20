e4fee0d5f6792f3936b7e7b614c509628e125890
mathurin <mpetit@actualys.com>
2012-06-20 17:26:53 +0200

    Mis en place de la colonne de droite pour les contrats vrac

---
+++ modules/vrac/templates/_actions.php
@@ -0,0 +1,20 @@
+<aside id="colonne">	
+    <div id="action" class="bloc_col">
+            <h2>Action</h2>
+
+            <div class="contenu">
+            </div>
+    </div>
+
+    <div id="contrat_aide" class="bloc_col">
+            <h2>Aide</h2>
+
+            <div class="contenu">
+                    <ul>
+                            <li class="raccourcis"><a href="#">Raccourcis clavier</a></li>
+                            <li class="assistance"><a href="#">Assistance</a></li>
+                            <li class="contact"><a href="#">Contacter le support</a></li>
+                    </ul>
+            </div>
+    </div>	
+</aside>
