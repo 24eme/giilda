6fd9f4362e4c81d727b5c35fe2c71c0b35f03404
mathurin <mpetit@actualys.com>
2012-06-20 12:10:21 +0200

    petite modif

--- project/plugins/VracPlugin/modules/vrac/templates/_table_contrats.php
+++ project/plugins/VracPlugin/modules/vrac/templates/_table_contrats.php
@@ -2,26 +2,34 @@
 use_helper('Vrac');
 ?>
 <div style="margin: 10px;">
-<p><form>
-<input name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>"/> <input type="submit" value="recherche"/>
-</form></p>
+    <p>
+        <form>
+            <input name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>"/> <input type="submit" value="recherche"/>
+        </form>
+    </p>
 </div>
 <style>
-.odd {background-color: #BBBBBB;}
+.odd {background-color: #BBBBBB; width: 700px;}
 td{padding: 0px 10px;}
+.status { width: 5%; }
+.num_contrat { width: 10%; }
+.soussigne { width: 15%; }
+.type { width: 10%; }
+.vol { width: 5%; }
 </style>
+
 <table>    
     <thead>
-        <tr class="odd">
-            <th>Statut</th>
-            <th>N° Contrat</th>
-            <th>Acheteur</th>
-            <th>Vendeur</th>
-            <th>Mandataire</th>
-            <th>Type</th>
-            <th>Produit</th>
-            <th>Vol. com.</th>
-            <th>Vol. enlv.</th>
+        <tr class="odd" style="width:300px;">
+            <th class="status">Statut</th>
+            <th class="num_contrat">N° Contrat</th>
+            <th class="soussigne">Acheteur</th>
+            <th class="soussigne">Vendeur</th>
+            <th class="soussigne">Mandataire</th>
+            <th class="type">Type</th>
+            <th class="soussigne">Produit</th>
+            <th class="vol">Vol. com.</th>
+            <th class="vol">Vol. enlv.</th>
         </tr>
     </thead>
     <tbody>
@@ -29,8 +37,8 @@ td{padding: 0px 10px;}
 foreach ($vracs->rows as $value) {    $cpt *= -1;
             $elt = $value->getRawValue()->value;
         ?>
-        <tr<?php if($cpt > 0) echo ' class="odd"'; ?>>
-            <td>
+        <tr<?php if($cpt > 0) echo ' class="odd"'; ?>  style="width:300px;">
+            <td class="status">
                 
                 <?php 
                       $statutImg = statutImg($elt[0]);
@@ -40,14 +48,14 @@ foreach ($vracs->rows as $value) {    $cpt *= -1;
                             src="<?php echo $statutImg->src; ?>" />
                 <?php } ?>
             </td>
-	      <td><?php $vracid = preg_replace('/VRAC-/', '', $elt[1]); echo link_to($vracid, '@vrac_termine?numero_contrat='.$vracid); ?></td>
-	      <td><?php echo ($elt[2]) ? link_to($elt[3], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[2])) : ''; ?></td>
-	      <td><?php echo ($elt[4]) ? link_to($elt[5], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[4])) : ''; ?></td>
-	      <td><?php echo ($elt[6]) ? link_to($elt[7], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[6])) : ''; ?></td>
-              <td><?php echo ($elt[8])? typeProduit($elt[8]) : ''; ?></td>
-	      <td><?php echo ($elt[9])? ConfigurationClient::getCurrent()->get($elt[9])->libelleProduit() : ''; ?></td>
-	      <td><?php echo $elt[10]; ?></td>
-	      <td><?php echo $elt[11]; ?></td>
+            <td class="num_contrat"><?php $vracid = preg_replace('/VRAC-/', '', $elt[1]); echo link_to($vracid, '@vrac_termine?numero_contrat='.$vracid); ?></td>
+            <td class="soussigne"><?php echo ($elt[2]) ? link_to($elt[3], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[2])) : ''; ?></td>
+	      <td class="soussigne"><?php echo ($elt[4]) ? link_to($elt[5], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[4])) : ''; ?></td>
+	      <td class="soussigne"><?php echo ($elt[6]) ? link_to($elt[7], 'vrac/rechercheSoussigne?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $elt[6])) : ''; ?></td>
+              <td class="type"><?php echo ($elt[8])? typeProduit($elt[8]) : ''; ?></td>
+	      <td class="soussigne"><?php echo ($elt[9])? ConfigurationClient::getCurrent()->get($elt[9])->libelleProduit() : ''; ?></td>
+              <td class="vol"><?php echo $elt[10]; ?></td>
+              <td class="vol"><?php echo $elt[11]; ?></td>
         </tr>
         <?php } ?>
     </tbody>
