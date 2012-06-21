4efab35b5888cde280879239856de997dec5c4f1
mathurin <mpetit@actualys.com>
2012-06-21 11:45:08 +0200

    Mise en place de l'autocomplete dans la liste des vrac

--- modules/vrac/templates/_table_contrats.php
+++ modules/vrac/templates/_table_contrats.php
@@ -1,10 +1,36 @@
 <?php
 use_helper('Vrac');
+
+$etablissements = array('' => '');
+$datas = EtablissementClient::getInstance()->findAll()->rows;
+foreach($datas as $data) 
+{
+        $labels = array($data->key[4], $data->key[3], $data->key[1]);
+        $etablissements[$data->id] = implode(', ', array_filter($labels));
+}
 ?>
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+       $('.autocomplete').combobox(); 
+    });
+
+</script>
 <div style="margin: 10px;">
-    <p><form method="get" action="<?php echo url_for('vrac_recherche'); ?>">
-<input name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>"/> <input type="submit" value="recherche"/>
-</form></p>
+    <p>
+        <form method="get" action="<?php echo url_for('vrac_recherche'); ?>">
+            <select name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>" class="autocomplete">
+                <?php foreach ($etablissements as $id => $name)
+                {
+                ?>
+                    <option value="<?php echo preg_replace('/ETABLISSEMENT-/', '',$id); ?>"><?php echo $name; ?></option>
+                <?php
+                }
+                ?>
+            </select>
+            <input type="submit" value="recherche"/>
+        </form>
+    </p>
 </div>
 <style>
 td{padding: 0px 10px;}
