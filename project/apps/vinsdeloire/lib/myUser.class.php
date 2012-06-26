2007c8bd89278981e27e55c724d4ebd1e130d520
Vincent LAURENT <vlaurent@actualys.com>
2012-06-26 23:39:32 +0200

    myUser temporaire pour la DRM

--- project/apps/vinsdeloire/lib/myUser.class.php
+++ project/apps/vinsdeloire/lib/myUser.class.php
@@ -2,4 +2,54 @@
 
 class myUser extends sfBasicSecurityUser
 {
+	const CREDENTIAL_ADMIN = 'admin';
+
+	protected $tiers = null;
+	protected $drm = null;
+	protected $historique = null;
+
+
+	public function getTiers() {
+		if (is_null($this->tiers)) {
+			$this->tiers = EtablissementClient::getInstance()->findByIdentifiant('111849');
+		}
+		
+		return $this->tiers;
+	}
+
+	public function getDrm() {
+    	if (is_null($this->drm)) {
+    		$lastDrm = $this->getDrmHistorique()->getLastDrm();
+
+    		if ($lastDrm && $drm = DRMClient::getInstance()->find(key($lastDrm))) {
+    			if (!$drm->isValidee()) {
+    				$this->drm = $drm;
+    			} else {
+    				$this->drm = $drm->generateSuivante($this->getCampagneDrm());
+    			}
+    		} else {
+    			$this->drm = new DRM();
+    			$this->drm->identifiant = $this->getTiers()->identifiant;
+    			$this->drm->campagne = $this->getCampagneDrm();
+    		}
+    	}
+    	return $this->drm;
+    }
+
+    public function getDrmHistorique() {
+    	if (is_null($this->historique)) {
+        	$this->historique = new DRMHistorique($this->getTiers()->identifiant);
+    	}
+
+    	return $this->historique;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCampagneDrm() {
+      
+      return CurrentClient::getCurrent()->campagne;
+    }
+
 }
