9dc5937af4b453b2af11072f679a24c80a86e7ff
Vincent LAURENT <vlaurent@actualys.com>
2012-06-26 23:34:43 +0200

    Copie du DRMPlugin de declarvin

---
+++ project/plugins/DrmPlugin/modules/drm/templates/modificationInfosSuccess.php
@@ -0,0 +1,20 @@
+
+
+<section id="contenu">
+
+    <?php include_partial('drm/header', array('drm' => $drm)); ?>
+    <?php include_component('drm', 'etapes', array('drm' => $drm, 'etape' => 'informations', 'pourcentage' => '5')); ?>
+
+    <!-- #principal -->
+    <section id="principal">
+        <div id="application_dr">
+            <div id="drm_informations">
+                <p><?php echo //acCouchdbManager::getClient('Messages')->getMessage('msg_modification_infos'); ?></p>
+                <br /><br />
+                <a href="<?php echo url_for('drm_mon_espace') ?>" class="btn_suiv">
+                    <span>Retour à mon espace</span>
+                </a>
+            </div>
+        </div>
+    </section>
+</section>
