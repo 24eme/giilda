69b552a6e5e5ff0574e96d0fdd27d50ed695af33
Charlotte Devichet <cdevichet@actualys.com>
2012-06-26 18:02:18 +0000

    integration home

--- modules/vrac/templates/indexSuccess.php
+++ modules/vrac/templates/indexSuccess.php
@@ -20,27 +20,37 @@ foreach($datas as $data)
     <div id="rub_contrats">
         <section id="principal">
              <?php include_partial('fil_ariane'); ?>
-            <section id="contenu_etape">
-                <div style="margin: 10px;">
-                    <h2>Rechercher un opérateur : </h2>
-                    <p>
-                        <form method="get" action="<?php echo url_for('vrac_recherche'); ?>">
-                            <select name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>" class="autocomplete">
-                                <?php foreach ($etablissements as $id => $name)
-                                {
-                                ?>
+            <div id="contenu_etape">
+                <div id="recherche_operateur" class="section_label_maj">
+                    <label>Rechercher un opérateur : </label>
+                    <form method="get" action="<?php echo url_for('vrac_recherche'); ?>">
+                        <select name="identifiant" value="<?php echo (isset($identifiant)) ? $identifiant : '' ; ?>" class="autocomplete">
+                            <?php foreach ($etablissements as $id => $name){  ?>
                                 <option value="<?php echo preg_replace('/ETABLISSEMENT-/', '',$id); ?>"><?php echo $name; ?></option>
-                                <?php
-                                }
-                                ?>
-                            </select>
-                            <input type="submit" value="recherche"/>
-                        </form>
-                    </p>
+                            <?php } ?>
+                        </select>
+                        <button type="submit" id="btn_rechercher">Rechercher</button>
+                    </form>
+                    <span id="recherche_avancee"><a href="">> Recherche avancée</a></span>
                 </div>
-                <h2> 10 derniers contrats saisis : </h2>
-                <?php include_partial('table_contrats', array('vracs' => $vracs)); ?>
-            </section>
+                <br />
+                <div class="section_label_maj"> 
+                    <label>10 derniers contrats saisis : </label>
+                    <a id="ouvrir_excel" class="btn_majeur btn_excel" href="#">Ouvrir avec Excel</a>
+                    <div class="legende">
+                        <div><span class="type_raisins">type_raisins</span><span class="legende_type_texte">Raisins</span></div>
+                        <div><span class="type_mouts">type_mouts</span><span class="legende_type_texte">Mouts</span></div>
+                        <div><span class="type_vin_vrac">type_vin_vrac</span><span class="legende_type_texte">Vrac</span></div>
+                        <div><span class="type_vin_bouteille">type_vin_bouteille</span><span class="legende_type_texte">Bouteilles</span></div>
+                    </div>
+                    <div class="legende">
+                        <div><span class="statut statut_solde"></span><span class="legende_statut_texte">Soldé</span></div>
+                        <div><span class="statut statut_non-solde"></span><span class="legende_statut_texte">Non-soldé</span></div>
+                        <div><span class="statut statut_annule"></span><span class="legende_statut_texte">Annulé</span></div>
+                    </div>
+                    <?php include_partial('table_contrats', array('vracs' => $vracs)); ?>
+                </div>
+            </div>
         </section>
         <?php include_partial('actions'); ?>
     </div>
