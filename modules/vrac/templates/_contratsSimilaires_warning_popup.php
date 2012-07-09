0b6b680482f16754aa9406a534360bcccf811b08
mathurin <mpetit@actualys.com>
2012-07-09 17:18:12 +0200

    le retour du popup se fait à l'étape 2 (marche)

--- modules/vrac/templates/_contratsSimilaires_warning_popup.php
+++ modules/vrac/templates/_contratsSimilaires_warning_popup.php
@@ -10,7 +10,7 @@
         ?>
         <div id="ligne_btn">
                 <div class="btnRetour">
-                        <a href="<?php echo url_for('vrac_condition', $vrac); ?>" class="btn_majeur btn_etape_prec"><span>Retour à l'étape 3</span></a>
+                        <a href="<?php echo url_for('vrac_marche', $vrac); ?>" class="btn_majeur btn_etape_prec"><span>Retour à l'étape 2</span></a>
                 </div>
                 <div class="btnValidation">
                         <a href="#" class="btn_majeur btn_etape_suiv" id="validation"><span>Terminer la saisie</span></a>
