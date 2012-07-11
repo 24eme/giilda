79b2342452df9dd95b8755524e934dcc9dee86d7
Vincent LAURENT <vlaurent@actualys.com>
2012-07-11 20:10:06 +0200

    Ajout d'une fonction nécéssaire à la DRM

--- project/apps/vinsdeloire/lib/myUser.class.php
+++ project/apps/vinsdeloire/lib/myUser.class.php
@@ -52,4 +52,28 @@ class myUser extends sfBasicSecurityUser
       return CurrentClient::getCurrent()->campagne;
     }
 
+    public function createDRMByCampagne($campagne = null) {
+    	if (!$campagne) {
+    		$campagne = date('Y-m');
+    	}
+    	$campagneTab = explode('-', $campagne);
+    	$date_campagne = new DateTime($campagneTab[0].'-'.$campagneTab[1].'-01');
+       	$date_campagne->modify('-1 month');
+       	$prev_campagne = DRMClient::getInstance()->getCampagne($date_campagne->format('Y'), $date_campagne->format('m'));
+       	$prev_drm = DRMClient::getInstance()->findLastByIdentifiantAndCampagne($this->getTiers()->identifiant, $prev_campagne);
+       	if ($prev_drm) {
+           $this->_drm = $prev_drm->generateSuivante($campagne);
+       	} else {
+       		$lastDRM = $this->getDRMHistorique()->getLastDRM();
+    		if ($lastDRM && $drm = DRMClient::getInstance()->find(key($lastDRM))) {
+    			$this->_drm = $drm->generateSuivante($campagne, false);
+    		} else {
+		    	$this->_drm = new DRM();
+		    	$this->_drm->identifiant = $this->getTiers()->identifiant;
+		    	$this->_drm->campagne = $campagne;
+    		}
+       	}
+        return $this->_drm;
+    }
+
 }
