1ed91ff22f526a2413baa4258a93fe28bf6d42f6
Vincent LAURENT <vlaurent@actualys.com>
2012-07-17 11:29:36 +0200

    Import des produits avec mention

--- lib/csv/ProduitCsvFile.class.php
+++ lib/csv/ProduitCsvFile.class.php
@@ -47,7 +47,7 @@ class ProduitCsvFile extends CsvFile
   	$hash = 'certifications/'.$this->getKey($line[self::CSV_PRODUIT_CATEGORIE_CODE]).
                 '/genres/'.$this->getKey($line[self::CSV_PRODUIT_GENRE_CODE], true).
                 '/appellations/'.$this->getKey($line[self::CSV_PRODUIT_DENOMINATION_CODE], true).
-                '/mentions/'.Configuration::DEFAULT_KEY.
+                '/mentions/'.$this->getKey($line[self::CSV_PRODUIT_MENTION_CODE], true).
                 '/lieux/'.$this->getKey($line[self::CSV_PRODUIT_LIEU_CODE], true).
                 '/couleurs/'.strtolower($this->couleurKeyToCode($line[self::CSV_PRODUIT_COULEUR_CODE])).
                 '/cepages/'.$this->getKey($line[self::CSV_PRODUIT_CEPAGE_CODE], true);
