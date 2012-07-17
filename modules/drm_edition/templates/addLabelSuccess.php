632f35fd778a7f202df7326af95bcf9e86a6c880
Vincent LAURENT <vlaurent@actualys.com>
2012-07-17 22:08:49 +0200

    mise en forme rapide tu template d'ajout de label

--- modules/drm_edition/templates/addLabelSuccess.php
+++ modules/drm_edition/templates/addLabelSuccess.php
@@ -1,7 +1,9 @@
-<div id="contenu">
-<form method="POST">
-<?php echo $form->renderHiddenFields(); ?>
-<?php echo $form->render(); ?>
-<input type="submit" value="valider"/>
-</form>
+<div id="contenu" style="background: #fff;">
+	<section id="principal">
+		<form method="POST">
+		<?php echo $form->renderHiddenFields(); ?>
+		<?php echo $form->render(); ?>
+		<input type="submit" value="valider"/>
+		</form>
+	</section>
 </div>
