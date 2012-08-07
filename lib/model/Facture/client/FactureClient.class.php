5650c3f729efe561597abb8ced6c2e57d750297b
Tangui Morlier <tangui@tangui.eu.org>
2012-08-07 18:17:31 +0200

    Ajout de la date du document d'origine et mise en cohérence des noms des champs et des constantes

--- lib/model/Facture/client/FactureClient.class.php
+++ lib/model/Facture/client/FactureClient.class.php
@@ -5,8 +5,8 @@ class FactureClient extends acCouchdbClient {
     const FACTURE_LIGNE_ORIGINE_TYPE_DRM = "DRM";
     const FACTURE_LIGNE_ORIGINE_TYPE_SRV = "SRV";
 
-    const FACTURE_LIGNE_SORTIE_TYPE_PROPRIETE = "Propriete";
-    const FACTURE_LIGNE_SORTIE_TYPE_CONTRAT = "Contrat";
+    const FACTURE_LIGNE_MOUVEMENT_TYPE_PROPRIETE = "Propriete";
+    const FACTURE_LIGNE_MOUVEMENT_TYPE_CONTRAT = "Contrat";
 
     const FACTURE_LIGNE_PRODUIT_TYPE_VINS = "Vins";
     const FACTURE_LIGNE_PRODUIT_TYPE_MOUTS = "Mouts";
