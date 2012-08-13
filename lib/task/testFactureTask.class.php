4d28621bcc77dc742f6c090ee02459f92aa0baf4
Tangui Morlier <tangui@tangui.eu.org>
2012-08-13 16:20:38 +0200

    coquille + exemple de facture en base

--- lib/task/testFactureTask.class.php
+++ lib/task/testFactureTask.class.php
@@ -19,7 +19,7 @@ class testFactureTask extends sfBaseTask
 The [testFacture|INFO] task does things.
 Call it with:
 
-  [php symfony testFacture|INFO]
+    [php symfony test:Facture|INFO]
 EOF;
   }
 
@@ -29,11 +29,16 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
+    $facture = FactureClient::getInstance()->find('FACTURE-20120201-123');
+    if($facture) {
+      $facture->delete();
+    }
+
     // add your code here
     $facture = new Facture();
-    $facture->_id = 'FACTURE-20120101-123';
-    $facture->identifiant = '20120101-123';
-    $facture->date_emission = '2012-01-01';
+    $facture->_id = 'FACTURE-20120201-123';
+    $facture->identifiant = '20120201-123';
+    $facture->date_emission = '2012-02-01';
     $facture->campagne = '2011-2012';
     $facture->emetteur->adresse = "Chateau InterLoire";
     $facture->emetteur->code_postal = '44120';
@@ -42,6 +47,16 @@ EOF;
     $facture->emetteur->telephone = '0212321232';
     $facture->client_identifiant = 'ETABLISSEMENT-123';
     $facture->client_reference = '123';
+    $facture->client->raison_sociale = "Garage d'actualys";
+    $facture->client->adresse = "1 rue garnier";
+    $facture->client->code_postal = "92100";
+    $facture->client->ville = "Neuilly sur seine";
+    $facture->add("lignes")->add(0, array('origine_type' => 'DRM', 'origine_identifiant' => 'DRM-123-2012-01', 'origine_date' => '2012-01', 'produit_type' => 'Vin', 'produit_libelle' => 'Anjou rouge', 'produit_hash' => 'AOC/.../TOU', 'mouvement_type' => 'propriete', 'contrat_identifiant' => '', 'contrat_libelle' => '', 'echeance_code' => 'A', 'volume' => 10, 'cotisation_taux' => 4.5, 'montant_ht' => 45));
+    $facture->lignes->add(1, array('origine_type' => 'DRM', 'origine_identifiant' => 'DRM-123-2012-01', 'origine_date' => '2012-01', 'produit_type' => 'Vrac', 'produit_libelle' => 'Anjou rouge', 'produit_hash' => 'AOC/.../TOU', 'mouvement_type' => 'contrat', 'contrat_identifiant' => 'VRAC-123-2012-01-12', 'contrat_libelle' => 'contrat avec bidule', 'echeance_code' => 'B', 'volume' => 10, 'cotisation_taux' => 5.5, 'montant_ht' => 55));
+    $facture->echeances->add(0, array('echeance_code' => 'A', 'echeance_date' => '2012-03-31', 'montant_ttc' => 53.82));
+    $facture->echeances->add(1, array('echeance_code' => 'B', 'echeance_date' => '2012-03-31', 'montant_ttc' => 32.89));
+    $facture->echeances->add(2, array('echeance_code' => 'B', 'echeance_date' => '2012-05-31', 'montant_ttc' => 32.89));
+
     $facture->total_ht = 100;
     $facture->total_ttc = 119.6;
     $facture->origines = array('DRM-123-2012-01' => 'DRM de janvier', 'DRM-123-2011-12' => 'DRM de décembre');
