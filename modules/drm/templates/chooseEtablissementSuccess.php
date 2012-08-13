a1b81bc8a0da84ab7740570460022051dceda4da
Charlotte Devichet <cdevichet@actualys.com>
2012-08-13 13:26:31 +0000

    debut inte drm

--- modules/drm/templates/chooseEtablissementSuccess.php
+++ modules/drm/templates/chooseEtablissementSuccess.php
@@ -1,9 +1,14 @@
-<section id="contenu">
-<section id="etablissement">
-<h2>Etablissements</h2>
-<?php include_component('drm', 'chooseEtablissement', array('form' => $form));  ?>
-</section>
-<section id="historique">
-<h2>Historique</h2>
-</section>
-</section>
+<div id="contenu" class="drm">
+    <section id="principal">
+        <p id="fil_ariane">
+            <a href=""><strong>Page d'accueil</strong></a>
+        </p>
+        <div id="etablissement" class="section_label_maj recherche_etablissement">
+            <label>Selectionner un établissement</label>
+            <?php include_component('drm', 'chooseEtablissement', array('form' => $form)); ?>
+        </div>
+        <div id="historique">
+            <h2>Historique</h2>
+        </div>
+    </section>
+</div>
