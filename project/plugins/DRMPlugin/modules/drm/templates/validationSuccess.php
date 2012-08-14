e8f2486a99446b33c0de88db06fd4a46a1035442
Vincent LAURENT <vlaurent@actualys.com>
2012-08-14 22:20:16 +0200

    tableau des mouvements et racap avec le css generique

--- project/plugins/DRMPlugin/modules/drm/templates/validationSuccess.php
+++ project/plugins/DRMPlugin/modules/drm/templates/validationSuccess.php
@@ -1,20 +1,81 @@
-<section id="contenu" style="background: #fff; padding: 0 10px;">
+<div id="contenu" class="drm">
 
-    <?php include_partial('drm/header', array('drm' => $drm)); ?>
-    <?php /*include_partial('etapes', array('drm' => $drm, 
-                                                   'etape' => 'mouvements', 
-                                                   'pourcentage' => '10'));*/ ?>
-    <?php include_partial('drm/controlMessage'); ?>
+    <!-- #principal -->
+    <section id="principal" style="width: auto;">
 
-    <form action="" method="post">    
+        <?php include_partial('drm/header', array('drm' => $drm)); ?>
 
-        <!-- #principal -->
-        <section id="principal" style="width: auto;">
+        <h2>Déclaration Récapitulative Mensuelle</h2>
+
+        <div id="recap_infos_header">
+            <div><label>Nom de l'opérateur : </label>Ackerman Rémy Pannier</div>
+            <div><label>Période : </label>Aout 2012</div>
+        </div>
+
+        <?php include_partial('drm/etapes'); ?>
+
+        <form action="" method="post">
             <?php include_partial('drm/recap', array('drm' => $drm)) ?>
             <?php include_partial('drm/mouvements', array('mouvements' => $drm->declaration->getMouvements())) ?>
-        </section>
 
-        <input type="submit" value="valider" />
-    </form>
+            <div id="btn_etape_dr">
+                <a href="<?php echo url_for('drm_edition', $drm) ?>" class="btn_etape_prec" id="facture"><span>Précédent</span></a> 
+                <button type="submit" class="btn_etape_suiv" id="facture"><span>Suivant</span></a> 
+            </div>
+        </form>
 
-</section>
+    </section>
+    <aside id="colonne">
+
+        <div id="contrat_progression" class="bloc_col">
+            <h2>Campagne viticole : 2011-2012</h2>
+
+            <div class="contenu">
+                <p><strong>10%</strong> de la DRM a été saisi</p>
+
+                <div id="barre_progression">
+                    <span style="width: 10%;"></span>
+                </div>
+            </div>
+        </div>
+
+        <div id="contrat_aide" class="bloc_col">
+            <h2>Aide</h2>
+
+            <div class="contenu">
+                <ul>
+                    <li class="raccourcis"><a href="" data-popup="#raccourci_clavier" class="btn_popup" data-popup-config="configDefaut">Raccourcis clavier</a></li>
+                    <li class="assistance"><a href="#">Assistance</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="infos_contact" class="bloc_col">
+            <h2>Infos contact</h2>
+
+            <div class="contenu">
+                <ul>
+                    <li id="infos_contact_vendeur">
+                        <a href="#">Coordonnées opérateur</a>
+                        <ul>
+                            <li class="nom">Nom du vendeur</li>
+                            <li class="tel">00 00 00 00 00</li>
+                            <li class="fax">00 00 00 00 00</li>
+                            <li class="email"><a href="mailto:email@email.com">email@email.com</a></li>
+                        </ul>
+                    </li>
+                    <li id="infos_contact_acheteur">
+                        <a href="#">Coordonnées recette locale</a>
+                        <ul>
+                            <li class="nom">Nom du vendeur</li>
+                            <li class="tel">00 00 00 00 00</li>
+                            <li class="fax">00 00 00 00 00</li>
+                            <li class="email"><a href="mailto:email@email.com">email@email.com</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </aside>
+</div>
