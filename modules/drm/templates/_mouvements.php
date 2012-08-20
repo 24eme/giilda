9514ba1fe8a58f87c8eb346059257dc9bced42dd
mathurin <mpetit@actualys.com>
2012-08-20 11:52:37 +0200

    dans la recap des mvt, les volume positifs sont préfixé par '+' et les cases sont de couleur différentes (rouge -> sorties)/(vert -> entrées) la colonne détail disparait, les détail pour les destination/num contrat et num coop sont dans type

--- modules/drm/templates/_mouvements.php
+++ modules/drm/templates/_mouvements.php
@@ -1,4 +1,4 @@
-<?php use_helper('Float') ?>
+<?php use_helper('Float');?>
 <fieldset id="mouvement_drm">
     <legend>Mouvements</legend>
     <table class="table_recap">
@@ -7,18 +7,18 @@
                 <th style="width: 200px;">Produits</th>
                 <th>Type</th>
                 <th>Volume</th>
-                <th>Détail</th>
             </tr>
         </thead>
         <tbody>
         <?php $i = 1; ?>
         <?php foreach($mouvements as $mouvement): ?>
         <?php $i++; ?>
-            <tr <?php if($i%2!=0) echo ' class="alt"'; ?>>
-                <td><?php echo $mouvement->produit_libelle ?></td>
-                <td><?php echo $mouvement->type_libelle ?></td>
-                <td><?php echoFloat($mouvement->volume) ?></td>
-                <td><?php echo $mouvement->detail_libelle ?></td>
+            <tr <?php if($i%2!=0) echo ($mouvement->volume>0)? ' class="alt"' : 'class="alt"';  ?>>
+                <td><?php echo $mouvement->produit_libelle ?> </td>
+                <td><?php echo $mouvement->type_libelle.' '.$mouvement->detail_libelle ?></td>
+                <td <?php echo ($mouvement->volume>0)? ' class="positif"' : 'class="negatif"';?> >
+                    <?php  echoSignedFloat($mouvement->volume); ?>
+                </td>
             </tr>
         <?php endforeach; ?>
         </tbody>
