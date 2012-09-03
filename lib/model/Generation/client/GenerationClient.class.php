165fe236156f4ae04e3ab2e2bcedc01843ea82a8
mathurin <mpetit@actualys.com>
2012-09-03 15:00:41 +0200

    Affichage des dernières générations de factures dans l'accueil facture

--- lib/model/Generation/client/GenerationClient.class.php
+++ lib/model/Generation/client/GenerationClient.class.php
@@ -2,6 +2,13 @@
 
 class GenerationClient extends acCouchdbClient {
 
+    const HISTORY_KEYS_ID = 0;
+    
+    const HISTORY_VALUES_DATE = 0;
+    const HISTORY_VALUES_NBDOC = 1;
+    const HISTORY_VALUES_DOCUMENTS = 2;    
+    const HISTORY_VALUES_SOMME = 3;
+    
     public static function getInstance() {
         return acCouchdbManager::getClient("Generation");
     }
@@ -9,7 +16,11 @@ class GenerationClient extends acCouchdbClient {
     public function getId($type_document,$date) {
         return 'Generation-' . $type_document . '-' . $date;
     }
-
-
-
+    
+    public function findHistory($limit = 10) {
+        return acCouchdbManager::getClient()
+                        ->limit($limit)
+                        ->getView("generation", "history")
+                ->rows;
+    }
 }
