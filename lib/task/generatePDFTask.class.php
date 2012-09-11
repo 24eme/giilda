2f74d2f79e2ba8fb3fd7768d8b3cbc77be3704ae
Tangui Morlier <tangui@tangui.eu.org>
2012-09-12 01:17:55 +0200

    Mis en tache de la génération en masse des pdf

---
+++ lib/task/generatePDFTask.class.php
@@ -0,0 +1,46 @@
+<?php
+
+class generatePDFTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    $this->addArguments(array(
+    ));
+
+    $this->addOptions(array(
+			    new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'vinsdeloire'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      new sfCommandOption('generation', null, sfCommandOption::PARAMETER_REQUIRED, 'The generation id'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'generate';
+    $this->name             = 'PDF';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [generatePDF|INFO] task does things.
+Call it with:
+
+  [php symfony generatePDF|INFO]
+EOF;
+  }
+  
+  protected function execute($arguments = array(), $options = array())
+  {
+    sfContext::createInstance($this->configuration);
+
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    $generation = GenerationClient::getInstance()->find($options['generation']);
+    if (!$generation) {
+      echo $options['generation']." n'est pas un document valide\n";
+      exit(1);
+    }
+    $g = new GenerationPDF($generation, $this->configuration);
+    echo $g->generatePDF();
+  }
+}
