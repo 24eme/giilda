a2a493f81d42654351b1e6e729b01ed320339e5b
Vincent LAURENT <vlaurent@actualys.com>
2012-09-11 19:27:01 +0200

    Utilisation des nouvelles cle dans les vues des mouvements

--- project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.facturation.map.view.js
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.facturation.map.view.js
@@ -13,13 +13,12 @@ function(doc) {
         for(key in doc.mouvements[identifiant]) {
             var mouv = doc.mouvements[identifiant][key];
             doc.region = 'tours';
-	if(doc.type == "SV12")
-	{
-		doc.identifiant = doc.identifiant.substr(0,6);
-		mouv.cvo = 4.5;
-	}
-	    var vrac_id = (mouv.type_hash == 'sorties/vrac' || mouv.type_hash == 'raisins' || mouv.type_hash == 'mouts')? mouv.detail_identifiant : null;
-            emit([mouv.facture, mouv.facturable, doc.region, doc.identifiant, doc.type, mouv.categorie, mouv.produit_hash, doc.periode, vrac_id, mouv.type_hash, mouv.detail_identifiant], [mouv.produit_libelle, mouv.type_libelle, mouv.volume, mouv.cvo, mouv.date, mouv.detail_libelle, doc.type+'-'+doc.identifiant+'-'+doc.periode, key]);
+        	if(doc.type == "SV12")
+        	{
+        		doc.identifiant = doc.identifiant.substr(0,6);
+        		mouv.cvo = 4.5;
+        	}
+            emit([mouv.facture, mouv.facturable, doc.region, doc.identifiant, doc.type, mouv.categorie, mouv.produit_hash, doc.periode, mouv.vrac_numero, mouv.type_hash, mouv.detail_identifiant], [mouv.produit_libelle, mouv.type_libelle, mouv.volume, mouv.cvo, mouv.date, mouv.vrac_destinataire, mouv.detail_libelle, doc.type+'-'+doc.identifiant+'-'+doc.periode, key]);
         } 
    }
 }
