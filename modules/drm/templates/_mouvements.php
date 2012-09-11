33d391c26fc17e6b9404ceb0b9541b51376045c5
Vincent LAURENT <vlaurent@actualys.com>
2012-09-11 21:47:35 +0200

    Dans le listing mouvement si pas de version on met 'M00'

--- modules/drm/templates/_mouvements.php
+++ modules/drm/templates/_mouvements.php
@@ -15,7 +15,7 @@
         <?php foreach($mouvements as $mouvement): ?>
         <?php $i++; ?>
             <tr <?php if($i%2!=0) echo ($mouvement->volume > 0)? ' class="alt"' : 'class="alt"';  ?>>
-                <td><?php echo $mouvement->version.' - '.format_date($mouvement->date_version);?></td>
+                <td><?php echo sprintf("%s - %s", ($mouvement->version) ? $mouvement->version : 'M00', format_date($mouvement->date_version));?></td>
                 <td><?php echo $mouvement->produit_libelle ?> </td>
                 <td><?php echo $mouvement->type_libelle.' '.$mouvement->detail_libelle ?></td>
                 <td <?php echo ($mouvement->volume > 0)? ' class="positif"' : 'class="negatif"';?> >
