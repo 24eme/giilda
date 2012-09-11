0cc81296550fed7297b7ae4ac660eb2051a86223
mathurin <mpetit@actualys.com>
2012-09-11 17:07:31 +0200

    +maj de la vue +facture id avec num incremental

--- project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.facturation.map.view.js
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.facturation.map.view.js
@@ -12,10 +12,14 @@ function(doc) {
     for(identifiant in doc.mouvements) {
         for(key in doc.mouvements[identifiant]) {
             var mouv = doc.mouvements[identifiant][key];
-            mouv.facture = 0;
             doc.region = 'tours';
-	    var vrac_id = (mouv.type_hash == 'sorties/vrac')? mouv.detail_identifiant : null;
-            emit([mouv.facture, mouv.facturable, doc.region, doc.identifiant, doc.type, mouv.categorie, mouv.produit_hash, doc.periode, vrac_id, mouv.type_hash, mouv.detail_identifiant], [mouv.produit_libelle, mouv.type_libelle, mouv.volume, mouv.cvo, mouv.date, mouv.detail_libelle, 'DRM-'+doc.identifiant+'-'+doc.periode, key]);
+	if(doc.type == "SV12")
+	{
+		doc.identifiant = doc.identifiant.substr(0,6);
+		mouv.cvo = 4.5;
+	}
+	    var vrac_id = (mouv.type_hash == 'sorties/vrac' || mouv.type_hash == 'raisins' || mouv.type_hash == 'mouts')? mouv.detail_identifiant : null;
+            emit([mouv.facture, mouv.facturable, doc.region, doc.identifiant, doc.type, mouv.categorie, mouv.produit_hash, doc.periode, vrac_id, mouv.type_hash, mouv.detail_identifiant], [mouv.produit_libelle, mouv.type_libelle, mouv.volume, mouv.cvo, mouv.date, mouv.detail_libelle, doc.type+'-'+doc.identifiant+'-'+doc.periode, key]);
         } 
    }
 }
