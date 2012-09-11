263105f1c3fb65ca09cce366992b001042c17370
Tangui Morlier <tangui@tangui.eu.org>
2012-09-11 19:43:34 +0200

    Génération des PDF

--- project/plugins/acVinGenerationPlugin/lib/model/Generation/Generation.class.php
+++ project/plugins/acVinGenerationPlugin/lib/model/Generation/Generation.class.php
@@ -5,15 +5,18 @@
  */
 
 class Generation extends BaseGeneration {
-    public function constructId() {
-        
-        $this->setDateEmission(date('YmdH:i'));
-        $this->setIdentifiant($this->type_document.'-'.$this->date_emission);
-        $this->set_id('GENERATION-'.$this->identifiant);
-    }
-    
-    public function save() {
-        $this->nb_documents = count($this->documents);
-        parent::save();
-    }
+  const GENERATION_STATUT_ENCOURS = "En cours";
+  const GENERATION_STATUT_GENERE = "Généré";
+
+  public function constructId() {
+    $this->setDateEmission(date('YmdH:i'));
+    $this->setIdentifiant($this->type_document.'-'.$this->date_emission);
+    $this->set_id('GENERATION-'.$this->identifiant);
+    $this->setStatut(self::GENERATION_STATUT_ENCOURS);
+  }
+  
+  public function save() {
+    $this->nb_documents = count($this->documents);
+    parent::save();
+  }
 }
