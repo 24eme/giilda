f10fe4a3f592f095d438c70787720dbcce5d0fa7
Tangui Morlier <tangui@tangui.eu.org>
2012-09-12 01:17:55 +0200

    Mis en tache de la génération en masse des pdf

--- project/plugins/acVinGenerationPlugin/lib/GenerationPDF.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationPDF.class.php
@@ -2,33 +2,47 @@
 
 class GenerationPDF {
 
-  private $generation;
-
-  function __construction(Generation $g) {
+  private $generation = null;
+  private $config = null;
+  
+  function __construct(Generation $g, $config = null) {
     $this->generation = $g;
+    $this->config = $config;
   }
 
   function concatenatePDFs($pdffiles) {
+    $fileres = rand().".pdf";
+    exec('pdftk "'.implode('" "', $pdffiles).'" cat output "'.$fileres.'.pdf"');
+    return $fileres;
   }
 
   function generatePDFOnePageRange($pdfs) {
     $files = array();
-    foreach ($pdsf as $pdf) {
+    foreach ($pdfs as $pdf) {
       $files[] = $pdf->getPDFFile();
     }
     return $this->concatenatePDFs($files);
   }
 
   function generatePDF() {
+    if (!$this->generation) 
+      throw new sfException('Object generation should not be null');
     $factures = array();
     foreach ($this->generation->documents as $factureid) {
       $facture = FactureClient::getInstance()->find($factureid);
-      $pdf = new FactureLatex($facture);
+      if (!$facture) {
+	echo("Facture $factureid doesn't exist\n");
+	continue;
+
+      }
+      $pdf = new FactureLatex($facture, $this->config);
+      if (!isset($factures[$pdf->getNbPages()]))
+	$factures[$pdf->getNbPages()] = array();
       array_push($factures[$pdf->getNbPages()], $pdf);
     }
     $pages = array();
     foreach ($factures as $page => $pdfs) {
-      $pages[$page] = $this->generatePDFOnePageRange();
+      $pages[$page] = $this->generatePDFOnePageRange($pdfs);
     }
     return $this->concatenatePDFs($pages);
   }
