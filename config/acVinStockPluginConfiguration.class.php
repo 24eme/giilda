c1a7163ba26e4abb73d93157a65d31f42c3f5a92
mathurin <mpetit@actualys.com>
2012-09-20 17:34:04 +0200

    suppression des fichier commités dans master Stock

--- config/acVinStockPluginConfiguration.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class acVinStockPluginConfiguration extends sfPluginConfiguration
-{
-  public function initialize()
-  {
-    $this->dispatcher->connect('routing.load_configuration', array('StockRouting', 'listenToRoutingLoadConfigurationEvent'));
-  }
-}
