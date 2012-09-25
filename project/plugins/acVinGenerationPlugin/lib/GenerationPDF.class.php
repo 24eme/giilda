1ab1720df04cfe0502ab59e1bb11c0628a30d4b5
Tangui Morlier <tangui@tangui.eu.org>
2012-09-25 20:06:12 +0200

    Génération en concatenant par page

--- project/plugins/acVinGenerationPlugin/lib/GenerationPDF.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationPDF.class.php
@@ -16,12 +16,44 @@ class GenerationPDF {
     return $fileres;
   }
 
-  function generatePDFOnePageRange($pdfs) {
+  function generateAPDFForAPageId($pdf, $pageid) {
+    $fileres = rand().".pdf";
+    exec('pdftk "'.$pdf.'" cat '.intval($pageid).' output "'.$fileres.'"');
+    return $fileres;    
+  }
+
+  function concatenatePDFsForAPageId($pdfs, $pageid) {
+    $files = array();
+    foreach ($pdfs as $pdf) {
+      $files[] = $this->generateAPDFForAPageId($pdf, $pageid);
+    }
+    $fileres = $this-> concatenatePDFs($files);
+    $this->cleanFiles($files);
+    return $filesres;
+  }
+
+  private function generatePDFFile($pdfs) {
     $files = array();
     foreach ($pdfs as $pdf) {
       $files[] = $pdf->getPDFFile();
     }
-    return $this->concatenatePDFs($files);
+    return $files;
+  }
+  
+  function generatePDFAndConcatenateThem($pdfs) {
+    return $this->concatenatePDFs($this->generatePDFFile($pdfs));
+  }
+
+  function generatePDFGroupByPageNumberAndConcatenateThem($pdfs, $pagenumber) {
+    $files = $this->generatePDFFile($pdfs);
+    $filesbypage = array();
+    for($i = 1 ; $i <= $pagenumber ; $i++) {
+      $filesbypage[] = $this->concatenatePDFsForAPageId($pdfs, $i);
+    }
+    $this->cleanFiles($files);
+    $res = $this->concatenatePDFs($filesbypage);
+    $this->cleanFiles($filesbypage);
+    return $res;
   }
 
   private function cleanFiles($files) {
@@ -48,11 +80,12 @@ class GenerationPDF {
     }
     $pages = array();
     foreach ($factures as $page => $pdfs) {
-      $pages[$page] = $this->generatePDFOnePageRange($pdfs);
+      if (isset($this->config['page'.$page.'perpage']) && $this->config['page'.$page.'perpage']) {
+	$this->generation->fichiers->add('Documents de '.$page.' page(s)', $this->generatePDFGroupByPageNumberAndConcatenateThem($pdfs));
+      }else{
+	$this->generation->fichiers->add('Documents de '.$page.' page(s)', $this->generatePDFAndConcatenateThem($pdfs));
+      }
     }
-    $pdffinal = $this->concatenatePDFs($pages);
-    rename($pdffinal, 'data/latex/generation.pdf');
-    $this->generation->setGenere('data/latex/generation.pdf');
     $this->generation->save();
     $this->cleanFiles($pages);
   }
