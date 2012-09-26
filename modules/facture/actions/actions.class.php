3d99e572ccc20648e7f6e1d4032fcbfa9d03bb71
mathurin <mpetit@actualys.com>
2012-09-26 16:20:01 +0200

    changement d'affichage des facture en pdf plutot que latex

--- modules/facture/actions/actions.class.php
+++ modules/facture/actions/actions.class.php
@@ -71,8 +71,7 @@ class factureActions extends sfActions {
         $this->facture = FactureClient::getInstance()->findByEtablissementAndId($this->getRoute()->getEtablissement()->identifiant, $request->getParameter('factureid'));
         $this->forward404Unless($this->facture);
 	$latex = new FactureLatex($this->facture);
-        $latex->echoFactureWithHTTPHeader('latex');
-	//$latex->echoFactureWithHTTPHeader($request->getParameter('type'));
+	$latex->echoFactureWithHTTPHeader($request->getParameter('type'));
         exit;
     }
     
