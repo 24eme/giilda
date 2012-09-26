baea527c640aa23a8389f028ca40400566563fd9
mathurin <mpetit@actualys.com>
2012-09-26 12:19:17 +0200

    uniformisation des libelles produits dans la ds

--- lib/model/DS/DSProduit.class.php
+++ lib/model/DS/DSProduit.class.php
@@ -9,7 +9,7 @@ class DSProduit extends BaseDSProduit {
     function updateProduit($produit)
     {
         $this->produit_hash = $produit->getHash();
-        $this->produit_libelle = $produit->getLibelle();
+        $this->produit_libelle = $produit->getLibelle("%g% %a% %m% %l% %co% %ce% %la%");
         $this->stock_initial = $produit->total;
         $this->stock_revendique = 0;
     }
