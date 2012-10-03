a8be36252397e3cdee620d3e9a6786441fe0d8e4
Tangui Morlier <tangui@tangui.eu.org>
2012-10-03 15:18:23 +0200

    Généricisation de la consultation d'une génération

--- modules/generation/actions/actions.class.php
+++ modules/generation/actions/actions.class.php
@@ -1,7 +1,7 @@
 <?php
 class generationActions extends sfActions {
     
-  public function executeFacture(sfWebRequest $request) {
+  public function executeView(sfWebRequest $request) {
       $this->generation = GenerationClient::getInstance()->find(GenerationClient::getInstance()->getId($request['type_document'], $request['date_emission']));
 
   }
