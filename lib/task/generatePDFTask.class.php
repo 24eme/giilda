ecf98fcf05f755a2ce4270d0bc74c9ba0b5361b7
mathurin <mpetit@actualys.com>
2012-10-05 19:06:16 +0200

    Revert "maj plugins"
    This reverts commit 446ecc7da4c355cc4c555a3e1313c18cd7762cf8.

--- lib/task/generatePDFTask.class.php
+++ lib/task/generatePDFTask.class.php
@@ -9,11 +9,11 @@ class generatePDFTask extends sfBaseTask
     ));
 
     $this->addOptions(array(
-                        new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'vinsdeloire'),
-                        new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
-                        new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-                        new sfCommandOption('generation', null, sfCommandOption::PARAMETER_REQUIRED, 'The generation id'),
-                        new sfCommandOption('page2perpage', null, sfCommandOption::PARAMETER_REQUIRED, 'The generation id', true),
+			    new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'vinsdeloire'),
+			    new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
+			    new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+			    new sfCommandOption('generation', null, sfCommandOption::PARAMETER_REQUIRED, 'The generation id'),
+			    new sfCommandOption('page2perpage', null, sfCommandOption::PARAMETER_REQUIRED, 'The generation id', true),
       // add your own options here
     ));
 
@@ -50,17 +50,7 @@ EOF;
         echo $options['generation']." n'est pas un document valide\n";
 	continue;
       }
-      $g = null;
-      switch ($generation->type_document) {
-            case GenerationClient::TYPE_DOCUMENT_DS:
-                $g = new GenerationDSPDF($generation, $this->configuration, $options);
-                break;
-
-            case GenerationClient::TYPE_DOCUMENT_FACTURES:
-                $g = new GenerationFacturePDF($generation, $this->configuration, $options);
-                break;
-        }
-      $g->preGeneratePDF();
+      $g = new GenerationPDF($generation, $this->configuration, $options);
       echo $g->generatePDF()."\n";
     }
   }
