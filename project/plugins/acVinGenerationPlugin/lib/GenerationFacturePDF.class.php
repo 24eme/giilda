19b107a86878fa38a7210c91653ecda864e2496e
mathurin <mpetit@actualys.com>
2012-10-11 15:02:55 +0200

    Mise en place de la prégeneration (création de la liste de documents) des factures en vue de la créations des pdf

--- project/plugins/acVinGenerationPlugin/lib/GenerationFacturePDF.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationFacturePDF.class.php
@@ -16,7 +16,22 @@ class GenerationFacturePDF extends GenerationPDF {
     }
     
     public function preGeneratePDF() {
-        parent::preGeneratePDF();
+       parent::preGeneratePDF();     
+       $regions = explode(',',$this->generation->arguments->regions);
+       $allMouvementsByRegion = FactureClient::getInstance()->getMouvementsForMasse($regions,9); 
+       $mouvementsByEtb = FactureClient::getInstance()->getMouvementsNonFacturesByEtb($allMouvementsByRegion); 
+       $mouvementsByEtb = FactureClient::getInstance()->filterWithParameters($mouvementsByEtb,$this->generation->arguments->toArray());
+       $this->generation->documents = array();
+       $this->generation->somme = 0;
+       $cpt = 0;
+       foreach ($mouvementsByEtb as $etablissementID => $mouvementsEtb) {
+            $etablissement = EtablissementClient::getInstance()->findByIdentifiant($etablissementID);
+            $facture = FactureClient::getInstance()->createDoc($mouvementsEtb, $etablissement, $date_facturation);
+            $facture->save();
+            $this->generation->somme += $facture->total_ttc;
+            $this->generation->documents->add($cpt, $facture->_id);
+            $cpt++;
+        }
     }
     
     public function generatePDF() {
