7173229cd3204f0d0eb3ff6b4c32441755c25ccf
mathurin <mpetit@actualys.com>
2012-10-11 14:58:54 +0200

    ajout d'une class html pour refaire fonctionner le popup des label

--- modules/drm_edition/templates/_labelsList.php
+++ modules/drm_edition/templates/_labelsList.php
@@ -1,2 +1,2 @@
 <?php echo $form->getObject()->getLabelsLibelle() ?> <?php echo $form->getObject()->label_supplementaire ?>
-<a href="<?php echo url_for("drm_edition_produit_addlabel", $form->getObject()) ?>" class="btn_edition_label" title="Choix du/des labels">Editer</a>
+<a href="<?php echo url_for("drm_edition_produit_addlabel", $form->getObject()) ?>" class="btn_edition_label labels_lien" title="Choix du/des labels">Editer</a>
