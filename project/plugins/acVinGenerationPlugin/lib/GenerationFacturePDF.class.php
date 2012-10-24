b635f14771b74ec774d2e462cdb916a86eabbf98
Tangui Morlier <tangui@tangui.eu.org>
2012-10-24 20:22:21 +0200

    Refactorisation de la generation PDF

--- project/plugins/acVinGenerationPlugin/lib/GenerationFacturePDF.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationFacturePDF.class.php
@@ -35,32 +35,16 @@ class GenerationFacturePDF extends GenerationPDF {
         }
     }
     
-    public function generatePDF() {
-        parent::generatePDF();
-        $factures = array();
-        foreach ($this->generation->documents as $factureid) {
-        $facture = FactureClient::getInstance()->find($factureid);
-        if (!$facture) {
-            echo("Facture $factureid doesn't exist\n");
-            continue;
-
-        }
-        $pdf = new FactureLatex($facture, $this->config);
-        if (!isset($factures[$pdf->getNbPages()]))
-            $factures[$pdf->getNbPages()] = array();
-        array_push($factures[$pdf->getNbPages()], $pdf);
-        }
-        $pages = array();
-        foreach ($factures as $page => $pdfs) {
-        if (isset($this->options['page'.$page.'perpage']) && $this->options['page'.$page.'perpage']) {
-            $this->generation->add('fichiers')->add($this->generatePDFGroupByPageNumberAndConcatenateThem($pdfs), 'Documents de '.$page.' page(s) trié par numéro de page');
-        }else{
-            $this->generation->add('fichiers')->add($this->generatePDFAndConcatenateThem($pdfs), 'Documents de '.$page.' page(s)');
-        }
-        }
-        $this->generation->save();
-        $this->cleanFiles($pages);
+    protected function generatePDFForADocumentId($factureid) {
+      $facture = FactureClient::getInstance()->find($factureid);
+      if (!$facture) {
+	throw new sfException("Facture $factureid doesn't exist\n");
+      }
+      return new FactureLatex($facture, $this->config);
     }
-}
 
-?>
+    protected function getDocumentName() {
+      return "Factures";
+    }
+
+}
