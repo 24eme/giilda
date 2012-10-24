f25509b762b34f18237c276a27430e6f441e338e
Tangui Morlier <tangui@tangui.eu.org>
2012-10-24 20:50:09 +0200

    Ajout du prefixe du module lors de l'appel des partial pour que ca résiste à l'appel Génération

--- modules/facture/templates/_facture1Page.php
+++ modules/facture/templates/_facture1Page.php
@@ -1,24 +1,24 @@
-<?php include_partial('templateEntete', array('facture' => $facture)); ?>
+<?php include_partial('facture/templateEntete', array('facture' => $facture)); ?>
 
 \begin{document}
     <?php 
-    include_partial('templateTitreAdresse', array('facture' => $facture)); 
-    include_partial('templateNumPage', array('nb_page' => $nb_page));
-    include_partial('templateHeadTable');
+    include_partial('facture/templateTitreAdresse', array('facture' => $facture)); 
+    include_partial('facture/templateNumPage', array('nb_page' => $nb_page));
+    include_partial('facture/templateHeadTable');
     $nb_ligne_current = 0;
     $nb_ligne_current += count($facture->lignes);
     foreach ($facture->lignes as $type => $typeLignes) :
-        include_partial('templateTableTypeRow', array('type' => $type));
+        include_partial('facture/templateTableTypeRow', array('type' => $type));
         $produits = FactureClient::getInstance()->getProduitsFromTypeLignes($typeLignes);
         foreach ($produits as $prodHash => $p) :   
             foreach ($p as $produit):
                 $nb_ligne_current ++;
-                include_partial('templateTableRow', array('produit' => $produit->getRawValue()));
+                include_partial('facture/templateTableRow', array('produit' => $produit->getRawValue()));
         endforeach;
         endforeach;
     endforeach;
-    include_partial('templateEndTableWithMention', array('max_lignes' => FactureLatex::MAX_LIGNE_TABLE_ONEPAGE, 'nb_lignes' => $nb_ligne_current)); 
-    include_partial('templateReglement', array('facture' => $facture)); 
-    include_partial('templateEcheances', array('echeances' => $facture->echeances)); 
+    include_partial('facture/templateEndTableWithMention', array('max_lignes' => FactureLatex::MAX_LIGNE_TABLE_ONEPAGE, 'nb_lignes' => $nb_ligne_current)); 
+    include_partial('facture/templateReglement', array('facture' => $facture)); 
+    include_partial('facture/templateEcheances', array('echeances' => $facture->echeances)); 
     ?>
 \end{document}
