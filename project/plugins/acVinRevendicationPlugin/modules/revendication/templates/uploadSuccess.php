8813f345567c592babf93c14ac123ebb99088d11
Hamza Iqbal <hiqbal@actualys.com>
2012-10-30 12:50:44 +0100

    Corrections intégration

--- project/plugins/acVinRevendicationPlugin/modules/revendication/templates/uploadSuccess.php
+++ project/plugins/acVinRevendicationPlugin/modules/revendication/templates/uploadSuccess.php
@@ -17,25 +17,26 @@
                 <div class="generation_facture_options">
                     <ul>
                         <li>
-                        <span>1. <?php  echo $form['odg']->renderlabel(); ?></span>
+	                        <span>1. <?php  echo $form['odg']->renderlabel(); ?></span>
                             <?php echo $form['odg']->renderError() ?> 
                             <?php  echo $form['odg']->render(); ?>        
                         </li>
 
                         <li>
-                        <span>2. <?php  echo $form['campagne']->renderlabel(); ?></span>                
+                        	<span>2. <?php  echo $form['campagne']->renderlabel(); ?></span>
                             <?php echo $form['campagne']->renderError() ?> 
                             <?php  echo $form['campagne']->render(); ?>
                         </li>
                         <li>
-                                <span>3. <?php  echo $form['file']->renderlabel(); ?></span>      
-                                <?php echo $form['file']->renderError(); ?>  
-                                <?php  echo $form['file']->render(); ?>
+							<span>3. <?php  echo $form['file']->renderlabel(); ?></span>      
+							<?php echo $form['file']->renderError(); ?>  
+							<?php  echo $form['file']->render(); ?>
                         </li>
                     </ul>    
                 </div>
-                
-                <input type="submit" class="btn_majeur btn_valider" />
+				<div class="btn_etape">
+					<button type="submit" class="btn_majeur btn_valider">Valider</button>
+				</div>			
             </form>
             <?php 
             if(count($errors) > 0)
