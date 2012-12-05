73062b6638e5497a2c791f7acb0bac50e937a062
Vincent LAURENT <vince.laurent@gmail.com>
2012-12-05 20:38:25 +0100

    - component du recap des stocks - suppression du titre dans le partial des mouvements pour plus de compatibilité

---
+++ modules/drm/templates/_stocksRecap.php
@@ -0,0 +1,37 @@
+<?php use_helper('Float'); ?>
+
+<table class="table_recap">
+    <thead>
+        <tr>
+            <th>Produit</th>
+            <th>Déb.&nbsp;(DS)</th>
+            <th>Entrées</th>
+            <th>dont Rev.&nbsp;(ODG)</th>
+            <th>Sorties&nbsp;(Ventes)</th>
+            <th>Fin&nbsp;(DS)</th>
+            <th>dont Com.</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($recaps->getRawValue(ESC_RAW) as $recap): ?>
+            <tr>
+                <td><?php echo $recap['produit'] ?></td>
+                <td>
+                    <?php echoFloat($recap['volume_stock_debut']) ?>&nbsp;<?php if($recap['volume_stock_debut_ds'] !== null): ?>(<?php echoFloat($recap['volume_stock_debut_ds']) ?>)<?php else: ?>(Abs.)<?php endif; ?>    
+                </td>
+                <td><?php echoFloat($recap['volume_entrees']) ?></td>
+                <td>
+                    <?php echoFloat($recap['volume_revendique']) ?>&nbsp;<?php if($recap['volume_revendique_drev'] !== null): ?>(<?php echoFloat($recap['volume_revendique_drev']) ?>)<?php else: ?>(Abs.)<?php endif; ?>
+                </td>
+                <td><?php echoFloat($recap['volume_sorties']) ?>&nbsp;(0.00)</td>
+                <td>
+                    <?php echoFloat($recap['volume_stock_fin']) ?>&nbsp;<?php if($recap['volume_stock_fin_ds'] !== null): ?>(<?php echoFloat($recap['volume_stock_fin_ds']) ?>)<?php else: ?>(Abs.)<?php endif; ?> 
+                </td>
+                <td>N.I.</td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+<tbody>
+
+</tbody>
+</table>
