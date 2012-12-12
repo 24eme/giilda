377da450bb0ed80d6a5d0c18d31cf21f3dd2fcae
mikael <mguillin@actualys.com>
2012-12-12 10:11:49 +0100

    Ajout d'un bouton "Retour accueil" dans Contrats

--- modules/vrac/templates/_actions.php
+++ modules/vrac/templates/_actions.php
@@ -2,7 +2,10 @@
     <div id="action" class="bloc_col">
             <h2>Action</h2>
 
-            <div class="contenu">
+            <div id="contrat_actions" class="contenu">
+                <div class="btnRetourAccueil">
+                    <a href="<?php echo url_for('vrac'); ?>" class="btn_majeur">Retour accueil</a>
+                </div>
                 <div class="btnNouveau">
                     <a href="<?php echo url_for('vrac_nouveau'); ?>" class="btn_majeur btn_nouveau"><span>Saisir un nouveau contrat</span></a>
                 </div>
