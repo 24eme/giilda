ab88457603ca7c0db2b06b96c4af81aab91bec0e
mathurin <mpetit@actualys.com>
2012-12-18 14:39:51 +0100

    Pour la génération des DS on catch l'exception levé si les ds sont pour la meme période et on continue la génération

--- project/plugins/acVinGenerationPlugin/lib/GenerationDSPDF.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationDSPDF.class.php
@@ -10,46 +10,51 @@
  * @author mathurin
  */
 class GenerationDSPDF extends GenerationPDF {
-    
-  function __construct(Generation $g, $config = null, $options = null) {
-    parent::__construct($g, $config, $options);
-  }
-  
-  public function preGeneratePDF() {
-    parent::preGeneratePDF();
-    
-    $etablissementsViews = array(); 
-    $operateur_types = array(EtablissementFamilles::FAMILLE_PRODUCTEUR, EtablissementFamilles::FAMILLE_NEGOCIANT);
-    if ($this->generation->arguments->exist('operateur_types')) {
-      $operateur_types = explode(',',$this->generation->arguments->operateur_types);
+
+    function __construct(Generation $g, $config = null, $options = null) {
+        parent::__construct($g, $config, $options);
     }
-    foreach ($operateur_types as $operateur_type) {
-      if(EtablissementFamilles::FAMILLE_PRODUCTEUR != $operateur_type && EtablissementFamilles::FAMILLE_NEGOCIANT != $operateur_type)
-	throw new sfException("this operateur type $operateur_type isn't a valid operateur type");
-      $etablissementsViews = array_merge($etablissementsViews, EtablissementClient::getInstance()->findByFamille($operateur_type,null)->rows);
+
+    public function preGeneratePDF() {
+        parent::preGeneratePDF();
+
+        $etablissementsViews = array();
+        $operateur_types = array(EtablissementFamilles::FAMILLE_PRODUCTEUR, EtablissementFamilles::FAMILLE_NEGOCIANT);
+        if ($this->generation->arguments->exist('operateur_types')) {
+            $operateur_types = explode(',', $this->generation->arguments->operateur_types);
+        }
+        foreach ($operateur_types as $operateur_type) {
+            if (EtablissementFamilles::FAMILLE_PRODUCTEUR != $operateur_type && EtablissementFamilles::FAMILLE_NEGOCIANT != $operateur_type)
+                throw new sfException("this operateur type $operateur_type isn't a valid operateur type");
+            $etablissementsViews = array_merge($etablissementsViews, EtablissementClient::getInstance()->findByFamille($operateur_type, null)->rows);
+        }
+
+        $dsClient = DSClient::getInstance();
+        $cpt = 0;
+        foreach ($etablissementsViews as $etablissement) {
+            try {
+                $ds = $dsClient->createDsByEtbId($etablissement->key[5], $this->generation->arguments->date_declaration);
+                $ds->save();
+                $this->generation->documents->add($cpt, $ds->_id);
+                $cpt++;
+            } catch (sfException $exc) {
+                echo $exc->getMessage();
+                continue;
+            }
+        }
     }
-    
-    $dsClient = DSClient::getInstance();
-    $cpt = 0;
-    foreach ($etablissementsViews as $etablissement) {
-      $ds = $dsClient->createDsByEtbId($etablissement->key[5], $this->generation->arguments->date_declaration);
-      $ds->save();
-      $this->generation->documents->add($cpt, $ds->_id);
-      $cpt++;
+
+    protected function getDocumentName() {
+        return "Declarations de stock";
     }
-  }
-    
-  protected function getDocumentName() {
-    return "Declarations de stock";
-  }
-  
-  protected function generatePDFForADocumentID($docid) {
-    $ds = DSClient::getInstance()->find($docid);
-    if (!$ds) {
-      throw new sfException("DS $dsid doesn't exist\n");
-      continue;
+
+    protected function generatePDFForADocumentID($docid) {
+        $ds = DSClient::getInstance()->find($docid);
+        if (!$ds) {
+            throw new sfException("DS $dsid doesn't exist\n");
+            continue;
+        }
+        return new DSLatex($ds, $this->config);
     }
-    return new DSLatex($ds, $this->config);
-  }
-  
+
 }
