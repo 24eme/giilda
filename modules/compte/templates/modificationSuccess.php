eba3f8144f60fd604a6d9baaec072e02fa44dece
mathurin <mpetit@actualys.com>
2012-12-19 17:36:10 +0100

    changement avec la colonne de droite ajout des contacts de la societe dans les comptes recupération des societe de compte dans les actions

--- modules/compte/templates/modificationSuccess.php
+++ modules/compte/templates/modificationSuccess.php
@@ -1,31 +1,38 @@
-<div id="contenu">
+<!-- #principal -->
+<section id="principal">
+    <p id="fil_ariane"><strong>Page d'accueil > Contact > </strong>Modification d'un Contact</p>
 
-    <!-- #principal -->
-    <section id="principal">
-        <p id="fil_ariane"><strong>Page d'accueil > Contact > </strong>Modification d'un Contact</p>
-
-        <!-- #contenu_etape -->
-        <section id="contenu_etape">
-            <h2>Modification d'un établissement</h2>
-            <form action="<?php echo url_for('compte_modification', array('identifiant' => $compteModificationForm->getObject()->identifiant)); ?>" method="post">
-                <?php 
-                    
-                    include_partial('compte/modification', array('compteForm' => $compteModificationForm));
-                 ?>
-            </form>
-        </section>
+    <!-- #contenu_etape -->
+    <section id="contenu_etape">
+        <h2>Modification d'un établissement</h2>
+        <form action="<?php echo url_for('compte_modification', array('identifiant' => $compteModificationForm->getObject()->identifiant)); ?>" method="post">
+            <?php
+            include_partial('compte/modification', array('compteForm' => $compteModificationForm));
+            ?>
+        </form>
     </section>
-    <aside id="colonne">
-        <div class="bloc_col" id="contrat_aide">
-            <h2>Aide</h2>
-
-            <div class="contenu">
-                <ul>
-                    <li class="raccourcis"><a href="#">Raccourcis clavier</a></li>
-                    <li class="assistance"><a href="#">Assistance</a></li>
-                    <li class="contact"><a href="#">Contacter le support</a></li>
-                </ul>
-            </div>
+</section>
+<?php
+slot('colButtons');
+?>
+<div id="action" class="bloc_col">
+    <h2>Action</h2>
+    <div class="contenu">
+        <div class="btnRetourAccueil">
+            <a href="<?php echo url_for('societe'); ?>" class="btn_majeur btn_acces"><span>Accueil des sociétés</span></a>
         </div>
-    </aside>
+    </div>
+        <div class="contenu">
+        <div class="btnRetourAccueil">
+            <a href="<?php echo url_for('societe_visualisation', array('identifiant' => $societe->identifiant)); ?>" class="btn_majeur btn_acces"><span>Accueil de la société</span></a>
+        </div>
+    </div>
+    <div class="contenu">
+        <div class="btnRetourAccueil">
+            <a href="<?php echo url_for('compte_modification', array('identifiant' => $compteModificationForm->getObject()->identifiant) ); ?>" class="btn_majeur btn_acces"><span>Modifier le compte</span></a>
+        </div>
+    </div>
 </div>
+<?php
+end_slot();
+?>
--- modules/compte/templates/nouveauSuccess.php
+++ modules/compte/templates/nouveauSuccess.php
@@ -1,33 +1,18 @@
-<div id="contenu">
-    <!-- #principal -->
-    <section id="principal">
-        <p id="fil_ariane"><a href="#">Page d'accueil</a> &gt; <a href="#">Contacts</a> &gt; <a href="#"><?php echo $societe->raison_sociale; ?></a> &gt; <strong>Nouveau contact</strong></p>
+<!-- #principal -->
+<section id="principal">
+    <p id="fil_ariane"><a href="#">Page d'accueil</a> &gt; <a href="#">Contacts</a> &gt; <a href="#"><?php echo $societe->raison_sociale; ?></a> &gt; <strong>Nouveau contact</strong></p>
 
-        <!-- #contacts -->
-        <section id="contacts">
-            <div id="creation_societe">
-                <h1>Création d'une nouvelle société</h1>
+    <!-- #contacts -->
+    <section id="contacts">
+        <div id="creation_societe">
+            <h1>Création d'une nouvelle société</h1>
             <form action="<?php echo url_for('compte_new', array('identifiant' => $compte->identifiant)); ?>" method="post">
                 <button id="btn_valider" type="submit">Valider</button>
-                
-                <?php 
-                include_partial('modificationDetail', array('compteForm' => $compteForm)); 
-                include_partial('modification', array('compteForm' => $compteForm)); 
+
+                <?php
+                include_partial('modificationDetail', array('compteForm' => $compteForm));
+                include_partial('modification', array('compteForm' => $compteForm));
                 ?>
             </form>
-        </section>
     </section>
-    <aside id="colonne">
-        <div class="bloc_col" id="contrat_aide">
-            <h2>Aide</h2>
-
-            <div class="contenu">
-                <ul>
-                    <li class="raccourcis"><a href="#">Raccourcis clavier</a></li>
-                    <li class="assistance"><a href="#">Assistance</a></li>
-                    <li class="contact"><a href="#">Contacter le support</a></li>
-                </ul>
-            </div>
-        </div>
-    </aside>
-</div>
+</section>
