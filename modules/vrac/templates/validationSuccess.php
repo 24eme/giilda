28536c5e57fc421e461045d82524d264dedd7618
mathurin <mpetit@actualys.com>
2012-12-19 17:46:35 +0100

    changement de l'affichage des colonnes dans les contrats

--- modules/vrac/templates/validationSuccess.php
+++ modules/vrac/templates/validationSuccess.php
@@ -12,61 +12,73 @@ use_helper('Vrac');
     $(document).ready(function()
     {
 <?php echo ($contratsSimilairesExist) ? 'initValidationWithPopup();' : 'initValidation();'; ?>
-        });
+    });
 </script>
 
-<div id="contenu">
-    <div id="rub_contrats" >
-        <section id="principal">
-            <?php include_partial('headerVrac', array('vrac' => $vrac, 'actif' => 4)); ?>        
-            <div id="contenu_etape"> 
-                <form id="vrac_validation" method="post" action="<?php echo url_for('vrac_validation', $vrac) ?>">
-                    
-                    <?php if(count($validation->getErrors()) > 0): ?>
-                        <div id="points_bloquants">
-                            <p>Points bloquants :</p>
-                            <ul>
-                                <?php foreach($validation->getErrors() as $message): ?>
-                                    <li class="error">
-                                        <span><?php echo $message ?></span>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    <?php endif; ?>
-                    
-                    <?php if(count($validation->getWarnings()) > 0): ?>
-                        <div id="points_vigilance">
-                            <p>Points de vigilance :</p>
-                            <ul>
-                                <?php foreach($validation->getWarnings() as $message): ?>
-                                    <li class="warning">
-                                        <span><?php echo $message ?></span>
-                                    </li>
-                                <?php endforeach; ?>
-                            </ul>
-                        </div>
-                    <?php endif; ?>
-                    
-                    <div id="titre"><span class="style_label">Récapitulatif de la saisie</span></div>
+<section id="principal">
+    <?php include_partial('headerVrac', array('vrac' => $vrac, 'actif' => 4)); ?>        
+    <div id="contenu_etape"> 
+        <form id="vrac_validation" method="post" action="<?php echo url_for('vrac_validation', $vrac) ?>">
 
-                    <?php include_partial('showContrat', array('vrac' => $vrac)); ?>
+            <?php if (count($validation->getErrors()) > 0): ?>
+                <div id="points_bloquants">
+                    <p>Points bloquants :</p>
+                    <ul>
+                        <?php foreach ($validation->getErrors() as $message): ?>
+                            <li class="error">
+                                <span><?php echo $message ?></span>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
 
-                    <div class="btn_etape">
-                        <a href="<?php echo url_for('vrac_condition', $vrac); ?>" class="btn_etape_prec"><span>Etape précédente</span></a>
-                        <?php if ($validation->isValid()) : ?>
-                            <a id="btn_validation" class="btn_validation"><span>Terminer la saisie</span></a>  
-                        <?php endif; ?>
-                    </div> 
-                </form>
-            </div>
-        </section>
-        <aside id="colonne">
-            <?php include_partial('colonne', array('vrac' => $vrac, 'contratNonSolde' => $contratNonSolde)); ?>
-        </aside>       
-        <?php
-        if ($contratsSimilairesExist)
-            include_partial('contratsSimilaires_warning_popup', array('vrac' => $vrac));
-        ?>
+            <?php if (count($validation->getWarnings()) > 0): ?>
+                <div id="points_vigilance">
+                    <p>Points de vigilance :</p>
+                    <ul>
+                        <?php foreach ($validation->getWarnings() as $message): ?>
+                            <li class="warning">
+                                <span><?php echo $message ?></span>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+            <div id="titre"><span class="style_label">Récapitulatif de la saisie</span></div>
+
+            <?php include_partial('showContrat', array('vrac' => $vrac)); ?>
+
+            <div class="btn_etape">
+                <a href="<?php echo url_for('vrac_condition', $vrac); ?>" class="btn_etape_prec"><span>Etape précédente</span></a>
+                <?php if ($validation->isValid()) : ?>
+                    <a id="btn_validation" class="btn_validation"><span>Terminer la saisie</span></a>  
+                <?php endif; ?>
+            </div> 
+        </form>
     </div>
-</div>
+</section>
+<?php
+slot('colApplications');
+/*
+ * Inclusion du panel de progression d'édition du contrat
+ */
+if (!$contratNonSolde)
+    include_partial('contrat_progression', array('vrac' => $vrac));
+
+/*
+ * Inclusion du panel pour les contrats similaires
+ */
+include_partial('contratsSimilaires', array('vrac' => $vrac));
+
+/*
+ * Inclusion des Contacts
+ */
+include_partial('contrat_infos_contact', array('vrac' => $vrac));
+
+end_slot();
+
+if ($contratsSimilairesExist)
+    include_partial('contratsSimilaires_warning_popup', array('vrac' => $vrac));
+?>
--- modules/vrac/templates/visualisationSuccess.php
+++ modules/vrac/templates/visualisationSuccess.php
@@ -7,56 +7,60 @@
  * Derniere date de modification : 29-05-12
  */
 ?>
-<div id="contenu">
-    <div id="rub_contrats">
-        <section id="principal">      
-            <div id="contenu_etape">
-                <div id="vrac_visualisation"> 
-                    <div id="titre">
-                   <span class="style_label">N° d'enregistrement du contrat : <?php echo preg_replace('/(\d{8})(\d+)/', '\1 \2', $vrac['numero_contrat']); ?></span>
-                    </div>
-                    <form id="vrac_condition" method="post" action="<?php echo url_for('vrac_visualisation',$vrac) ?>">  
-                        <div id="ss_titre" class="legende"><span class="style_label">Etat du contrat</span>
-                            <?php if($vrac->valide->statut!= VracClient::STATUS_CONTRAT_ANNULE) : ?>
-                                <?php if($vrac->valide->statut== VracClient::STATUS_CONTRAT_NONSOLDE) : ?>
-                                    <a href="<?php echo url_for('vrac_solder',$vrac) ?>" class="btn_majeur btn_vert f_right">Solder le contrat</a>
-                                <?php endif; ?>
-                                <?php if($vrac->valide->statut== VracClient::STATUS_CONTRAT_SOLDE) : ?>
-                                    <a href="<?php echo url_for('vrac_nonsolder',$vrac) ?>" class="btn_majeur btn_orange f_right">Annuler le solde</a>
-                                <?php endif; ?>
-                            <?php endif; ?>
-                            <div>
-                            <?php 
-
-                            if($vrac->valide->statut == VracClient::STATUS_CONTRAT_NONSOLDE){ 
-                                $class = 'statut_non-solde';
-                            }elseif($vrac->valide->statut == VracClient::STATUS_CONTRAT_ANNULE){ 
-                                $class = 'statut_annule';
-                            }else{ 
-                                $class = 'statut_solde';
-                            } ?>
-
-                            <span class="statut <?php echo $class; ?>"></span><span class="legende_statut_texte"><?php echo $vrac->valide->statut; ?></span>
-                            </div>                            
-                        </div>
-                        <div id="ligne_btn">
-                            <?php if(!is_null($vrac->valide->statut) 
-                                    && $vrac->valide->statut != VracClient::STATUS_CONTRAT_ANNULE 
-                                    && (is_null($vrac->volume_enleve) || ($vrac->volume_enleve==0))): ?>
-                                <a id="btn_editer_contrat" href="<?php echo url_for('vrac_soussigne', $vrac); ?>"> Editer le contrat</a>
-                                <button id="btn_annuler_contrat" type="submit">Annuler le contrat</button>                                
-                            <?php endif; ?>                                 
-                        </div>
-                    </form>
-                    <?php include_partial('showContrat', array('vrac' => $vrac)); ?>
-                </div>
+<section id="principal">      
+    <div id="contenu_etape">
+        <div id="vrac_visualisation"> 
+            <div id="titre">
+                <span class="style_label">N° d'enregistrement du contrat : <?php echo preg_replace('/(\d{8})(\d+)/', '\1 \2', $vrac['numero_contrat']); ?></span>
             </div>
-        </section>
-        <aside id="colonne">
-            <?php include_partial('actions_visu', array('vrac' => $vrac)); ?>
-            <?php include_partial('contrat_aide'); ?>
-            <?php include_partial('contrat_campagne',array('vrac' => $vrac, 'visualisation' => true)); ?>
-            <?php include_partial('contrat_infos_contact',array('vrac' => $vrac)); ?>
-        </aside>
+            <form id="vrac_condition" method="post" action="<?php echo url_for('vrac_visualisation', $vrac) ?>">  
+                <div id="ss_titre" class="legende"><span class="style_label">Etat du contrat</span>
+                    <?php if ($vrac->valide->statut != VracClient::STATUS_CONTRAT_ANNULE) : ?>
+                        <?php if ($vrac->valide->statut == VracClient::STATUS_CONTRAT_NONSOLDE) : ?>
+                            <a href="<?php echo url_for('vrac_solder', $vrac) ?>" class="btn_majeur btn_vert f_right">Solder le contrat</a>
+                        <?php endif; ?>
+                        <?php if ($vrac->valide->statut == VracClient::STATUS_CONTRAT_SOLDE) : ?>
+                            <a href="<?php echo url_for('vrac_nonsolder', $vrac) ?>" class="btn_majeur btn_orange f_right">Annuler le solde</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                    <div>
+                        <?php
+                        if ($vrac->valide->statut == VracClient::STATUS_CONTRAT_NONSOLDE) {
+                            $class = 'statut_non-solde';
+                        } elseif ($vrac->valide->statut == VracClient::STATUS_CONTRAT_ANNULE) {
+                            $class = 'statut_annule';
+                        } else {
+                            $class = 'statut_solde';
+                        }
+                        ?>
+
+                        <span class="statut <?php echo $class; ?>"></span><span class="legende_statut_texte"><?php echo $vrac->valide->statut; ?></span>
+                    </div>                            
+                </div>
+                <div id="ligne_btn">
+                    <?php
+                    if (!is_null($vrac->valide->statut)
+                            && $vrac->valide->statut != VracClient::STATUS_CONTRAT_ANNULE
+                            && (is_null($vrac->volume_enleve) || ($vrac->volume_enleve == 0))):
+                        ?>
+                        <a id="btn_editer_contrat" href="<?php echo url_for('vrac_soussigne', $vrac); ?>"> Editer le contrat</a>
+                        <button id="btn_annuler_contrat" type="submit">Annuler le contrat</button>                                
+            <?php endif; ?>                                 
+                </div>
+            </form>
+<?php include_partial('showContrat', array('vrac' => $vrac)); ?>
+        </div>
     </div>
-</div>
+</section>
+<?php
+slot('colButtons');
+include_partial('actions_visu', array('vrac' => $vrac));
+end_slot();
+
+slot('colApplications');
+include_partial('contrat_campagne', array('vrac' => $vrac, 'visualisation' => true));
+include_partial('contrat_infos_contact', array('vrac' => $vrac));
+
+end_slot();
+?>
+
