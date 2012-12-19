3ecc46389381e968283ca00b29018f4d7d6a9702
mathurin <mpetit@actualys.com>
2012-12-19 17:44:23 +0100

    ajout des interlocuteur +btn de retour dans la colonne de droite

--- modules/facture/templates/indexSuccess.php
+++ modules/facture/templates/indexSuccess.php
@@ -1,5 +1,4 @@
-<div id="contenu" class="sv12">
-    
+
     <!-- #principal -->
     <section id="principal">
         <p id="fil_ariane"><strong>Page d'accueil</strong></p>
@@ -15,19 +14,3 @@
     </section>
     <!-- fin #principal -->
     
-    <!-- #colonne -->
-    <aside id="colonne">
-        <div class="bloc_col" id="contrat_aide">
-            <h2>Aide</h2>
-            
-            <div class="contenu">
-                <ul>
-                    <li class="raccourcis"><a href="#">Raccourcis clavier</a></li>
-                    <li class="assistance"><a href="#">Assistance</a></li>
-                    <li class="contact"><a href="#">Contacter le support</a></li>
-                </ul>
-            </div>
-        </div>
-    </aside>
-    <!-- fin #colonne -->
-</div>
--- modules/facture/templates/monEspaceSuccess.php
+++ modules/facture/templates/monEspaceSuccess.php
@@ -1,54 +1,50 @@
 <?php
 use_helper('Float');
-?>
-<div id="contenu" class="facture">
-    
-    <!-- #principal -->
-    <section id="principal">
-        <p id="fil_ariane"><a href="<?php echo url_for('facture') ?>">Page d'accueil</a> &gt; <strong><?php echo $etablissement->nom ?></strong></p>
-        
-        <!-- #contenu_etape -->
-        <section id="contenu_etape">
-          <?php include_component('facture', 'chooseEtablissement', array('identifiant' => $etablissement->identifiant)); ?>
-        </section>
-        <br />
-        
-        <?php 
-        include_partial('historiqueFactures',array('etablissement' => $etablissement,'factures' => $factures));
-        ?>
-          <hr />
-          <h2>Génération de facture</h2>
-          <br />
-          <?php include_partial('facture/mouvements', array('mouvements' => $mouvements, 'etablissement' => $etablissement, 'form'=>$form)) ?>
+?>    
+<!-- #principal -->
+<section id="principal">
+    <p id="fil_ariane"><a href="<?php echo url_for('facture') ?>">Page d'accueil</a> &gt; <strong><?php echo $etablissement->nom ?></strong></p>
+
+    <!-- #contenu_etape -->
+    <section id="contenu_etape">
+        <?php include_component('facture', 'chooseEtablissement', array('identifiant' => $etablissement->identifiant)); ?>
     </section>
-    <!-- fin #principal -->
-    
-    <!-- #colonne -->
-    <aside id="colonne">
-        <div class="bloc_col" id="contrat_aide">
-            <h2>Aide</h2>
-            
-            <div class="contenu">
-                <ul>
-                    <li class="raccourcis"><a href="#">Raccourcis clavier</a></li>
-                    <li class="assistance"><a href="#">Assistance</a></li>
-                    <li class="contact"><a href="#">Contacter le support</a></li>
-                </ul>
-            </div>
+    <br />
+
+    <?php
+    include_partial('historiqueFactures', array('etablissement' => $etablissement, 'factures' => $factures));
+    ?>
+    <hr />
+    <h2>Génération de facture</h2>
+    <br />
+    <?php include_partial('facture/mouvements', array('mouvements' => $mouvements, 'etablissement' => $etablissement, 'form' => $form)) ?>
+</section>
+<!-- fin #principal -->
+
+<?php
+slot('colButtons');
+?>
+<div id="action" class="bloc_col">
+    <h2>Action</h2>
+    <div class="contenu">
+        <div class="btnRetourAccueil">
+            <a href="<?php echo url_for('facture'); ?>" class="btn_majeur btn_acces"><span>Retour à l'accueil</span></a>
         </div>
-    </aside>
-    <!-- fin #colonne -->
+    </div>
 </div>
+<?php
+end_slot();
+?>
 
 <script type="text/javascript">
     
     $(document).ready( function()
-	{
-            $('#generation_facture').bind('click', function()
-            {
-                $('form#generation_form').submit();
-            });
+    {
+        $('#generation_facture').bind('click', function()
+        {
+            $('form#generation_form').submit();
         });
+    });
     
 </script>
 
