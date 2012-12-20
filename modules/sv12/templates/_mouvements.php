75d4f3ca1d9a12ab36470d862a5edd736b3ee091
Tangui Morlier <tangui@tangui.eu.org>
2012-12-20 20:36:02 +0100

    Affiche le nom du vendeur même qd pas de contrat

--- modules/sv12/templates/_mouvements.php
+++ modules/sv12/templates/_mouvements.php
@@ -18,11 +18,11 @@
             <tr class="<?php if($i%2!=0) echo 'alt'; if($mouvement->facturable) echo " facturable"; ?>">
                 <td><?php echo sprintf("%s - %s", ($mouvement->version) ? $mouvement->version : 'M00', format_date($mouvement->date_version));?></td>
                 <td>
-                	<?php if (!$mouvement->vrac_numero): ?>
-                	-
-                	<?php else: ?>
+ <?php if ($mouvement->vrac_numero) { ?>
                     <a href="<?php echo url_for(array('sf_route' => 'vrac_visualisation', 'numero_contrat' => $mouvement->vrac_numero)) ?>"><?php echo VracClient::getInstance()->getLibelleFromId($mouvement->vrac_numero, '&nbsp;') ?></a> <?php echo sprintf("(%s, %s)", $mouvement->type_libelle, $mouvement->vrac_destinataire); ?>
-                    <?php endif; ?>
+     <?php }else if ($mouvement->vrac_destinataire) {
+   echo "SANS CONTRAT ".sprintf("(%s, %s)", $mouvement->type_libelle, $mouvement->vrac_destinataire);
+} else {  echo '-'; } ?>
                 </td>
                 <td>
                     <?php echo $mouvement->produit_libelle; ?>
