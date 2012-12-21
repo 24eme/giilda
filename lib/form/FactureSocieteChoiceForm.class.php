0d8085429d7658f6068b2021ae25c1745602e17f
Tangui Morlier <tangui@tangui.eu.org>
2012-12-21 16:11:15 +0100

    Type societe plutot que famille

--- lib/form/FactureSocieteChoiceForm.class.php
+++ lib/form/FactureSocieteChoiceForm.class.php
@@ -5,7 +5,7 @@ class FactureSocieteChoiceForm extends SocieteChoiceForm {
     public function configure()
     {
         parent::configure();
-        $this->configureFamilles(array(EtablissementFamilles::FAMILLE_PRODUCTEUR, EtablissementFamilles::FAMILLE_NEGOCIANT));
+        $this->configureTypeSociete(array(SocieteClient::SUB_TYPE_VITICULTEUR, SocieteClient::SUB_TYPE_NEGOCIANT));
     }
 
 }
