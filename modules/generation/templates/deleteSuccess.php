ce53fc6461bac3d2e26fd8fad769c911662cad17
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 21:06:10 +0100

    Possibilité de supprimer une génération

---
+++ modules/generation/templates/deleteSuccess.php
@@ -0,0 +1,10 @@
+   
+    <!-- #principal -->
+    <section id="principal" class="generation_facturation">
+       <form method="POST">
+       Voulez vous supprimer la génération <?php echo $generation->_id; ?> ?
+    <input type="submit" value="Oui" name="delete"/><input type="submit" value="Non"/>
+       </form>
+    </section>
+    <!-- fin #principal -->
+ 
