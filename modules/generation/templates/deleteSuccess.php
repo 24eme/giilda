87f7c705076c1207f6a12e826a3b1565a493bd11
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 21:34:44 +0100

    Présentation et un lien qui marche

--- modules/generation/templates/deleteSuccess.php
+++ modules/generation/templates/deleteSuccess.php
@@ -1,10 +1,8 @@
-   
-    <!-- #principal -->
-    <section id="principal" class="generation_facturation">
-       <form method="POST">
-       Voulez vous supprimer la génération <?php echo $generation->_id; ?> ?
-    <input type="submit" value="Oui" name="delete"/><input type="submit" value="Non"/>
-       </form>
-    </section>
-    <!-- fin #principal -->
+<section id="principal" class="generation_delete">
+<h2>Suppression d'une génération</h2>
+   <form method="POST">
+   Voulez vous supprimer <?php echo $generation->_id; ?> ?
+   <input type="submit" value="Oui" name="delete"/><input type="submit" value="Non"/>
+   </form>
+</section>
  
