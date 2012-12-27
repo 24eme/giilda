e52c1832fc90e37e446fbb7af33193c58af8f956
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 20:25:23 +0100

    Si l'obj generation n'existe pas, on renvoie vers une 404

--- modules/generation/actions/actions.class.php
+++ modules/generation/actions/actions.class.php
@@ -6,6 +6,7 @@ class generationActions extends sfActions {
       $this->identifiant = isset($request['identifiant'])? $request['identifiant'] : null;
       $this->nom = ($this->identifiant)? EtablissementClient::getInstance()->retrieveById($this->identifiant)->nom : null;
       $this->generation = GenerationClient::getInstance()->find(GenerationClient::getInstance()->getId($this->type, $request['date_emission']));
+      $this->forward404Unless($this->generation);
 
   }
   
