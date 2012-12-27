3cabf79b2b61afd2cd59c8537426188839794898
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 20:03:00 +0100

    Affiche même les arguments non déclarés

--- project/plugins/acVinGenerationPlugin/lib/helper/GenerationHelper.php
+++ project/plugins/acVinGenerationPlugin/lib/helper/GenerationHelper.php
@@ -6,5 +6,9 @@ function getLabelForKeyArgument($key)
        case 'regions' : return 'Régions :';
        case 'operateur_types' : return "Types d'opérateur :";
        case 'date_declaration' : return 'Date de déclaration :';
+       case 'date_facturation' : return 'Date de facturation :';
+       case 'date_mouvement' : return 'Date de prise en compte des mouvements :';
+       case 'date_seuil' : return 'Seuil :';
+       default: return "$key :";
    }
 }
