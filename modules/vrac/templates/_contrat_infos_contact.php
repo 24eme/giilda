e987696bed551f8504ada13a2b0c6ecb02896067
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 12:20:29 +0100

    Clean la gestion des contacts

--- modules/vrac/templates/_contrat_infos_contact.php
+++ modules/vrac/templates/_contrat_infos_contact.php
@@ -1,10 +1,13 @@
 <?php
-$nouveau = is_null($vrac);
-
-$vendeur_coord = $vrac->getCoordonneesVendeur();
-$acheteur_coord = $vrac->getCoordonneesAcheteur();
-if($vrac->mandataire_identifiant)
+$vendeur_coord = null;
+$acheteur_coord = null;
+$mandataire_coord = null;
+if ($vrac) {
+  $vendeur_coord = $vrac->getCoordonneesVendeur();
+  $acheteur_coord = $vrac->getCoordonneesAcheteur();
+  if($vrac->mandataire_identifiant)
     $mandataire_coord = $vrac->getCoordonneesMandataire();
+}
 ?>
 
 <div id="infos_contact" class="bloc_col">
@@ -12,11 +15,12 @@ if($vrac->mandataire_identifiant)
 
     <div class="contenu">
         <ul>
+  <?php if ($vendeur_coord) : ?>
             <li id="infos_contact_vendeur">
                 <a href="<?php echo ($vendeur_coord->identifiant)? url_for('compte_modification',
                         array('identifiant' => $vendeur_coord->identifiant)) : '#'; ?>">Coordonnées vendeur</a>
                 <ul>
-                    <li class="nom"><?php echo (!$nouveau) ? $vendeur_coord->nom_a_afficher : 'Nom du vendeur'; ?></li>
+                    <li class="nom"><?php echo $vendeur_coord->nom_a_afficher; ?></li>
                     <?php if ($vendeur_coord->telephone_bureau != ""): ?>
                         <li class="tel"><?php echo $vendeur_coord->telephone_bureau; ?></li>
                     <?php endif; ?>
@@ -28,11 +32,13 @@ if($vrac->mandataire_identifiant)
                     <?php endif; ?>
                 </ul>
             </li>
+<?php endif; ?>
+<?php if ($acheteur_coord) : ?>
             <li id="infos_contact_acheteur">
                 <a href="<?php echo ($acheteur_coord->identifiant)? url_for('compte_modification',
                         array('identifiant' => $acheteur_coord->identifiant)) : '#'; ?>">Coordonnées acheteur</a>
                 <ul>
-                    <li class="nom"><?php echo (!$nouveau) ? $acheteur_coord->nom_a_afficher : 'Nom du acheteur'; ?></li>
+                    <li class="nom"><?php echo $acheteur_coord->nom_a_afficher; ?></li>
                     <?php if ($acheteur_coord->telephone_bureau != ""): ?>
                         <li class="tel"><?php echo $acheteur_coord->telephone_bureau; ?></li>
                     <?php endif; ?>
@@ -44,12 +50,13 @@ if($vrac->mandataire_identifiant)
                     <?php endif; ?>
                 </ul>
             </li>
-            <?php if($vrac->mandataire_identifiant): ?>
+<?php endif; ?>
+<?php if($mandataire_coord): ?>
             <li id="infos_contact_mendataire">
                 <a href="<?php echo ($mandataire_coord->identifiant)? url_for('compte_modification',
                         array('identifiant' => $mandataire_coord->identifiant)) : '#'; ?>">Coordonnées mandataire</a>
                 <ul>
-                    <li class="nom"><?php echo (!$nouveau) ? $mandataire_coord->nom_a_afficher : 'Nom du acheteur'; ?></li>
+                    <li class="nom"><?php echo $mandataire_coord->nom_a_afficher; ?></li>
                     <?php if ($mandataire_coord->telephone_bureau != ""): ?>
                         <li class="tel"><?php echo $mandataire_coord->telephone_bureau; ?></li>
                     <?php endif; ?>
@@ -61,7 +68,7 @@ if($vrac->mandataire_identifiant)
                     <?php endif; ?>
                 </ul>
             </li>
-            <?php endif; ?>
+<?php endif; ?>
         </ul>
     </div>
 </div>
