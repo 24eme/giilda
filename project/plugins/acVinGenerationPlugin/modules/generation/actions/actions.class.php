45a12effb6293b2167f6d34d7ad0b64e2ae1417f
Tangui Morlier <tangui@tangui.eu.org>
2012-12-27 21:06:10 +0100

    Possibilité de supprimer une génération

--- project/plugins/acVinGenerationPlugin/modules/generation/actions/actions.class.php
+++ project/plugins/acVinGenerationPlugin/modules/generation/actions/actions.class.php
@@ -1,18 +1,33 @@
 <?php
 class generationActions extends sfActions {
     
-  public function executeView(sfWebRequest $request) {
-      $this->type = $request['type_document'];
-      $this->identifiant = isset($request['identifiant'])? $request['identifiant'] : null;
-      $this->nom = ($this->identifiant)? EtablissementClient::getInstance()->retrieveById($this->identifiant)->nom : null;
-      $this->generation = GenerationClient::getInstance()->find(GenerationClient::getInstance()->getId($this->type, $request['date_emission']));
-      $this->forward404Unless($this->generation);
-
+  private function getGenerationFromRequest(sfWebRequest $request) {
+    $this->type = $request['type_document'];
+    $this->identifiant = isset($request['identifiant'])? $request['identifiant'] : null;
+    $this->nom = ($this->identifiant)? EtablissementClient::getInstance()->retrieveById($this->identifiant)->nom : null;
+    $this->date_emission = $request['date_emission'];
+    $this->generation = GenerationClient::getInstance()->find(GenerationClient::getInstance()->getId($this->type, $this->date_emission));
+    $this->forward404Unless($this->generation);
+    return $this->generation;
   }
   
-public function executeList(sfWebRequest $request) {
+  public function executeView(sfWebRequest $request) {
+    $this->generation = $this->getGenerationFromRequest($request);
+  }
+  
+  public function executeList(sfWebRequest $request) {
       $this->type = $request['type_document'];
       $this->historyGeneration = GenerationClient::getInstance()->findHistoryWithType($this->type);
+  }
+
+  public function executeDelete(sfWebRequest $request) {
+    $this->generation = $this->getGenerationFromRequest($request);
+    if ($request->isMethod(sfWebRequest::POST)) {
+      if ($request->getParameter('delete')) {
+	$this->generation->delete();
+      }
+      return $this->redirect('generation_list', array('type_document' => $this->type));
+    }
   }
     
 }
