fe6c957e4c4cc6b8f2d30176f3c750cacba0b20f
Tangui Morlier <tangui@tangui.eu.org>
2013-01-07 20:35:32 +0100

    Wording de annuler à abandonner

--- modules/drm_edition/templates/addLabelSuccess.php
+++ modules/drm_edition/templates/addLabelSuccess.php
@@ -5,7 +5,7 @@
 			<?php echo $form->renderHiddenFields(); ?>
 			<?php echo $form->render(); ?>
 		</div>
-		<a href="<?php echo url_for('drm_edition_detail', $detail); ?>" id="drm_labels_annuler" class="btn_majeur btn_annuler">Annuler</a>
+		<a href="<?php echo url_for('drm_edition_detail', $detail); ?>" id="drm_labels_annuler" class="btn_majeur btn_annuler">Abandonner</a>
 		<button type="submit" class="btn_majeur btn_valider" >Valider</button>
 		</form>
 	</section>
