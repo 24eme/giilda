e2384e038c0eb78b5724bedeb875caa7d8078192
Tangui Morlier <tangui@tangui.eu.org>
2013-01-07 13:54:33 +0100

    Gestion des points de vigilance contrats vracs basés sur les mouvements et non sur les détails

--- lib/controle/DRMValidation.class.php
+++ lib/controle/DRMValidation.class.php
@@ -22,32 +22,42 @@ class DRMValidation extends DocumentValidation
 			if($detail->total < 0) {
 				$this->addPoint('vigilance', 'total_negatif', $detail->getLibelle(), $this->generateUrl('drm_edition_detail', $detail));
 			}
+		}
 
-			foreach($detail->sorties->vrac_details as $vrac_detail) {
-				$vrac = $vrac_detail->getVrac();
+		$volumes_restant = array();
+		foreach ($this->document->getMouvementsCalculeByIdentifiant($this->document->identifiant) as $mouvement) {
+		  if ($mouvement->isVrac()) {
+		    $vrac = $mouvement->getVrac();
+		      if(!$vrac) {
+			$this->addPoint('erreur', 'vrac_detail_exist', sprintf("%s, Contrat n°%s avec %s", $mouvement->produit_libelle, $mouvement->detail_libelle, $mouvement->vrac_destinataire), $this->generateUrl('drm_edition_detail', $detail));
+			continue;
+		      }
 
-				if(!$vrac) {
-					$this->addPoint('erreur', 'vrac_detail_exist', sprintf("%s, Contrat %s", $detail->getLibelle(), $vrac_detail->identifiant), $this->generateUrl('drm_edition_detail', $detail));
-					continue;
-				}
+		    if ($vrac->valide->statut != VracClient::STATUS_CONTRAT_NONSOLDE) {
+		      $this->addPoint('erreur', 'vrac_detail_nonsolde', sprintf("Contrat %s", $mouvement->produit_libelle, $vrac->__toString()), $this->generateUrl('vrac_visualisation', $vrac));
+		      continue;
+		    }
+		    $id_volume_restant = $mouvement->produit_hash.$mouvement->vrac_numero;
+		    if (!isset($volumes_restant[$id_volume_restant])) {
+		      $volumes_restant[$id_volume_restant]['volume'] = $vrac->volume_propose - $vrac->volume_enleve;
+		      $volumes_restant[$id_volume_restant]['vrac'] = $vrac;
+		    }
+		    $volumes_restant[$id_volume_restant]['volume'] +=  $mouvement->volume;
+		  }
+		}
 
-				if ($vrac->valide->statut != VracClient::STATUS_CONTRAT_NONSOLDE) {
-					$this->addPoint('erreur', 'vrac_detail_nonsolde', sprintf("%s, Contrat %s", $detail->getLibelle(), $vrac->__toString()), $this->generateUrl('vrac_visualisation', $vrac));
-					continue;
-				}
-
-				$volume_restant = $vrac->volume_propose - ($vrac->volume_enleve + $vrac_detail->volume); 
-				if($volume_restant < 0) {
-					$this->addPoint('vigilance', 
-									'vrac_detail_negatif', 
-									sprintf("%s, Contrat %s (%01.02f hl enlevé / %01.02f hl proposé)", 
-									     	$detail->getLibelle(), 
-									     	$vrac->__toString(), 
-									     	$vrac->volume_enleve + $vrac_detail->volume,
-									     	$vrac->volume_propose), 
-									$this->generateUrl('drm_edition_detail', $detail));
-				}
-			}
+		foreach ($volumes_restant as $is => $restant) {
+		    if($restant['volume'] < 0) {
+		      $vrac = $restant['vrac'];
+		      $this->addPoint('vigilance', 
+				      'vrac_detail_negatif', 
+				      sprintf("%s, Contrat %s (%01.02f hl enlevé / %01.02f hl proposé)", 
+					      $mouvement->produit_libelle, 
+					      $vrac->__toString(), 
+					      $vrac->volume_propose - $restant['volume'],
+					      $vrac->volume_propose), 
+				      $this->generateUrl('drm_edition', $this->document));
+		    }
 		}
 
 		if ($total_entrees_replis != $total_sorties_replis) {
