1dcf9ded9a65b3ee3445f37fc7eb34ddfa295e20
Vincent LAURENT <vlaurent@actualys.com>
2013-01-09 00:15:54 +0100

    Stock nego mouvement de la sv12 et de la drm + recap raisins, mouts et vins

--- project/plugins/acVinStocksPlugin/modules/stocks/templates/_recap.php
+++ project/plugins/acVinStocksPlugin/modules/stocks/templates/_recap.php
@@ -1,9 +1,9 @@
 <?php if($etablissement->famille == EtablissementFamilles::FAMILLE_PRODUCTEUR): ?>
-<h2>Stocks vins (DRMs <?php echo $campagne ?>)</h2>
+<h2>Stocks (<?php echo $campagne ?>)</h2>
 <?php include_component('drm', 'stocksRecap', array('campagne' => $campagne, 'etablissement' => $etablissement)); ?>
 <?php endif; ?>
 
 <?php if($etablissement->famille == EtablissementFamilles::FAMILLE_NEGOCIANT): ?>
-<h2>Synthèse raisins et moûts (SV12s <?php echo $campagne ?>)</h2>
-<?php include_component('sv12', 'stocksRecap', array('campagne' => $campagne, 'etablissement' => $etablissement)); ?>
+<h2>Stocks (<?php echo $campagne ?>)</h2>
+<?php include_component('stocks', 'recapNegociant', array('campagne' => $campagne, 'etablissement' => $etablissement)); ?>
 <?php endif ?>
