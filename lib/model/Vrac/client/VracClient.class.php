6e78051e0a2a68fc875f247aa715b3eccc42383d
mathurin <mpetit@actualys.com>
2013-01-08 22:31:56 +0100

    ajout d'une fxct pour recuperer le num d'archivage et la date de signature sous forme de libelle getNumeroArchiveEtDate (pour facture pdf)

--- lib/model/Vrac/client/VracClient.class.php
+++ lib/model/Vrac/client/VracClient.class.php
@@ -367,6 +367,11 @@ class VracClient extends acCouchdbClient {
         return $jour.'/'.$mois.'/'.$annee.' n° '.$num;
     }
     
+    public function getNumeroArchiveEtDate($id) {
+        $c = $this->findByNumContrat($id);
+        return $c->numero_archive.' du '.$c->date_signature;
+    }
+    
     public function retreiveByStatutsTypes($statuts,$types) {
         return VracStatutAndTypeView::getInstance()->findContatsByStatutsAndTypes($statuts,$types);
     }
