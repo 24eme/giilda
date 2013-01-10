10b334d250151507fb23569db8819e2cfc620bb8
Vincent LAURENT <vlaurent@actualys.com>
2013-01-10 11:51:46 +0100

    Delete from view avec les constantes de base

--- project/bin/delete_from_view.sh
+++ project/bin/delete_from_view.sh
@@ -1,4 +1,6 @@
+. bin/config.inc
+
 curl -s $1 | grep '"id":' | sed 's/.*"id":"//' | sed 's/".*//' | while read OBJ; do
-	OBJREV=$(curl -s http://localhost:5984/vinsdeloire/$OBJ | sed 's/{"_id":"//' | sed 's/","_rev":"/?rev=/' | sed 's/".*//')
-	curl -s -X DELETE http://localhost:5984/vinsdeloire/$OBJREV
+	OBJREV=$(curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$OBJ | sed 's/{"_id":"//' | sed 's/","_rev":"/?rev=/' | sed 's/".*//')
+	curl -s -X DELETE http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$OBJREV
 done
