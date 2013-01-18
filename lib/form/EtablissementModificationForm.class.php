543fd35a31e7fe52c2c940b8ff634b3012ec5897
mathurin <mpetit@actualys.com>
2013-01-18 14:04:40 +0100

    affichage des * dans les champs obligatoires

--- lib/form/EtablissementModificationForm.class.php
+++ lib/form/EtablissementModificationForm.class.php
@@ -23,69 +23,54 @@ class EtablissementModificationForm extends acCouchdbObjectForm {
     public function configure() {
         $this->setWidget('nom', new sfWidgetFormInput());
         $this->setWidget('statut', new sfWidgetFormChoice(array('choices' => $this->getStatuts(), 'multiple' => false, 'expanded' => true)));
-
-        if (!$this->etablissement->isCourtier()) {
-            $this->setWidget('cvi', new sfWidgetFormInput());
-            $this->setWidget('relance_ds', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
-            if (!$this->etablissement->isNegociant()) {
-                $this->setWidget('raisins_mouts', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
-                $this->setWidget('exclusion_drm', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
-                $this->setWidget('type_dr', new sfWidgetFormChoice(array('choices' => $this->getTypeDR())));
-            }
-        }
         $this->setWidget('region', new sfWidgetFormChoice(array('choices' => $this->getRegions())));
-
-
         $this->embedForm('liaisons_operateurs', new LiaisonsItemForm($this->getObject()->liaisons_operateurs));
-
-        $this->setWidget('site_fiche', new sfWidgetFormInput());
-        if ($this->etablissement->isCourtier()) {
-            $this->setWidget('carte_pro', new sfWidgetFormInput());
-        }
         $this->setWidget('no_accises', new sfWidgetFormInput());
         $this->setWidget('commentaire', new sfWidgetFormTextarea(array(), array('style' => 'width: 100%;resize:none;')));
+        $this->setWidget('site_fiche', new sfWidgetFormInput());
 
-        $this->widgetSchema->setLabel('nom', 'Nom du chai');
-        $this->widgetSchema->setLabel('statut', 'Statut');
 
-        if (!$this->etablissement->isCourtier()) {
-            $this->widgetSchema->setLabel('cvi', 'CVI');
-            $this->widgetSchema->setLabel('relance_ds', 'Relance DS');
-            if (!$this->etablissement->isNegociant()) {
-                $this->widgetSchema->setLabel('raisins_mouts', 'Raisins et Moûts');
-                $this->widgetSchema->setLabel('exclusion_drm', 'Exclusion DRM');
-                $this->widgetSchema->setLabel('type_dr', 'Type de DR');
-            }
-        }
-        $this->widgetSchema->setLabel('region', 'Région viticole');
-
-        $this->widgetSchema->setLabel('site_fiche', 'Site Fiche Publique');
-        if ($this->etablissement->isCourtier()) {
-            $this->widgetSchema->setLabel('carte_pro', 'N° Carte professionnel');
-        }
+        $this->widgetSchema->setLabel('nom', 'Nom du chai *');
+        $this->widgetSchema->setLabel('statut', 'Statut *');
+        $this->widgetSchema->setLabel('region', 'Région viticole *');
         $this->widgetSchema->setLabel('no_accises', "N° d'Accise");
         $this->widgetSchema->setLabel('commentaire', 'Commentaire');
+        $this->widgetSchema->setLabel('site_fiche', 'Site Fiche Publique');
+
 
 
         $this->setValidator('nom', new sfValidatorString(array('required' => true)));
         $this->setValidator('statut', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getStatuts()))));
+        $this->setValidator('region', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getRegions()))));
+        $this->setValidator('site_fiche', new sfValidatorString(array('required' => false)));
+        $this->setValidator('no_accises', new sfValidatorString(array('required' => false)));
+        $this->setValidator('commentaire', new sfValidatorString(array('required' => false)));
+
+
         if (!$this->etablissement->isCourtier()) {
+            $this->setWidget('cvi', new sfWidgetFormInput());
+            $this->setWidget('relance_ds', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
+            $this->widgetSchema->setLabel('cvi', 'CVI');
+            $this->widgetSchema->setLabel('relance_ds', 'Relance DS *');
             $this->setValidator('cvi', new sfValidatorString(array('required' => false)));
             $this->setValidator('relance_ds', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getOuiNonChoices()))));
+            
             if (!$this->etablissement->isNegociant()) {
+                $this->setWidget('raisins_mouts', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
+                $this->setWidget('exclusion_drm', new sfWidgetFormChoice(array('choices' => $this->getOuiNonChoices())));
+                $this->setWidget('type_dr', new sfWidgetFormChoice(array('choices' => $this->getTypeDR())));
+                $this->widgetSchema->setLabel('raisins_mouts', 'Raisins et Moûts *');
+                $this->widgetSchema->setLabel('exclusion_drm', 'Exclusion DRM *');
+                $this->widgetSchema->setLabel('type_dr', 'Type de DR *');
                 $this->setValidator('raisins_mouts', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getOuiNonChoices()))));
                 $this->setValidator('exclusion_drm', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getOuiNonChoices()))));
                 $this->setValidator('type_dr', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getTypeDR()))));
             }
-        }
-        $this->setValidator('region', new sfValidatorChoice(array('required' => true, 'choices' => array_keys($this->getRegions()))));
-
-        $this->setValidator('site_fiche', new sfValidatorString(array('required' => false)));
-        if ($this->etablissement->isCourtier()) {
+        } else {
+            $this->setWidget('carte_pro', new sfWidgetFormInput());
+            $this->widgetSchema->setLabel('carte_pro', 'N° Carte professionnel');
             $this->setValidator('carte_pro', new sfValidatorString(array('required' => false)));
         }
-        $this->setValidator('no_accises', new sfValidatorString(array('required' => false)));
-        $this->setValidator('commentaire', new sfValidatorString(array('required' => false)));
         $this->widgetSchema->setNameFormat('etablissement_modification[%s]');
     }
 
@@ -104,19 +89,19 @@ class EtablissementModificationForm extends acCouchdbObjectForm {
     public function getTypeDR() {
         return EtablissementClient::getTypeDR();
     }
-    
-     protected function doSave($con = null) {
+
+    protected function doSave($con = null) {
         if (null === $con) {
             $con = $this->getConnection();
         }
-         $this->updateObject();
-         
-         $this->etablissement->remove('liaisons_operateurs');
-         $this->etablissement->add('liaisons_operateurs');
-         
+        $this->updateObject();
+
+        $this->etablissement->remove('liaisons_operateurs');
+        $this->etablissement->add('liaisons_operateurs');
+
         foreach ($this->getEmbeddedForms() as $key => $form) {
-            
-            foreach ($this->values[$key] as $liaison){
+
+            foreach ($this->values[$key] as $liaison) {
                 $this->etablissement->addLiaison($liaison['type_liaison'], EtablissementClient::getInstance()->find($liaison['id_etablissement']));
             }
         }
