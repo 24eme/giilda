6b9274b18fd56600e1102cebf696fb9b153e025d
Tangui Morlier <tangui@tangui.eu.org>
2013-01-21 20:33:19 +0100

    Gestion du code comptable/analytique

--- lib/model/Facture/FactureLigne.class.php
+++ lib/model/Facture/FactureLigne.class.php
@@ -26,7 +26,30 @@ class FactureLigne extends BaseFactureLigne {
             $mouv->facturer();
         }
     }
+   
+    public function setProduitHash($ph) {
+      $ret = $this->_set('produit_hash', $ph);
+      //Remove identifiant_analytique from cache and set the new one
+      $this->_set('produit_identifiant_analytique', null);
+      $this->getProduitIdentifiantAnalytique();
+      return $ret;
+    }
     
+
+    public function getProduitIdentifiantAnalytique() {
+      $id = $this->_get('produit_identifiant_analytique');
+      if ($id) {
+	return $id;
+      }
+      $code = $this->getConfProduit()->getCodeComptable();
+      $this->_set('produit_identifiant_analytique', $code);
+      return $code;
+    }
+
+    public function getConfProduit() {
+      return ConfigurationClient::getCurrent()->get($this->produit_hash);
+    }
+
     public function defacturerMouvements() {
         foreach ($this->getMouvements() as $mouv) {
                $mouv->defacturer();
