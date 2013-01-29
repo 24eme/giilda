011129161a5369783d523dfd13d99b83df000cdc
mathurin <mpetit@actualys.com>
2013-01-29 13:04:49 +0100

    prise en compte du nouveau label de bouton validé

--- modules/etablissement/templates/modificationSuccess.php
+++ modules/etablissement/templates/modificationSuccess.php
@@ -1,48 +1,53 @@
+<?php
+$button_label = 'Valider';
+if (!$etablissement->isNew() && $etablissement == EtablissementClient::STATUT_ACTIF && $etablissement->isSameContactThanSociete()) {
+    $button_label = "Valider et saisir l'interlocuteur";
+}
+?>
 <!-- #principal -->
 <section id="principal">
-    <p id="fil_ariane"><a href="<?php echo url_for('societe');?>">Page d'accueil</a> &gt; Contacts
-            &gt; <a href="<?php echo url_for('societe_visualisation',array('identifiant'=> $societe->identifiant));?>">
+    <p id="fil_ariane"><a href="<?php echo url_for('societe'); ?>">Page d'accueil</a> &gt; Contacts
+        &gt; <a href="<?php echo url_for('societe_visualisation', array('identifiant' => $societe->identifiant)); ?>">
             <?php echo $societe->raison_sociale; ?></a> &gt;
-                <strong>
-                    <?php echo ($etablissement->isNew())? 'Nouvel établissement' : 'Modification établissement' ;?>
-                </strong></p>
+        <strong>
+            <?php echo ($etablissement->isNew()) ? 'Nouvel établissement' : 'Modification établissement'; ?>
+        </strong></p>
 
-        <!-- #contenu_etape -->
-        <section id="contacts">
-            <div id="nouveau_etablissement">
-                <h2><?php echo ($etablissement->isNew())? 'Nouvel établissement' : $etablissement->nom; ?></h2>
-				
-				
-                <form action="<?php echo ($etablissement->isNew())? url_for('etablissement_ajout', array('identifiant' => $societe->identifiant)) : url_for('etablissement_modification', array('identifiant' => $etablissement->identifiant)); ?>" method="post">
-				<div class="form_btn">
-					<a href="<?php echo url_for('societe_visualisation',array('identifiant'=> $societe->identifiant));?>" type="submit" class="btn_majeur btn_annuler">Annuler</a>
-					<button id="btn_valider" type="submit" class="btn_majeur btn_valider">Valider</button>
-				</div>
-					
-					<div id="detail_etablissement" class="etablissement form_section ouvert">
-						<h3>Détail de l'établissement</h3>
-						<?php include_partial('etablissement/modification', array('etablissementForm' => $etablissementModificationForm, 'etablissement' => $etablissement)); ?>
-					</div>
-                    <div id="coordonnees_etablissement" class="etablissement form_section ouvert">
-                        <h3>Coordonnées de l'établissement</h3>
-						
-                        <div class="form_contenu">
-                            <div class="form_ligne">
-                                <?php echo $etablissementModificationForm['adresse_societe']->renderError(); ?>
-                                <?php echo $etablissementModificationForm['adresse_societe']->renderLabel('Même adresse que la société ?', array('class' => 'label_liste')); ?>
-                                <?php echo $etablissementModificationForm['adresse_societe']->render(); ?>
-                            </div>
-                            <?php //include_partial('compte/modification', array('compteForm' => $compteModificationForm)); ?>
+    <!-- #contenu_etape -->
+    <section id="contacts">
+        <div id="nouveau_etablissement">
+            <h2><?php echo ($etablissement->isNew()) ? 'Nouvel établissement' : $etablissement->nom; ?></h2>
+
+
+            <form action="<?php echo ($etablissement->isNew()) ? url_for('etablissement_ajout', array('identifiant' => $societe->identifiant)) : url_for('etablissement_modification', array('identifiant' => $etablissement->identifiant)); ?>" method="post">
+                <div class="form_btn">
+                    <a href="<?php echo url_for('societe_visualisation', array('identifiant' => $societe->identifiant)); ?>" type="submit" class="btn_majeur btn_annuler">Annuler</a>
+                    <button id="btn_valider" type="submit" class="btn_majeur btn_valider"><?php echo $button_label; ?></button>
+                </div>
+
+                <div id="detail_etablissement" class="etablissement form_section ouvert">
+                    <h3>Détail de l'établissement</h3>
+                    <?php include_partial('etablissement/modification', array('etablissementForm' => $etablissementModificationForm, 'etablissement' => $etablissement)); ?>
+                </div>
+                <div id="coordonnees_etablissement" class="etablissement form_section ouvert">
+                    <h3>Coordonnées de l'établissement</h3>
+
+                    <div class="form_contenu">
+                        <div class="form_ligne">
+                            <?php echo $etablissementModificationForm['adresse_societe']->renderError(); ?>
+                            <?php echo $etablissementModificationForm['adresse_societe']->renderLabel('Même adresse que la société ?', array('class' => 'label_liste')); ?>
+                            <?php echo $etablissementModificationForm['adresse_societe']->render(); ?>
                         </div>
-                    </div>  
-				<div class="form_btn">
-					<a href="<?php echo url_for('societe_visualisation',array('identifiant'=> $societe->identifiant));?>" type="submit" class="btn_majeur btn_annuler">Annuler</a>
-					<button id="btn_valider" type="submit" class="btn_majeur btn_valider">Valider</button>
-				</div>
-                </form>	
-            </div>
-        </section>
+                    </div>
+                </div>  
+                <div class="form_btn">
+                    <a href="<?php echo url_for('societe_visualisation', array('identifiant' => $societe->identifiant)); ?>" type="submit" class="btn_majeur btn_annuler">Annuler</a>
+                    <button id="btn_valider" type="submit" class="btn_majeur btn_valider"><?php echo $button_label; ?></button>
+                </div>
+            </form>	
+        </div>
     </section>
+</section>
 <?php
 slot('colButtons');
 ?>
