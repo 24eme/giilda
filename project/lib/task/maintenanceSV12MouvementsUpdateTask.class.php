3ef05aa1815500208e4f19c363eb1f38bd1a00d0
Vincent LAURENT <vlaurent@actualys.com>
2013-02-25 16:34:10 +0100

    tache

--- project/lib/task/maintenanceSV12MouvementsUpdateTask.class.php
+++ project/lib/task/maintenanceSV12MouvementsUpdateTask.class.php
@@ -36,8 +36,9 @@ EOF;
     $rows = SV12AllView::getInstance()->findAll();
 
     foreach($rows as $row) {
+      try {
+      echo $row->id."\n";
       $sv12 = SV12Client::getInstance()->find($row->id, acCouchdbClient::HYDRATE_JSON);
-      echo $sv12->_id."\n";
       foreach($sv12->mouvements as $etablissement_id => $mouvements) {
         foreach($mouvements as $mouvement) {
           $mouvement->date = SV12Client::getInstance()->buildDate($sv12->periode);
@@ -47,16 +48,12 @@ EOF;
             continue;
           }
 
-          try {
-            $vrac = VracClient::getInstance()->find($mouvement->detail_identifiant, acCouchdbClient::HYDRATE_JSON);
-            if(!$vrac) {
-              throw new sfException("trouve pas le contrat");
-            }
-          } catch (Exception $e) {
-            echo sprintf("trouve pas le contrat %s \n", $mouvement->detail_identifiant);
-            continue;
+          
+          $vrac = VracClient::getInstance()->find($mouvement->detail_identifiant, acCouchdbClient::HYDRATE_JSON);
+          if(!$vrac) {
+            throw new sfException("trouve pas le contrat %s", $mouvement->detail_identifiant);
           }
-
+         
           $mouvement->detail_libelle = $vrac->numero_archive;
           $sv12_contrat = $sv12->contrats->{$mouvement->vrac_numero};
 
@@ -71,6 +68,10 @@ EOF;
       }
 
       SV12Client::getInstance()->storeDoc($sv12);
+      } catch (Exception $e) {
+        echo sprintf("%s\n", $e->getMessage());
+        continue;
+      }
     }
   }
 }
