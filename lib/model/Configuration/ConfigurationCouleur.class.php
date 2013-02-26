fac594870980e8bb19575aaf07dea96f596a6f6e
Vincent LAURENT <vlaurent@actualys.com>
2013-02-26 18:08:38 +0100

    Gestion des droits possible au niveau couleur

--- lib/model/Configuration/ConfigurationCouleur.class.php
+++ lib/model/Configuration/ConfigurationCouleur.class.php
@@ -26,22 +26,29 @@ class ConfigurationCouleur extends BaseConfigurationCouleur {
     }
     
     public function setDonneesCsv($datas) {
+      parent::setDonneesCsv($datas);
+
     	$this->getLieu()->setDonneesCsv($datas);
     	$this->libelle = ($datas[ProduitCsvFile::CSV_PRODUIT_COULEUR_LIBELLE])? $datas[ProduitCsvFile::CSV_PRODUIT_COULEUR_LIBELLE] : null;
     	$this->code = ($datas[ProduitCsvFile::CSV_PRODUIT_COULEUR_CODE])? $datas[ProduitCsvFile::CSV_PRODUIT_COULEUR_CODE] : null;
+
+      $this->setDroitDouaneCsv($datas, ProduitCsvFile::CSV_PRODUIT_COULEUR_CODE_APPLICATIF_DROIT);
+      $this->setDroitCvoCsv($datas, ProduitCsvFile::CSV_PRODUIT_COULEUR_CODE_APPLICATIF_DROIT); 
+      
+      $this->setDepartementCsv($datas);
     }
     
   	public function hasDepartements() {
-  		return false;
+  		return true;
   	}
   	public function hasDroits() {
-  		return false;
+  		return true;
   	}
   	public function hasLabels() {
   		return false;
   	}
   	public function hasDetails() {
-  		return false;
+  		return true;
   	}
   	public function getTypeNoeud() {
   		return self::TYPE_NOEUD;
