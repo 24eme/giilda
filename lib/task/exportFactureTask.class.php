f0f7cc9e0e2915c35d4ac2b2cf43d0944b156cb8
Tangui Morlier <tangui@tangui.eu.org>
2013-02-28 19:15:01 +0100

    Ajoute le numéro du document qui a généré chacune des lignes

--- lib/task/exportFactureTask.class.php
+++ lib/task/exportFactureTask.class.php
@@ -29,27 +29,27 @@ EOF;
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-    echo "code journal;date;date de saisie;numero de facture;libelle;compte general;compte tiers;compte analytique;date echeance;sens;montant;piece;reference;\n";
+    echo "code journal;date;date de saisie;numero de facture;libelle;compte general;compte tiers;compte analytique;date echeance;sens;montant;piece;reference;id couchdb\n";
 
     foreach(FactureEtablissementView::getInstance()->getFactureNonVerseeEnCompta() as $vfacture) {
 	$facture = FactureClient::getInstance()->find($vfacture->key[FactureEtablissementView::KEYS_FACTURE_ID]);
 	foreach($facture->lignes as $t => $lignes) {
 		foreach($lignes as $l) {
 			echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' ('.$l->produit_libelle
-.');70610000;;'.$l->produit_identifiant_analytique.';;CREDIT;'.$l->montant_ht.";;\n";
+.');70610000;;'.$l->produit_identifiant_analytique.';;CREDIT;'.$l->montant_ht.';;;'.$facture->_id.";\n";
 
 		}
         }
-	echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (TVA);44570000;;;;CREDIT;'.$facture->taxe.";;\n";
+	echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (TVA);44570000;;;;CREDIT;'.$facture->taxe.';;;'.$facture->_id.";\n";
 	$nbecheance = count($facture->echeances);
 	if ($nbecheance) {
 	  $i = 0;
 	  foreach($facture->echeances as $e) {
 	    $i++;
-	    echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance '.($nbecheance - $i + 1).'/'.$nbecheance.');41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$e->echeance_date.';DEBIT;'.$e->montant_ttc.";;\n";
+	    echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance '.($nbecheance - $i + 1).'/'.$nbecheance.');41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$e->echeance_date.';DEBIT;'.$e->montant_ttc.';;;'.$facture->_id.";\n";
 	  } 
 	}else{
-	    echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance unique);41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$facture->date_facturation.';DEBIT;'.$facture->total_ttc.";;\n";
+	  echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance unique);41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$facture->date_facturation.';DEBIT;'.$facture->total_ttc.';;;'.$facture->_id.";\n";
 	}
     }
   }
