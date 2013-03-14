9e8a689eb1a4b9c5102c22872763b54ce3ef0bde
mathurin <mpetit@actualys.com>
2013-03-14 11:42:22 +0100

    pour l'import des societes on utilise plus les fopurnisseur MDV/PLV => il seront tous labelliser fournisseur (seul les fournisseurs MDV) sont gardés

--- project/plugins/ImportPlugin/lib/csv/SocieteCsvFile.class.php
+++ project/plugins/ImportPlugin/lib/csv/SocieteCsvFile.class.php
@@ -102,7 +102,8 @@ class SocieteCsvFile extends CsvFile
         if($line[self::CSV_PARTENAIRE_CODE_FOURNISSEUR])
                 $s->code_comptable_fournisseur = sprintf('F%07d', $line[self::CSV_PARTENAIRE_CODE_FOURNISSEUR]);
         if($line[self::CSV_PARTENAIRE_TYPE_FOURNISSEUR]){
-                $s->add('type_fournisseur',str_replace('Fournisseur ','',$line[self::CSV_PARTENAIRE_TYPE_FOURNISSEUR]));
+                $s->add('type_fournisseur',str_replace('Fournisseur MDV','Fournisseur',$line[self::CSV_PARTENAIRE_TYPE_FOURNISSEUR]));
+                $s->add('type_fournisseur',str_replace('Fournisseur PLV','Fournisseur',$line[self::CSV_PARTENAIRE_TYPE_FOURNISSEUR]));
             
         }
       	$s->save();
