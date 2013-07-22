35e0d8a7a8d5a2e572355828e3cfd95d19231e1b
Mikael Guillin <mguillin@actualys.com>
2013-07-22 14:57:45 +0200

    Style de la pagination de l'historique des alertes et maj plugin

--- project/plugins/acVinAlertePlugin/modules/alerte/templates/_history_alertes_pagination.php
+++ project/plugins/acVinAlertePlugin/modules/alerte/templates/_history_alertes_pagination.php
@@ -1,9 +1,9 @@
-<div id="consultation_pagination">
+<div class="pagination">
 	<?php if ($page > 1): ?>
-	<a class="pagination_link" href="<?php echo url_for('alerte', array('p' => ($page - 1))) ?>&<?php echo esc_raw($consultationFilter) ?>">&lt;&lt;</a>
+	<a class="btn_majeur page_precedente" href="<?php echo url_for('alerte', array('p' => ($page - 1))) ?>&<?php echo esc_raw($consultationFilter) ?>">Page précedente</a>
 	<?php endif; ?>
-	(<strong><?php echo $page ?></strong>/<?php echo $nbPage ?>)
+	<span>(<strong><?php echo $page ?></strong>/<?php echo $nbPage ?>)</span>
 	<?php if ($page < $nbPage): ?>
-	<a class="pagination_link" href="<?php echo url_for('alerte', array('p' => ($page + 1))) ?>&<?php echo esc_raw($consultationFilter) ?>">&gt;&gt;</a>
+	<a class="btn_majeur page_suivante" href="<?php echo url_for('alerte', array('p' => ($page + 1))) ?>&<?php echo esc_raw($consultationFilter) ?>">Page suivante</a>
 	<?php endif; ?>
 </div>
