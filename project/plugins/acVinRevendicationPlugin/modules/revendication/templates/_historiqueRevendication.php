cb2a44a153b00fda345e2cf652beccaff4370cb2
mathurin <mpetit@actualys.com>
2013-12-11 15:42:42 +0100

    ajout d'un confirm pour la supression des revendications

--- project/plugins/acVinRevendicationPlugin/modules/revendication/templates/_historiqueRevendication.php
+++ project/plugins/acVinRevendicationPlugin/modules/revendication/templates/_historiqueRevendication.php
@@ -14,14 +14,25 @@ if (count($historiqueImport)):
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($historiqueImport as $import) : ?>
+            <?php foreach ($historiqueImport as $import) : 
+                $date = format_date($import->key[RevendicationHistoryView::KEYS_DATE], 'dd/MM/yyyy');
+                $campagne = $import->key[RevendicationHistoryView::KEYS_CAMPAGNE];
+                $odg = $import->key[RevendicationHistoryView::KEYS_ODG];
+                ?>
                 <tr>
+                        <script>
+   $(document).ready(function() {
+   $(".remove_revendication_<?php echo $import->id; ?>").click(function() {
+       return confirm('Voulez vous vraiment supprimer les volumes revendiquées <?php echo $campagne; ?> de la région <?php echo $odg; ?>? O/N"');
+     });
+     });
+</script>
                     <td><?php echo link_to(RevendicationClient::getInstance()->getRevendicationLibelle($import->id), 'revendication_edition', RevendicationClient::getInstance()->getParametersFromId($import->id));
         ?></td>
-                    <td><?php echo format_date($import->key[RevendicationHistoryView::KEYS_DATE], 'dd/MM/yyyy'); ?></td>
-                    <td><?php echo $import->key[RevendicationHistoryView::KEYS_CAMPAGNE]; ?></td>
-                    <td><?php echo $import->key[RevendicationHistoryView::KEYS_ODG]; ?></td>
-                    <td><a href="<?php echo url_for('revendication_delete', array('odg' => $import->key[RevendicationHistoryView::KEYS_ODG], 'campagne' => $import->key[RevendicationHistoryView::KEYS_CAMPAGNE])); ?>">Supprimer</a></td>
+                    <td><?php echo $date; ?></td>
+                    <td><?php echo $campagne; ?></td>
+                    <td><?php echo $odg; ?></td>
+                    <td><a class="remove_revendication_<?php echo $import->id; ?>" href="<?php echo url_for('revendication_delete', array('odg' => $odg, 'campagne' => $campagne)); ?>">Supprimer</a></td>
                 </tr>
     <?php endforeach; ?>
         </tbody>
