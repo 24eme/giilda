86574a65cdbadfb2c86089875b577d9e380b0366
mathurin <mpetit@actualys.com>
2013-12-30 15:47:03 +0100

    mise a jour de la liste des raccourcis

--- project/apps/vinsdeloire/templates/_shortcutKeys.php
+++ project/apps/vinsdeloire/templates/_shortcutKeys.php
@@ -7,12 +7,12 @@
 			<li><kbd>Ctrl</kbd> + <kbd>V</kbd> : Coller</li>
 			<li><kbd>Ctrl</kbd> + <kbd>A</kbd> : Tout sélectionner</li>
                         <li><kbd>Ctrl</kbd> + <kbd>F</kbd> : Rechercher</li>
+                        <li><kbd>Ctrl</kbd> + <kbd>T</kbd> : Afficher un nouvel onglet</li>    
 			<li><kbd>Tab</kbd> : Changer de champs</li>  
-                        <li><kbd>Ctrl</kbd> + <kbd>Poupi</kbd> : Appeler <a href="mailto:apinot@actualys.com">Alexandre</a></li>    
 		</ul>
                 <h2>Dans les Drm : </h2>
                 <ul>                    
-                        <li><kbd>Ctrl</kbd> + <kbd>M</kbd> : Commencer la saisie de la colonne courante</li>
+                        <li><kbd>Ctrl</kbd> + <kbd>P</kbd> : Sélectionner un produit</li>
                         <li><kbd>Echap</kbd> : Réinitialiser les valeurs de la colonne courante</li>
                         <li><kbd>Entrée</kbd> : Valider la colonne courante</li>
 		</ul>
