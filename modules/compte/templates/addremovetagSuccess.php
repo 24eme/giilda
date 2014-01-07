6de2cb6e609302645e9bf1a5939f7b6f3d4a6271
mathurin <mpetit@actualys.com>
2014-01-07 17:07:44 +0100

    ajout du btn pour revenir a la recherche av

--- modules/compte/templates/addremovetagSuccess.php
+++ modules/compte/templates/addremovetagSuccess.php
@@ -8,12 +8,15 @@
 <?php
 	slot('colButtons'); 
 ?>
- <div class="bloc_col" >
+ <div class="bloc_col" id="action" >
 	<h2>Actions</h2>
 
 	<div class="contenu">
 		<ul>
 			<li class=""><a class="btn_majeur btn_acces" href="<?php echo url_for('societe');?>">Accueil des contacts</a></li>
+                </ul>
+                <ul>
+                        <li class=""><a class="btn_majeur btn_acces" href="<?php echo url_for('compte_search');?>">Accueil rech. avancée</a></li>
 		</ul>
 	</div>
 </div>
