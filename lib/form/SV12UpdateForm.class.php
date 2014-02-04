4d7414f7670ae4c68f52aed32e06316ecdf357cc
Vincent LAURENT <vlaurent@actualys.com>
2014-02-04 16:25:20 +0100

    Bug de syntaxe

--- lib/form/SV12UpdateForm.class.php
+++ lib/form/SV12UpdateForm.class.php
@@ -16,7 +16,7 @@ class SV12UpdateForm  extends acCouchdbForm {
     public function configure() {  
     	foreach ($this->getDocument()->getContrats() as $value) {
                 $this->setWidget($value->getKey(), new sfWidgetFormInputFloat(array()));
-                $this->setValidator($value->getKey(), new sfValidatorNumber(array('required' => false, 'min' => 0), array('min' => "La saisie d'un nombre négatif est interdite"))));
+                $this->setValidator($value->getKey(), new sfValidatorNumber(array('required' => false, 'min' => 0), array('min' => "La saisie d'un nombre négatif est interdite")));
     	}  
         
         $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
