e1769b4f2466ac9d2b1e105017907b1b682a2c66
Vincent LAURENT <vlaurent@actualys.com>
2014-02-04 15:58:43 +0100

    volume négatif interdit

--- lib/form/SV12UpdateForm.class.php
+++ lib/form/SV12UpdateForm.class.php
@@ -16,7 +16,7 @@ class SV12UpdateForm  extends acCouchdbForm {
     public function configure() {  
     	foreach ($this->getDocument()->getContrats() as $value) {
                 $this->setWidget($value->getKey(), new sfWidgetFormInputFloat(array()));
-                $this->setValidator($value->getKey(), new sfValidatorNumber(array('required' => false)));
+                $this->setValidator($value->getKey(), new sfValidatorNumber(array('required' => false, 'min' => 0)));
     	}  
         
         $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
