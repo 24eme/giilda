a16555103e36326259417362a27eb425f7598279
Vincent LAURENT <vlaurent@actualys.com>
2014-02-07 17:18:25 +0100

    Fixe libellé Seuil dans la facturation

--- lib/helper/GenerationHelper.php
+++ lib/helper/GenerationHelper.php
@@ -4,11 +4,12 @@ function getLabelForKeyArgument($key)
 {
    switch($key) {
        case 'regions' : return 'Régions :';
+       case 'type_document' : return 'Type de document :';
        case 'operateur_types' : return "Types d'opérateur :";
        case 'date_declaration' : return 'Date de déclaration :';
        case 'date_facturation' : return 'Date de facturation :';
        case 'date_mouvement' : return 'Date de prise en compte des mouvements :';
-       case 'date_seuil' : return 'Seuil :';
+       case 'seuil' : return 'Seuil :';
        default: return "$key :";
    }
 }
