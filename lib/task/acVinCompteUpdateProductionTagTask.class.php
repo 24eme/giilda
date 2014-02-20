d1a1b7d87456a2e55b7a42d89bc41bbb852ee6eb
Vincent LAURENT <vlaurent@actualys.com>
2014-02-20 11:16:04 +0100

    Tache de mise à jour des tags produits et export : * Ajout d'une option pour la campagne * Ajout d'options pour reset les tags produits et export * On prend le libéllé produit dans la conf

--- lib/task/acVinCompteUpdateProductionTagTask.class.php
+++ lib/task/acVinCompteUpdateProductionTagTask.class.php
@@ -25,60 +25,81 @@
  */
 class acVinCompteUpdateProductionTagTask extends sfBaseTask
 {
-  protected function configure()
-  {
-    $this->addOptions(array(
-      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'vinsdeloire'),
-      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
-      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
-    ));
+    protected function configure()
+    {
+        $this->addOptions(array(
+            new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'vinsdeloire'),
+            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+            new sfCommandOption('campagne', null, sfCommandOption::PARAMETER_OPTIONAL, 'Campagne', null),
+            new sfCommandOption('reinitialisation_tags_produit', null, sfCommandOption::PARAMETER_REQUIRED, 'Reset tags', false),
+            new sfCommandOption('reinitialisation_tags_export', null, sfCommandOption::PARAMETER_REQUIRED, 'Reset tags', false),
+        ));
 
-    $this->namespace        = 'compte';
-    $this->name             = 'updateProductionTag';
-    $this->briefDescription = '';
-    $this->detailedDescription = <<<EOF
-
-EOF;
-  }
-
-  protected function execute($arguments = array(), $options = array())
-  {
-    $databaseManager = new sfDatabaseManager($this->configuration);
-    $context = sfContext::createInstance($this->configuration);
-    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
-
-    foreach(EtablissementAllView::getInstance()->findByInterproStatutAndFamilleVIEW('INTERPRO-inter-loire', EtablissementClient::STATUT_ACTIF,null) as $e) {
-      $this->logSection("test", $compte->identifiant);
-      $id = $e->key[EtablissementAllView::KEY_ETABLISSEMENT_ID];
-      $tags = array('export' => array(), 'produit' => array());
-      $campagne = ConfigurationClient::getInstance()->getCurrentCampagne();
-      $mvts = SV12MouvementsConsultationView::getInstance()->getByIdentifiantAndCampagne($id, $campagne);
-      foreach($mvts as $m) {
-	$tags['produit'][$m->produit_libelle] = 1;
-      }
-      $mvts = DRMMouvementsConsultationView::getInstance()->getByIdentifiantAndCampagne($id, $campagne);
-      foreach($mvts as $m) {
-	$tags['produit'][$m->produit_libelle] = 1;
-	if ($m->detail_libelle && $m->type_libelle == 'Export') {
-	  $tags['export'][$m->detail_libelle] = 1;
-	}
-
-      }
-      $etablissement = EtablissementClient::getInstance()->findByIdentifiant(str_replace('ETABLISSEMENT-', '' ,$id));
-      if (!$etablissement) {
-	throw new sfException("etablissement $id non trouvé");
-      }
-      $compte = $etablissement->getContact();
-      if (!count($tags)) {
-	continue;
-      }
-      foreach ($tags as $type => $ttags) {
-	foreach ($ttags as $t => $null) {
-	  $compte->addTag($type, $t);
-	}
-      }
-      $compte->save();
-      $this->logSection("done", $compte->identifiant);
+        $this->namespace        = 'compte';
+        $this->name             = 'updateProductionTag';
+        $this->briefDescription = '';
+        $this->detailedDescription = '';
     }
-  }
+
+    protected function execute($arguments = array(), $options = array())
+    {
+        $databaseManager = new sfDatabaseManager($this->configuration);
+        $context = sfContext::createInstance($this->configuration);
+        $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        if(!isset($options['campagne']) || !$options['campagne']) {
+            $campagne = ConfigurationClient::getInstance()->getCurrentCampagne();
+        }
+
+        $this->logSection("campagne use", $campagne);
+
+        foreach(EtablissementAllView::getInstance()->findByInterproStatutAndFamilleVIEW('INTERPRO-inter-loire', EtablissementClient::STATUT_ACTIF,null) as $e) {
+            $id = $e->key[EtablissementAllView::KEY_ETABLISSEMENT_ID];
+            $tags = array('export' => array(), 'produit' => array());
+            
+            $mvts = SV12MouvementsConsultationView::getInstance()->getByIdentifiantAndCampagne($id, $campagne);
+            foreach($mvts as $m) {
+                $produit_libelle = $this->getProduitLibelle($m->produit_hash);
+      	        $tags['produit'][$produit_libelle] = 1;
+            }
+            $mvts = DRMMouvementsConsultationView::getInstance()->getByIdentifiantAndCampagne($id, $campagne);
+            foreach($mvts as $m) {
+                $produit_libelle = $this->getProduitLibelle($m->produit_hash);
+              	$tags['produit'][$produit_libelle] = 1;
+              	if ($m->detail_libelle && $m->type_libelle == 'Export') {
+              	  $tags['export'][$m->detail_libelle] = 1;
+              	}
+            }
+            $etablissement = EtablissementClient::getInstance()->findByIdentifiant(str_replace('ETABLISSEMENT-', '' ,$id));
+            if (!$etablissement) {
+      	        throw new sfException("etablissement $id non trouvé");
+            }
+            $compte = $etablissement->getContact();
+            if (!count($tags)) {
+      	        continue;
+            }
+            if($options['reinitialisation_tags_export']) {
+                $compte->tags->remove('export');
+                $this->logSection("reset tag export", $compte->identifiant);
+            }
+            if($options['reinitialisation_tags_produit']) {
+                $compte->tags->remove('produit');
+                $this->logSection("reset tag produit", $compte->identifiant);
+            }
+            foreach ($tags as $type => $ttags) {
+              	foreach ($ttags as $t => $null) {
+              	  $compte->addTag($type, $t);
+              	}
+            }
+            $compte->save();
+            $this->logSection("done", $compte->identifiant);
+        }
+    }
+
+    public function getProduitLibelle($hash) {
+        $configuration =  ConfigurationClient::getInstance()->getCurrent();
+
+        return $configuration->get($hash)->getLibelleFormat(array(), "%format_libelle%");
+    }
 }
