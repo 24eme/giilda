5e9637e49e40691bb1ea4a00a22b91ee6e61a725
Vincent LAURENT <vince.laurent@gmail.com>
2014-03-02 22:44:05 +0100

    Suppresion des sorties textes

--- project/bin/maintenance_sv12_mouvements_sans_contrat.sh
+++ project/bin/maintenance_sv12_mouvements_sans_contrat.sh
@@ -1,6 +1,6 @@
 while read ligne  
 do
-    echo "Mise à jour de la SV12 : $ligne"
-    echo "----------------------------------------------------"
+    #echo "Mise à jour de la SV12 : $ligne"
+    #echo "----------------------------------------------------"
     php symfony sv12:regenerate_mouvements $ligne
 done < /tmp/sv12bug.list
