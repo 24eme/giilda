f11a44ae65116b3adfb27124471bb18ede03f30e
Vincent LAURENT <vlaurent@actualys.com>
2014-03-21 13:12:50 +0100

    Présence systématique du code client

--- lib/export/ExportCSV.php
+++ lib/export/ExportCSV.php
@@ -13,7 +13,7 @@ class ExportCSV
     }
 
     public static function printHeader() {
-        echo "code journal;date;date de saisie;numero de facture;libelle;compte general;compte tiers;compte analytique;date echeance;sens;montant;piece;reference;id couchdb;type ligne;nom client;origine type;produit type;origine id; volume; cvo\n";
+        echo "code journal;date;date de saisie;numero de facture;libelle;compte general;compte tiers;compte analytique;date echeance;sens;montant;piece;reference;id couchdb;type ligne;nom client;code comptable client;origine type;produit type;origine id; volume; cvo\n";
     }
 
     public function printFacture($doc_or_id) {
@@ -32,21 +32,21 @@ class ExportCSV
         foreach($facture->lignes as $t => $lignes) {
             foreach($lignes as $l) {
                 echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' ('.$l->produit_libelle
-    .');70610000;;'.$l->produit_identifiant_analytique.';;CREDIT;'.$l->montant_ht.';;;'.$facture->_id.';'.self::TYPE_LIGNE_LIGNE.';'.$facture->declarant->nom.';'.$l->origine_type.';'.$l->produit_type.';'.$l->origine_identifiant.';'.$l->volume.';'.$l->cotisation_taux
+    .');70610000;;'.$l->produit_identifiant_analytique.';;CREDIT;'.$l->montant_ht.';;;'.$facture->_id.';'.self::TYPE_LIGNE_LIGNE.';'.$facture->declarant->nom.";".sprintf("%08d", $facture->code_comptable_client).';'.$l->origine_type.';'.$l->produit_type.';'.$l->origine_identifiant.';'.$l->volume.';'.$l->cotisation_taux
 .";\n";
 
             }
         }
-        echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (TVA);'.$this->getSageCompteGeneral($facture).';;;;CREDIT;'.$facture->taxe.';;;'.$facture->_id.';'.self::TYPE_LIGNE_TVA.';'.$facture->declarant->nom.";\n";
+        echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (TVA);'.$this->getSageCompteGeneral($facture).';;;;CREDIT;'.$facture->taxe.';;;'.$facture->_id.';'.self::TYPE_LIGNE_TVA.';'.$facture->declarant->nom.";".sprintf("%08d", $facture->code_comptable_client).";\n";
         $nbecheance = count($facture->echeances);
         if ($nbecheance) {
             $i = 0;
             foreach($facture->echeances as $e) {
                 $i++;
-                echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance '.($nbecheance - $i + 1).'/'.$nbecheance.');41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$e->echeance_date.';DEBIT;'.$e->montant_ttc.';;;'.$facture->_id.';'.self::TYPE_LIGNE_ECHEANCE.';'.$facture->declarant->nom.";\n";
+                echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance '.($nbecheance - $i + 1).'/'.$nbecheance.');41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$e->echeance_date.';DEBIT;'.$e->montant_ttc.';;;'.$facture->_id.';'.self::TYPE_LIGNE_ECHEANCE.';'.$facture->declarant->nom.";".sprintf("%08d", $facture->code_comptable_client)."\n";
             } 
         } else {
-            echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance unique);41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$facture->date_facturation.';DEBIT;'.$facture->total_ttc.';;;'.$facture->_id.';'.self::TYPE_LIGNE_ECHEANCE.';'.$facture->declarant->nom.";\n";
+            echo 'VEN;'.$facture->date_facturation.';'.$facture->date_emission.';'.$facture->numero_interloire.';Facture n°'.$facture->numero_interloire.' (Echeance unique);41100000;'.sprintf("%08d", $facture->code_comptable_client).';;'.$facture->date_facturation.';DEBIT;'.$facture->total_ttc.';;;'.$facture->_id.';'.self::TYPE_LIGNE_ECHEANCE.';'.$facture->declarant->nom.";".sprintf("%08d", $facture->code_comptable_client).";\n";
         } 
     }
 
