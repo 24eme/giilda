6f6a6576f274050d2ae48a3c4f8bdbba4ab8406d
Vincent LAURENT <vlaurent@actualys.com>
2014-07-08 17:06:52 +0200

    Suppression du myUser en double Suppression de fichier ignoré dans le gitignore

--- project/apps/vinsdeloire/lib/myUser.class.php
+++ project/apps/vinsdeloire/lib/myUser.class.php
@@ -1,11 +1,40 @@
 <?php
 
-/**
- * Description of myUser
- *
- * @author vince
- */
-class myUser extends CompteSecurityUser {
-    
-}
+class myUser extends TeledeclarationSecurityUser
+{
+    const CREDENTIAL_ADMIN = 'admin';
 
+    protected $tiers = null;
+
+    public function getTiers() {        
+            return $this->tiers;
+    }
+
+  public function getCompte() {
+    $user = parent::getCompte();  
+      if($user){
+          return $user;
+      }
+    $user = new stdClass();
+    $user->_id = $this->getAttribute('AUTH_USER');
+    $user->prenom = $user->_id;
+    $user->nom = $user->_id;
+
+    return $user;
+  }
+
+  /**
+   * Récupération de l'interpro
+   * @return Interpro
+   */
+  public function getInterpro()
+  {
+      $interpro = new Interpro();
+      $interpro->identifiant = 'inter-loire';
+      $interpro->nom = "Inter Loire";
+      $interpro->set('_id', 'INTERPRO-'.$interpro->identifiant);
+
+      return $interpro;
+  }
+
+}
