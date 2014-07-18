b280eab487a96787cb9a27950c43e7b60b8690a0
Mikael Guillin <mguillin@actualys.com>
2014-07-18 12:19:54 +0200

    ajout classes sur bloc soussignes de la fiche contrat pour afficher les pictos de statut

--- modules/vrac/templates/_soussigneRecapitulatif.php
+++ modules/vrac/templates/_soussigneRecapitulatif.php
@@ -6,15 +6,15 @@
  */
 ?>
 <div class="bloc_form bloc_form_condensed">
-    <div id="soussigne_recapitulatif_vendeur" class="ligne_form">
+    <div id="soussigne_recapitulatif_vendeur" class="ligne_form contrat_signe_moi">
         <label>Vendeur :</label>
         <span><a href="<?php echo url_for('vrac/recherche?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $vrac->vendeur_identifiant)) ?>"><?php echo $vrac->getVendeurObject()->getNom(); ?></a></span>
     </div>
-    <div id="soussigne_recapitulatif_acheteur" class="ligne_form ligne_form_alt">
+    <div id="soussigne_recapitulatif_acheteur" class="ligne_form ligne_form_alt contrat_attente">
         <label>Acheteur :</label>
         <span><a href="<?php echo url_for('vrac/recherche?identifiant='.preg_replace('/ETABLISSEMENT-/', '', $vrac->acheteur_identifiant)) ?>"><?php echo $vrac->getAcheteurObject()->getNom(); ?></a></span>
     </div>
-    <div id="soussigne_recapitulatif_mandataire" class="ligne_form">
+    <div id="soussigne_recapitulatif_mandataire" class="ligne_form contrat_signe_soussigne">
         <label>Contrat interne :</label>
         <span><?php echo ($vrac->interne) ? 'Oui' : 'Non'; ?></span>
     </div>
