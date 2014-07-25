68b4d719b906adb3950ca33422e2dca4cfe94a55
mathurin <mpetit@actualys.com>
2014-07-25 16:35:12 +0200

    modification de la popup signature

--- project/web/js/teledeclaration_vrac.js
+++ project/web/js/teledeclaration_vrac.js
@@ -2,16 +2,20 @@
 
 var initSignaturePopup = function() {
 
-    $('#signature_popup').fancybox({
+    $('a.signature_popup').fancybox({
         autoSize: true,
         autoCenter: true,
         height: 'auto',
         width: 'auto',
     });
-    $('#signature_popup').click();
 };
+
+var triggerSignaturePopup = function() {
+    $('a.signature_popup').click();
+}
 
 $(document).ready(function()
 {
     initSignaturePopup();    
 });
+
