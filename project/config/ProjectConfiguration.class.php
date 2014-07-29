6e4fdc6257e7250a20efc07265de02389ed5e6d4
mathurin <mpetit@actualys.com>
2014-07-29 13:52:51 +0200

    ajout dans la conf du plugin CAS

--- project/config/ProjectConfiguration.class.php
+++ project/config/ProjectConfiguration.class.php
@@ -33,7 +33,9 @@ class ProjectConfiguration extends sfProjectConfiguration
         $this->enablePlugins('acLdapPlugin');
         $this->enablePlugins('acExceptionNotifierPlugin');
         $this->enablePlugins('acVinAnnuairePlugin');
+        $this->enablePlugins('acCASPlugin');
 
-	    $this->dispatcher->connect('application.throw_exception', array('acError500', 'handleException'));
+
+        $this->dispatcher->connect('application.throw_exception', array('acError500', 'handleException'));
   	}
 }
