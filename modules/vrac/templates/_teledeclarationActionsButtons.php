3cf45cd8e8e95932a964bd6769e863390e2aa3eb
Mikael Guillin <mguillin@actualys.com>
2014-07-30 15:45:32 +0200

    Corrections sur le haut de l'historique des contrats

--- modules/vrac/templates/_teledeclarationActionsButtons.php
+++ modules/vrac/templates/_teledeclarationActionsButtons.php
@@ -1,13 +1,13 @@
-<div id="ligne_btn" class="txt_droite">
-    <a class="btn_majeur" href="<?php echo url_for('vrac_history', array('identifiant' => $etablissementPrincipal->identifiant, 'campagne' => ConfigurationClient::getInstance()->getCurrentCampagne(), 'etablissement' => 'all')); ?>">
-        Voir tout l'historique
-    </a>
-    <?php if($etablissementPrincipal->isCourtier() || $etablissementPrincipal->isNegociant()): ?>
-    <a class="btn_vert btn_majeur" href="<?php echo url_for('annuaire', array('identifiant' => $etablissementPrincipal->identifiant)); ?>">
-        Annuaire
-    </a>        
+<div class="ligne_btn txt_droite">
+    <?php if($etablissementPrincipal->isCourtier() || $etablissementPrincipal->isNegociant()): ?>      
     <a class="btn_orange btn_majeur" href="<?php echo url_for('vrac_nouveau', array('etablissement' => $etablissementPrincipal->identifiant)); ?>">
         Nouveau contrat
     </a>
     <?php endif; ?>
+
+    <a class="btn_majeur" href="<?php echo url_for('vrac_history', array('identifiant' => $etablissementPrincipal->identifiant, 'campagne' => ConfigurationClient::getInstance()->getCurrentCampagne(), 'etablissement' => 'all')); ?>">
+        Voir tout l'historique
+    </a>
+
+    <a href="#" class="btn_majeur btn_excel">Exporter les contrats en CSV</a>
 </div>
--- modules/vrac/templates/societeSuccess.php
+++ modules/vrac/templates/societeSuccess.php
@@ -7,32 +7,28 @@ use_helper('Float');
     <h2 class="titre_societe">
         Espace de <?php echo $societe->raison_sociale; ?>
     </h2>
-    <div>
-        <div id="actions_etablissement_<?php echo $etablissementPrincipal->identifiant; ?>" class="">           
-            <div id="nb_contrat_a_signe">
-                <?php echo $contratsSocietesWithInfos->infos->attente_signature; ?> contrats à signer 
-            </div>
-            <div id="nb_contrat_brouillon">
+    <div class="clearfix">
+        <ul class="stats_contrats">
+            <li>
+                <?php echo $contratsSocietesWithInfos->infos->attente_signature; ?> contrats à signer
+            </li>
+            <li>
                 <?php echo $contratsSocietesWithInfos->infos->brouillon; ?> contrats en brouillon
-            </div>
-            <div id="nb_contrat_valide">
+            </li>
+            <li>
                 <?php echo $contratsSocietesWithInfos->infos->valide; ?> contrats validés
-            </div>
-        </div>
+            </li>
+        </ul>
 
-        <div id="etablissement_<?php echo $etablissementPrincipal->identifiant; ?>" class="">
-            <h3><?php echo $societe->raison_sociale; ?></h3>
-            <ul id="liste_statuts_nb" class="">    
-
-            </ul>
+        <div id="etablissement_<?php echo $etablissementPrincipal->identifiant; ?>" class="infos_etablissement">
             <div id="num_etb">
-                N° <?php echo $societe->identifiant; ?>
+                <span>N° :</span> <?php echo $societe->identifiant; ?>
             </div>
             <div id="cp_etb">
-                Code postal: <?php echo $societe->siege->code_postal; ?>
+                <span>Code postal :</span> <?php echo $societe->siege->code_postal; ?>
             </div>
             <div id="commune_etb">
-                Commune: <?php echo $societe->siege->commune; ?>
+                <span>Commune :</span> <?php echo $societe->siege->commune; ?>
             </div>
         </div>
 
