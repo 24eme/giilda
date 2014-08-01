8dcd9bad5a9ffac5c2c8df88d22848785261e55c
Vincent LAURENT <vince.laurent@gmail.com>
2014-08-01 17:21:09 +0200

    Rollback d'un commit qui était pour la télédéclaration

--- project/plugins/acVinSocietePlugin/lib/model/Societe/Societe.class.php
+++ project/plugins/acVinSocietePlugin/lib/model/Societe/Societe.class.php
@@ -319,16 +319,5 @@ class Societe extends BaseSociete {
         return $this->exist('type_societe') && ($this->type_societe == SocieteClient::SUB_TYPE_SYNDICAT);
     }
     
-    public function getDroitsCompte() {
-        $compte = $this->getMasterCompte();
-        if(!$compte){
-            return null;
-        }
-       $droits = $compte->getDroitsLabelsArray();
-       if(!$droits){
-           return null;
-       }
-      return $droits;
-    }
     
 }
