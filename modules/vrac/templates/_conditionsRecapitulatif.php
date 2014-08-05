cfd95a9e53afa86115d67834547586b4e07a937d
mathurin <mpetit@actualys.com>
2014-08-05 11:57:44 +0200

    maj visu des vracs

--- modules/vrac/templates/_conditionsRecapitulatif.php
+++ modules/vrac/templates/_conditionsRecapitulatif.php
@@ -12,53 +12,66 @@ use_helper('Date');
         <label>Type de contrat&nbsp;:</label>
         <span><?php echo $vrac->type_contrat; ?></span>
     </div>
-    <?php if(!$vrac->isTeledeclare()): ?>
-    <div id="conditions_recapitulatif_isvariable" class="ligne_form ligne_form_alt">
-        <label>Prix variable :</label>
-        <span><?php echo ($vrac->prix_variable) ? 'Oui' : 'Non';
-        echo ($vrac->prix_variable)? ' ('.$vrac->part_variable.'%)' : '';
-        ?>
-        </span>
-    </div>
-    
-    <div id="conditions_cvo_nature" class="ligne_form">
-        <label>Nature de la transaction&nbsp;: </label>
-        <span><?php echo $vrac->cvo_nature ?></span>
-    </div>
+    <?php if (!$vrac->isTeledeclare()): ?>
+    <div id="conditions_enlevement_date" class="ligne_form ">
+            <label>Date d'enlèvement</label>
+            <span><?php
+                echo ($vrac->enlevement_date) ? $vrac->enlevement_date : '';
+                ?>
+            </span>
+        </div>
+
+        <div id="conditions_enlevement_frais_garde" class="ligne_form ligne_form_alt">
+            <label>Frais de garde&nbsp;: </label>
+            <span><?php echo $vrac->enlevement_frais_garde ?></span>
+        </div>
     <?php endif; ?>
     
-    <?php if(!$isTeledeclarationMode): ?>
-    <div id="conditions_recapitulatif_cvo" class="ligne_form ligne_form_alt">
-        <label>Repartition de la CVO&nbsp;: </label>
-        <span><?php echo VracClient::$cvo_repartition[$vrac->cvo_repartition] ?></span>
-    </div>
+    <?php if (!$vrac->isTeledeclare()): ?>
+        <div id="conditions_recapitulatif_isvariable" class="ligne_form ligne_form_alt">
+            <label>Prix variable :</label>
+            <span><?php
+                echo ($vrac->prix_variable) ? 'Oui' : 'Non';
+                echo ($vrac->prix_variable) ? ' (' . $vrac->part_variable . '%)' : '';
+                ?>
+            </span>
+        </div>
+
+        <div id="conditions_cvo_nature" class="ligne_form">
+            <label>Nature de la transaction&nbsp;: </label>
+            <span><?php echo $vrac->cvo_nature ?></span>
+        </div>
     <?php endif; ?>
-    
-    <?php if($vrac->date_signature) : ?>
-    <div id="conditions_recapitulatif_date_signature" class="ligne_form">
-        <label>Date de signature&nbsp;: </label>
-        <span><?php echo $vrac->date_signature; ?></span>
-    </div>
+<?php if (!$isTeledeclarationMode): ?>
+        <div id="conditions_recapitulatif_cvo" class="ligne_form ligne_form_alt">
+            <label>Repartition de la CVO&nbsp;: </label>
+            <span><?php echo VracClient::$cvo_repartition[$vrac->cvo_repartition] ?></span>
+        </div>
+    <?php if ($vrac->date_signature) : ?>
+            <div id="conditions_recapitulatif_date_signature" class="ligne_form">
+                <label>Date de signature&nbsp;: </label>
+                <span><?php echo $vrac->date_signature; ?></span>
+            </div>
+        <?php endif; ?> 
+
+    <?php if ($vrac->date_campagne && !$sf_user->hasTeledeclarationVrac()) : ?>
+            <div id="conditions_recapitulatif_date_saisie" class="ligne_form ligne_form_alt">
+                <label>Date de campagne (statistique)&nbsp;: </label>
+                <span><?php echo $vrac->date_campagne; ?></span>
+            </div>
+        <?php endif; ?> 
+
+    <?php if ($vrac->valide->date_saisie) : ?>
+            <div id="conditions_recapitulatif_date_saisie" class="ligne_form">
+                <label>Date de saisie&nbsp;: </label>
+                <span><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?></span>
+            </div>
+        <?php endif; ?> 
     <?php endif; ?> 
-    
-    <?php if($vrac->date_campagne && !$sf_user->hasTeledeclarationVrac()) : ?>
-    <div id="conditions_recapitulatif_date_saisie" class="ligne_form ligne_form_alt">
-        <label>Date de campagne (statistique)&nbsp;: </label>
-        <span><?php echo $vrac->date_campagne; ?></span>
-    </div>
-    <?php endif; ?> 
-    
-    <?php if($vrac->valide->date_saisie) : ?>
-    <div id="conditions_recapitulatif_date_saisie" class="ligne_form">
-        <label>Date de saisie&nbsp;: </label>
-        <span><?php echo format_date($vrac->valide->date_saisie, 'dd/MM/yyyy'); ?></span>
-    </div>
-    <?php endif; ?> 
-    
-    <?php if(!$isTeledeclarationMode): ?>
-    <div id="conditions_recapitulatif_commentaire" class="ligne_form <?php echo ($vrac->valide->date_saisie)? 'ligne_form_alt' : '' ;?>">
-        <label>Commentaires&nbsp;: </label>
-        <pre class="commentaire"><?php echo $vrac->commentaire;?></pre>
-    </div>
-    <?php endif; ?> 
+<?php if (!$isTeledeclarationMode): ?>
+        <div id="conditions_recapitulatif_commentaire" class="ligne_form <?php echo ($vrac->valide->date_saisie) ? 'ligne_form_alt' : ''; ?>">
+            <label>Commentaires&nbsp;: </label>
+            <pre class="commentaire"><?php echo $vrac->commentaire; ?></pre>
+        </div>
+<?php endif; ?> 
 </div>
