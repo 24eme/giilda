f507158305c45454fed266068d8c0ee2ea73ccb9
mathurin <mpetit@actualys.com>
2014-08-12 18:39:17 +0200

    intégration de la page

--- modules/compte_teledeclarant/templates/firstSuccess.php
+++ modules/compte_teledeclarant/templates/firstSuccess.php
@@ -1,20 +1,20 @@
 <!-- #principal -->
-<form action="" method="post" id="principal" name ="firstConnection">
+<div class="contenu">
+    <form action="" method="post" id="principal" name ="firstConnection">
 
-    <h2 class="titre_principal">Premiere connexion</h2>
+        <h2 class="titre_principal">Premiere connexion</h2>
 
-    <!-- #application_dr -->
-    <div class="clearfix" id="application_dr">
+        <p class="titre_section">Afin d'accèder à la plateforme de télédéclaration, veuillez remplir les champs suivants :</p>
+        <br/>
+        <div id="nouvelle_declaration" class="fond clearfix" >
+            <div class="bloc_form bloc_form_condensed">
 
-        <!-- #nouvelle_declaration -->
-        <div id="nouvelle_declaration">
-            <h3 class="titre_section">Créer votre compte</h3>
-            <div class="contenu_section">
-   				
-                <div class="ligne_form ligne_form_label">
-                    <?php echo $form->renderHiddenFields(); ?>
-                    <?php echo $form->renderGlobalErrors(); ?>
+                <!-- #nouvelle_declaration -->
 
+                <?php echo $form->renderHiddenFields(); ?>
+                <?php echo $form->renderGlobalErrors(); ?>
+
+                <div class="ligne_form ligne_form_label ligne_form_alt ">
                     <?php echo $form['login']->renderError() ?>
                     <?php echo $form['login']->renderLabel() ?>
                     <?php echo $form['login']->render() ?>
@@ -25,19 +25,31 @@
                     <?php echo $form['mdp']->render() ?>
                 </div>
 
-                <div class="ligne_form ligne_btn">
-                    <input type="image" alt="Valider" src="/images/boutons/btn_valider.png" name="boutons[valider]" class="btn">
-                </div>
             </div>
         </div>
-        <!-- fin #nouvelle_declaration -->
+                <div class="margin: 10px 0; clear: both;">
+                    <button class="btn_vert btn_majeur" type="submit">Valider</button> 
+                </div>
 
-        <!-- #precedentes_declarations -->
+    </form>
 
-        <!-- fin #precedentes_declarations -->
+    <?php slot('colReglementation'); ?>
+<div class="bloc_col" id="col_reglementation">
+    <h2>Réglementation Générale</h2>
+
+    <div class="contenu">
+        <p>
+            Vous pouvez télécharger la réglementation générale des transactions au format pdf.
+        </p>
+
+        <a href="<?php echo url_for('vrac_reglementation_generale_des_transactions'); ?>" class="lien_notice">Télécharger la réglementation</a>
     </div>
-    <!-- fin #application_dr -->
+</div>
+<?php
+end_slot();
+?>
+    
+    
+</div>
 
-</form>
-<!-- fin #principal -->
 
