4db4faf077f8dae705ae925e69a344248f7ec8ac
mathurin <mpetit@actualys.com>
2014-08-13 10:17:38 +0200

    intégration de la page de choix etablissement

--- modules/vrac/templates/choixEtablissementSuccess.php
+++ modules/vrac/templates/choixEtablissementSuccess.php
@@ -2,42 +2,34 @@
 ?>
 <section id="principal">
 
-    <h2 class="titre_societe">
-        Espace de <?php echo $societe->raison_sociale; ?>
-    </h2>
-    <div class="clearfix">
-
-        <div id="etablissement_<?php echo $etablissementPrincipal->identifiant; ?>" class="infos_etablissement">
-            <div id="num_etb">
-                <span>N° :</span> <?php echo $societe->identifiant; ?>
-            </div>
-            <div id="cp_etb">
-                <span>Code postal :</span> <?php echo $societe->siege->code_postal; ?>
-            </div>
-            <div id="commune_etb">
-                <span>Commune :</span> <?php echo $societe->siege->commune; ?>
-            </div>
-        </div>
-
-    </div>
+    
 
     <form id="vrac_choix_etablissement" method="post" action="<?php echo url_for('vrac_societe_choix_etablissement', array('identifiant' => $societe->identifiant)) ?>"> 
+        <h2 class="titre_societe">
+            Espace de <?php echo $societe->raison_sociale; ?>
+        </h2>
+        
+        <p class="titre_section">Votre société possède plusieurs établissements. Veuillez renseigner l'établissement en charge de ce contrat.</p>
+        <br/>
+         <div id="condition"class="fond" >
+            <div class="bloc_form bloc_form_condensed">
          <?php echo $form->renderHiddenFields() ?>
          <?php echo $form->renderGlobalErrors() ?>
-         <div id="condition">
-            <!--  Affichage du type de contrat (si standard la suite n'est pas affiché JS)  -->
-            <div id="type_contrat" class="section_label_maj">
+                
+            <div id="type_contrat" class="ligne_form">
                 <?php echo $form['etablissementChoice']->renderError() ?>        
                 <?php echo $form['etablissementChoice']->renderLabel() ?>
                 <?php echo $form['etablissementChoice']->render() ?>
             </div>            
          </div>
-        <div class="btn_etape">
-                <a href="<?php echo url_for('vrac_societe', array('identifiant' => $etablissementPrincipal->identifiant)); ?>" class="btn_etape_prec"><span>retourner à l'accueil</span></a>
-                <button class="btn_etape_suiv" type="submit"><span>Nouveau Contrat</span></button>
+             
+             <div class="ligne_btn">
+                     <a class="btn_orange btn_majeur" style="float: left;" href="<?php echo url_for('vrac_societe', array('identifiant' => $etablissementPrincipal->identifiant)); ?>">Retourner à l'espace contrats</a>
+                    <button class="btn_etape_suiv" style="cursor: pointer; float: right;" type="submit"><span>Nouveau Contrat</span></button>
+         
         </div>
-    </form>
-   
+         </div>
+    </form>   
 
 </section>
 
