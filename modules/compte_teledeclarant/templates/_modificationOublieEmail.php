ab089481458b41e9040290efed8d14e29b66fe57
mathurin <mpetit@actualys.com>
2014-08-13 11:11:03 +0200

    maj inté de la perte de mot de passe

--- modules/compte_teledeclarant/templates/_modificationOublieEmail.php
+++ modules/compte_teledeclarant/templates/_modificationOublieEmail.php
@@ -1,1 +1,1 @@
-Bonjour <?php echo $compte->nom ?>,\n\n votre mot de passe sur le site du CIVA vient d'etre modifié. \n\n Cordialement, \n\n Le CIVA
+Bonjour <?php echo $compte->nom ?>,\n\n votre mot de passe sur le site d'InterLoire vient d'etre modifié. \n\n Cordialement, \n\n Interloire
--- modules/compte_teledeclarant/templates/motDePasseOublieSuccess.php
+++ modules/compte_teledeclarant/templates/motDePasseOublieSuccess.php
@@ -1,36 +1,27 @@
 <!-- #principal -->
-<h2 class="titre_principal">Mon compte</h2>
+<div id="principal" class="clearfix">
+    <form action="" class="ui-tabs"  method="post" >
+        <h2 class="titre_principal">Mon compte - Mot de passe oublié</h2>
 
-<!-- #application_dr -->
-<div class="clearfix" id="application_dr">
-
-    <!-- #nouvelle_declaration -->
-    <div id="nouvelle_declaration">
-        <h3 class="titre_section">Mot de passe oublié</h3>
-        <div class="contenu_section">
-            <form action="" method="post" id="principal">
-                <p class="intro">Merci d'indiquer votre login :</p>
+        <p class="titre_section">Afin de récuperer votre mot de passe veuillez renseigner votre identifiant.</p>
+        <br/>
+        <div id="nouvelle_declaration" class="fond" >
+            <div class="bloc_form bloc_form_condensed">
+                <?php echo $form->renderHiddenFields(); ?>
+                <?php echo $form->renderGlobalErrors(); ?>
 
                 <div class="ligne_form">
-                    <?php echo $form->renderHiddenFields(); ?>
-                    <?php echo $form->renderGlobalErrors(); ?>
-
                     <?php echo $form['login']->renderError() ?>
                     <?php echo $form['login']->renderLabel() ?>
                     <?php echo $form['login']->render() ?>
                 </div>
-                <div class="ligne_form ligne_btn">
-                    <input type="image" alt="Valider" src="/images/boutons/btn_valider.png" name="boutons[valider]" class="btn">
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-    <!-- fin #nouvelle_declaration -->
-
-    <!-- #precedentes_declarations -->
-
-    <!-- fin #precedentes_declarations -->
+        <div style="margin: 10px 0; clear: both; float: right;">
+            <button class="btn_vert btn_majeur " type="submit">Valider</button> 
+        </div>
+    </form>
 </div>
-<!-- fin #application_dr -->
-
-<!-- fin #principal -->
+<?php slot('colReglementation'); ?>
+    <?php include_partial('vrac/colReglementation'); ?>
+<?php end_slot(); ?>
