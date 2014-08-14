96dd2f08b5d41d391a1a7608c33176184115351c
mathurin <mpetit@actualys.com>
2014-08-14 18:06:34 +0200

    redirect du forbidden vers la racine

--- project/apps/vinsdeloire/modules/auth/templates/forbiddenSuccess.php
+++ project/apps/vinsdeloire/modules/auth/templates/forbiddenSuccess.php
@@ -1,15 +1,13 @@
 <section id="principal">
 <h2>Vous n'avez pas l'autorisation pour accéder à cette page</h2>
-<?php if($sf_user->hasTeledeclarationVrac()):
-     $identifiantCompte = $sf_user->getCompte()->identifiant; 
-    ?>
+<?php if($sf_user->hasTeledeclarationVrac()): ?>
 <div class="text-center">
     <p>Vous allez être automatiquement rediriger vers l'accueil.</p>
 </div>
     
 <script>
 window.setInterval(function(){
-    window.location.assign("https://teledeclaration.vinsvaldeloire.pro/contrats/societe/"+<?php echo $identifiantCompte; ?>);
+    window.location.assign("https://teledeclaration.vinsvaldeloire.pro");
 }, 5000);
 
 </script>
