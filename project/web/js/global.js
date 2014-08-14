f1b6165b38a53910db35f03e6389bc23e6ada228
mathurin <mpetit@actualys.com>
2014-08-14 18:10:01 +0200

    Chrome ne supporte pas les .contains sur les string / ajout de la fct au global

--- project/web/js/global.js
+++ project/web/js/global.js
@@ -146,7 +146,13 @@ var fbConfig =
 		$.inputPlaceholder();
 		
 		$.initTableSelection();
-		
+	
+                if(!('contains' in String.prototype)) {
+                   String.prototype.contains = function(str, startIndex) {
+                    return -1 !== String.prototype.indexOf.call(this, str, startIndex);
+               };
+             }
+            
 	});
 	
 })(jQuery);
