c1bcff0646b50ee4e80777535ef13ba99292433c
Vincent LAURENT <vince.laurent@gmail.com>
2014-08-21 14:08:54 +0200

    Suppression de variable inutile dans le script de replication

--- project/bin/replicate_teledeclaration_start.sh
+++ project/bin/replicate_teledeclaration_start.sh
@@ -2,9 +2,6 @@
 
 . bin/config.inc
 
-DOC_FILTER_ID=replication_teledeclaration
-DOC_FILTER_REV=$(curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_design/replication_teledeclaration" | grep -Eo "_rev\":\"[0-9a-Z-]+\"" | sed 's/_rev":"//' | sed 's/"//')
-
 echo "{\"source\":\"$COUCHBASE\",\"target\":\"$REPLICATE_TELEDECLARATION_URL\",\"continuous\": true}" > /tmp/params_replication_teleclaration.json
 
 curl -H "Content-Type: application/json" -X POST -d '@/tmp/params_replication_teleclaration.json' "http://$COUCHHOST:$COUCHPORT/_replicate"
--- project/bin/replicate_teledeclaration_stop.sh
+++ project/bin/replicate_teledeclaration_stop.sh
@@ -2,8 +2,6 @@
 
 . bin/config.inc
 
-DOC_FILTER_ID=replication_teledeclaration
-
 echo "{\"source\":\"$COUCHBASE\",\"target\":\"$REPLICATE_TELEDECLARATION_URL\",\"continuous\": true, \"cancel\": true}" > /tmp/params_replication_teleclaration.json
 
 curl -H "Content-Type: application/json" -X POST -d '@/tmp/params_replication_teleclaration.json' "http://$COUCHHOST:$COUCHPORT/_replicate"
