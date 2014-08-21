2697c17bc4f95a4ea5baad28a5db2b6baf5ae41e
mathurin <mpetit@actualys.com>
2014-08-21 14:12:23 +0200

    maj mails avec l'interloc commercial

--- lib/model/Vrac/VracEmailManager.class.php
+++ lib/model/Vrac/VracEmailManager.class.php
@@ -36,7 +36,8 @@ class VracEmailManager {
         $responsableCourtier = ($createurObject->isCourtier() 
                 && $this->vrac->exist('interlocuteur_commercial')
                 && $this->vrac->interlocuteur_commercial
-                && $this->vrac->interlocuteur_commercial->exist('nom'))?
+                && $this->vrac->interlocuteur_commercial->exist('nom')
+                && $this->vrac->interlocuteur_commercial->nom)?
                 ', dont l\'interlocuteur commercial est '.$this->vrac->interlocuteur_commercial->nom : '.' ;
         
         $mess = 
@@ -52,7 +53,7 @@ Un contrat vient d'être initié par ".$responsableNom.", en voici un résumé :
 Ce contrat attend votre signature. Pour le visualiser et le signer cliquez sur le lien suivant : https://teledeclaration.vinsvaldeloire.pro/vrac/".$this->vrac->numero_contrat."/visualisation
 
  
-Pour être valable, le contrat doit être signé par toutes les parties. Le PDF correspondant avec le numéro d\'enregistrement INTERLOIRE vous sera alors envoyé par courriel.
+Pour être valable, le contrat doit être signé par toutes les parties. Le PDF correspondant avec le numéro d'enregistrement INTERLOIRE vous sera alors envoyé par courriel.
 
  
 Attention si le contrat n’est pas signé par toutes les parties dans les 5 jours à compter de sa date de création, il sera automatiquement supprimé.
@@ -95,7 +96,8 @@ Rappel de votre identifiant : IDENTIFIANT";
         $responsableCourtier = ($createur->isCourtier() 
                 && $this->vrac->exist('interlocuteur_commercial')
                 && $this->vrac->interlocuteur_commercial
-                && $this->vrac->interlocuteur_commercial->exist('nom'))?
+                && $this->vrac->interlocuteur_commercial->exist('nom')
+                && $this->vrac->interlocuteur_commercial->nom)?
                 ', dont l\'interlocuteur commercial est '.$this->vrac->interlocuteur_commercial->nom : '.' ;
         
         $resultEmailArr = array();
