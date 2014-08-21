3b7fb4faeca20634fd4faa0b14b1e373202de8de
Vincent LAURENT <vince.laurent@gmail.com>
2014-08-21 15:11:19 +0200

    \n dans un mail

--- modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
+++ modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
@@ -1,7 +1,9 @@
 Bonjour <?php echo $compte->nom ?>,
 
-vous avez oublié votre mot de passe.
+Vous avez oublié votre mot de passe.
 
-Pour le redéfinir merci de cliquer sur le lien suivant : <?php echo $lien ?>
+Pour le redéfinir merci de cliquer sur le lien suivant : <?php echo $lien ?> 
 
-Cordialement, \n\n Interloire
+Cordialement, 
+
+Interloire
