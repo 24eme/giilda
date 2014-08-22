88653757b7e098106198189a41ecde54f01d7c5a
mathurin <mpetit@actualys.com>
2014-08-22 16:00:55 +0200

    maj erreur 500 pour la teledeclaration

--- project/apps/vinsdeloire/modules/global/actions/actions.class.php
+++ project/apps/vinsdeloire/modules/global/actions/actions.class.php
@@ -1,22 +1,26 @@
 <?php
 
 class globalActions extends sfActions {
-	
-    public function executeError500(sfWebRequest $request) {
-       $this->exception = $request->getParameter('exception');
-	}
 
-	public function executeHome(sfWebRequest $request) {
-        if($this->getUser()->hasCredential('transactions')) {
-	        
+    public function executeError500(sfWebRequest $request) {
+        $this->exception = $request->getParameter('exception');
+        if ($this->getUser()->hasTeledeclarationVrac()) {
+            $this->setTemplate('error500Teledeclaration','global');
+        }
+    }
+
+    public function executeHome(sfWebRequest $request) {
+        if ($this->getUser()->hasCredential('transactions')) {
+
             return $this->redirect('vrac');
         }
 
-        if(!$this->getUser()->hasCredential('operateur')) {
+        if (!$this->getUser()->hasCredential('operateur')) {
 
             return $this->redirect('vrac_societe', array("identifiant" => $this->getUser()->getCompte()->identifiant));
         }
 
         return $this->redirect('societe');
-	}
+    }
+
 }
