de7ea18d82f23e5f6f663f1179a9fd1a1f831910
Vincent LAURENT <vince.laurent@gmail.com>
2014-08-22 13:21:14 +0200

    Utilisation d'un param dans le app pour les urls aboslute dans les mails depuis une tache

--- lib/task/TeledeclarationEnvoiEmailsContratsVisesTask.class.php
+++ lib/task/TeledeclarationEnvoiEmailsContratsVisesTask.class.php
@@ -28,7 +28,9 @@ EOF;
     // initialize the database connection
     $databaseManager = new sfDatabaseManager($this->configuration);
     $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    $routing = clone ProjectConfiguration::getAppRouting();
     $context = sfContext::createInstance($this->configuration);
+    $context->set('routing', $routing);
     
     $contrats = $this->getContratsValides();
     $this->sendEmails($contrats);
