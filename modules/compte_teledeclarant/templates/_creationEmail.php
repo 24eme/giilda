8ef7d7f380bf7cd4eeab30a02db2fc08055fc76f
mathurin <mpetit@actualys.com>
2014-08-27 15:07:53 +0200

    maj corps d'email => suppression du Cordialement 13578

--- modules/compte_teledeclarant/templates/_creationEmail.php
+++ modules/compte_teledeclarant/templates/_creationEmail.php
@@ -2,6 +2,4 @@ Bonjour <?php echo $compte->nom ?>,
 
 votre compte a bien été créé sur le portail de télédéclaration des contrats d’InterLoire. 
 
-Cordialement,
-
 L’application de télédéclaration des contrats d’InterLoire
--- modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
+++ modules/compte_teledeclarant/templates/_motDePasseOublieEmail.php
@@ -4,6 +4,4 @@ Vous avez oublié votre mot de passe.
 
 Pour le redéfinir merci de cliquer sur le lien suivant : <?php echo $lien ?> 
 
-Cordialement,
-
 L’application de télédéclaration des contrats d’InterLoire
