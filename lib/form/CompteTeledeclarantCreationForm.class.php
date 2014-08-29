62060c590e92fd99c3f8487060e83a37936bfab1
mathurin <mpetit@actualys.com>
2014-08-29 15:09:21 +0200

    le numéro d'accise est obligatoire il possède 13 chars Alphanumériques

--- lib/form/CompteTeledeclarantCreationForm.class.php
+++ lib/form/CompteTeledeclarantCreationForm.class.php
@@ -32,7 +32,13 @@ class CompteTeledeclarantCreationForm extends CompteTeledeclarantForm {
             
             $this->setWidget('num_accises', new sfWidgetFormInputText());
             $this->getWidget('num_accises')->setLabel("Numéro d'ACCISE* :");
-            $this->setValidator('num_accises', new sfValidatorString(array('required' => false)));
+            $this->setValidator('num_accises', new sfValidatorRegex(array('required' => true,
+                'pattern' => "/^[0-9A-Za-z]{13}$/",
+                'min_length' => 13,
+                'max_length' => 13), array('required' => "Le numéro d'ACCISE est obligatoire",
+                'invalid' => "Le numéro d'ACCISE doit être constitué de 13 caractères alphanumériques",
+                'min_length' => "Le numéro d'ACCISE doit être constitué de 13 caractères alphanumériques",
+                'max_length' => "Le numéro d'ACCISE doit être constitué de 13 caractères alphanumériques")));
         }
     }
 
@@ -45,7 +51,7 @@ class CompteTeledeclarantCreationForm extends CompteTeledeclarantForm {
         }
         if ((($this->typeCompte == SocieteClient::SUB_TYPE_NEGOCIANT) || ($this->typeCompte == SocieteClient::SUB_TYPE_VITICULTEUR)) 
                 && ($this->getValue('num_accises'))) {
-            $etbPrincipal->no_accises = $this->getValue('num_accises');
+            $etbPrincipal->no_accises = strtoupper($this->getValue('num_accises'));
             $etbPrincipal->save();
         }
     }
