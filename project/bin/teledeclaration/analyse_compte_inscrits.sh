4c1e4546e9bcd5195adf1c0422731df2318bc0a7
mathurin <mpetit@actualys.com>
2014-09-19 11:29:08 +0200

    petite correction dans le script d'analyse compte inscrit changement des  guides users

--- project/bin/teledeclaration/analyse_compte_inscrits.sh
+++ project/bin/teledeclaration/analyse_compte_inscrits.sh
@@ -2,7 +2,7 @@
 
 echo "Recupération des societes"
 
-#curl -s $1"/_design/societe/_view/all" > ./all_societe
+curl -s $1"/_design/societe/_view/all" > ./all_societe
 
 echo "Fin Recupération des societes"
 
@@ -15,9 +15,9 @@ SEDCOMPTE='sed -r '"'"'s|([0-9]{6})|curl -s "'$1'/COMPTE-\101"|g'"'"
 
 echo 'cat all_societe_id | '$SEDCOMPTE > ./extract_compte_principal.sh
 
-#bash ./extract_compte_principal.sh > ./exec_compte_principal.sh
+bash ./extract_compte_principal.sh > ./exec_compte_principal.sh
 
-#bash ./exec_compte_principal.sh > ./exctracted_compte_principal
+bash ./exec_compte_principal.sh > ./exctracted_compte_principal
 
 cat ./exctracted_compte_principal | grep 'teledeclaration_active":true' | sed 's/{"_id"://g' | cut -d "," -f 1 | sed 's/"COMPTE-//g' | sed 's/"//g' > compte_teledeclaration_active
 
