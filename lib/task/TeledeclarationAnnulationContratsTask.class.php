eb4b64fb98f540523770c4d1c311c5efefedce2b
mathurin <mpetit@actualys.com>
2014-09-24 18:10:03 +0200

    Bridage de la tache d'annulation/relance des contrats

--- lib/task/TeledeclarationAnnulationContratsTask.class.php
+++ lib/task/TeledeclarationAnnulationContratsTask.class.php
@@ -36,15 +36,15 @@ EOF;
         echo "\nDebut de la tache d'annulation du " . $this->date . " \n";
         $contrats_annulations_brouillons = $this->getContratsAnnulationBrouillons();
         $this->annulationContrats($contrats_annulations_brouillons);
-        
-        
+
+
         $contrats_annulations_attente_signature = $this->getContratsAnnulationAttenteSignature();
         $this->annulationContrats($contrats_annulations_attente_signature);
-        $this->mailsAnnulationAttenteSignatureContrats($contrats_annulations_attente_signature); 
-        
-        
+        $this->mailsAnnulationAttenteSignatureContrats($contrats_annulations_attente_signature);
+
+
         $contrats_rappel_attente_signature = $this->getContratsEnRappelAttenteSignature();
-        $this->mailsRappelAttenteSignatureContrats($contrats_rappel_attente_signature); 
+        $this->mailsRappelAttenteSignatureContrats($contrats_rappel_attente_signature);
     }
 
     protected function getContratsAnnulationBrouillons() {
@@ -56,6 +56,13 @@ EOF;
 
             $contrat = VracClient::getInstance()->find($contratBrouillonsView->id);
             if ($contrat->isTeledeclare() && $contrat->date_campagne) {
+                
+                //A RETIRER
+                if (($contrat->createur_identifiant != '80056301') || ($contrat->createur_identifiant != '80056401')) {
+                    continue;
+                }
+                
+                
                 $date_campagne_max_iso = Date::addDelaiToDate('+ 10 days', Date::getIsoDateFromFrenchDate($contrat->date_campagne));
                 $isMore = Date::sup($this->date, $date_campagne_max_iso);
                 if ($isMore) {
@@ -74,6 +81,13 @@ EOF;
 
             $contrat = VracClient::getInstance()->find($contratAttenteView->id);
             if ($contrat->isTeledeclare() && $contrat->valide->date_saisie) {
+                
+                //A RETIRER
+                if (($contrat->createur_identifiant != '80056301') || ($contrat->createur_identifiant != '80056401')) {
+                    continue;
+                }
+                
+                
                 $date_campagne_max_iso = Date::addDelaiToDate('+5 days', Date::getIsoDateFromFrenchDate($contrat->valide->date_saisie));
                 $isMore = Date::sup($this->date, $date_campagne_max_iso);
                 if ($isMore) {
@@ -83,7 +97,7 @@ EOF;
         }
         return $contrats;
     }
-    
+
     protected function getContratsEnRappelAttenteSignature() {
         $contrats = array();
         $contrats_attentes_signature = VracStatutAndTypeView::getInstance()->findContatsByStatut(VracClient::STATUS_CONTRAT_ATTENTE_SIGNATURE);
@@ -92,6 +106,14 @@ EOF;
 
             $contrat = VracClient::getInstance()->find($contratAttenteView->id);
             if ($contrat->isTeledeclare() && $contrat->valide->date_saisie) {
+                
+                
+                //A RETIRER
+                if (($contrat->createur_identifiant != '80056301') || ($contrat->createur_identifiant != '80056401')) {
+                    continue;
+                }
+                
+                
                 $date_contrat_rappel = Date::addDelaiToDate('+3 days', Date::getIsoDateFromFrenchDate($contrat->valide->date_saisie));
                 if ($date_contrat_rappel == date("Y-m-d")) {
                     $contrats[$contrat->_id] = $contrat;
@@ -102,34 +124,32 @@ EOF;
     }
 
     protected function mailsAnnulationAttenteSignatureContrats($contrats_annulations_attente_signature) {
-         $vracEmailManager = new VracEmailManager($this->getMailer());
-         foreach ($contrats_annulations_attente_signature as $contrat_attente_signature) {
-              $vracEmailManager->setVrac($contrat_attente_signature);
-              $vracEmailManager->sendMailAnnulation(true);
+        $vracEmailManager = new VracEmailManager($this->getMailer());
+        foreach ($contrats_annulations_attente_signature as $contrat_attente_signature) {
+            $vracEmailManager->setVrac($contrat_attente_signature);
+            $vracEmailManager->sendMailAnnulation(true);
             echo "Envoi des mails du contrat " . $contrat_attente_signature->numero_contrat . " visa " . $contrat_attente_signature->numero_archive . " \n";
         }
     }
-    
-    
+
     protected function annulationContrats($contrats) {
         if (!count($contrats)) {
             echo "Aucun contrat à annuler\n";
         }
         foreach ($contrats as $contrat) {
-            //   $vrac->valide->statut = VracClient::STATUS_CONTRAT_ANNULE;
-            //     $vrac->save();
+            $contrat->valide->statut = VracClient::STATUS_CONTRAT_ANNULE;
+            $contrat->save();
             echo "Annulation du contrat " . $contrat->numero_contrat . " visa " . $contrat->numero_archive . " \n";
         }
     }
 
     protected function mailsRappelAttenteSignatureContrats($contrats) {
         $vracEmailManager = new VracEmailManager($this->getMailer());
-         foreach ($contrats as $contrat_attente_signature_rappel) {
-              $vracEmailManager->setVrac($contrat_attente_signature_rappel);
-              $vracEmailManager->sendMailRappel();
+        foreach ($contrats as $contrat_attente_signature_rappel) {
+            $vracEmailManager->setVrac($contrat_attente_signature_rappel);
+            $vracEmailManager->sendMailRappel();
             echo "Envoi des mails du contrat " . $contrat_attente_signature_rappel->numero_contrat . " visa " . $contrat_attente_signature_rappel->numero_archive . " \n";
         }
-        
     }
-    
+
 }
