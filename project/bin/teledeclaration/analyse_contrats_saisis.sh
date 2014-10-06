6fc27d164f677aec60d9aac8afb74477db6ff91f
mathurin <mpetit@actualys.com>
2014-10-06 10:59:29 +0200

    petit changement dans le script d'analyse des contrats

--- project/bin/teledeclaration/analyse_contrats_saisis.sh
+++ project/bin/teledeclaration/analyse_contrats_saisis.sh
@@ -9,10 +9,11 @@ echo "Fin Recupération des contrats"
 
 cat ./all_contrats | sed 's/{"id"://g' | sed 's/"key":\[//g' | sed 's/\],"value":\[/,/g' | sed 's/\]},//g' > ./all_contrats_csv_raw
 
-cat ./all_contrats_csv_raw | cut -d "," -f 1,7,12,14,16,23,24,25 > ./all_contrats_signature.csv
+cat ./all_contrats_csv_raw | cut -d "," -f 1,4,7,12,14,16,23,24,25 | grep -vE "(800563|800564|800565)" > ./all_contrats_signature.csv
 
-cat ./all_contrats_signature.csv | grep -E "ATTENTE_SIGNATURE" > ./contrats_attente_signature.csv
-cat ./all_contrats_signature.csv | grep -E "BROUILLON" > ./contrats_brouillon.csv
+cat ./all_contrats_signature.csv | sort -t ',' -k 1,1 -u > ./all_contrats_signature.uniq.csv
 
-cat ./all_contrats_signature.csv | grep -E "(SOLDE|NONSOLDE)" | grep -E "([0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2}+)" > ./contrats_signes.csv
+cat ./all_contrats_signature.uniq.csv | grep -E "ATTENTE_SIGNATURE" > ./contrats_attente_signature.csv
+cat ./all_contrats_signature.uniq.csv | grep -E "BROUILLON" > ./contrats_brouillon.csv
+cat ./all_contrats_signature.uniq.csv | grep -E "(SOLDE|NONSOLDE)" | grep -E "([0-9]{4}-[0-9]{2}-[0-9]{2}\ [0-9]{2}:[0-9]{2}:[0-9]{2}+)" > ./contrats_signes.csv
 
