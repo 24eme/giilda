bc2aaa0b846d21026d1d620767404c8052dee02b
mathurin <mpetit@actualys.com>
2015-01-23 16:23:50 +0100

    script du lancement de mise a jour des alertes manuelles

--- project/bin/generation_alertes.sh
+++ project/bin/generation_alertes.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-. ../bin/config.inc
 
 DATE=$(date +%Y%m%d%H%M%S)
 
-mkdir -p $TMP"/generation_alertes" > /dev/null
+mkdir -p /tmp/generation_alertes > /dev/null
 
-TEMPFILE=$TMP"/generation_alertes/counterTask"
+TEMPFILE=/tmp/generation_alertes/counterTask
 
 echo 0 > $TEMPFILE
 
 ps ux | awk '/generate/ && !/awk/ {print $2}' | while read line
 do
        echo $DATE;
-       echo "GENERATION EN COURS D'ACTIVITE => "$DATE >> $TMP"/generation_alertes/activite_generation_alertes.log" 2>&1
+       echo "GENERATION EN COURS D'ACTIVITE => "$DATE >> "/tmp/generation_alertes/activite_generation_alertes.log" 2>&1
        echo 1 > $TEMPFILE
        break;
 done; 
@@ -26,4 +25,4 @@ fi
 
 unlink $TEMPFILE
 cd ..
-bash bin/alerte_creation_update 0 > $TMP"/generation_alertes/generation_alertes_"$DATE".log" &
+bash bin/alerte_creation_update 0 > "/tmp/generation_alertes/generation_alertes_"$DATE".log" &
