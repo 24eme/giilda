ff0a7919bb994a90193751fdd84b9589166eb5f1
mathurin <mpetit@actualys.com>
2015-04-01 17:09:19 +0200

    template monEspace

---
+++ modules/compte_teledeclarant/templates/monEspaceSuccess.php
@@ -0,0 +1,36 @@
+<!-- #principal -->
+<div id="principal" class="clearfix">
+
+    <h2 class="titre_principal">télédéclaration</h2>
+
+    <div id="mon_espace" >
+        <div class="block_teledeclaration">
+            <div class="title">ESPACE CONTRAT</div>
+            <div class="panel">
+                <ul>
+                    <li>Vous avez <a href="<?php echo url_for('vrac_societe', array('identifiant' => $identifiant)); ?>">3 contrats</a> en attente de signature</li>
+                    <li>Vous avez <a href="<?php echo url_for('vrac_societe', array('identifiant' => $identifiant)); ?>">3 contrats</a> en brouillon</li>
+                </ul>
+                <div class="acces">
+                    <a href="<?php echo url_for('vrac_societe', array('identifiant' => $identifiant)); ?>" class="btn_majeur">Acceder aux contrat</a>
+                </div>
+            </div>
+        </div>
+        <div class="block_teledeclaration">
+            <div class="title">ESPACE DRM / DRA</div>
+            <div class="panel">
+                <ul>
+                    <li class="text-error">Votre DRM : <a href="<?php echo url_for('drm_societe', array('identifiant' => $identifiant)); ?>">Mars 2015</a> est incomplète et non-validée</li>
+                </ul>
+                <div class="acces">
+                    <a class="btn_majeur" href="<?php echo url_for('drm_societe', array('identifiant' => $identifiant)); ?>">DRM</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
