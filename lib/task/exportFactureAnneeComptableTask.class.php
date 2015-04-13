e11fc5475ee96b04d85b63924ef87303fadf0bff
mathurin <mpetit@actualys.com>
2015-04-13 18:17:09 +0200

    correction de la tache d'export de la facturation annuelle (on donne maintenant un intervalle de mois

--- lib/task/exportFactureAnneeComptableTask.class.php
+++ lib/task/exportFactureAnneeComptableTask.class.php
@@ -5,7 +5,8 @@ class exportFactureAnneeComptableTask extends sfBaseTask {
     protected function configure() {
 
         $this->addArguments(array(
-            new sfCommandArgument('annee_comptable', null, sfCommandArgument::REQUIRED, date('Y')),
+            new sfCommandArgument('annee_mois_debut', null, sfCommandArgument::REQUIRED, date('Y-m')),
+            new sfCommandArgument('annee_mois_fin', null, sfCommandArgument::REQUIRED, date('Y-m')),
         ));
 
         $this->addOptions(array(
@@ -29,19 +30,33 @@ EOF;
         // initialize the database connection
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
-        $annee_comptable = $arguments['annee_comptable'];
-        $export = new ExportCSV();
-        $export->printHeaderAnneeComptable();
+        $annee_mois_debut = $arguments['annee_mois_debut'];
+        $annee_mois_fin = $arguments['annee_mois_fin'];
 
         ini_set('memory_limit', '2048M');
         set_time_limit(0);
 
+        $annee_mois_debutArr = explode('-', $annee_mois_debut);
+        $annee_mois_finArr = explode('-', $annee_mois_fin);
+
+        if ((count($annee_mois_debutArr) != 2) || (count($annee_mois_finArr) != 2)) {
+            throw new sfException('les paramètres de annee_mois_debut et annee_mois_fin doivent être au format Annee-mois');
+        }
+        $annee_debut = $annee_mois_debutArr[0];
+        $mois_debut = $annee_mois_debutArr[1];
+
+        $annee_fin = $annee_mois_finArr[0];
+        $mois_fin = $annee_mois_finArr[1];
+        
+        $export = new ExportCSV();
+        $export->printHeaderAnneeComptable();
         foreach (FactureEtablissementView::getInstance()->getAllFacturesForCompta() as $vfacture) {
-            if(substr($vfacture->value[FactureEtablissementView::VALUE_DATE_FACTURATION],0,4) == $annee_comptable){
-               
-            $export->printFacture($vfacture->key[FactureEtablissementView::KEYS_FACTURE_ID],true);           
+            $factureAnnee = substr($vfacture->value[FactureEtablissementView::VALUE_DATE_FACTURATION], 0, 4);
+            $factureMois = substr($vfacture->value[FactureEtablissementView::VALUE_DATE_FACTURATION], 5, 2);
+            if ((($annee_debut <= $factureAnnee) || ($factureAnnee <= $annee_fin)) &&
+                            (($mois_debut <= $factureMois) || ($factureMois <= $mois_fin))){
+                $export->printFacture($vfacture->key[FactureEtablissementView::KEYS_FACTURE_ID], true);
             }
-            
         }
     }
 
