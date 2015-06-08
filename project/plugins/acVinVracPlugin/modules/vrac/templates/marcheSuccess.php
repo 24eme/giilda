73736fb73bbe99dfd20cf54c01474fd97594563b
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-08 15:17:04 +0200

    Suppression de l'ancien code et ajout du prix total

--- project/plugins/acVinVracPlugin/modules/vrac/templates/marcheSuccess.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/marcheSuccess.php
@@ -1,13 +1,5 @@
-<?php
-/* Fichier : marcheSuccess.php
- * Description : Fichier php correspondant à la vue de vrac/XXXXXXXXXXX/marche
- * Formulaire d'enregistrement de la partie marche d'un contrat
- * Auteur : Petit Mathurin - mpetit[at]actualys.com
- * Version : 1.0.0 
- * Derniere date de modification : ${date}
- */
-$contratNonSolde = ((!is_null($form->getObject()->valide->statut)) && ($form->getObject()->valide->statut != VracClient::STATUS_CONTRAT_SOLDE));
-?>
+<?php use_helper('Float'); ?>
+<?php $contratNonSolde = ((!is_null($form->getObject()->valide->statut)) && ($form->getObject()->valide->statut != VracClient::STATUS_CONTRAT_SOLDE)); ?>
 
 <?php include_partial('vrac/etapes', array('vrac' => $form->getObject(), 'compte' => $compte, 'actif' => 2, 'urlsoussigne' => null, 'isTeledeclarationMode' => $isTeledeclarationMode)); ?>
 
@@ -127,6 +119,23 @@ $contratNonSolde = ((!is_null($form->getObject()->valide->statut)) && ($form->ge
                 </div>
             </div>
             <?php endif; ?>
+
+            <div class="form-group">
+                <?php if ($form->getObject()->hasPrixVariable()): ?>
+                <label class="col-sm-4 control-label">Prix initial total</label>
+                <?php else: ?>
+                <label class="col-sm-4 control-labell">Prix total</label>
+                <?php endif; ?>
+                <div class="col-sm-8"><?php echoFloat($form->getObject()->prix_initial_total) ?> <?php if(!is_null($form->getObject()->prix_initial_total)):?>€<?php endif; ?></div>
+            </div>
+
+            <?php if ($form->getObject()->hasPrixVariable()): ?>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">Prix définitif total</label>
+                <div class="col-sm-8"><?php echoFloat($form->getObject()->prix_total) ?> <?php if(!is_null($form->getObject()->prix_total)):?>€<?php endif; ?></div>
+            </div>
+            <?php endif; ?>
+
         </div>
     </div>
 
@@ -144,84 +153,6 @@ $contratNonSolde = ((!is_null($form->getObject()->valide->statut)) && ($form->ge
         </div>
     </div>
 </form>
-
-<section id="principal" style="display: none">
-    <?php //include_partial('headerVrac', array('vrac' => $vrac, 'compte' => $compte, 'actif' => 2, 'urlsoussigne' => null, 'isTeledeclarationMode' => $isTeledeclarationMode)); ?>
-    <div id="contenu_etape">  
-        <form id="vrac_marche" method="post" action="<?php echo url_for('vrac_marche', $vrac) ?>">    
-
-            <?php echo $form->renderHiddenFields() ?>
-            <?php echo $form->renderGlobalErrors() ?>
-
-
-            <div id="marche">
-
-                <?php if (isset($form['attente_original'])): ?>
-                    <!--  Affichage des l'option original  -->
-                    <div id="original" class="original section_label_strong">
-                        <?php echo $form['attente_original']->renderLabel() ?>
-                        <?php echo $form['attente_original']->render() ?>        
-                        <?php echo $form['attente_original']->renderError(); ?>
-                    </div>
-                <?php endif; ?>
-
-                <!--  Affichage des transactions disponibles  -->
-                <div id="type_transaction" class="type_transaction section_label_maj">
-                    <?php echo $form['type_transaction']->renderLabel() ?>
-                    <?php echo $form['type_transaction']->renderError(); ?>                    
-                    <?php echo $form['type_transaction']->render() ?>        
-                </div>
-
-                <!--  Affichage des produits, des labels et du stock disponible  -->
-                <div id="vrac_marche_produitLabel" class="section_label_maj">
-                    <?php include_partial('marche_produitLabel', array('form' => $form, 'isTeledeclarationMode' => $isTeledeclarationMode, 'defaultDomaine' => $defaultDomaine)); ?>
-                </div>
-
-                <!--  Affichage des volumes et des prix correspondant  -->
-                <div id="vrac_marche_volumePrix" class="section_label_maj">
-                    <?php include_partial('marche_volumePrix', array('form' => $form)); ?>
-                </div>
-
-            </div>
-
-            <div class="btn_etape">
-                <a href="<?php echo url_for('vrac_soussigne', $vrac); ?>" class="btn_etape_prec"><span>Etape précédente</span></a>
-                <?php if ($isTeledeclarationMode && $vrac->isBrouillon()) : ?>
-                    <a class="lien_contrat_supprimer_brouillon" href="<?php echo url_for('vrac_supprimer_brouillon', $vrac); ?>" style="margin-left: 10px">
-                        <span>Supprimer Brouillon</span>
-                    </a>
-                <?php endif; ?>
-                <button class="btn_etape_suiv" type="submit"><span>Etape Suivante</span></button>     
-            </div>
-        </form>
-    </div>      
-    <?php include_partial('popup_notices'); ?> 
-</section>
-
-<?php
-if ($isTeledeclarationMode):
-    include_partial('colonne_droite', array('societe' => $societe, 'etablissementPrincipal' => $etablissementPrincipal, 'retour' => true));
-else:
-    slot('colApplications');
-    /*
-     * Inclusion du panel de progression d'édition du contrat
-     */
-    if (!$contratNonSolde)
-        include_partial('contrat_progression', array('vrac' => $vrac));
-
-    /*
-     * Inclusion du panel pour les contrats similaires
-     */
-    include_partial('contratsSimilaires', array('vrac' => $vrac));
-
-    /*
-     * Inclusion des Contacts
-     */
-    include_partial('contrat_infos_contact', array('vrac' => $vrac));
-
-    end_slot();
-endif;
-?>
 
 <script type="text/javascript">
 
