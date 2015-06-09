1310dfddcb79f29aa303e7db05889b55fc51accd
Tangui Morlier <tangui@tangui.eu.org>
2015-06-09 14:47:35 +0200

    Auto log to admin if NO_AUTH

---
+++ project/plugins/acVinComptePlugin/lib/acAutoAdminFilter.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * HTTP Authentication connected to AD filter
+ * @author Tangui Morlier <tmorlier@actualys.com>
+ * Inspired by James McGlinn <james@mcglinn.org>
+ *
+ */
+class acAutoAdminFilter extends sfFilter
+{
+  /**
+   * Execute filter
+   *
+   * @param sfFilterChain $filterChain
+   */
+  public function execute ($filterChain)
+  {
+    $context = $this->getContext(); 	
+    $user = $context->getUser();
+
+    $rights = 'admin';
+
+    $user->setAttribute('AUTH_DESC', $rights);
+    $user->addCredential($rights);
+    if ($rights == 'admin') {
+      $user->addCredential('transactions');
+    }
+    if ($rights) {
+      $user->addCredential('contacts');
+    }
+    $filterChain->execute();
+  }
+ 
+
+  /**
+   * Sends HTTP Auth headers and exits
+   *
+   */
+  public static function logout ($dest = null)
+  {
+    return ;
+  }
+}
