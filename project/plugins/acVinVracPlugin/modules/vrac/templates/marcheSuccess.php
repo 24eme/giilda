97eed7e38f75824c4fe645c4d8c191446eaf7870
Vincent LAURENT <vince.laurent@gmail.com>
2015-06-09 16:17:18 +0200

    Bouton de bas de page

--- project/plugins/acVinVracPlugin/modules/vrac/templates/marcheSuccess.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/marcheSuccess.php
@@ -192,7 +192,7 @@
 
     <div class="row">
         <div class="col-xs-4 text-left">
-            <a href="<?php echo url_for('vrac_soussigne', $vrac); ?>" class="btn btn-default">Etape précédente</a>
+            <a href="<?php echo url_for('vrac_soussigne', $vrac); ?>" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Etape précédente</a>
         </div>
         <div class="col-xs-4 text-center">
             <?php if ($isTeledeclarationMode && $vrac->isBrouillon()) : ?>
@@ -200,7 +200,7 @@
             <?php endif; ?>  
         </div>
         <div class="col-xs-4 text-right">
-            <button type="submit" class="btn btn-default">Étape suivante</button>
+            <button type="submit" class="btn btn-success">Étape suivante <span class="glyphicon glyphicon-chevron-right"></span></button>
         </div>
     </div>
 </form>
