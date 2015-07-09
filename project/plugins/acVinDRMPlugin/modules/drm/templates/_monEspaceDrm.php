221cb113d7923f0201c8a7945b3edb75aff28bcd
Mikael Guillin <mguillin@actualys.com>
2015-07-09 18:07:36 +0200

    Amélioration de l'aspect visuel des blocs espace contrat et espace drm

--- project/plugins/acVinDRMPlugin/modules/drm/templates/_monEspaceDrm.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/_monEspaceDrm.php
@@ -1,35 +1,32 @@
-<fieldset id="espace_drm"> 
-    <div id="mon_espace">
+<div class="block_teledeclaration espace_drm">
+    <div class="title">ESPACE DRM</div>
+    <div class="panel">
+        <ul class="etablissements_drms">
+            <?php foreach ($drmToCompleteAndToStart as $etb => $drmsByEtb) : ?>
+                <li>
+                    <div class="etablissement_drms">
+                        <h2> <?php echo $drmsByEtb->nom . ' (' . $etb . ')'; ?></h2>
+                        <ul>
+                            <?php if ($drmsByEtb->nb_drm_to_finish): ?>
+                                <li class="statut_toFinish">
+                                    <span>  <?php echo $drmsByEtb->nb_drm_to_finish; ?> DRM en attente de validation
+                                    </span>
+                                </li>
+                            <?php endif; ?>
+                            <?php if ($drmsByEtb->nb_drm_to_create): ?>
+                                <li class="statut_toCreate">
+                                    <span> <?php echo $drmsByEtb->nb_drm_to_create; ?> DRM en attente de création
+                                    </span>
+                                </li>
+                            <?php endif; ?>
+                        </ul>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
 
-            <div class="block_teledeclaration espace_drm">
-                <div class="title">ESPACE DRM</div>
-                <div class="panel">
-                    <ul class="etablissements_drms">
-                        <?php foreach ($drmToCompleteAndToStart as $etb => $drmsByEtb) : ?>
-                            <li> 
-                                <div class="etablissement_drms">
-                                    <h2> <?php echo $drmsByEtb->nom . ' (' . $etb . ')'; ?></h2>
-                                    <ul>
-                                        <?php if ($drmsByEtb->nb_drm_to_finish): ?> 
-                                            <li  class="statut_toFinish"> 
-                                                <button >&nbsp;</button>
-                                                <span>  <?php echo $drmsByEtb->nb_drm_to_finish; ?> DRM en attente de validation
-                                                </span>
-                                            </li>
-                                        <?php endif; ?>
-                                        <?php if ($drmsByEtb->nb_drm_to_create): ?> 
-                                            <li class="statut_toCreate"> 
-                                                <button>&nbsp;</button>
-                                                <span> <?php echo $drmsByEtb->nb_drm_to_create; ?> DRM en attente de création
-                                                </span>
-                                            </li>
-                                        <?php endif; ?>
-                                    </ul>
-                                </div>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            </div>
+        <div class="acces">
+            <a href="#" class="btn_majeur">Acceder aux DRM</a>
+        </div>
     </div>
-</fieldset>
+</div>
