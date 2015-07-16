c921718a5f907f9f8a6c354be1c2d23bf49de8c6
mathurin <mathurin.petit@gmail.com>
2015-07-16 15:39:39 +0200

    maj inté des ligne de DRMs dans mon espace

--- project/plugins/acVinDRMPlugin/modules/drm/templates/_monEspaceDrm.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/_monEspaceDrm.php
@@ -1,3 +1,6 @@
+<?php use_helper('Date'); ?>
+<?php use_helper('DRM'); ?>
+<?php use_helper('Orthographe'); ?>
 <fieldset id="espace_drm">
     <div id="mon_espace">
         <div class="block_teledeclaration espace_drm">
@@ -8,22 +11,23 @@
                         <li>
                             <div class="etablissement_drms">
                                 <h2> <?php echo $drmsByEtb->nom . ' (' . $etb . ')'; ?></h2>
-                                <ul>
+                                <ul class="block_drm_espace">
                                     <?php if ($drmsByEtb->statut == DRMCalendrier::STATUT_EN_COURS): ?>
                                         <li class="statut_toFinish">
-                                            <span>  DRM de <?php echo $drmsByEtb->periode; ?> en attente de validation </span>
+                                            
+                                            <a href=""><span>Finir la DRM <?php echo elision('de', getFrPeriode($drmsByEtb->periode)) . ' ' . substr($drmsByEtb->periode, 0, 4); ?></span></a>
                                         </li>
                                     <?php endif; ?>
                                     <?php if ($drmsByEtb->statut == DRMCalendrier::STATUT_NOUVELLE): ?>
-                                        <li class="statut_toCreate">
-                                            <span>  DRM de <?php echo $drmsByEtb->periode; ?> en attente de création
-                                            </span>
-                                        </li>
+                                            <li class="statut_toCreate">
+                                                <a href=""> <span>Créer la DRM <?php echo getFrPeriodeElision($drmsByEtb->periode); ?></span></a>
+                                               
+                                            </li>
                                     <?php endif; ?>
                                     <?php if ($drmsByEtb->statut == DRMCalendrier::STATUT_VALIDEE): ?>
                                         <li class="statut_validee">
-                                            <span>  Visualiser votre DRM de <?php echo $drmsByEtb->periode; ?>
-                                            </span>
+                                            <a href=""> <span>Visualiser votre DRM <?php echo elision('de', getFrPeriode($drmsByEtb->periode)) . ' ' . substr($drmsByEtb->periode, 0, 4); ?>
+                                                </span></a>
                                         </li>
                                     <?php endif; ?>
                                 </ul>
@@ -31,10 +35,10 @@
                         </li>
                     <?php endforeach; ?>
                 </ul>
-                <?php if(isset($btnAccess)): ?>
-                <div class="acces">
-                    <a href="<?php echo url_for('drm_societe', array('identifiant' => $etablissement->getIdentifiant())) ?>" class="btn_majeur">Accéder aux DRM</a>
-                </div>
+                <?php if (isset($btnAccess)): ?>
+                    <div class="acces">
+                        <a href="<?php echo url_for('drm_societe', array('identifiant' => $etablissement->getIdentifiant())) ?>" class="btn_majeur">Accéder aux DRM</a>
+                    </div>
                 <?php endif; ?>
             </div>
         </div>
