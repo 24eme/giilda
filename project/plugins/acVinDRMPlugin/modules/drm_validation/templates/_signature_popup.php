c812f606efb44aff51f5628bb10fcca6a045cf95
Vincent LAURENT <vince.laurent@gmail.com>
2015-07-20 18:49:05 +0200

    Message popup validation

--- project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
+++ project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
@@ -2,6 +2,9 @@
     <div id="signature_drm_popup_content" class="popup_contenu">
         <h2>Veuillez confirmer la validation de la DRM </h2>
         <br/>
+        <p>
+            Vous êtes sur le point de valider votre DRM, une fois votre déclaration validée, vous ne pourrez plus la modifier.
+        </p>
         <div class="ligne_btn">
             <a id="signature_drm_popup_close" class="btn_rouge btn_majeur annuler" style="float: left;" href="#" >Annuler</a>
 
