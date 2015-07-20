8925a61f3a28b8b858d02dc3d10eebe4cd6e6689
mathurin <mathurin.petit@gmail.com>
2015-07-20 11:44:25 +0200

    vrai trie alphabétique par libéllé et non par clés

--- project/plugins/acVinDRMPlugin/modules/drm_edition/templates/_itemHeader.php
+++ project/plugins/acVinDRMPlugin/modules/drm_edition/templates/_itemHeader.php
@@ -24,7 +24,7 @@ $favoris_sorties = $favoris->sorties;
         <div class="groupe groupe_ouvert groupe_bloque favoris" data-groupe-id="2">
             <p>Entrées</p>
             <ul>
-                <?php foreach ($detailsNodes->getEntreesSorted() as $key => $item): ?>
+                <?php foreach ($detailsNodes->getEntreesSorted($drm->getDetailsConfigKey()) as $key => $item): ?>
                     <?php if ($favoris_entrees->exist($key)): ?>
                         <li <?php echo ($item->getFacturable() && !$isTeledeclarationMode) ? ' class="facturable"' : ''; ?> >
                             <span id="<?php echo 'star_favoris_entrees_' . $key ?>" class="categorie_libelle <?php echo 'entrees_'.$key; ?> <?php echo (count($favoris_entrees) > 1 ) ? 'clickable' : ''; ?>">
@@ -38,7 +38,7 @@ $favoris_sorties = $favoris->sorties;
             <div class="groupe no_favoris" data-groupe-id="3">
                 <p class="extendable">Autres entrées</p>
                 <ul style="display: none;">
-                    <?php foreach ($detailsNodes->getEntreesSorted() as $key => $item): ?>
+                    <?php foreach ($detailsNodes->getEntreesSorted($drm->getDetailsConfigKey()) as $key => $item): ?>
                         <?php if (!$favoris_entrees->exist($key)): ?>
                             <li <?php echo ($item->getFacturable() && !$isTeledeclarationMode) ? ' class="facturable"' : ''; ?> >      
                                 <span id="<?php echo 'star_favoris_entrees_' . $key ?>" class="categorie_libelle <?php echo 'entrees_'.$key; ?>  <?php echo (count($favoris_entrees) < DRMClient::$drm_max_favoris_by_types_mvt[DRMClient::DRM_TYPE_MVT_ENTREES] ) ? 'clickable' : ''; ?>">
@@ -54,7 +54,7 @@ $favoris_sorties = $favoris->sorties;
         <div class="groupe groupe_ouvert groupe_bloque favoris" data-groupe-id="4">
             <p>Sorties</p>
             <ul>
-                <?php foreach ($detailsNodes->getSortiesSorted() as $key => $item): ?>
+                <?php foreach ($detailsNodes->getSortiesSorted($drm->getDetailsConfigKey()) as $key => $item): ?>
                     <?php if ($favoris_sorties->exist($key)): ?>
                         <li <?php echo ($item->getFacturable() && !$isTeledeclarationMode) ? ' class="facturable"' : ''; ?> >    
                             <span id="<?php echo 'sorties_'.$key.' star_favoris_sorties_' . $key ?>" class="categorie_libelle <?php echo 'sorties_'.$key; ?> <?php echo (count($favoris_sorties) > 1 ) ? 'clickable' : ''; ?>">
@@ -67,7 +67,7 @@ $favoris_sorties = $favoris->sorties;
             <div class="groupe no_favoris" data-groupe-id="5">
                 <p class="extendable">Autres sorties</p>
                 <ul style="display: none;">
-                    <?php foreach ($detailsNodes->getSortiesSorted() as $key => $item): ?>
+                    <?php foreach ($detailsNodes->getSortiesSorted($drm->getDetailsConfigKey()) as $key => $item): ?>
                         <?php if (!$favoris_sorties->exist($key)): ?>
                             <li <?php echo ($item->getFacturable() && !$isTeledeclarationMode) ? ' class="facturable"' : ''; ?> >
                                 <span id="<?php echo 'sorties_'.$key.' star_favoris_sorties_' . $key ?>" class="categorie_libelle <?php echo 'sorties_'.$key; ?> <?php echo (count($favoris_sorties) < DRMClient::$drm_max_favoris_by_types_mvt[DRMClient::DRM_TYPE_MVT_SORTIES] ) ? 'clickable' : ''; ?>">
