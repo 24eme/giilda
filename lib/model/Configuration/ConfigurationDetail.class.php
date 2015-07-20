0c67859ef8234eccd2bf6a928ea8746d12ffd960
mathurin <mathurin.petit@gmail.com>
2015-07-20 11:44:25 +0200

    vrai trie alphabétique par libéllé et non par clés

--- lib/model/Configuration/ConfigurationDetail.class.php
+++ lib/model/Configuration/ConfigurationDetail.class.php
@@ -10,20 +10,25 @@ class ConfigurationDetail extends BaseConfigurationDetail {
     return $this->getParent()->getParent()->getCVO($periode, $interpro);
   }
   
-  public function getEntreesSorted() {
-      return $this->getDetailsSorted($this->getEntrees());
+  public function getEntreesSorted($configKey = DRMClient::DRM_CONFIGURATION_KEY_BEFORE_TELEDECLARATION) {
+      return $this->getDetailsSorted($this->getEntrees(),$configKey);
   }
   
-   public function getSortiesSorted() {
-      return $this->getDetailsSorted($this->getSorties());
+   public function getSortiesSorted($configKey = DRMClient::DRM_CONFIGURATION_KEY_BEFORE_TELEDECLARATION) {
+      return $this->getDetailsSorted($this->getSorties(),$configKey);
   }
   
-   public function getDetailsSorted($details) {
-        $detailsSorted = array();
+   public function getDetailsSorted($details,$configKey = DRMClient::DRM_CONFIGURATION_KEY_BEFORE_TELEDECLARATION) {
+        $detailsSortedLibelle = array();        
         foreach ($details as $key => $detail) {
-            $detailsSorted[$key] = $detail;
+            $detailsSortedLibelle[$detail->getLibelle($configKey)] = $detail;
+            
         }
-        ksort($detailsSorted);
+        ksort($detailsSortedLibelle);
+        $detailsSorted = array();     
+        foreach ($detailsSortedLibelle as $keyLibelle => $detail) {
+            $detailsSorted[$detail->getKey()] = $detail;
+        }
         
         return $detailsSorted;
     }
