82c749db965a475789665cff97e71425daa5e968
Tangui Morlier <tangui@tangui.eu.org>
2015-08-07 20:05:14 +0200

    Désactive les contrats hors interloire pour interloire (et inversement)

--- project/plugins/acVinDRMPlugin/lib/form/DRMDetailSortiesForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/DRMDetailSortiesForm.class.php
@@ -7,8 +7,15 @@ class DRMDetailSortiesForm  extends acCouchdbObjectForm {
         $certif = $this->getObject()->getParent()->getCertification()->getKey();
         $drm = $this->getObject()->getDocument();
     	foreach ($configurationDetail->getSortiesSorted() as $key => $value) {
+            $disabled = (!preg_match('/AOC|IGP/', $certif) && ($key == 'repli'));
+            if ($key == 'contrathorsinterpro' && preg_match('/_INTERLOIRE/', $certif)) {
+                $disabled = true;
+            }
+            if ($key == 'contrat' && !preg_match('/_INTERLOIRE/', $certif)) {
+                $disabled = true;
+            }
     		if ($value->readable) {
-	    		if (!$value->writable || (!preg_match('/AOC|IGP/', $certif) && ($key == 'repli'))) {
+	    		if (!$value->writable || $disabled) {
 	    			$this->setWidget($key, new sfWidgetFormInputFloat(array(), array('readonly' => 'readonly')));
 	    		} else {
 	    			$this->setWidget($key, new sfWidgetFormInputFloat());
