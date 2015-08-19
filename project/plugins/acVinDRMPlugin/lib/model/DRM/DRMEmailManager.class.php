468f36fc8039906dbdaf997c4c5e0b31c2146b32
Mathurin <mathurin.petit@gmail.com>
2015-08-19 18:05:41 +0200

    maj email validation avec try catch

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRMEmailManager.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRMEmailManager.class.php
@@ -50,7 +50,7 @@ Voici les différentes modifications enregistrées :
 
 ";
         foreach ($diff as $key => $value) {
-            $mess .= $key." : ".$value."
+            $mess .= $key . " : " . $value . "
 ";
         }
         $mess .= "
@@ -64,7 +64,7 @@ L’application de télédéclaration des contrats d’InterLoire";
 
         $message = $this->getMailer()->compose(array(sfConfig::get('app_mail_from_email') => sfConfig::get('app_mail_from_name')), $mailsInterloire, $subject, $mess);
         try {
-          //  $this->getMailer()->send($message);          
+            $this->getMailer()->send($message);
         } catch (Exception $e) {
             $this->getUser()->setFlash('error', 'Erreur de configuration : Mail de confirmation non envoyé, veuillez contacter INTERLOIRE');
             return null;
@@ -76,50 +76,56 @@ L’application de télédéclaration des contrats d’InterLoire";
 
         $etablissement = EtablissementClient::getInstance()->find($this->drm->identifiant);
         $contact = EtablissementClient::getInstance()->buildInfosContact($etablissement);
-        
-        
+
+
         $mess = "  
 
-Vous venez de déclarer la DRM ".getFrPeriodeElision($this->drm->periode)." électroniquement sur le portail de télédeclaration d'InterLoire.
+Vous venez de déclarer la DRM " . getFrPeriodeElision($this->drm->periode) . " électroniquement sur le portail de télédeclaration d'InterLoire.
 
 Vous pouvez la visualiser à tout moment en cliquant sur le lien suivant : " . $this->getUrlVisualisationDrm() . " .
 
 Votre DRM est également joint à ce mail en format PDF.
 
-Pour toutes questions, veuillez contacter " . $contact->nom . " - ".$contact->email." - ".$contact->telephone.".
+Pour toutes questions, veuillez contacter " . $contact->nom . " - " . $contact->email . " - " . $contact->telephone . ".
     
 --
 
 L’application de télédéclaration des DRM d’InterLoire
 
-Rappel de votre identifiant : ".substr($this->drm->identifiant,0,6);
+Rappel de votre identifiant : " . substr($this->drm->identifiant, 0, 6);
 
         $pdf = new DRMLatex($this->drm);
         $pdfContent = $pdf->getPDFFileContents();
         $pdfName = $pdf->getPublicFileName();
 
-            $subject = "Validation de votre DRM " . getFrPeriodeElision($this->drm->periode) . " créé le " . $this->getDateSaisieDrmFormatted() . " .";
+        $subject = "Validation de votre DRM " . getFrPeriodeElision($this->drm->periode) . " créé le " . $this->getDateSaisieDrmFormatted() . " .";
 
-            $message = $this->getMailer()->compose(array(sfConfig::get('app_mail_from_email') => sfConfig::get('app_mail_from_name')), $etablissement->getEmailTeledeclaration(), $subject, $mess);
+        $message = $this->getMailer()->compose(array(sfConfig::get('app_mail_from_email') => sfConfig::get('app_mail_from_name')), $etablissement->getEmailTeledeclaration(), $subject, $mess);
 
-            $message->attach(new Swift_Attachment($pdfContent, $pdfName, 'application/pdf'));
+        $message->attach(new Swift_Attachment($pdfContent, $pdfName, 'application/pdf'));
 
 //            $attachment = new Swift_Attachment(file_get_contents(dirname(__FILE__) . '/../../../../../web/data/reglementation_generale_des_transactions.pdf'), 'reglementation_generale_des_transactions.pdf', 'application/pdf');
 //            $message->attach($attachment);
 
+        try {
             $this->getMailer()->send($message);
             $resultEmailArr[] = $etablissement->getEmailTeledeclaration();
+        } catch (Exception $e) {
+            $this->getUser()->setFlash('error', 'Erreur de configuration : Mail de confirmation non envoyé, veuillez contacter INTERLOIRE');
+            return null;
+        }
+
         return $resultEmailArr;
     }
-    
+
     private function getMailer() {
         return $this->mailer;
     }
-    
-     private function getUrlVisualisationDrm() {
+
+    private function getUrlVisualisationDrm() {
         return sfContext::getInstance()->getRouting()->generate('drm_visualisation', $this->drm, true);
     }
-    
+
     protected function getDateSaisieDrmFormatted() {
         return date("d/m/Y", strtotime($this->drm->valide->date_saisie));
     }
