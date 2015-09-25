4a861262529cf0427e8000042f3d575d47a5be30
Mathurin <mathurin.petit@gmail.com>
2015-09-25 17:39:14 +0200

    maj retour du 16 sept

--- project/plugins/acVinDRMPlugin/modules/drm_edition/templates/_produitForm.php
+++ project/plugins/acVinDRMPlugin/modules/drm_edition/templates/_produitForm.php
@@ -1,5 +1,5 @@
 <form id="form_produit_declaration" method="post" action="<?php echo url_for("drm_edition_produit_ajout", $drm) ?>" class="section_label_strong">
-    <label>Saisir une appellation</label>
+    <label>Saisir un produit</label>
     
     <?php echo $form['hashref']->render(); ?>
     <?php echo $form->renderHiddenFields(); ?>
