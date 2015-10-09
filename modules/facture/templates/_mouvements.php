147d69ea3a88e93b5616c7c7c2a43537e6cba067
Mathurin <mathurin.petit@gmail.com>
2015-10-09 19:14:09 +0200

    maj Giilda Facture

--- modules/facture/templates/_mouvements.php
+++ modules/facture/templates/_mouvements.php
@@ -42,7 +42,7 @@ use_helper('Prix');
 <br /> 
 <div class="generation_facture_valid">
        <span>Cliquer sur "Générer" pour lancer la génération de la facture</span>
-    <a href="#" id="generation_facture" class="btn_majeur btn_refraichir">Générer</a>
+       <button id="generation_facture" class="btn btn-lg btn-success">Générer</button>
 </div>
 </form>
 <?php else : ?>
