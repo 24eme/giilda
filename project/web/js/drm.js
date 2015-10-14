ed499f8a87525eb5ae21d02e75de91b5eea736bf
Vincent LAURENT <vince.laurent@gmail.com>
2015-10-14 19:35:38 +0200

    Active sur la liste des produits

--- project/web/js/drm.js
+++ project/web/js/drm.js
@@ -36,6 +36,14 @@
                 $('#list-produits a[data-hash="'+colonne.getHash()+'"]').addClass('list-group-item-success');
             }
 
+            colonnes.event_focus = function(colonne) {
+                $('#list-produits a[data-hash="'+colonne.getHash()+'"]').addClass('active');
+            }
+
+            colonnes.event_unfocus = function(colonne) {
+                $('#list-produits a').removeClass('active');
+            }
+
             $.initProduitForm();
             //$.initDetailsPopups();
 
