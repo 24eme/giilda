9e5c58efad17f0b4b08f1815bbc2f85173871b39
Mathurin <mathurin.petit@gmail.com>
2015-10-26 18:13:15 +0100

    maj couleur PDF

--- project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateRecapMvtTex.php
+++ project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateRecapMvtTex.php
@@ -172,7 +172,7 @@ $mvtsSortiesForPdf = $drmLatex->getMvtsSortiesForPdf();
         \rowcolor{lightgray}
         \multicolumn{2}{|r|}{ \small{\textbf{TOTAL SORTIES}} } &
         <?php foreach ($produits_for_page as $counter => $produit): ?>
-            \multicolumn{1}{>{\columncolor[rgb]{0.6,0.6,0.6}}r|}{   \small{\textbf{<?php echoFloatWithHl($produit->total_sorties); ?>}} }
+            \multicolumn{1}{r|}{   \small{\textbf{<?php echoFloatWithHl($produit->total_sorties); ?>}} }
             <?php echo ($counter < count($produits_for_page) - 1) ? "&" : ''; ?>
         <?php endforeach; ?>  
         \\
@@ -185,7 +185,7 @@ $mvtsSortiesForPdf = $drmLatex->getMvtsSortiesForPdf();
         ?>
         \multicolumn{2}{|>{\columncolor[rgb]{0.6,0.6,0.6}}c|}{ \small{\color{white}{\textbf{STOCK FIN DE MOIS}} }} &
         <?php foreach ($produits_for_page as $counter => $produit): ?>
-            \multicolumn{1}{r|}{  \small{\color{white}{\textbf{<?php echoFloatWithHl($produit->stocks_fin->revendique); ?>}}}} 
+            \multicolumn{1}{>{\columncolor[rgb]{0.6,0.6,0.6}}r|}{  \small{\color{white}{\textbf{<?php echoFloatWithHl($produit->stocks_fin->revendique); ?>}}}} 
             <?php echo ($counter < count($produits_for_page) - 1) ? "&" : ''; ?>
         <?php endforeach; ?>  
         \\	
