c5625cca1f8ffacd19ab9a66f90f20a58f90b156
Vincent LAURENT <vince.laurent@gmail.com>
2015-11-03 17:28:43 +0100

    Gestion du nombre de décimal pour les floats dans la conf

--- project/plugins/acVinLibPlugin/lib/widget/bootstrap/bsWidgetFormInputFloat.class.php
+++ project/plugins/acVinLibPlugin/lib/widget/bootstrap/bsWidgetFormInputFloat.class.php
@@ -4,34 +4,26 @@ class bsWidgetFormInputFloat extends sfWidgetFormInputText
 {
   protected function configure($options = array(), $attributes = array())
   {
-    parent::configure($options, $attributes);
-    $this->addOption('decimal', 2);
-    $this->addOption('decimal_auto', 2);
+      parent::configure($options, $attributes);
+      $this->addOption('default_decimal_format', FloatHelper::getInstance()->getDefaultDecimalFormat());
+      $this->addOption('max_decimal_authorized', FloatHelper::getInstance()->getMaxDecimalAuthorized());
   }
   
   public function render($name, $value = null, $attributes = array(), $errors = array())
   {
-    sfApplicationConfiguration::getActive()->loadHelpers('Float');
-    
-    $int = $value;
-    $float = null;
-    if(count(explode(".", $value)) >= 2) {
-      list($int, $float) = explode(".", $value);
-    }
-    $format = ($value !== null && trim($value) !== "" && strlen($float) <= $this->getOption('decimal_auto'));
+      $defaultDecimalFormat = $this->getOption('default_decimal_format', FloatHelper::getInstance()->getDefaultDecimalFormat());
+      $maxDecimalAuhtorized = $this->getOption('max_decimal_authorized', FloatHelper::getInstance()->getMaxDecimalAuthorized());
+      
+      $value = FloatHelper::getInstance()->format($value, $defaultDecimalFormat, $maxDecimalAuhtorized);
+      
+      $attributes['autocomplete'] = 'off';
+      $attributes['data-decimal-auto'] = $defaultDecimalFormat;
+      $attributes['data-decimal'] = $maxDecimalAuhtorized;
+      
+      if(!isset($attributes['class'])) {
+          $attributes['class'] = 'form-control text-right input-float';            
+      }
 
-    if($format) {
-      $value = sprintFloat($value, "%01.0".$this->getOption('decimal_auto')."f")  ;
-    }
-
-    $attributes['autocomplete'] = 'off';
-    $attributes['data-decimal'] = $this->getOption('decimal');
-    $attributes['data-decimal-auto'] = $this->getOption('decimal_auto');
-    
-    if(!isset($attributes['class'])) {
-      $attributes['class'] = 'form-control text-right input-float';            
-    }
-
-    return parent::render($name, $value, $attributes, $errors);
+      return parent::render($name, $value, $attributes, $errors);
   }
 }
