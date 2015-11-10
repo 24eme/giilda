ca4c980810d0699059c13c6bc7524394b39e4285
Tangui Morlier <tangui@tangui.eu.org>
2015-11-10 18:11:27 +0100

    Typo

--- project/plugins/acVinDRMPlugin/modules/drm_crds/templates/_crd_regime_choice_popup.php
+++ project/plugins/acVinDRMPlugin/modules/drm_crds/templates/_crd_regime_choice_popup.php
@@ -17,10 +17,10 @@ if (isset($retour) && ($retour == 'crds')) {
             Il est nécessaire pour la suite de la saisie de choisir ici le régime CRD. Une fois choisi ce message n'apparaîtra plus. <br/>
              Si vous achetez des CRD Acquitées auprès de votre ODG, sélectionnez «&nbsp;collectif acquitté&nbsp;». Si utilisez des CRD personnalisées, cliquez sur «&nbsp;personnalisé&nbsp;». Pour les autres cas, sélectionnez «&nbsp;collectif suspendu&nbsp;»</p>
             <br/>
-            <p>Sur la DRM papier Interloire, le régime CRD est demandé dans le cadre des stock CRD :</p>
+            <p>Sur la DRM papier Interloire, le régime CRD est demandé dans le cadre des stock CRD&nbsp;:</p>
             <center><img src="/images/visuels/regime_crd_papier.jpg" width="400" height="125" stype="padding: 10px;" ></center>
             <br/>
-            <p><b>Votre régime CRD :</b></p>
+            <p><b>Votre régime CRD&nbsp;:</b></p>
             <?php echo $crdRegimeForm['crd_regime']->render(); ?>
             <br/>
             <div class="ligne_btn">
