768fd530cc445a9dd4c040eaddf608b02fda757c
Mathurin <mathurin.petit@gmail.com>
2015-11-13 15:44:13 +0100

    maj wording

--- project/plugins/acVinDRMPlugin/modules/drm_validation/templates/validationSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm_validation/templates/validationSuccess.php
@@ -52,7 +52,7 @@
                         <a id="signature_drm_popup" <?php if (!$validation->isValide()): ?>disabled="disabled"<?php endif; ?> href="#signature_drm_popup_content" class="btn_validation signature_drm<?php if ($validation->isValide()) echo '_popup'; ?>"><span>Valider</span></a>
                         <?php include_partial('drm_validation/signature_popup', array('drm' => $drm, 'societe' => $societe, 'etablissementPrincipal' => $etablissementPrincipal, 'validationForm' => $form)); ?>
 
-                        <a style="margin-left: 70px;" href="<?php echo url_for('drm_pdf', $drm); ?>" class="btn_majeur btn_pdf center" id="drm_pdf"><span>Vérifier votre PDF</span></a>
+                        <a style="margin-left: 70px;" href="<?php echo url_for('drm_pdf', $drm); ?>" class="btn_majeur btn_pdf center" id="drm_pdf"><span>Vérifier le PDF</span></a>
 
                     <?php else: ?>
                         <button type="submit" class="btn_etape_suiv" id="button_drm_validation" <?php if (!$validation->isValide()): ?>disabled="disabled"<?php endif; ?>><span>Valider</span></button>
