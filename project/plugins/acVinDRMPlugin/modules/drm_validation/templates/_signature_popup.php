40877f9801e8a7955b4ffabe73920e3b3111d06d
Tangui Morlier <tangui@tangui.eu.org>
2015-11-27 12:35:51 +0100

    Suppression du formulaire d'envoi de la DRM par mail

--- project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
+++ project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
@@ -5,16 +5,6 @@
             <p>
                 Vous êtes sur le point de valider votre DRM, une fois votre déclaration validée, vous ne pourrez plus la modifier.
             </p>
-            <p>
-                Après validation vous receverez votre DRM par mail et vous avez la possibilité de la transmettre ci dessous à un email supplémentaire.
-            </p>
-            <div class="ligne_form">       
-                <span>
-                    <?php echo $validationForm['email_transmission']->renderLabel(); ?>
-                    <?php echo $validationForm['email_transmission']->renderError(); ?>
-                    <input id="drm_email_transmission_visible" type="text"  value="<?php echo ($drm->exist('email_transmission') && $drm->email_transmission)? $drm->email_transmission : ''; ?>">
-                </span>
-            </div>
             <div class="ligne_btn">
                 <a id="signature_drm_popup_close" class="btn_rouge btn_majeur annuler" style="float: left;" href="#" >Annuler</a>
 
