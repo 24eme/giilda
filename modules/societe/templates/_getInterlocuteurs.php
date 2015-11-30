724a3560abfcb0c1bc8ccfffb2d63b3a2da0e183
Vincent LAURENT <vince.laurent@gmail.com>
2015-11-30 16:08:57 +0100

    Début d'adaption des interfaces des contacts en bootstrap

--- modules/societe/templates/_getInterlocuteurs.php
+++ modules/societe/templates/_getInterlocuteurs.php
@@ -1,52 +1,42 @@
 <?php
 if (!is_null($contacts)):
     ?>
-    <div class="bloc_col" id="infos_contact">
         <h2>Interlocuteurs</h2>
 
         <div class="contenu">
-            <ul>
+            <ul class="list-group">
                 <?php foreach ($contacts as $id => $contact) :
                     ?>
-                    <li id="infos_contact_vendeur">
-                        <ul>
-                            <li class="titre <?php if ($contact->compte_type == CompteClient::TYPE_COMPTE_SOCIETE) {
-                echo 'societe';
-            } else if ($contact->compte_type == CompteClient::TYPE_COMPTE_ETABLISSEMENT) {
-                echo 'etablissement';
-            } else {
-                echo 'nom';
-            } ?>">
+                    <li class="list-group-item">
                             <?php if (!isset($no_link) || !$no_link): ?>
-                                    <a title="<?php echo $contact->compte_type ?>" href="<?php echo url_for('compte_visualisation', array('identifiant' => $contact->identifiant)); ?>"><?php echo $contact->nom_a_afficher; ?></a>
+                                    <h4 class="list-group-item-heading"><a title="<?php echo $contact->compte_type ?>" href="<?php echo url_for('compte_visualisation', array('identifiant' => $contact->identifiant)); ?>"><?php echo $contact->nom_a_afficher; ?></a></h4>
                             <?php else: ?>
                                 <?php echo $contact->nom_a_afficher; ?>
                             <?php endif; ?>
-                            </li>
-                            <?php if ($contact->statut && ($contact->statut == SocieteClient::STATUT_SUSPENDU)): ?>
-                                <li style="color: red"><?php echo $contact->statut; ?></li>
-                            <?php endif; ?>
-                            <?php if ($contact->telephone_perso): ?>
-                                <li class="tel_perso"><?php echo $contact->telephone_perso; ?></li>
-                            <?php endif; ?>
-                            <?php if ($contact->telephone_mobile): ?>
-                                <li class="tel_mobile"><?php echo $contact->telephone_mobile; ?></li>
-                            <?php endif; ?>
-                            <?php if ($contact->telephone_bureau): ?>
-                                <li class="tel"><?php echo $contact->telephone_bureau; ?></li>
+                        <p class="list-group-item-text">
+                            <ul class="list-unstyled">
+                                <?php if ($contact->statut && ($contact->statut == SocieteClient::STATUT_SUSPENDU)): ?>
+                                    <li><?php echo $contact->statut; ?></li>
                                 <?php endif; ?>
-        <?php if ($contact->fax): ?>
-                                <li class="fax"><?php echo $contact->fax; ?></li>
-                    <?php endif; ?>
-        <?php if (trim($contact->email)): ?>
-                                <li class="email"><a href="mailto:<?php echo $contact->email; ?>"><?php echo $contact->email; ?></a></li>
-        <?php endif; ?>    
-                        </ul>
+                                <?php if ($contact->telephone_perso): ?>
+                                    <li><?php echo $contact->telephone_perso; ?></li>
+                                <?php endif; ?>
+                                <?php if ($contact->telephone_mobile): ?>
+                                    <li><?php echo $contact->telephone_mobile; ?></li>
+                                <?php endif; ?>
+                                <?php if ($contact->telephone_bureau): ?>
+                                    <li><?php echo $contact->telephone_bureau; ?></li>
+                                    <?php endif; ?>
+                                <?php if ($contact->fax): ?>
+                                    <li><?php echo $contact->fax; ?></li>
+                                <?php endif; ?>
+                                <?php if (trim($contact->email)): ?>
+                                    <li><a href="mailto:<?php echo $contact->email; ?>"><?php echo $contact->email; ?></a></li>
+                                <?php endif; ?>
+                            </ul>
+                        </p>
                     </li>
     <?php endforeach; ?>
             </ul>
         </div>
-    </div>
-    <?php
-endif;
-?>
+<?php endif; ?>
