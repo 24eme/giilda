bafa11f12d53b21cbacc49d531bfd3571ac27d37
Vincent LAURENT <vince.laurent@gmail.com>
2015-12-03 16:41:56 +0100

    Import complé la conf à partir d'un dossier

--- project/plugins/ImportPlugin/lib/task/importConfigurationTask.class.php
+++ project/plugins/ImportPlugin/lib/task/importConfigurationTask.class.php
@@ -5,7 +5,8 @@ class importConfigurationTask extends sfBaseTask {
     protected function configure() {
         // // add your own arguments here
         $this->addArguments(array(
-            new sfCommandArgument('file', sfCommandArgument::REQUIRED, "Fichier csv pour l'import"),
+            new sfCommandArgument('configuration_id', sfCommandArgument::REQUIRED, "ID couchdb du document à importer"),
+            new sfCommandArgument('csv_dir', sfCommandArgument::REQUIRED, "Répertoire contenant les fichiers"),
         ));
 
         $this->addOptions(array(
@@ -29,10 +30,48 @@ EOF;
     protected function execute($arguments = array(), $options = array()) {
         $databaseManager = new sfDatabaseManager($this->configuration);
         $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+        $import_dir = $arguments['csv_dir'];
         
-        $configuration = new Configuration();
-        $csv = new ProduitCsvFile($configuration, $arguments['file']);   
+        $configuration = ConfigurationClient::getInstance()->find($arguments['configuration_id']);
+
+        if(!$configuration) {
+            $configuration = new Configuration();
+            $configuration->_id = $arguments['configuration_id'];
+        }
+
+        $csv = new ProduitCsvFile($configuration, $import_dir."/produits.csv");   
         $csv->importProduits();
+
+        foreach (file($import_dir . '/details.csv') as $line) {
+            if (preg_match('/^#/', $line))
+                continue;
+            $datas = explode(";", preg_replace('/"/', '', str_replace("\n", "", $line)));
+            $detail = $configuration->get($datas[0])->getOrAdd($datas[1])->add($datas[2])->add($datas[3]);
+            $detail->readable = (int) $datas[4];
+            $detail->writable = (int) $datas[5];
+            $detail->details = (int) $datas[6];
+            $detail->mouvement_coefficient = (int) $datas[7];
+            $detail->vrac = (int) $datas[8];
+            $detail->facturable = (int) $datas[9];
+            $detail->douane_type = $datas[10];
+            $detail->douane_cat = $datas[11];
+            $detail->taxable_douane = (int) $datas[12];
+        }
+
+        foreach (file($import_dir . '/libelle_detail_ligne.csv') as $line) {
+            $datas = explode(";", preg_replace('/"/', '', str_replace("\n", "", $line)));
+            $detail = $configuration->libelle_detail_ligne->getOrAdd($datas[0])->getOrAdd($datas[1]);
+            $detail->libelle = $datas[2];
+            $detail->libelle_long = $datas[3];
+            $detail->description = $datas[4];
+        }
+
+        foreach (file($import_dir . '/mouvements_favoris.csv') as $mvtLine) {
+            $mvt = explode(";", preg_replace('/"/', '', str_replace("\n", "", $mvtLine)));
+            $configuration->getOrAdd('mvts_favoris')->add($mvt[0], $mvt[0]);
+        }
+
         $configuration->save();
     }
 
