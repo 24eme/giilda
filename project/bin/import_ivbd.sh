a174d14aad7224075c29c694c81de3a662d8cfa3
Vincent LAURENT <vince.laurent@gmail.com>
2015-12-17 11:45:43 +0100

    Import du catalogue produit ivbd

--- project/bin/import_ivbd.sh
+++ project/bin/import_ivbd.sh
@@ -19,6 +19,10 @@ if test "$1"; then
 
     rm $TMP/data_ivbd.tgz
 
+    cd IVBD
+
+    rename 'y/A-Z/a-z/' *
+
     cd $SYMFODIR
 
     echo "Conversion des fichiers en utf8"
@@ -28,10 +32,32 @@ if test "$1"; then
 
     ls $TMP/data_ivbd_origin/IVBD | while read ligne  
     do
-        echo $TMP/data_ivbd_origin/IVBD/$ligne
+        echo $DATA_DIR/$ligne
         iconv -f utf-16 -t utf-8 $TMP/data_ivbd_origin/IVBD/$ligne > $DATA_DIR/$ligne
     done
+
+    cp $TMP/data_ivbd_origin/IVBD/base_ppm.csv $DATA_DIR/base_ppm.csv
 
     rm -rf $TMP/data_ivbd_origin
 fi
 
+echo "Import de la configuration"
+
+curl -X DELETE "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/CONFIGURATION"?rev=$(curl -sX GET "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/CONFIGURATION" | grep -Eo '"_rev":"[a-z0-9-]+"' | sed 's/"//g' | sed 's/_rev://')
+php symfony import:configuration CONFIGURATION data/import/configuration/ivbd
+php symfony cc > /dev/null
+
+echo "Import des contacts"
+
+cat /tmp/giilda/data_ivbd_csv/base_ppm.csv | tr -d "\n" | tr "\r" "\n" | awk -F ";" '
+{
+    nom=$10 " " $11 " " $12; statut=($18) ? "SUSPENDU" : "ACTIF";  print $2 ";VITICULTEUR;" nom ";;" statut ";;" $26 ";;;adresse;;;;code_postal;commune;cedex;pays;email;tel_bureau;tel_perso;mobile;fax;web;commentaire"
+}' > $DATA_DIR/societes.csv
+
+cat /tmp/giilda/data_ivbd_csv/base_ppm.csv | tr -d "\n" | tr "\r" "\n" | awk -F ";" '
+{
+    nom=$10 " " $11 " " $12; statut=($18) ? "SUSPENDU" : "ACTIF";  print $2  ";" $2 ";VITICULTEUR;" nom ";" statut ";HORS_REGION;cvi;no_accises;carte_pro;recette_locale:adresse;;;;code_postal;commune;cedex;pays;email;tel_bureau;tel_perso;mobile;fax;web;commentaire"
+}' > $DATA_DIR/etablissements.csv
+
+php symfony import:societe $DATA_DIR/societes.csv
+php symfony import:etablissement $DATA_DIR/etablissements.csv
