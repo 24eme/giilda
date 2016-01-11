3dacd804b94b7ee93d64d9cd2e9d3259c2c4208d
Vincent LAURENT <vince.laurent@gmail.com>
2016-01-11 21:35:33 +0100

    Mise en forme des contacts

--- modules/societe/templates/visualisationSuccess.php
+++ modules/societe/templates/visualisationSuccess.php
@@ -1,94 +1,109 @@
+<?php use_helper('Compte') ?>
+<?php use_helper('Date'); ?>
+
 <ol class="breadcrumb">
-    <li><a href="<?php echo url_for('societe') ?>">Page d'accueil</a></li>
-    <li class="active"><?php echo $societe->raison_sociale; ?></li>
+    <li><a href="<?php echo url_for('societe') ?>">Accueil des contacts</a></li>
+    <li class="active"><a href="<?php echo url_for('societe_visualisation', array('identifiant' => $societe->identifiant)); ?>"><span class="<?php echo comptePictoCssClass($societe->getRawValue()) ?>"></span> <?php echo $societe->raison_sociale; ?></a></li>
 </ol>
 
 <section class="row">
-    <div class="col-xs-9">
-        <h2><?php echo $societe->raison_sociale; ?></h2>
-
-        <div class="row">
-            <?php include_partial('visualisationPanel', array('societe' => $societe, 'modification' => $modification, 'reduct_rights' => $reduct_rights)); ?>
-            <div class="col-xs-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Coordonnées de la société</div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <?php include_partial('compte/coordonneesVisualisation', array('compte' => $societe->getMasterCompte())); ?>
-                        </div>
-                    </div>
-                </div>
+    <div class="col-xs-12">
+        <div class="list-group">
+            <div class="list-group-item">
+                <h2><span class="<?php echo comptePictoCssClass($societe->getRawValue()) ?>"></span> <?php echo $societe->raison_sociale; ?> 
+                <span class="text-muted">(Société)</span>
+                <?php if($modification || $reduct_rights) : ?>
+                <a href="<?php echo url_for('societe_modification', array('identifiant' => $societe->identifiant)); ?>" class="btn btn-default">Modifier</a>
+                <?php endif; ?>
+                <p class="lead">
+                    <span class="label label-primary"><?php echo $societe->type_societe; ?></span>
+                    <span class="label label-success"><?php echo $societe->statut; ?></span>
+                    <small><?php if($societe->date_creation) : ?><span class="label label-default">Crée le <?php echo format_date($societe->date_creation,'dd/MM/yyyy'); ?></span><?php endif; ?>
+                     <?php if($societe->date_modification) : ?><span class="label label-default">Dernière modification le <?php echo format_date($societe->date_modification,'dd/MM/yyyy'); ?></span><?php endif; ?></small>
+                </p>
             </div>
-        </div>
-
-        <?php if ($societe->getMasterCompte()->exist('droits') && $societe->getMasterCompte()->hasDroit(Roles::TELEDECLARATION)): ?>
-            <div id="detail_societe_coordonnees" class="form_section ouvert">
-                <h3>Télédeclaration</h3>
-                <div class="form_contenu">
-                    <div class="form_ligne">  
-                        <label for="teledeclaration_login" class="label_liste">
-                            Login :
-                        </label>
-                        <?php echo $societe->identifiant; ?>
-                    </div>  
-                    <?php
-                    if ($societe->isTransaction()):
-                        if ($societe->getEtablissementPrincipal() && $societe->getEtablissementPrincipal()->getEmailTeledeclaration() && $societe->getMasterCompte()->isTeledeclarationActive()) :
-                            ?>
-                            <div class="form_ligne">  
-                                <label for="teledeclaration_email" class="label_liste">
-                                    Email :
-                                </label>
-                            <?php echo $societe->getEtablissementPrincipal()->getEmailTeledeclaration(); ?>
-                            </div>  
+            <div class="list-group-item">
+                <?php include_partial('compte/coordonneesVisualisation', array('compte' => $societe->getMasterCompte(), 'modification' => $modification, 'reduct_rights' => $reduct_rights)); ?>
+            </div>
+            <?php if ($societe->getMasterCompte()->exist('droits')): ?>
+            <div class="list-group-item">
+                    <?php if ($societe->getMasterCompte()->exist('droits') && $societe->getMasterCompte()->hasDroit(Roles::TELEDECLARATION)): ?>
+                    <p>
+                        <strong>Login de télédéclaration :</strong> <?php echo $societe->identifiant; ?>
+                        <?php if ($societe->getMasterCompte()->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_NOUVEAU) : ?>
+                            <span class="text-muted">(code de création : <?php echo str_replace('{TEXT}', '', $societe->getMasterCompte()->mot_de_passe); ?>)</span>
+                        <?php else: ?>
+                                <span class="text-muted">(code de création : Compte déjà crée</span>
                         <?php endif; ?>
-                    <?php else: ?>
-                        <?php if ($societe->getEmailTeledeclaration() && $societe->getMasterCompte()->isTeledeclarationActive()) :
-                            ?>
-                            <div class="form_ligne">  
-                                <label for="teledeclaration_email" class="label_liste">
-                                    Email :
-                                </label>
-                            <?php echo $societe->getEmailTeledeclaration(); ?>
-                            </div>  
+                        <?php if ($societe->isTransaction()):
+                            if ($societe->getEtablissementPrincipal() && $societe->getEtablissementPrincipal()->getEmailTeledeclaration() && $societe->getMasterCompte()->isTeledeclarationActive()) :
+                                ?>
+                                <li>Email de télédéclaration : <?php echo $societe->getEtablissementPrincipal()->getEmailTeledeclaration(); ?></li>
+                            <?php endif; ?>
+                        <?php else: ?>
+                            <?php if ($societe->getEmailTeledeclaration() && $societe->getMasterCompte()->isTeledeclarationActive()) :
+                                ?>
+                                <li>Email de télédéclaration : <?php echo $societe->getEmailTeledeclaration(); ?></li>
+                            <?php endif; ?>
                         <?php endif; ?>
+                        
+                    </ul>
+                    </p>
+                <?php endif; ?>
+                <p><?php if($societe->getMasterCompte()->exist('droits')): ?>
+                <strong>Droits :</strong>
+                    <?php foreach ($societe->getMasterCompte()->getDroits() as $droit) : ?>
+                        <button class="btn btn-sm btn-default"><?php echo $droit; ?></button>
+                    <?php endforeach; ?>
+                <?php endif; ?></p>
+            </div>
+            <?php endif; ?>
+            <div class="list-group-item">
+                <ul class="list-inline">
+                    <li><attr>N° SIRET :</attr> <?php echo $societe->siret; ?></li>
+                    <?php if ($societe->code_naf) : ?>
+                    <li><attr>Code NAF :</attr> <?php echo $societe->code_naf; ?></li>
                     <?php endif; ?>
-                    
-    <?php if ($societe->getMasterCompte()->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_NOUVEAU) : ?>
-                        <div class="form_ligne">  
-                            <label for="teledeclaration_mot_de_passe" class="label_liste">
-                                Code de création : 
-                            </label>
-                        <?php echo str_replace('{TEXT}', '', $societe->getMasterCompte()->mot_de_passe); ?>
-                        </div>    
-    <?php else: ?>
-                        <div class="form_ligne">  
-                            <label for="teledeclaration_email" class="label_liste">
-                                Code de création : 
-                            </label>
-                            Compte déjà crée
-                        </div>    
-    <?php endif; ?>
-                </div>
-            </div>                 
-    <?php endif; ?>
+                    <?php if ($societe->code_comptable_client) : ?>
+                    <li><attr>N° Compta Client :</attr> <?php echo $societe->code_comptable_client; ?></li>
+                    <?php endif; ?>
+                    <?php if ($societe->code_comptable_fournisseur) : ?>
+                    <li><attr>N° Compta Fournisseur :</attr> <?php echo $societe->code_comptable_fournisseur; ?></li>
+                    <?php endif; ?>
+                    <?php if ($societe->no_tva_intracommunautaire) : ?>
+                    <li>TVA intracom : <?php echo $societe->no_tva_intracommunautaire; ?>
+                    <?php endif; ?>
+                    <?php if ($societe->exist('type_fournisseur') && count($societe->type_fournisseur)) : ?>
+                    <li>Type de Fournisseur : <?php foreach ($societe->type_fournisseur as $type_fournisseur) : ?> <?php echo $type_fournisseur; ?>&nbsp;<?php endforeach; ?>
+                    <?php endif; ?>
+                </ul>
+                
+                <?php if ($societe->commentaire) : ?>  
+                    <strong>Commentaires :</strong> <?php echo $societe->commentaire;?>
+                <?php endif; ?>
+            </div> 
 
-
-        <?php if (count($etablissements)): ?>
+        </div>
+    </div>
+    <?php if (count($etablissements)): ?>
         <?php endif; ?>
         <?php
-        foreach ($etablissements as $etablissementId => $etb) :
-            include_partial('etablissement/visualisation', array('etablissement' => $etb->etablissement, 'ordre' => $etb->ordre, 'fromSociete' => true));
+        foreach ($etablissements as $etablissementId => $etb) : ?>
+    <div class="col-xs-12">
+        <div class="list-group">
+<?php include_partial('etablissement/visualisation', array('etablissement' => $etb->etablissement, 'ordre' => $etb->ordre, 'fromSociete' => true, 'modification' => $modification, 'reduct_rights' => $reduct_rights));
         endforeach;
         ?>
+        </div>
     </div>
-    <div class="col-xs-3">
-        <?php if ($modification || $reduct_rights) : ?>  
+    <!--<div class="col-xs-3">
+    <?php if ($modification || $reduct_rights) : ?>  
             <a href="<?php echo url_for('compte_ajout', array('identifiant' => $societe->identifiant)); ?>" class="btn btn-default btn-block">Nouvel interlocuteur</a>
             <?php if (!$reduct_rights && $societe->canHaveChais()) : ?>  
                 <a href="<?php echo url_for('etablissement_ajout', array('identifiant' => $societe->identifiant)); ?>" class="btn btn-default btn-block">Nouvel Etablissement</a>
             <?php endif; ?>
-        <?php endif; ?>  
+        <?php endif; ?> 
+            
         <?php include_component('societe', 'getInterlocuteursWithSuspendus'); ?>
-    </div>
+    </div>-->
 </section>
