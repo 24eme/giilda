41282a437a6f8097f78804b659020a5892bf3d59
Tangui Morlier <tangui@tangui.eu.org>
2016-01-11 20:34:30 +0100

    La suppression accessible au premier ecran

--- project/plugins/acVinVracPlugin/modules/vrac/templates/soussigneSuccess.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/soussigneSuccess.php
@@ -277,14 +277,14 @@ endif;
     <div class="row">
         <div class="col-xs-4 text-left">
             <?php if ($isTeledeclarationMode): ?>
-                <a tabindex="-1" href="<?php echo url_for('vrac_societe', array('identifiant' => $etablissementPrincipal->identifiant)); ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Annuler la saisie</a> 
+                <a tabindex="-1" href="<?php echo url_for('vrac_societe', array('identifiant' => $etablissementPrincipal->identifiant)); ?>" class="btn btn-default">Suspendre la saisie</a> 
             <?php else: ?>                        
-                <a tabindex="-1" href="<?php echo url_for('vrac'); ?>" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Annuler la saisie</a> 
+                <a tabindex="-1" href="<?php echo url_for('vrac'); ?>" class="btn btn-default">Suspendre la saisie</a> 
             <?php endif; ?>
         </div>
         <div class="col-xs-4 text-center">
-            <?php if ($isTeledeclarationMode && $vrac->isBrouillon()) : ?>
-                <a tabindex="-1" class="btn btn-default" href="<?php echo url_for('vrac_supprimer_brouillon', $vrac); ?>">Supprimer le brouillon</a>
+            <?php if ($vrac->isBrouillon()) : ?>
+                <a tabindex="-1" class="btn btn-danger" href="<?php echo url_for('vrac_supprimer_brouillon', $vrac); ?>"><span class="glyphicon glyphicon-trash"></span> Supprimer le brouillon</a>
             <?php endif; ?>  
         </div>
         <div class="col-xs-4 text-right">
