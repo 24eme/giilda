ea2b2998f1b9d391ed37fa173eff48c9b7d32b5b
actualys <actualys@vinsvaldeloire.pro>
2016-02-03 13:07:56 +0100

    Amélioration des exports

--- project/bin/exportSage.sh
+++ project/bin/exportSage.sh
@@ -2,23 +2,23 @@
 
 . bin/config.inc
 
-if test "$SAMBA_IP" && test "$SAMBA_SHARE" && test "$SAMBA_AUTH" && test "$SAMBA_SAGESUBDIR" && test "$SAMBA_SAGEFILE"; then
+if ! test "$1" && test "$SAMBA_IP" && test "$SAMBA_SHARE" && test "$SAMBA_AUTH" && test "$SAMBA_SAGEEXP_SUBDIR" && test "$SAMBA_SAGEFILE"; then
     cd $TMP
-    if smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; get $SAMBA_SAGEFILE" | grep NT_STATUS_OBJECT_NAME_NOT_FOUND ; then
+    if smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEIMP_SUBDIR ; get $SAMBA_SAGEFILE" | grep NT_STATUS_OBJECT_NAME_NOT_FOUND ; then
 	 echo "$SAMBA_SAGEFILE expected, not found" 1>&2
 	 echo -n $(date '+%d/%m/%Y %H:%M')" : " >> $TMP/$SAGE_EMAILFILE
 	 echo "ERREUR le fichier $SAMBA_SAGEFILE (export des infos societe SAGE) n'est pas present ($0)" >> $TMP/$SAGE_EMAILFILE
          echo "DIAGNOSTIQUE: SAGE ne semble pas avoir généré le fichier $SAMBA_SAGEFILE qui est nécessaire pour exporter les societes" >> $TMP/$SAGE_EMAILFILE
 	 exit 2
     fi
-    if test "$SAMBA_SAGEVERIFY" && ! smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; ls  $VINSIEXPORT" | grep "NT_STATUS_NO_SUCH_FILE" ; then
+    if test "$SAMBA_SAGEVERIFY" && ! smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEEXP_SUBDIR ; ls  $VINSIEXPORT" | grep "NT_STATUS_NO_SUCH_FILE" ; then
 	    echo "$VINSIEXPORT should not be present" 1>&2
         echo -n $(date '+%d/%m/%Y %H:%M')" : " >> $TMP/$SAGE_EMAILFILE
         echo "ERREUR le fichier $VINSIEXPORT (fichier précédemment exporté par VINSI) ne devrait pas être present ($0)" >> $TMP/$SAGE_EMAILFILE
 	echo "DIAGNOSTIQUE: un import SAGE précédent ne s'est pas bien déroulé (pas executé ou executé de manière partiel). Vérifiez SAGE, supprimer le fichier $VINSIEXPORT pour que la tache d'export s'execute correctement lors de son prochain passage" >> $TMP/$SAGE_EMAILFILE
 	exit 3
     fi
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; rm factures.csv ; rm societes.csv"
+    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEEXP_SUBDIR ; rm factures.csv ; rm societes.csv"
     cd -
 fi
 
@@ -40,14 +40,14 @@ cat $TMP/factures.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $T
 echo "#FIN" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 
 
-if test "$SAMBA_IP" && test "$SAMBA_SHARE" && test "$SAMBA_AUTH" && test "$SAMBA_SAGESUBDIR"; then
+if test "$SAMBA_IP" && test "$SAMBA_SHARE" && test "$SAMBA_AUTH" && test "$SAMBA_SAGEEXP_SUBDIR"; then
     cd $TMP
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put $VINSIEXPORT"
+    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEEXP_SUBDIR ; put $VINSIEXPORT"
     recode UTF8..ISO88591 societesWithSageData.csv
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put societesWithSageData.csv societes.csv"
+    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEEXP_SUBDIR ; put societesWithSageData.csv societes.csv"
     recode UTF8..ISO88591 factures.csv
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put factures.csv"
-    test "$SAMBA_SAGEVERIFY" && smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; rm $SAMBA_SAGEFILE"
+    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEEXP_SUBDIR ; put factures.csv"
+    test "$SAMBA_SAGEVERIFY" && smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGEIMP_SUBDIR ; rm $SAMBA_SAGEFILE"
     cd -
     echo -n $(date '+%d/%m/%Y %H:%M')" : " >> $TMP/$SAGE_EMAILFILE
     echo "$VINSIEXPORT a été mis à disposition avec succès" >> $TMP/$SAGE_EMAILFILE
