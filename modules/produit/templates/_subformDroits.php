31730a029bf8baaf519a226fdec5d2166528217b
Mathurin <mathurin.petit@gmail.com>
2016-02-05 16:08:36 +0100

    changement de la configuration

--- modules/produit/templates/_subformDroits.php
+++ modules/produit/templates/_subformDroits.php
@@ -1,10 +1,19 @@
-<div class="ligne_form" data-key="<?php echo $form->getName() ?>">
-    <span><?php echo $form['date']->renderError() ?></span>
-    <?php echo $form['date']->renderLabel() ?>
-    <br />
-    <?php echo $form['date']->render() ?>
+<div class="col-sm-12" data-key="<?php echo $form->getName() ?>">
+    <div class="row">
+        <div class="col-sm-6">
+            <?php echo $form['date']->renderError() ?>
+            <div class="form-group <?php if ($form['date']->hasError()): ?>has-error<?php endif; ?>" >
+                <?php echo $form['date']->render(array('class' => 'form-control', 'placeholder' => 'Date')); ?>
+                <span class="text-muted"><?php echo $form['date']->renderHelp() ?></span>
+            </div>
+        </div>
+        <div class="col-sm-6">
 
-    <span ><?php echo $form['taux']->renderError() ?></span>
-    <?php echo $form['taux']->renderLabel() ?><br /><?php echo $form['taux']->render() ?>
-
+            <?php echo $form['taux']->renderError() ?>
+            <div class="form-group <?php if ($form['taux']->hasError()): ?>has-error<?php endif; ?>" >
+                <?php echo $form['taux']->render(array('class' => 'form-control ', 'placeholder' => 'Taux')); ?>
+                <span class="text-muted"><?php echo $form['taux']->renderHelp() ?></span>
+            </div>
+        </div>
+    </div>
 </div>
