f8c7d22f77e88ea7d528d360467d802fcb89a1e8
Mathurin <mathurin.petit@gmail.com>
2016-02-05 11:36:36 +0100

    maj configuration + cvo et débridage de l'admin

--- project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationDroit.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Configuration/ConfigurationDroit.class.php
@@ -1,13 +1,29 @@
 <?php
+
 /**
  * Model for ConfigurationDroit
  *
  */
-
 class ConfigurationDroit extends BaseConfigurationDroit {
 
     public function getNoeud() {
 
         return $this->getParent()->getNoeud();
     }
+
+    public function getTaux() {
+        $taux = $this->_get('taux');
+        if (!is_string($taux)) {
+            return $taux;
+        }
+        $matched = array();
+        preg_match('/^([-+]{1})([0-9.]+)$/', $taux, $matched);
+        if (count($matched)) {
+            $sign = $matched[1];
+            $addTaux = $matched[2];
+            return array($sign,$addTaux);
+        } 
+        return $taux;
+    }
+
 }
