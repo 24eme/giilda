80b7d34e8b6d8b536873e72928293f9a246a8643
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-12 12:34:06 +0100

    Mise en forme générer un avoir

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_historiqueFactures.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_historiqueFactures.php
@@ -17,31 +17,14 @@ use_helper('Date');
                     <li class="list-group-item col-xs-12">
                         <span class="col-xs-1"><?php if ($f->isAvoir()): ?>AVOIR<?php else: ?>FACTURE<?php endif; ?></span>
                         <span class="col-xs-2">N° <?php echo $f->numero_piece_comptable ?></span>
-                        <span class="col-xs-4"><?php echo $date; ?></span>
+                        <span class="col-xs-4"><?php echo $date; ?> <?php if($f->isRedressee()): ?><span class="label label-warning">Redressée</span><?php endif;?></span>
                         <span class="col-xs-2 text-right"><?php echo echoFloat($f->total_ttc); ?> € TTC</span>
                         <span class="col-xs-3 text-right">
-                            <div class="btn-group text-left">                               
+                            <div class="btn-group text-left">
+                                <?php if ($f->isRedressable()): ?>
+                                    <a onclick="return confirm('Êtes-vous sur de vouloir créer annuler cette facture en créant un avoir ?');" href="<?php echo url_for("facture_avoir", array("id" => $f->_id)) ?>" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-repeat"></span>&nbsp;Défacturer</a>
+                                <?php endif; ?>                            
                                 <a href="<?php echo url_for("facture_pdf", array("id" => $f->_id)) ?>" class="btn btn-sm btn-default"><span class="glyphicon glyphicon-file"></span>&nbsp;Visualiser</a>
-                                <button type="button" class="btn btn-default btn-default-step btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Actions
-                                    <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <?php if (!$f->isPayee() && !$f->versement_comptable): ?>
-                                        <li><a href="<?php if (!$f->isPayee() && !$f->versement_comptable): ?><?php echo url_for("facture_edition", array("id" => $f->_id)) ?><?php endif; ?>">Modifier</a></li>
-                                    <?php else: ?>
-                                        <li class="disabled"><a href="">Modifier</a></li>
-                                    <?php endif; ?>
-                                    <?php if (!$f->isAvoir()): ?>
-                                        <li><a href="<?php echo url_for("facture_avoir", array("id" => $f->_id)) ?>">Créér un avoir <small>(à partir de cette facture)</small></a></li>
-                                    <?php endif; ?>
-                                    <?php if (!$f->isAvoir() && !$f->isPayee() && !$f->versement_comptable): ?>
-                                        <li><a onclick='return confirm("Étes vous sûr de vouloir regénérer la facture ?");' href="<?php echo url_for("facture_regenerate", array("id" => $f->_id)) ?>">Regénerer</a></li>
-                                    <?php elseif (!$f->isAvoir()): ?>
-                                        <li class="disabled"><a href="#">Regénerer</a></li>
-                                    <?php endif; ?>
-                                  
-                                </ul>
                             </div>
                         </span>
                     </li>
