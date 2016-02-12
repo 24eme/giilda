ff4b686ca82fbf733d99d425812b35585b9b4073
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-12 13:33:49 +0100

    * Mouvements facturé à l'import * Breadcrumb produit

--- modules/produit/templates/indexSuccess.php
+++ modules/produit/templates/indexSuccess.php
@@ -1,1 +1,7 @@
+<ol class="breadcrumb">
+    <li class="active">
+        <a href="<?php echo url_for("produits") ?>">Produits</a>
+    </li>
+</ol>
+
 <?php include_component('produit', 'index', array('id' => $id, 'rev' => $rev)); ?>
