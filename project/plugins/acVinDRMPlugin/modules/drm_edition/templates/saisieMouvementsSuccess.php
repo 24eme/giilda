ae4e5f7706092a10d4f04f62ac8ac85b4940f816
Mathurin <mathurin.petit@gmail.com>
2016-02-12 11:52:20 +0100

    maj oeil dans la saisie DRM

--- project/plugins/acVinDRMPlugin/modules/drm_edition/templates/saisieMouvementsSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm_edition/templates/saisieMouvementsSuccess.php
@@ -33,32 +33,36 @@
                         <?php include_component('drm_edition', 'produitForm', array('drm' => $drm, 'config' => $config)) ?>
                     </div>
                     <ul id="list-produits" class="list-group drm_fil_edition_produit">
-                    <?php foreach ($details as $produit) : ?>
-                        <?php if(!$produit->hasMovements()): continue; endif; ?> 
-                            <a data-hash="<?php echo $produit->getHash() ?>" <?php echo (!$produit->hasMovements())? 'style="display:none;"' : '' ?> class="list-group-item <?php echo ($produit->isEdited()) ? 'edited list-group-item-success' : '' ?>" href="#">
-                            <small><?php echo $produit->getLibelle("%format_libelle%"); ?></small>
+                        <?php foreach ($details as $produit) : ?>
+                            <?php if (!$produit->hasMovements()): continue;
+                            endif;
+                            ?> 
+                            <a style="position: relative;" data-hash="<?php echo $produit->getHash() ?>" <?php echo (!$produit->hasMovements()) ? 'style="display:none;"' : '' ?> class="list-group-item <?php echo ($produit->isEdited()) ? 'edited list-group-item-success' : '' ?>">
+                                
+                                    <small><?php echo $produit->getLibelle("%format_libelle%"); ?></small>
+                                <span style="position:absolute; right: 1px; top:12px;" class="btn btn-xs btn-link glyphicon glyphicon-eye-open "></span>
                             </a>
-                    <?php endforeach; ?>
+<?php endforeach; ?>
                     </ul>
                 </div>
             </div>
         </div>
         <div id="navigation_etapes" class="row">
             <div class="col-xs-4 text-left">
-                <a tabindex="-1" href="<?php echo ($isTeledeclarationMode)? url_for('drm_choix_produit', $drm) : url_for('drm_etablissement', $drm); ?>" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Etape précédente</a>
+                <a tabindex="-1" href="<?php echo ($isTeledeclarationMode) ? url_for('drm_choix_produit', $drm) : url_for('drm_etablissement', $drm); ?>" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span> Etape précédente</a>
             </div>
             <div class="col-xs-4 text-center">
                 <?php if (!$isTeledeclarationMode): ?>
                     <a href="<?php echo url_for('drm_etablissement', $drm->getEtablissement()); ?>" class="btn btn-default">Enregistrer en brouillon</a>
-                <?php endif; ?>
+<?php endif; ?>
                 <a class="btn btn-default" href="#drm_delete_popup">
                     <span>Supprimer la DRM</span>
                 </a> 
             </div>
             <div class="col-xs-4 text-right">
                 <form action="<?php echo url_for('drm_edition', $formValidation->getObject()) ?>" method="post">
-                <?php echo $formValidation->renderHiddenFields(); ?>
-                <button type="submit" class="btn btn-success">Étape suivante <span class="glyphicon glyphicon-chevron-right"></span></button>
+<?php echo $formValidation->renderHiddenFields(); ?>
+                    <button type="submit" class="btn btn-success">Étape suivante <span class="glyphicon glyphicon-chevron-right"></span></button>
                 </form>
             </div>
         </div>
