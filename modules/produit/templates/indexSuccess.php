a934753fb1d80fcfb5a333041d1f3f8ac9361576
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-16 17:34:31 +0100

    Fixe problème de calcul de cvo sur plusieurs date

--- modules/produit/templates/indexSuccess.php
+++ modules/produit/templates/indexSuccess.php
@@ -4,4 +4,4 @@
     </li>
 </ol>
 
-<?php include_component('produit', 'index', array('id' => $id, 'rev' => $rev)); ?>
+<?php include_component('produit', 'index', array('id' => $id, 'rev' => $rev, 'date' => $date));
