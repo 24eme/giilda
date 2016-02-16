e04a1930a65db7de1472db7022f57b351e58e1c1
Jb Lm <lemetayer.jb@gmail.com>
2016-02-16 18:02:27 +0100

    Vrac format identifiant

--- project/plugins/acVinVracPlugin/lib/form/VracCreationForm.class.php
+++ project/plugins/acVinVracPlugin/lib/form/VracCreationForm.class.php
@@ -7,26 +7,32 @@ class VracCreationForm extends BaseForm
     {
         $this->setWidget('annee', new bsWidgetFormInput());
         $this->setWidget('bordereau', new bsWidgetFormInput());
-        $dateRegexpOptions = array('required' => false, 'pattern' => "/^[0-9]{4}[a-zA-Z0-9]{0,1}$/");
+        $dateRegexpOptions = array('required' => false, 'pattern' => "/^[0-9]{4,5}$/");
         $dateRegexpErrors = array('required' => 'Champ obligatoire', 'invalid' => 'Année invalide (le format doit être aaaa(+x))');
         $this->setValidators(array(
         	'annee' => new sfValidatorRegex($dateRegexpOptions, $dateRegexpErrors),
             'bordereau' => new sfValidatorString(array('required' => false))
         ));
+        $this->validatorSchema->setPostValidator(new ValidatorVracCreation());
         $this->widgetSchema->setNameFormat('vrac_creation[%s]');
     }
     
     public function getIdVrac()
     {
     	if ($values = $this->getValues()) {
-    		$prefixe = null;
-    		$annee = ($values['annee'])? $values['annee'] : date('Y');
-    		if (preg_match('/^([0-9]{4})([a-zA-Z0-9]{1})$/', $annee, $m)) {
-    			$annee = $m[1];
-    			$prefixe = $m[2];
+    		if (!$values['annee']) {
+    			$annee = date('Y');
+    			$type = date('md');
+    		} else {
+    			$annee = $values['annee'];
+    			$type = 0;
+	    		if (preg_match('/^([0-9]{4})([0-9]{1})$/', $annee, $m)) {
+	    			$annee = $m[1];
+	    			$type = $m[2];
+	    		}
     		}
     		$bordereau = ($values['bordereau'])? $values['bordereau'] : null;
-    		return VracClient::getInstance()->buildNumeroContrat($annee, 0, $bordereau, $prefixe);
+    		return VracClient::getInstance()->buildNumeroContrat($annee, $type, 0, $bordereau);
     	}
     	return null;
     }
