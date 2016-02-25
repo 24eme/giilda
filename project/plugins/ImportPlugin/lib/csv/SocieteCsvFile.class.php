3a68f441fc73cb12f30b7686d1ce73291636b072
Vincent LAURENT <vince.laurent@gmail.com>
2016-02-25 18:21:13 +0100

    récupération du pays dans le csv

--- project/plugins/ImportPlugin/lib/csv/SocieteCsvFile.class.php
+++ project/plugins/ImportPlugin/lib/csv/SocieteCsvFile.class.php
@@ -86,18 +86,28 @@ class SocieteCsvFile extends CsvFile
                         $c->adresse_complementaire .= " ; ".trim(preg_replace('/,/', '', $line[self::CSV_ADRESSE_COMPLEMENTAIRE_2]));
                     }
                 }
+
+                if($line[self::CSV_CEDEX]) {
+                    $c->adresse_complementaire .= (echo ($c->adresse_complementaire) ?  " ; " : null).$line[self::CSV_CEDEX];
+                }
+
                 $c->code_postal = trim($line[self::CSV_CODE_POSTAL]);
 
                 if(!$c->code_postal) {
-                     echo "WARNING: le code postal est vide pour la société ".$s->_id."\n";
+                    echo "WARNING: le code postal est vide pour la société ".$s->_id."\n";
                 }
 
                 if($c->code_postal && !preg_match("/^[0-9]{5}$/", $c->code_postal)) {
-                     echo "WARNING: le code postal ne semple pas correct : ".$c->code_postal." pour la société ".$s->_id."\n";
+                    echo "WARNING: le code postal ne semple pas correct : ".$c->code_postal." pour la société ".$s->_id."\n";
                 }
 
                 $c->commune = $line[self::CSV_COMMUNE];
                 $c->insee = $line[self::CSV_INSEE];
+
+                if(!$c->commune) {
+                    echo "WARNING: la commune (".$c->insee.") est vide pour la société ".$s->_id.":".implode(";", $line)."\n";
+                }
+
                 $c->pays = 'FR';
                 $c->email = $this->formatAndVerifyEmail($line[self::CSV_EMAIL]);
                 $c->fax = $this->formatAndVerifyPhone($line[self::CSV_FAX]);
