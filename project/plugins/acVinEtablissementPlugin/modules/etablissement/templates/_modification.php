05ab584260b682ef7c5adceecafaab1f39d49995
Mathurin <mathurin.petit@gmail.com>
2016-02-26 12:57:02 +0100

    retrait du js dans le template pour gerer les enseigne

--- project/plugins/acVinEtablissementPlugin/modules/etablissement/templates/_modification.php
+++ project/plugins/acVinEtablissementPlugin/modules/etablissement/templates/_modification.php
@@ -18,25 +18,13 @@
             <?php echo $etablissementForm['region']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
             <div class="col-xs-8"><?php echo $etablissementForm['region']->render(); ?></div>
     </div>
-<?php /*
-             <div id="liaisons_list">
-            <?php
-            foreach ($etablissementForm['liaisons_operateurs'] as $liaisonForm) {
-                include_partial('itemLiaison', array('form' => $liaisonForm));
-            }
-            ?></div>
-            <div class="form-group">
-                <a class="btn_ajouter_ligne_template" data-container="#liaisons_list" data-template="#template_liaison" href="#">Ajouter une liaison</a>
-            </div>
-<?php */
-    if (!$etablissement->isCourtier()):
-        ?>
+<?php if (!$etablissement->isCourtier()): ?>
         <div class="form-group<?php if($etablissementForm['cvi']->hasError()): ?> has-error<?php endif; ?>">
             <?php echo $etablissementForm['cvi']->renderError(); ?>
             <?php echo $etablissementForm['cvi']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
             <div class="col-xs-8"><?php echo $etablissementForm['cvi']->render(); ?></div>
         </div> 
-        <?php endif; ?>
+<?php endif; ?>
      <?php if ($etablissement->isCourtier()): ?>
     <div class="form-group<?php if($etablissementForm['carte_pro']->hasError()): ?> has-error<?php endif; ?>">
         <?php echo $etablissementForm['carte_pro']->renderError(); ?>
@@ -56,61 +44,3 @@
     </div>
 
 </div>
-<?php include_partial('templateLiaisonItem', array('form' => $etablissementForm->getFormTemplate()));
-?>
-<script type="text/javascript">    
-    (function($)
-    {
-        $(document).ready(function()
-        {
-            initCollectionAddTemplate('.btn_ajouter_ligne_template', /var---nbItem---/g, callbackAddTemplate);
-            initCollectionDeleteTemplate();
-        });
-
-        var callbackAddTemplate = function(bloc)
-        {
-          
-        }
-
-       
-        var initCollectionAddTemplate = function(element, regexp_replace, callback)
-        {
-       
-            $(element).live('click', function()
-            {
-                
-        console.log($($(this).attr('data-template')).html());
-                var bloc_html = $($(this).attr('data-template')).html().replace(regexp_replace, UUID.generate());
-
-                try {
-                    var params = jQuery.parseJSON($(this).attr('data-template-params'));
-                } catch (err) {
-
-                }
-
-                for(key in params) {
-                    bloc_html = bloc_html.replace(new RegExp(key, "g"), params[key]);
-                }
-
-                var bloc = $($(this).attr('data-container')).before(bloc_html);
-
-                if(callback) {
-                    callback(bloc);
-                }
-                return false;
-            });
-        }
-   
-        var initCollectionDeleteTemplate = function()
-        {
-            $('.btn_supprimer_ligne_template').live('click',function()
-            {
-                var element = $(this).attr('data-container');
-                $(this).parent(element).remove();
-   
-                return false;
-            });
-        }
-    })(jQuery);
-    
-</script>
