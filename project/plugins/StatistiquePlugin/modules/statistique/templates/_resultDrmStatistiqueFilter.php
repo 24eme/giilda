245072083ac943849268e5f30c6496eca29499ec
Jb Lm <lemetayer.jb@gmail.com>
2016-02-26 17:54:39 +0100

    Amelioration des statistiques

--- project/plugins/StatistiquePlugin/modules/statistique/templates/_resultDrmStatistiqueFilter.php
+++ project/plugins/StatistiquePlugin/modules/statistique/templates/_resultDrmStatistiqueFilter.php
@@ -2,17 +2,15 @@
 <table class="table table-striped">
 	<thead>
 		<tr>
-			<th>Statut</th>
-			<th>Id.</th>
 			<th>Période</th>
 			<th style="width: 110px;">Date</th>
 			<th>Etablissement</th>
-			<th>Total début de mois (hl)</th>
-			<th>Total entrées (hl)</th>
-			<th>Total récolte (hl)</th>
-			<th>Total sorties (hl)</th>
-			<th>Total facturable (hl)</th>
-			<th>Total fin de mois (hl)</th>
+			<th>Total début</th>
+			<th>Total entrées</th>
+			<th>Total récolte</th>
+			<th>Total sorties</th>
+			<th>Total facturable</th>
+			<th>Total fin</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -24,20 +22,8 @@
 	$parameters['periode_version'] = ($item['doc']['version'])? $item['doc']['periode'].'-'.$item['doc']['version'] : $item['doc']['periode'];
 	?>
 		<tr>
-			<td>
-				<?php echo $item['doc']['valide']['statut']; ?>
-			</td>
-			<td>
-				<a href="<?php echo url_for('drm_visualisation', $parameters) ?>"><?php echo $hit->getId() ?></a>
-					<br />
-                    <?php if($item['doc']['teledeclare']): ?>
-                    Télédeclaré
-                    <?php endif; ?>
-			</td>
-			<td><?php echo $item['doc']['periode'] ?></td>
+			<td><a href="<?php echo url_for('drm_visualisation', $parameters) ?>"><?php echo $item['doc']['periode'] ?></a></td>
 				<td>
-					<?php echo ($item['doc']['valide']['date_signee'])? '<span class="glyphicon glyphicon-pencil" aria-hidden="true" title="Date de signature"></span> ' . strftime('%d/%m/%Y', strtotime($item['doc']['valide']['date_signee'])) : null; ?>
-					<br />
 					<?php echo ($item['doc']['valide']['date_saisie'])? '<span class="text-muted"><span class="glyphicon glyphicon-check" aria-hidden="true" title="Date de saisie (validation interpro)"></span> ' . strftime('%d/%m/%Y', strtotime($item['doc']['valide']['date_saisie'])) : null; ?>
 				</td>
 			<td><a href="<?php echo url_for('drm_etablissement', array('identifiant' => $item['doc']['identifiant'])) ?>"><?php echo $item['doc']['declarant']['nom'] ?></a></td>
