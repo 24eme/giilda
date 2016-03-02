a57cfa29c34d18fd8a972b8fc22151a9ffb414a2
Jb Lm <lemetayer.jb@gmail.com>
2016-03-02 19:23:12 +0100

    inte

--- project/plugins/acVinFacturePlugin/modules/facture/templates/monEspaceSuccess.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/monEspaceSuccess.php
@@ -3,8 +3,8 @@ use_helper('Float');
 ?>
 
 <ol class="breadcrumb">
-    <li><a href="<?php echo url_for('facture') ?>">Factures</a></li>
-    <li><a href="<?php echo url_for('facture_societe', $societe) ?>" class="active"><?php echo $societe->raison_sociale ?> (<?php echo $societe->identifiant ?>)</a></li>
+    <li class="visited"><a href="<?php echo url_for('facture') ?>">Factures</a></li>
+    <li class="active"><a href="<?php echo url_for('facture_societe', $societe) ?>" class="active"><?php echo $societe->raison_sociale ?> (<?php echo $societe->identifiant ?>)</a></li>
 </ol>
 
 <div class="row">
