822dc870bf3788179630e030c129f657a2632f20
Vincent LAURENT <vince.laurent@gmail.com>
2016-03-03 14:13:53 +0100

    Tache pour save un doc

---
+++ project/plugins/acCouchdbPlugin/lib/task/document/acCouchdbDocumentSaveTask.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class acCouchdbDocumentGetTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    $this->addArguments(array(
+       new sfCommandArgument('doc_id', sfCommandArgument::REQUIRED, 'ID du document'),
+    ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'document';
+    $this->name             = 'save';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [maintenanceCompteStatut|INFO] task does things.
+Call it with:
+
+  [php symfony maintenanceCompteStatut|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    $doc = acCouchdbManager::getClient()->find($arguments['doc_id']);
+    $doc->save();    
+  }
+} 
