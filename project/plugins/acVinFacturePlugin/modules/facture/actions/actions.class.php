cf783c6d8866eeb1c4c55c96458e281f4e419328
Mathurin <mathurin.petit@gmail.com>
2016-03-10 15:10:27 +0100

    clean action de facture

--- project/plugins/acVinFacturePlugin/modules/facture/actions/actions.class.php
+++ project/plugins/acVinFacturePlugin/modules/facture/actions/actions.class.php
@@ -167,7 +167,7 @@ class factureActions extends sfActions {
         $this->societe = $this->getRoute()->getSociete();
         $this->values = array();
 
-        $this->form = new FactureGenerationForm($default);
+        $this->form = new FactureGenerationForm();
         if (!$request->isMethod(sfWebRequest::POST)) {
 
             return sfView::SUCCESS;
@@ -184,7 +184,7 @@ class factureActions extends sfActions {
         $mouvementsBySoc = array($this->societe->identifiant => FactureClient::getInstance()->getFacturationForSociete($this->societe));
         
         $mouvementsBySocFiltered = FactureClient::getInstance()->filterWithParameters($mouvementsBySoc, $filters_parameters);           
-        
+      
         if ($mouvementsBySocFiltered) {
             $generation = FactureClient::getInstance()->createFacturesBySoc($mouvementsBySocFiltered, $filters_parameters['modele'], $filters_parameters['date_mouvement'], $filters_parameters['message_communication']);
             $generation->save();
@@ -199,27 +199,6 @@ class factureActions extends sfActions {
         }
         $this->redirect('facture_societe', array('identifiant' => $this->facture->identifiant));
     }
-
-//    public function executeGenerer(sfWebRequest $request) {
-//        $parameters = $request->getParameter('facture_generation');
-//        $date_facturation = (!isset($parameters['date_facturation'])) ? null : DATE::getIsoDateFromFrenchDate($parameters['date_facturation']);
-//        $message_communication = (!isset($parameters['message_communication'])) ? null : $parameters['message_communication'];
-//        $parameters['date_mouvement'] = (isset($parameters['date_mouvement']) && $parameters['date_mouvement'] != '') ? $parameters['date_mouvement'] : $date_facturation;
-//        if (!isset($parameters['type_document']) || !$parameters['type_document'] || $parameters['type_document'] == FactureGenerationMasseForm::TYPE_DOCUMENT_TOUS) {
-//            unset($parameters['type_document']);
-//        }
-//        
-//        $this->societe = $this->getRoute()->getSociete();
-//
-//        $mouvementsBySoc = array($this->societe->identifiant => FactureClient::getInstance()->getFacturationForSociete($this->societe));
-//        // $mouvementsBySoc = FactureClient::getInstance()->filterWithParameters($mouvementsBySoc, $parameters);
-//
-//        if ($mouvementsBySoc) {
-//            $generation = FactureClient::getInstance()->createFacturesBySoc($mouvementsBySoc, $date_facturation, $message_communication);
-//            $generation->save();
-//        }
-//        $this->redirect('facture_societe', $this->societe);
-//    }
 
     public function executeRedirect(sfWebRequest $request) {
         $iddoc = $request->getParameter('iddocument');
