cc7baa67ef40a30a5e66a8d583013d7c648c88aa
Vincent LAURENT <vince.laurent@gmail.com>
2016-03-17 18:03:12 +0100

    Famille modifiable dans la société

--- project/plugins/acVinSocietePlugin/modules/societe/templates/_societeModification.php
+++ project/plugins/acVinSocietePlugin/modules/societe/templates/_societeModification.php
@@ -3,9 +3,10 @@ echo $societeForm->renderHiddenFields();
 echo $societeForm->renderGlobalErrors();
 ?>
 <div class="panel-body">
-    <div class="form-group">
-                <label class="col-xs-4 control-label" for="type_societe">Type de la société</label>
-                <span class="col-xs-8 text-left"><?php echo $societeForm->getObject()->type_societe; ?></span>
+    <div class="form-group<?php if ($societeForm['type_societe']->hasError()): ?> has-error<?php endif; ?>">
+        <?php echo $societeForm['type_societe']->renderError(); ?>
+        <?php echo $societeForm['type_societe']->renderLabel("Type de la société", array('class' => 'col-xs-4 control-label')); ?>
+        <div class="col-xs-8"><?php echo $societeForm['type_societe']->render(); ?></div>
     </div>
     <div class="form-group<?php if($societeForm['raison_sociale']->hasError()): ?> has-error<?php endif; ?>">
         <?php echo $societeForm['raison_sociale']->renderError(); ?>
@@ -26,7 +27,7 @@ echo $societeForm->renderGlobalErrors();
     <?php endif; ?>
     <?php if (isset($societeForm['type_numero_compte_fournisseur']) || isset($societeForm['type_numero_compte_client'])): ?>
     <div class="form-group<?php if($societeForm['type_numero_compte_client']->hasError()): ?> has-error<?php endif; ?>">
-<?php echo $societeForm['type_numero_compte_client']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+        <?php echo $societeForm['type_numero_compte_client']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
             <?php if ($societeForm->getObject()->isOperateur()) : ?>
                 <?php echo $societeForm['type_numero_compte_client']->renderError(); ?>
                 <div class="col-xs-8"><?php echo $societeForm['type_numero_compte_client']->render(); ?></div>
@@ -37,34 +38,27 @@ echo $societeForm->renderGlobalErrors();
             <?php endif; ?>
     </div>
     <?php endif; ?>
-              
-        <div class="form-group<?php if($societeForm['siret']->hasError()): ?> has-error<?php endif; ?>">
-<?php echo $societeForm['siret']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-				<div class="col-xs-8"><?php echo $societeForm['siret']->render(); ?></div>
-				<?php echo $societeForm['siret']->renderError(); ?>
-			</div>
-			<div class="form-group<?php if($societeForm['code_naf']->hasError()): ?> has-error<?php endif; ?>">
-                  <?php echo $societeForm['code_naf']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-				<div class="col-xs-8"><?php echo $societeForm['code_naf']->render(); ?></div>
-				<?php echo $societeForm['code_naf']->renderError(); ?>
-        </div> 
-        <div class="form-group<?php if($societeForm['no_tva_intracommunautaire']->hasError()): ?> has-error<?php endif; ?>">
-<?php echo $societeForm['no_tva_intracommunautaire']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
-            <div class="col-xs-8"><?php echo $societeForm['no_tva_intracommunautaire']->render(); ?></div>
-            <?php echo $societeForm['no_tva_intracommunautaire']->renderError(); ?>
-        </div> 
-    
-        <div id="enseignes_list">           
-<!--            <div class="form-group">
-                <a class="btn_ajouter_ligne_template" data-container="#enseignes_list" data-template="#template_enseigne" href="#">Ajouter une enseigne</a>
-            </div>-->
-        </div>
+
+    <div class="form-group<?php if($societeForm['siret']->hasError()): ?> has-error<?php endif; ?>">
+        <?php echo $societeForm['siret']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+		<div class="col-xs-8"><?php echo $societeForm['siret']->render(); ?></div>
+		<?php echo $societeForm['siret']->renderError(); ?>
+	</div>
+	<div class="form-group<?php if($societeForm['code_naf']->hasError()): ?> has-error<?php endif; ?>">
+        <?php echo $societeForm['code_naf']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+		<div class="col-xs-8"><?php echo $societeForm['code_naf']->render(); ?></div>
+		<?php echo $societeForm['code_naf']->renderError(); ?>
+    </div>
+    <div class="form-group<?php if($societeForm['no_tva_intracommunautaire']->hasError()): ?> has-error<?php endif; ?>">
+        <?php echo $societeForm['no_tva_intracommunautaire']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+        <div class="col-xs-8"><?php echo $societeForm['no_tva_intracommunautaire']->render(); ?></div>
+        <?php echo $societeForm['no_tva_intracommunautaire']->renderError(); ?>
+    </div>
     <div class="form-group<?php if($societeForm['commentaire']->hasError()): ?> has-error<?php endif; ?>">
-<?php echo $societeForm['commentaire']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+        <?php echo $societeForm['commentaire']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
         <div class="col-xs-8"><?php echo $societeForm['commentaire']->render(); ?></div>
         <?php echo $societeForm['commentaire']->renderError(); ?>
     </div>
 </div>
 <?php // include_partial('templateEnseigneItem', array('form' => $societeForm->getFormTemplate()));
 ?>
-
