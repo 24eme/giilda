2628f2dbcbdfef2aa7228de11b7e48b9662c5648
Vincent LAURENT <vince.laurent@gmail.com>
2016-03-18 19:16:48 +0100

    Mise en page

--- modules/produit/templates/indexSuccess.php
+++ modules/produit/templates/indexSuccess.php
@@ -1,14 +1,14 @@
 <ol class="breadcrumb">
-    <li class="active">
-        <a href="<?php echo url_for("produits") ?>">Produits</a>
-    </li>
+    <li><a href="<?php echo url_for("produits") ?>">Produits</a></li>
+    <li class="active"><a href="<?php echo url_for("produits", array('date' => $date)) ?>"><?php echo $date ?></a></li>
+    <li class="text-muted"><?php echo $config->_id ?><small>@<?php echo $config->_rev ?></small></li>
 </ol>
 
 <?php slot('global_css_class', 'no_right_col')?>
 
 <a href="<?php echo url_for('produit_nouveau') ?>" class="btn btn-default pull-right"><span class="glyphicon glyphicon-plus"></span> Ajouter un produit</a>
 
-<h2>Produits <span class="text-muted"> / <?php echo $config->_id ?><small style="font-size: 10px;">@<?php echo $config->_rev ?></small></span></h2>
+<h2>Produits</h2>
 
 <table class="table table-condensed table-striped table-bordered">
     <thead>
