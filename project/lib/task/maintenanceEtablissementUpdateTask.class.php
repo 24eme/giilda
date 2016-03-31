8d81ca336be04f9b3b0407ad01d8b00ea071c57a
Mathurin <mathurin.petit@gmail.com>
2016-03-31 16:22:48 +0200

    ajout d'une tache pour save tous les etb et gestion du champs interpro dans societe

---
+++ project/lib/task/maintenanceEtablissementUpdateTask.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class maintenanceCompteStatutTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    // // add your own arguments here
+    // $this->addArguments(array(
+    //   new sfCommandArgument('my_arg', sfCommandArgument::REQUIRED, 'My argument'),
+    // ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
+      // add your own options here
+    ));
+
+    $this->namespace        = 'maintenance';
+    $this->name             = 'compte-statut';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [maintenanceCompteStatut|INFO] task does things.
+Call it with:
+
+  [php symfony maintenanceCompteStatut|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+    
+    $rows = CompteAllView::getInstance()->findByInterproVIEW('INTERPRO-declaration');
+
+    foreach($rows as $row) {
+      $compte = CompteClient::getInstance()->find($row->id);
+
+      $societe = SocieteClient::getInstance()->find($compte->id_societe);
+
+      $compte->add('statut', $societe->statut);
+
+      $compte->save();
+
+      echo $compte->_id."\n";
+    }
+  }
+}
