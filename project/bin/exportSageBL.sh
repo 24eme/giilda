563763de31607671251e86d05846d6ef351ff141
Tangui Morlier <tangui@tangui.eu.org>
2016-03-31 18:31:27 +0200

    Derniers bugs de l'export SAGE ?

--- project/bin/exportSageBL.sh
+++ project/bin/exportSageBL.sh
@@ -26,7 +26,7 @@ php symfony export:societe > $TMP/societes.csv
 sort -t ';' -k 1,1 $TMP/societes.csv > $TMP/societes.sorted.csv
 sort -t ';' -k 1,1 $TMP/$SAMBA_SAGEFILE | iconv -f ISO88591 -t UTF8 | sed 's/\([^;a-z0-9éèçàïëê]*\)$/;\1/i' > $TMP/InfosClientsSage.sorted.txt
 join -t ';' -1 1 -2 1 -a 1 $TMP/societes.sorted.csv $TMP/InfosClientsSage.sorted.txt > $TMP/societesWithSageData.csv
-cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/societes.sage
+cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGEv9.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/societes.sage
 
 php symfony export:facture --horstaxe=true > $TMP/factures.csv
 cat $TMP/factures.csv | perl bin/convertExportFacture2BLSAGE.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.sage
