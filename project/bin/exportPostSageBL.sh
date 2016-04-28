a47b994b6a524a46139ea8c5b3e4712f3391599e
Tangui Morlier <tangui@tangui.eu.org>
2016-04-28 19:24:46 +0200

    Exclude non factureid

--- project/bin/exportPostSageBL.sh
+++ project/bin/exportPostSageBL.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat $1 | awk -F ';' '{print $14}' | sort | uniq | while read FACTUREID; do
+cat $1 | awk -F ';' '{print $14}' | sort | uniq | grep 2[0-9][0-9][0-9] | while read FACTUREID; do
     php symfony facture:setexported $FACTUREID;
 done
 
