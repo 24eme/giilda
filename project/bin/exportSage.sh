98925c20b9c3fa906022989551a3648e39dcc157
Tangui Morlier <tangui@tangui.eu.org>
2016-06-08 17:42:33 +0200

    ajout de l'application en parametre

--- project/bin/exportSage.sh
+++ project/bin/exportSage.sh
@@ -2,14 +2,14 @@
 
 . bin/config.inc
 
-php symfony export:societe --env=$SYMFONYENV > $TMP/societes.csv
+php symfony export:societe --application=declaration --env=$SYMFONYENV > $TMP/societes.csv
 
 sort -t ';' -k 1,1 $TMP/societes.csv > $TMP/societes.sorted.csv
 sort -t ';' -k 1,1 $TMP/$SAMBA_SAGEFILE | iconv -f ISO88591 -t UTF8 | sed 's/\([^;a-z0-9éèçàïëê]*\)$/;\1/i' > $TMP/InfosClientsSage.sorted.txt
 join -t ';' -1 1 -2 1 -a 1 $TMP/societes.sorted.csv $TMP/InfosClientsSage.sorted.txt > $TMP/societesWithSageData.csv
 cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl > $TMP/societes.sage
 
-php symfony export:facture --env=$SYMFONYENV > $TMP/factures.csv
+php symfony export:facture --application=declaration --env=$SYMFONYENV > $TMP/factures.csv
 cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.txt
 
 echo -n > $TMP/$VINSIEXPORT
