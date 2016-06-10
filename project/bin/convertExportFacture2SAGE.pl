2b7af0d4fea2c7cdbdd2a2e143ea3f88f323730d
Tangui Morlier <tangui@tangui.eu.org>
2016-06-10 19:38:22 +0200

    Modification des éléments SAGE (troncage des champ,s ....) pour IVSO

--- project/bin/convertExportFacture2SAGE.pl
+++ project/bin/convertExportFacture2SAGE.pl
@@ -1,12 +1,16 @@
 #!/usr/bin/perl
 
+use Encode;
+
 $verbose = shift;
 
 while(<STDIN>) {
 	chomp;
 	@field = split/;/ ;
-	next if ($field[0] ne 'VEN' && $field[0] !~ /^[0-2]+$/);
+        next if ($field[0] ne 'VEN' && $field[0] !~ /^[02]+$/);
 	next if (!$field[10]); #si montant à 0, l'ignorer
+        $field[10] = sprintf("%.2f", $field[10]);
+	$field[10] =~ s/\./,/;
 	print "Ecriture générale;" if ($verbose);
 	print "#MECG\n";
 	print "code journal;" if ($verbose);
@@ -33,18 +37,18 @@ while(<STDIN>) {
         print "numero compte tiers contre partie;" if ($verbose);
         print "\n";
         print "intitule;" if ($verbose);
-        print substr($field[4], 0, 35)."\n";
+        print encode_utf8(substr(decode_utf8($field[4]), 0, 35))."\n";
         print "numero reglement;" if ($verbose);
         print "\n";
         print "date echeance;" if ($verbose);
 	$field[8] =~ s/\d{2}(\d{2})-(\d{2})-(\d{2})/${3}${2}${1}/;
         print $field[8]."\n";
         print "partie;" if ($verbose);
-        print "\n";
+        print "0,000000\n";
         print "quantite;" if ($verbose);
         print "\n";
         print "numero devis;" if ($verbose);
-        print "\n";
+        print "0\n";
         print "sens (credit = 1 / debit = 0);" if ($verbose);
         if ($field[9] eq 'CREDIT') {
 		print "1\n"; 
@@ -60,7 +64,7 @@ while(<STDIN>) {
         print "numero pointage;" if ($verbose);
         print "\n";
         print "numero rappel;" if ($verbose);
-        print "\n";
+        print "0\n";
         print "type niveau;" if ($verbose);
         print "0\n";
         print "type revision;" if ($verbose);
@@ -68,7 +72,11 @@ while(<STDIN>) {
         print "montant devise;" if ($verbose);
         print "\n";
         print "code taxe;" if ($verbose);
-        print "\n";
+        if ($field[9] eq 'CREDIT') {
+		print "C03\n";
+	}else{
+	        print "\n";
+	}
         print "norme;" if ($verbose);
         print "0\n";
         print "provenance;" if ($verbose);
@@ -79,17 +87,22 @@ while(<STDIN>) {
         print "\n";
         print "date de rapprochement;" if ($verbose);
         print "\n";
+        print "????;" if ($verbose);
 	print "\n"; # ??????????
         print "reference;" if ($verbose);
         print $field[12]."\n";
         print "status reglement;" if ($verbose);
-        print "\n";
+        print "0\n";
         print "montant refle;" if ($verbose);
-        print "\n";
+        print "0,00\n";
         print "date dernier reglement;" if ($verbose);
         print "\n";
         print "date operation;" if ($verbose);
         print "\n";
+	print "?;" if ($verbose);
+	print "0\n";
+        print "?;" if ($verbose);
+	print "\n";
 	if ($field[7]) {
 	        print "Ecriture Analytique;" if ($verbose);
 	        print "#MECA\n";
@@ -100,6 +113,14 @@ while(<STDIN>) {
 	        print "montant;" if ($verbose);
 	        print $field[10]."\n";
 	        print "quantite;" if ($verbose);
-	        print "0.00\n";
+	        print "0,00\n";
+	}
+	if ($field[4] !~ /TVA/) {
+		print "MIVA;" if ($verbose);
+	        print "#MIVA\n";
+		print "empty;" if ($verbose);
+	        print "\n";
+		print "tiers;" if ($verbose);
+		print encode_utf8(substr(decode_utf8($field[15]), 0, 30))."\n";
 	}
 }
