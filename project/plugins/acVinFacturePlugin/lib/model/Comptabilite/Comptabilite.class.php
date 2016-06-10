73b77fe94d15f882a35ddf45f923ce75cabd09cc
Mathurin Petit <mathurin.petit@gmail.com>
2016-06-10 13:11:38 +0200

    correctif pour l'export sage de l'enregistrement des code compta

--- project/plugins/acVinFacturePlugin/lib/model/Comptabilite/Comptabilite.class.php
+++ project/plugins/acVinFacturePlugin/lib/model/Comptabilite/Comptabilite.class.php
@@ -6,12 +6,12 @@
 
 class Comptabilite extends BaseComptabilite {
 
-    
+
     public function getAllIdentifiantsAnalytiquesArrayForCompta() {
         $identifiant_analytique= array();
         foreach ($this->identifiants_analytiques as $key => $identifiant_analytique) {
-            $results[$key] = $identifiant_analytique->identifiant_analytique_libelle_compta;
-        } 
+            $results[$identifiant_analytique->identifiant_analytique_numero_compte.'_'.$identifiant_analytique->identifiant_analytique] = $identifiant_analytique->identifiant_analytique_libelle_compta;
+        }
         return $results;
     }
 }
