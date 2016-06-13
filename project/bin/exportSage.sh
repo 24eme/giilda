11ff84185802ea175387232d6914e36e606e73aa
Tangui Morlier <tangui@tangui.eu.org>
2016-06-13 17:17:49 +0200

    Convertion du CSV pour avoir les CVO chapeau/sections

--- project/bin/exportSage.sh
+++ project/bin/exportSage.sh
@@ -9,8 +9,9 @@ sort -t ';' -k 1,1 $TMP/$SAMBA_SAGEFILE | iconv -f ISO88591 -t UTF8 | sed 's/\([
 join -t ';' -1 1 -2 1 -a 1 $TMP/societes.sorted.csv $TMP/InfosClientsSage.sorted.txt > $TMP/societesWithSageData.csv
 cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl > $TMP/societes.sage
 
-php symfony export:facture --application=declaration --env=$SYMFONYENV > $TMP/factures.csv
-cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.txt
+php symfony export:csv-configuration --application=declaration --env=$SYMFONYENV > $TMP/produits.csv
+php symfony export:facture --application=declaration --env=$SYMFONYENV | perl bin/preconvertExportFactureChapeau.pl $TMP/produits.csv > $TMP/factures.csv
+cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl $TMP/produits.csv | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.txt
 
 echo -n > $TMP/$VINSIEXPORT
 #echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
