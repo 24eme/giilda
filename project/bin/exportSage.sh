ae00efbe2edb17286fc82caa4d6bc8015dd182f4
Tangui Morlier <tangui@tangui.eu.org>
2016-07-28 15:04:00 +0200

    IVSO branch Version

--- project/bin/exportSage.sh
+++ project/bin/exportSage.sh
@@ -5,35 +5,37 @@
 php symfony export:societe --application=declaration --env=$SYMFONYENV > $TMP/societes.csv
 
 sort -t ';' -k 1,1 $TMP/societes.csv > $TMP/societes.sorted.csv
+touch $TMP/$SAMBA_SAGEFILE
 sort -t ';' -k 1,1 $TMP/$SAMBA_SAGEFILE | iconv -f ISO88591 -t UTF8 | sed 's/\([^;a-z0-9éèçàïëê]*\)$/;\1/i' > $TMP/InfosClientsSage.sorted.txt
 join -t ';' -1 1 -2 1 -a 1 $TMP/societes.sorted.csv $TMP/InfosClientsSage.sorted.txt > $TMP/societesWithSageData.csv
-cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl > $TMP/societes.sage
+cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/'  > $TMP/societes.txt
 
 php symfony export:csv-configuration --application=declaration --env=$SYMFONYENV > $TMP/produits.csv
-php symfony export:facture --application=declaration --env=$SYMFONYENV | perl bin/preconvertExportFactureChapeau.pl $TMP/produits.csv > $TMP/factures.csv
-cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl $TMP/produits.csv > $TMP/factures.sage
+php symfony export:facture --application=declaration --env=$SYMFONYENV | perl bin/preconvertExportFactureChapeau.pl $TMP/produits.csv data/export/ivso_comptes2analytiques.csv > $TMP/factures.csv
+cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl $TMP/produits.csv | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.txt
 
 echo -n > $TMP/$VINSIEXPORT
-#echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
+echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 echo "#VER 14" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
-cat $TMP/societes.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
-cat $TMP/factures.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
+cat $TMP/societes.txt >> $TMP/$VINSIEXPORT
+cat $TMP/factures.txt >> $TMP/$VINSIEXPORT
 echo "#FIN" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 
-echo "#FLG 001" | sed 's/$/\r/'  > $TMP/factures.withHeader.sage
-echo "#VER 18"  | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
-echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
-cat $TMP/factures.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
-echo "#FIN" | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
-mv $TMP/factures.withHeader.sage $TMP/factures.sage
+echo -n > $TMP/factures.sage
+echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/factures.sage
+echo "#VER 18" | sed 's/$/\r/' >> $TMP/factures.sage
+echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/factures.sage
+cat $TMP/factures.txt >> $TMP/factures.sage
+echo "#FIN" | sed 's/$/\r/' >> $TMP/factures.sage
 
-echo "#FLG 001" | sed 's/$/\r/'  > $TMP/socites.withHeader.sage
-echo "#VER 14"  | sed 's/$/\r/' >> $TMP/socites.withHeader.sage
-echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/socites.withHeader.sage
-cat $TMP/societes.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/societes.withHeader.sage
-echo "#FIN" | sed 's/$/\r/' >> $TMP/societes.withHeader.sage
-mv $TMP/societes.withHeader.sage $TMP/societes.sage
+
+echo -n > $TMP/societes.sage
+echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/societes.sage
+echo "#VER 14" | sed 's/$/\r/' >> $TMP/societes.sage
+echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/societes.sage
+cat $TMP/societes.txt >> $TMP/societes.sage
+echo "#FIN" | sed 's/$/\r/' >> $TMP/societes.sage
 
 echo "$TMP/societes.sage|societes.sage|Export SAGE des sociétés"
 echo "$TMP/societes.sorted.csv|societes.csv|Export CSV des sociétés"
