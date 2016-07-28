7cc2f3e05cc384561b62139abb02224e38b32bd1
Tangui Morlier <tangui@tangui.eu.org>
2016-07-28 14:53:35 +0200

    Export compatible avec le generationShell

--- project/bin/exportSage.sh
+++ project/bin/exportSage.sh
@@ -11,7 +11,7 @@ cat $TMP/societesWithSageData.csv | perl bin/convertExportSociete2SAGE.pl > $TMP
 
 php symfony export:csv-configuration --application=declaration --env=$SYMFONYENV > $TMP/produits.csv
 php symfony export:facture --application=declaration --env=$SYMFONYENV | perl bin/preconvertExportFactureChapeau.pl $TMP/produits.csv > $TMP/factures.csv
-cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl $TMP/produits.csv | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' > $TMP/factures.txt
+cat $TMP/factures.csv | perl bin/convertExportFacture2SAGE.pl $TMP/produits.csv > $TMP/factures.sage
 
 echo -n > $TMP/$VINSIEXPORT
 #echo  "#FLG 001" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
@@ -21,18 +21,22 @@ cat $TMP/societes.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $T
 cat $TMP/factures.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 echo "#FIN" | sed 's/$/\r/' >> $TMP/$VINSIEXPORT
 
+echo "#FLG 001" | sed 's/$/\r/'  > $TMP/factures.withHeader.sage
+echo "#VER 18"  | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
+echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
+cat $TMP/factures.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
+echo "#FIN" | sed 's/$/\r/' >> $TMP/factures.withHeader.sage
+mv $TMP/factures.withHeader.sage $TMP/factures.sage
 
-if test "$SAMBA_IP" && test "$SAMBA_SHARE" && test "$SAMBA_AUTH" && test "$SAMBA_SAGESUBDIR"; then
-    cd $TMP
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put $VINSIEXPORT"
-    recode UTF8..ISO88591 societesWithSageData.csv
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put societesWithSageData.csv societes.csv"
-    recode UTF8..ISO88591 factures.csv
-    smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; put factures.csv"
-    test "$SAMBA_SAGEVERIFY" && smbclient //$SAMBA_IP/$SAMBA_SHARE -A $SAMBA_AUTH -c "cd $SAMBA_SAGESUBDIR ; rm $SAMBA_SAGEFILE"
-    cd -
-    echo -n $(date '+%d/%m/%Y %H:%M')" : " >> $TMP/$SAGE_EMAILFILE
-    echo "$VINSIEXPORT a été mis à disposition avec succès" >> $TMP/$SAGE_EMAILFILE
-else
-    cat $TMP/$VINSIEXPORT
-fi
+echo "#FLG 001" | sed 's/$/\r/'  > $TMP/socites.withHeader.sage
+echo "#VER 14"  | sed 's/$/\r/' >> $TMP/socites.withHeader.sage
+echo "#DEV EUR" | sed 's/$/\r/' >> $TMP/socites.withHeader.sage
+cat $TMP/societes.sage | iconv -f UTF8 -t IBM437//TRANSLIT | sed 's/$/\r/' >> $TMP/societes.withHeader.sage
+echo "#FIN" | sed 's/$/\r/' >> $TMP/societes.withHeader.sage
+mv $TMP/societes.withHeader.sage $TMP/societes.sage
+
+echo "$TMP/societes.sage|societes.sage|Export SAGE des sociétés"
+echo "$TMP/societes.sorted.csv|societes.csv|Export CSV des sociétés"
+echo "$TMP/factures.sage|factures.sage|Export SAGE des factures"
+echo "$TMP/factures.csv|factures.csv|Export CSV des factures"
+
