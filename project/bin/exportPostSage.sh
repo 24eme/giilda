22ed1d62551429eb10d1724ea3686fe44e065549
Tangui Morlier <tangui@tangui.eu.org>
2016-08-01 16:37:13 +0200

    Ajout de l'envirronement d'execution

--- project/bin/exportPostSage.sh
+++ project/bin/exportPostSage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+. bin/config.inc
+
 cat $1 | awk -F ';' '{print $14}' | sort | uniq | grep 2[0-9][0-9][0-9] | while read FACTUREID; do
-    php symfony facture:setexported $FACTUREID;
+    php symfony facture:setexported --application=declaration --env=$SYMFONYENV  $FACTUREID;
 done
 
 cat $1 >> web/export/bi/export_bi_factures.csv
