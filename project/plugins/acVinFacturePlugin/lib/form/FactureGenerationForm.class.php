caf5011dc0334ffd7e226d8da853b64bfdb1eadd
Vincent LAURENT <vince.laurent@gmail.com>
2016-09-02 16:10:43 +0200

    Ajout de la date de prise en compte des mouvements dans le formulaire de génération de facture

--- project/plugins/acVinFacturePlugin/lib/form/FactureGenerationForm.class.php
+++ project/plugins/acVinFacturePlugin/lib/form/FactureGenerationForm.class.php
@@ -7,6 +7,7 @@ class FactureGenerationForm extends BaseForm {
 
     public function __construct($defaults = array(), $options = array(), $CSRFSecret = null) {
         $defaults['date_facturation'] = date('d/m/Y');
+        $defaults['date_mouvement'] = date('d/m/Y');
         $this->withExport = false;
         if (isset($options['export']) && $options['export']) {
 		$this->withExport = true;
