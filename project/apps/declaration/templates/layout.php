317bf5edd034da503cb2d4605e64b13f047bfca7
Jb Lm <lemetayer.jb@gmail.com>
2016-09-06 15:02:06 +0200

    Correction decalage colonne drm

--- project/apps/declaration/templates/layout.php
+++ project/apps/declaration/templates/layout.php
@@ -14,6 +14,7 @@
     <link href="/components/select2/select2.css" rel="stylesheet">
     <link href="/components/select2/select2-bootstrap.min.css" rel="stylesheet">
     <link href="/components/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
     
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
