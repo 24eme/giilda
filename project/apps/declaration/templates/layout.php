ebfe2b5352ee6c40bc5fcf4248a34268b4f11e34
Vincent LAURENT <vince.laurent@gmail.com>
2016-09-29 20:17:57 +0200

    Modification du layout

--- project/apps/declaration/templates/layout.php
+++ project/apps/declaration/templates/layout.php
@@ -10,12 +10,14 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <!-- Bootstrap core CSS -->
-    <link href="/components/bootstrap/bootstrap.css" rel="stylesheet">
+    
+    <link href="/css/bootstrap.css" rel="stylesheet">
     <link href="/components/select2/select2.css" rel="stylesheet">
     <link href="/components/select2/select2-bootstrap.min.css" rel="stylesheet">
     <link href="/components/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <link href="/components/vins/vins.css" rel="stylesheet">
     <link href="/css/style.css" rel="stylesheet">
-    
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
