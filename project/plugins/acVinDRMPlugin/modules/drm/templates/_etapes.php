8ff1bf63d168630fc283665dcdb7135cc1b21bd1
Mathurin Petit <mathurin.petit@gmail.com>
2016-09-30 15:07:05 +0200

    maj rail d'étape pourri

--- project/plugins/acVinDRMPlugin/modules/drm/templates/_etapes.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/_etapes.php
@@ -6,29 +6,32 @@
             <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_CHOIX_PRODUITS, DRMClient::$drm_etapes))); ?>
             <li class="<?php if($actif): ?>active<?php endif; ?> <?php if (!$past && !$actif): ?>disabled<?php endif; ?> <?php if ($past && !$actif): ?>visited<?php endif; ?>">
                 <a href="<?php echo url_for('drm_choix_produit', $drm); ?>">
-                    <span><?php echo $cpt_etape++; ?>.&nbsp;&nbsp;Produits</span>
+                    <span>Produits</span>
                     <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
                 </a>
             </li>
+        <?php $cpt_etape++; ?>
         <?php endif; ?>
         <?php $actif = ($etape_courante == DRMClient::ETAPE_SAISIE_SUSPENDU); ?>
         <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_SAISIE_SUSPENDU, DRMClient::$drm_etapes))); ?>
         <?php $isDouaneTypeSuspendu = $drm->isDouaneType(DRMClient::TYPE_DRM_SUSPENDU); ?>
         <li style="<?php if(!$isDouaneTypeSuspendu): ?>opacity: 0.5;<?php endif ?>" class="<?php if($actif): ?>active<?php endif; ?> <?php if (!$past && !$actif): ?>disabled<?php endif; ?> <?php if ($past && !$actif && $isDouaneTypeSuspendu): ?>visited<?php endif; ?>">
             <a href="<?php echo url_for('drm_edition_details', array('sf_subject' => $drm, 'details' => DRM::DETAILS_KEY_SUSPENDU)); ?>">
-                <span><?php echo $cpt_etape++; ?>.&nbsp;&nbsp;Mouvements Suspendus</span>
+                <span>Mouvements Suspendus</span>
                 <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
             </a>
         </li>
+        <?php $cpt_etape++; ?>
         <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_SAISIE_ACQUITTE, DRMClient::$drm_etapes))); ?>
         <?php $actif = ($etape_courante == DRMClient::ETAPE_SAISIE_ACQUITTE); ?>
         <?php $isDouaneTypeAcquitte = $drm->isDouaneType(DRMClient::TYPE_DRM_ACQUITTE); ?>
         <li style="<?php if(!$drm->isDouaneType(DRMClient::TYPE_DRM_ACQUITTE)): ?>opacity: 0.5;<?php endif ?>" class="<?php if($actif): ?>active<?php endif; ?> <?php if (!$past && !$actif): ?>disabled<?php endif; ?> <?php if ($past && !$actif && $isDouaneTypeAcquitte): ?>visited<?php endif; ?>">
             <a href="<?php echo url_for('drm_edition_details', array('sf_subject' => $drm, 'details' => DRM::DETAILS_KEY_ACQUITTE)); ?>">
-                <span><?php echo $cpt_etape++; ?>.&nbsp;&nbsp;Mouvements Acquittés</span>
+                <span>Mouvements Acquittés</span>
                 <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
             </a>
         </li>
+        <?php $cpt_etape++; ?>
         <?php if (isset($isTeledeclarationMode) && $isTeledeclarationMode) : ?>
             <?php $actif = ($etape_courante == DRMClient::ETAPE_CRD); ?>
             <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_CRD, DRMClient::$drm_etapes))); ?>
@@ -38,24 +41,27 @@
                     <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
                 </a>
             </li>
+        <?php $cpt_etape++; ?>
         <?php endif; ?>
         <?php if (isset($isTeledeclarationMode) && $isTeledeclarationMode) : ?>
             <?php $actif = ($etape_courante == DRMClient::ETAPE_ADMINISTRATION); ?>
             <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_ADMINISTRATION, DRMClient::$drm_etapes))); ?>
             <li class="<?php if($actif): ?>active<?php endif; ?> <?php if (!$past && !$actif): ?>disabled<?php endif; ?> <?php if ($past && !$actif): ?>visited<?php endif; ?>">
                 <a href="<?php echo url_for('drm_annexes', $drm); ?>">
-                    <span><?php echo $cpt_etape++; ?>.&nbsp;&nbsp;Annexes</span>
+                    <span>Annexes</span>
                     <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
                 </a>
             </li>
+        <?php $cpt_etape++; ?>
         <?php endif; ?>
         <?php $actif = ($etape_courante == DRMClient::ETAPE_VALIDATION); ?>
         <?php $past = ((!$actif) && (array_search($drm->etape, DRMClient::$drm_etapes) >= array_search(DRMClient::ETAPE_VALIDATION, DRMClient::$drm_etapes))); ?>
         <li class="<?php if($actif): ?>active<?php endif; ?> <?php if (!$past && !$actif): ?>disabled<?php endif; ?> <?php if ($past && !$actif): ?>visited<?php endif; ?>">
             <a href="<?php echo url_for('drm_validation', $drm); ?>">
-               <span><?php echo $cpt_etape++; ?>.&nbsp;&nbsp;Validation</span>
+               <span>Validation</span>
                <small class="hidden">Etape <?php echo $cpt_etape; ?></small>
             </a>
         </li>
+        <?php $cpt_etape++; ?>
     </ul>
 </nav>
