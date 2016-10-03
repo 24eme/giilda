a63dbf6a8ab1f7041c617ccc14f500a7cdf1f499
Vincent LAURENT <vince.laurent@gmail.com>
2016-10-03 15:24:34 +0200

    Convention d'adhésion

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/cguSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/cguSuccess.php
@@ -1,13 +1,20 @@
 <div class="row">
     <div class="col-xs-8 col-xs-offset-2">
         <div class="panel panel-default">
-            <div class="panel-heading"><h3 class="panel-title">Condition général</h3></div>
+            <div class="panel-heading"><h2 class="panel-title">Activation de votre espace de télédéclaration</h2></div>
             <div class="panel-body">
                 <form action="<?php echo url_for("compte_teledeclarant_cgu") ?>" method="post">
+                    <p>
+                        L'IVBD met à disposition de ses ressortissants un portail de télédéclaration pour les DRM et les contrats d’achats&nbsp;:&nbsp;«&nbsp;ivbdpro.fr&nbsp;».
+                    </p>
+                    <p>
+                        Pour activer votre espace de téléclaration, vous devez prendre connaissance et accepter le contrat d'adhésion aux télédéclarations : <a href="">Voir la convention d'adhésion</a>.
+                    </p>
+
                     <div class="checkbox">
                       <label>
                         <input required="required" type="checkbox" name="accepter" value="1">
-                        Accepter les conditions
+                        J'accepte la <a href="">convention d'adhésion</a> aux télédéclarations
                       </label>
                     </div>
                     <div class="row" >
