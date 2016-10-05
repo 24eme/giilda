b7383e0a1c6f4586c8668920741a43f297c6d3cb
Vincent LAURENT <vince.laurent@gmail.com>
2016-10-05 15:05:38 +0200

    Test de l'existence des champs paiement douane

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
@@ -1257,8 +1257,8 @@ class DRM extends BaseDRM implements InterfaceMouvementDocument, InterfaceVersio
         $drm_societe->add('email', $societe->getEmailTeledeclaration());
         $drm_societe->add('telephone', $societe->telephone);
         $drm_societe->add('fax', $societe->fax);
-        $drm_societe->add('paiement_douane_moyen', $societe->paiement_douane_moyen);
-        $drm_societe->add('paiement_douane_frequence', $societe->paiement_douane_frequence);
+        $drm_societe->add('paiement_douane_moyen', ($societe->exist('paiement_douane_moyen')) ? $societe->paiement_douane_moyen : null);
+        $drm_societe->add('paiement_douane_frequence', ($societe->exist('paiement_douane_frequence')) ? $societe->paiement_douane_frequence : null);
     }
 
     public function getCoordonneesSociete() {
