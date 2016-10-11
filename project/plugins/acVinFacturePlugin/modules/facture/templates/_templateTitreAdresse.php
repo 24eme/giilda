c67b6b724a45da8fb8a1038799bf8a4b09662803
Mathurin Petit <mathurin.petit@gmail.com>
2016-10-11 17:35:30 +0200

    adresse complémentaire sur ligne supplémentaire

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_templateTitreAdresse.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_templateTitreAdresse.php
@@ -4,7 +4,7 @@ use_helper('Display');
 \noindent{
 \begin{minipage}[t]{0.5\textwidth}
 	\begin{flushleft}
-	
+
 	\textbf{<?php echo ($avoir)? 'AVOIR' : 'FACTURE'; ?>} <?php if($facture->numero_piece_comptable_origine): ?>\small{(Facture n°~<?php echo $facture->numero_piece_comptable_origine ?>)}<?php endif; ?> \\
 	\vspace{0.2cm}
 	\begin{tikzpicture}
@@ -15,7 +15,7 @@ use_helper('Display');
   				\FactureNum & \FactureDate & \FactureRefClient
 			\end{tabular}
 		};
-		\node[draw=gray, inner sep=0pt, rounded corners=3pt, line width=2pt, fit=(tab0.north west) (tab0.north east) (tab0.south east) (tab0.south west)] {};	
+		\node[draw=gray, inner sep=0pt, rounded corners=3pt, line width=2pt, fit=(tab0.north west) (tab0.north east) (tab0.south east) (tab0.south west)] {};
 	\end{tikzpicture}
 	\\
         <?php if($facture->hasMessageCommunication() && !$avoir): ?>
@@ -26,7 +26,7 @@ use_helper('Display');
   				<?php display_latex_message_communication($facture->getMessageCommunicationWithDefault()); ?>
 			\end{tabular}
 		};
-		\node[draw=gray, inner sep=0pt, rounded corners=3pt, line width=1pt, fit=(tab0.north west) (tab0.north east) (tab0.south east) (tab0.south west)] {};	
+		\node[draw=gray, inner sep=0pt, rounded corners=3pt, line width=1pt, fit=(tab0.north west) (tab0.north east) (tab0.south east) (tab0.south west)] {};
 	\end{tikzpicture}
         <?php else : ?>
         	\vspace{0.5cm}
@@ -37,9 +37,9 @@ use_helper('Display');
 \hspace{2cm}
 \begin{minipage}[t]{0.5\textwidth}
 \vspace{1cm}
-		\begin{flushleft}		
-			\textbf{\FactureClientNom \\}				
-				\FactureClientAdresse \\
+		\begin{flushleft}
+			\textbf{\FactureClientNom \\}
+				\FactureClientAdresse \FactureClientAdresseComplementaire \\
 				\FactureClientCP ~\FactureClientVille \\
 			\end{flushleft}
 		\hspace{6cm}
