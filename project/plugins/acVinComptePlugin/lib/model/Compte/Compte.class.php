4a0c4bc86c21df45122a60a2f600bc18e5ad16f6
Mathurin Petit <mathurin.petit@gmail.com>
2016-10-24 17:27:34 +0200

    reparation synchro pour nomAAfficher

--- project/plugins/acVinComptePlugin/lib/model/Compte/Compte.class.php
+++ project/plugins/acVinComptePlugin/lib/model/Compte/Compte.class.php
@@ -44,7 +44,10 @@ class Compte extends BaseCompte implements InterfaceCompteGenerique {
         if (!$this->nom) {
             return;
         }
-
+        if($this->isSocieteContact()){
+            $this->nom_a_afficher = trim(sprintf('%s', $this->nom));
+            return;
+        }
         $this->nom_a_afficher = trim(sprintf('%s %s %s', $this->civilite, $this->prenom, $this->nom));
     }
 
@@ -126,7 +129,6 @@ class Compte extends BaseCompte implements InterfaceCompteGenerique {
 
         $societe = $this->getSociete();
         if ($this->isSocieteContact()) {
-            $this->updateNomAAfficher();
             $this->addTag('automatique', 'Societe');
             $this->addTag('automatique', $societe->type_societe);
             if ($this->getFournisseurs()) {
