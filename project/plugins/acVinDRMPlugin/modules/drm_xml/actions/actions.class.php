71c9045fe8dea8ddc4f11687dad1f0aeab705331
Tangui Morlier <tangui@tangui.eu.org>
2016-10-28 18:30:02 +0200

    Envoi d'un mail si erreur de tranmission

--- project/plugins/acVinDRMPlugin/modules/drm_xml/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm_xml/actions/actions.class.php
@@ -8,7 +8,7 @@ class drm_xmlActions extends drmGeneriqueActions {
 
     public function executeWait(sfWebRequest $request) {
 	$this->setLayout(false);
-        $this->drm = $this->getRoute()->getDRM();    
+        $this->drm = $this->getRoute()->getDRM();
     }
 
     public function executeTransfert(sfWebRequest $request) {
@@ -25,14 +25,24 @@ class drm_xmlActions extends drmGeneriqueActions {
         } else {
                 $this->cielResponse = "Une erreur est survenue à la génération du XML.";
         }
-	$this->drm->add('transmission_douane')->add('xml', $this->cielResponse);
+        $this->drm->add('transmission_douane')->add('xml', $this->cielResponse);
         $this->drm->add('transmission_douane')->add('success', false);
-	if (preg_match('/identifiant-declaration>([^<]*)<.*horodatage-depot>([^<]+)</', $this->cielResponse, $m)) {
-		$this->drm->add('transmission_douane')->add('success', true);
-                $this->drm->add('transmission_douane')->add('horodatage', $m[2]);
-                $this->drm->add('transmission_douane')->add('id_declaration', $m[1]);
-	}
-	$this->drm->save();
+        if (preg_match('/identifiant-declaration>([^<]*)<.*horodatage-depot>([^<]+)</', $this->cielResponse, $m)) {
+          $this->drm->add('transmission_douane')->add('success', true);
+          $this->drm->add('transmission_douane')->add('horodatage', $m[2]);
+          $this->drm->add('transmission_douane')->add('id_declaration', $m[1]);
+        }
+        if (!$this->drm->transmission_douane->success) {
+          $to = sfConfig::get('app_ac_exception_notifier_email');
+          $to = ($to && isset($to->to)) ? $to->to : 'vins@actualys.com';
+          $msg = $this->getMailer()->compose(array(sfConfig::get('app_mail_from_email') => sfConfig::get('app_mail_from_name')),
+                                             $to,
+                                             "Erreur transmision XML pour ".$this->_id,
+                                             "Une transmission vient d'écouchouer pour la DRM ".$this->_id);
+          $this->getMailer()->send($msg);
+        }
+        $this->drm->save();
+        return $this->redirect('drm_ciel', $this->drm);
     }
 
     public function executePrint(sfWebRequest $request) {
