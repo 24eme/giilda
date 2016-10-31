7c09fe35f5ed9e12858ee572e20a6c01b6a13992
Tangui Morlier <tangui@tangui.eu.org>
2016-10-31 17:23:45 +0100

    Widget password oublié

---
+++ project/plugins/acVinLibPlugin/lib/widget/bootstrap/bsWidgetFormInputPassword.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class bsWidgetFormInputPassword extends sfWidgetFormInputPassword
+{
+    public function render($name, $value = null, $attributes = array(), $errors = array())
+    {
+        if(!isset($attributes['class'])) {
+            $attributes['class'] = 'form-control';            
+        }
+        
+        return $this->renderTag('input', array_merge(array('type' => $this->getOption('type'), 'name' => $name, 'value' => $value), $attributes));
+    }
+}
