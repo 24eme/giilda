a74cd41068b0a2c5820657eeefa776386fcc6522
Mathurin Petit <mathurin.petit@gmail.com>
2016-11-07 11:59:35 +0100

    les Autres ne sont ni tranq / ni mousseux

--- project/plugins/acVinDRMPlugin/lib/form/DRMAnnexesForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/DRMAnnexesForm.class.php
@@ -64,8 +64,10 @@ class DRMAnnexesForm extends acCouchdbObjectForm {
 
         foreach ($this->drm->getProduits() as $produit) {
             $genre = $produit->getConfig()->getGenre();
+            if($genre->getKey() == 'DEFAUT') { continue; }
             $droit = $genre->getDroitDouane($this->drm->getFirstDayOfPeriode());
             $genreKey = DRMDroits::$correspondanceGenreKey[$genre->getKey()];
+
             $this->setWidget('cumul_' . $genreKey, new sfWidgetFormInputFloat(array('float_format' => "%d")));
             $this->setValidator('cumul_' . $genreKey, new sfValidatorNumber(array('required' => false)));
 
