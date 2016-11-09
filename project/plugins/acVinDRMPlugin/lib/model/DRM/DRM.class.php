e9fb5e97d44dbc972a1898deeb0342f8b8626c10
Mathurin Petit <mathurin.petit@gmail.com>
2016-11-09 17:51:04 +0100

    on transfert TOUT les mvt acq => susp ou susp => Acq même s'ils valent null

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
@@ -1057,7 +1057,7 @@ private function switchDetailsCrdRegime($produit,$newCrdRegime, $typeDrm = DRM::
                 foreach($details->get($mvtType) as $key => $value) {
                         if(!preg_match('/_details/',$key)){
                           $detailConf = $detailsConfig->get($mvtType)->get($key);
-                          if($detailConf && $value && $detailConf->exist('switch_regime'))
+                          if($detailConf && $detailConf->exist('switch_regime'))
                           {
                             if((($detailConf->douane_type == DRMClient::CRD_TYPE_SUSPENDU) && ($newCrdRegime == EtablissementClient::REGIME_CRD_COLLECTIF_ACQUITTE))
                                 || (($detailConf->douane_type == DRMClient::CRD_TYPE_ACQUITTE)
