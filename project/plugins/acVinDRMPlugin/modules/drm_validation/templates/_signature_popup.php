f348dedc9264a95e8efe572f77348d538d40a15d
Tangui Morlier <tangui@tangui.eu.org>
2016-11-28 19:01:34 +0100

    Message pour les CRD Acquittées

--- project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
+++ project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
@@ -5,11 +5,14 @@
             <p>
                 Vous êtes sur le point de valider votre DRM. Une fois validée, vous recevrez votre DRM par mail et vous ne pourrez plus la modifier.<br /><br />
             </p>
-          <?php if($compte->hasDroit("teledeclaration_douane")): ?>
+        <?php if($compte->hasDroit(Roles::TELEDECLARATION_DOUANE) && ! $drm->crds->exist('COLLECTIFACQUITTE')): ?>
     	    <p>Si vous le souhaitez, en cliquant sur l'option ci-dessous, vous pouvez transmettre cette DRM directement sur le portail de la douane, qui apparaitra en mode brouillon sur le portail pro.douane.gouv.fr. Il vous restera alors à la valider en ligne sur le site web douanier.</p>
         <?php endif; ?>
 	    <p>Si vous décidez de transmettre le document par courrier postal ou par mail, n'oubliez pas que la DRM doit être signée manuellement pour être valable.</p>
-          <?php if($compte->hasDroit("teledeclaration_douane")): ?>
+        <?php if($compte->hasDroit("teledeclaration_douane")): ?>
+          <?php if($drm->crds->exist('COLLECTIFACQUITTE')): ?>
+            <p><strong>Les douanes ne nous permettant pas de leur transmettre des CRD en régime acquitté, nous ne pouvons pas vous permettre de télédéclarer votre DRM sur le portail des douanes pour le moment.</strong></p>
+          <?php else: ?>
           <p>
             <div class="ligne_form">
                 <span>
@@ -19,12 +22,11 @@
                 </span>
             </div>
           </p>
-          <?php endif; ?>
+        <?php endif; endif;?>
             <div class="ligne_btn">
                 <a id="signature_drm_popup_close" class="btn_rouge btn_majeur annuler" style="float: left;" href="#" >Annuler</a>
 
                 <a id="signature_drm_popup_confirm" class="btn_validation" ><span>Valider la DRM</span></a>
-
             </div>
         </div>
 </div>
