bb3fa3a65701bce25709ffcf8d7ce7d970457e76
Tangui Morlier <tangui@tangui.eu.org>
2016-12-02 11:41:36 +0100

    Meilleure gestion des observations

--- project/plugins/acVinDRMPlugin/modules/drm_annexes/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm_annexes/actions/actions.class.php
@@ -6,7 +6,7 @@ class drm_annexesActions extends drmGeneriqueActions {
         $this->initSocieteAndEtablissementPrincipal();
         $this->drm = $this->getRoute()->getDRM();
         $this->isTeledeclarationMode = $this->isTeledeclarationDrm();
-        
+        $this->drm->update();
         $this->initDeleteForm();
         $this->drm->initReleveNonApurement();
         $this->annexesForm = new DRMAnnexesForm($this->drm);
