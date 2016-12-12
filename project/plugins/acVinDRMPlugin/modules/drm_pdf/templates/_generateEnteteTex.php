c298efbe59b58e9904ee362d186cbbebd28abdbf
Tangui Morlier <tangui@tangui.eu.org>
2016-12-12 18:46:02 +0100

    Ajout de la transmission dans l'entête de PDF

--- project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateEnteteTex.php
+++ project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateEnteteTex.php
@@ -11,17 +11,17 @@ if($drm->declarant->caution){
 }
 ?>
 
-\def\InterloireAdresse{<?php echo getAdresseInterloire(); ?>} 
-\def\InterloireContact{<?php echo getDrmContact($drm); ?>} 
+\def\InterloireAdresse{<?php echo getAdresseInterloire(); ?>}
+\def\InterloireContact{<?php echo getDrmContact($drm); ?>}
 
 \def\DRMSocieteRaisonSociale{<?php echo $drm->societe->raison_sociale; ?>}
-\def\DRMSocieteAdresse{<?php echo getDrmSocieteAdresse($drm); ?>}   
+\def\DRMSocieteAdresse{<?php echo getDrmSocieteAdresse($drm); ?>}
 
-\def\DRMAdresseChai{<?php echo getDrmEtablissementAdresse($drm); ?>}   
-\def\DRMAdresseCompta{} 
+\def\DRMAdresseChai{<?php echo getDrmEtablissementAdresse($drm); ?>}
+\def\DRMAdresseCompta{}
 
-\def\DRMNumAccise{<?php echo $drm->declarant->no_accises; ?>} 
-\def\DRMCvi{<?php echo $drm->declarant->cvi; ?>}              
+\def\DRMNumAccise{<?php echo $drm->declarant->no_accises; ?>}
+\def\DRMCvi{<?php echo $drm->declarant->cvi; ?>}
 
 \def\DRMSiret{<?php echo $drm->societe->siret; ?>}
 \def\DRMIdentifiantIL{<?php echo $drm->identifiant; ?>}
@@ -42,15 +42,20 @@ Caution : \textbf{<?php echo $caution; ?>} \\
 Organisme cautionneur : \textbf{<?php echo $organismeCautionneur; ?>} \\
 <?php endif; ?>
  }
- 
+
 \rhead{\includegraphics[scale=1]{<?php echo realpath(dirname(__FILE__)."/../../../../../web/data")."/logo_new.jpg"; ?>}  \\
 \vspace{-2cm}
 \InterloireAdresse \\
- \begin{small} \InterloireContact \\ 
- \end{small} 
+ \begin{small} \InterloireContact \\
+ \end{small}
  \begin{large}
-\textbf{Signé électroniquement le <?php echo $drm->getEuValideDate(); ?>}
+\textbf{Signé électroniquement le <?php echo $drm->getEuValideDate();
+if ($drm->hasTransmissionDate()) {
+  echo " et transmis aux douanes le ".$drm->getEuTransmissionDate();
+}
+
+  ?>}
 \end{large}
  }
- 
+
 \rfoot{page \thepage\ / <?php echo $nbPages ?>}
