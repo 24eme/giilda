1a8d00433c6539e57cdc68687b849161943114be
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-14 11:31:54 +0100

    Libellé date de prise en compte des mouvements

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_generationMasse.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_generationMasse.php
@@ -5,29 +5,29 @@
         <?php echo $generationForm->renderGlobalErrors(); ?>
         <div class="form-group <?php if ($generationForm["modele"]->hasError()): ?>has-error<?php endif; ?>">
             <?php echo $generationForm["modele"]->renderError() ?>
-            <?php echo $generationForm["modele"]->renderLabel("Type de facture", array("class" => "col-xs-4 control-label")); ?>
-            <div class="col-xs-8">
+            <?php echo $generationForm["modele"]->renderLabel("Type de facture", array("class" => "col-xs-5 control-label")); ?>
+            <div class="col-xs-7">
                 <?php echo $generationForm["modele"]->render(); ?>
             </div>
         </div>
         <div class="form-group <?php if ($generationForm["date_facturation"]->hasError()): ?>has-error<?php endif; ?>">
             <?php echo $generationForm["date_facturation"]->renderError(); ?>
-            <?php echo $generationForm["date_facturation"]->renderLabel("Date de facturation", array("class" => "col-xs-4 control-label")); ?>
-            <div class="col-xs-8 date-picker">
+            <?php echo $generationForm["date_facturation"]->renderLabel("Date de facturation", array("class" => "col-xs-5 control-label")); ?>
+            <div class="col-xs-7 date-picker">
                     <?php echo $generationForm["date_facturation"]->render(array("class" => "form-control", "placeholder" => "Date de facturation")); ?>
             </div>
         </div>
         <div class="form-group <?php if ($generationForm["date_mouvement"]->hasError()): ?>has-error<?php endif; ?>">
             <?php echo $generationForm["date_mouvement"]->renderError(); ?>
-            <?php echo $generationForm["date_mouvement"]->renderLabel("Date de mouvements", array("class" => "col-xs-4 control-label")); ?>
-            <div class="col-xs-8 date-picker">
+            <?php echo $generationForm["date_mouvement"]->renderLabel("Date de prise en compte des mouvements", array("class" => "col-xs-5 control-label")); ?>
+            <div class="col-xs-7 date-picker">
                     <?php echo $generationForm["date_mouvement"]->render(array("class" => "form-control", "placeholder" => "Date de prise en compte des mouvements")); ?>
             </div>
         </div>
         <div class="form-group <?php if ($generationForm["message_communication"]->hasError()): ?>has-error<?php endif; ?>">
             <?php echo $generationForm["message_communication"]->renderError(); ?>
-            <?php echo $generationForm["message_communication"]->renderLabel("Cadre de communication", array("class" => "col-xs-4 control-label")); ?>
-            <div class="col-xs-8">
+            <?php echo $generationForm["message_communication"]->renderLabel("Cadre de communication", array("class" => "col-xs-5 control-label")); ?>
+            <div class="col-xs-7">
 
                     <?php echo $generationForm["message_communication"]->render(array("class" => "form-control")); ?>
 
