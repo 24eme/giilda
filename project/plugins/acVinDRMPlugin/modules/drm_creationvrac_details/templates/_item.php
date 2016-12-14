c8379cee78f7c2b0777c5ef043d4949500a6f9ed
Vincent LAURENT <vince.laurent@gmail.com>
2016-12-14 12:17:41 +0100

    Titre et formatage des popups details

--- project/plugins/acVinDRMPlugin/modules/drm_creationvrac_details/templates/_item.php
+++ project/plugins/acVinDRMPlugin/modules/drm_creationvrac_details/templates/_item.php
@@ -1,35 +1,33 @@
 <?php use_helper('PointsAides'); ?>
 <?php $ligneId = "ligne_" . str_replace(array("[", "]"), array("-", ""), $form->renderName()) ?>
 <tr id="<?php echo $ligneId ?>" class="row">
-    <td class="form-group <?php if ($form['acheteur']->hasError()): ?>has-error<?php endif; ?> col-xs-4">
+    <td class="form-group <?php if ($form['acheteur']->hasError()): ?>has-error<?php endif; ?> col-xs-5">
         <?php echo $form['acheteur']->renderError(); ?>
         <?php echo $form['acheteur']->render(array("class" => "form-control select2autocomplete ")); ?>
     </td>
-
-
-    <td class="form-group volume <?php if ($form['volume']->hasError()): ?>has-error<?php endif; ?>" >
+    <td class="form-group volume <?php if ($form['volume']->hasError()): ?>has-error<?php endif; ?> col-xs-2" >
         <?php
         echo $form['volume']->renderError();
         ?>
         <div class="input-group" class="">
-            <?php echo $form['volume']->render(array("class" => "input-float form-control text-right","placeholder" => "Volume enlevé")); ?>
+            <?php echo $form['volume']->render(array("class" => "input-float form-control text-right","placeholder" => "Volume")); ?>
             <div class="input-group-addon">hl</div>
         </div>
     </td>
 
-    <td class="form-group <?php if ($form['prixhl']->hasError()): ?>has-error<?php endif; ?>" >
+    <td class="form-group <?php if ($form['prixhl']->hasError()): ?>has-error<?php endif; ?> col-xs-2" >
       <?php
       echo $form['prixhl']->renderError();
       ?>
       <div class="input-group" class="">
-          <?php echo $form['prixhl']->render(array("class" => "form-control text-right","placeholder" => "Prix unitaire")); ?>
+          <?php echo $form['prixhl']->render(array("class" => "form-control text-right","placeholder" => "Prix")); ?>
           <div class="input-group-addon">€/hl</div>
       </div>
     </td>
 
-    <td class="form-group <?php if ($form['identifiant']->hasError()): ?>has-error<?php endif; ?> col-xs-3">
+    <td class="form-group <?php if ($form['identifiant']->hasError()): ?>has-error<?php endif; ?> col-xs-2">
       <?php echo $form['identifiant']->renderError(); ?>
-      <?php echo $form['identifiant']->render(array("class" => "form-control", "autofocus" => "autofocus","placeholder" => "Identifiant du contrat")); ?>
+      <?php echo $form['identifiant']->render(array("class" => "form-control", "autofocus" => "autofocus","placeholder" => "Num. du contrat")); ?>
     </td>
 
     <td class="text-right">
