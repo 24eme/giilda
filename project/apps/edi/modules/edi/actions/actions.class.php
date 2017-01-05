a6430557234931a121d3701335dd6d7af3b10126
Mathurin Petit <mathurin.petit@gmail.com>
2017-01-05 16:41:32 +0100

    maj app EDI pour le traitement de l'edi

---
+++ project/apps/edi/modules/edi/actions/actions.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class ediActions extends sfActions {
+
+    public function executeDrmCreationEdi(sfWebRequest $request) {
+
+      var_dump("coucou"); exit;
+    }
+
+}
---
+++ project/apps/edi/modules/edi/templates/error404Success.php
@@ -0,0 +1,5 @@
+<section id="principal">
+    <center>
+        <p><img height="381" width="680" src="/images/404.jpg"/></p>
+    </center>
+</section>
