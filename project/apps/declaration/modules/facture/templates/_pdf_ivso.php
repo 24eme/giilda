37963df441c3148a0dff0cea7edfed6fd87063ba
Mathurin Petit <mathurin.petit@gmail.com>
2017-01-20 16:12:45 +0100

    parametrage de _pdf_sancerre

---
+++ project/apps/declaration/modules/facture/templates/_pdf_ivso.php
@@ -0,0 +1,53 @@
+<?php
+$avoir = ($facture->total_ht <= 0);
+var_dump("dedze"); exit;
+include_partial('facture/templateEntete', array('facture' => $facture));
+?>
+\begin{document}
+<?php
+include_partial('facture/templateTitreAdresse', array('facture' => $facture, 'avoir' => $avoir));
+include_partial('facture/templateNumPage', array('nb_page' => $nb_pages));
+include_partial('facture/templateHeadTable',array('facture' => $facture));
+$line_nb_current_page = FactureLatex::NB_LIGNES_ENTETE * ($nb_pages > 1);
+$current_avg_nb_lines_per_page = floor($nb_lines / $nb_pages);
+$max_line_nb_current_page = FactureLatex::MAX_LIGNES_PERPAGE - FactureLatex::NB_LIGNES_ENTETE;
+$current_total_line_nb = 0;
+$current_nb_pages = 0;
+foreach ($facture->lignes as $type => $typeLignes) {
+    $line_nb_current_page++;
+    ?>
+    \small{\textbf{<?php echo $typeLignes->getLibellePrincipal(); ?>}<?php if($typeLignes->getLibelleSecondaire()): ?> <?php echo $typeLignes->getLibelleSecondaire(); ?><?php endif; ?>} &
+    \multicolumn{1}{r|}{~} &
+    \multicolumn{1}{r|}{~} &
+    \multicolumn{1}{r}{~}
+    \\
+    <?php
+    foreach ($typeLignes->details as $prodHash => $produit) {
+        include_partial('facture/templateTableRow', array('produit' => $produit->getRawValue(), 'facture' => $facture));
+        $line_nb_current_page++;
+        if ($line_nb_current_page > $current_avg_nb_lines_per_page || $line_nb_current_page >= $max_line_nb_current_page) {
+            include_partial('facture/templateEndTableWithMention', array('add_blank_lines' => ($max_line_nb_current_page - $line_nb_current_page), 'avoir' => $avoir));
+            echo "\\newpage\n";
+            include_partial('facture/templateNumPage', array('nb_page' => $nb_pages));
+            include_partial('facture/templateHeadTable',array('facture' => $facture));
+            $current_total_line_nb += $line_nb_current_page;
+            $line_nb_current_page = 0;
+            $current_nb_pages++;
+            $max_line_nb_current_page = FactureLatex::MAX_LIGNES_PERPAGE;
+            $current_avg_nb_lines_per_page = ($nb_lines - $current_total_line_nb) / ($nb_pages - $current_nb_pages);
+        }
+    }
+}
+$nb_blank = FactureLatex::MAX_LIGNES_PERPAGE - $line_nb_current_page - FactureLatex::NB_LIGNES_REGLEMENT;
+$nb_echeances = count($facture->getEcheancesPapillon());
+if ($nb_echeances)
+    $nb_blank += - FactureLatex::NB_LIGNES_PAPILLONS_PAR_ECHEANCE * $nb_echeances - FactureLatex::NB_LIGNES_PAPILLONS_FIXE;
+if (!$current_nb_pages)
+    $nb_blank -= FactureLatex::NB_LIGNES_ENTETE;
+
+include_partial('facture/templateEndTableWithMention', array('add_blank_lines' => $nb_blank, 'end_document' => true, 'avoir' => $avoir));
+include_partial('facture/templateReglement', array('facture' => $facture));
+if ($nb_echeances && !$avoir)
+    include_partial('facture/templateEcheances', array('echeances' => $facture->getEcheancesPapillon(), 'societe' => $facture->getSociete()));
+?>
+\end{document}
