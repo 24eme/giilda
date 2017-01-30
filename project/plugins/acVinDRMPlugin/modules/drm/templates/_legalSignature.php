413eb0a8e24d8bd601a600cb8db30b9d2f4bb9af
Mathurin Petit <mathurin.petit@gmail.com>
2017-01-30 17:46:23 +0100

    ajout du contrat de service v2

--- project/plugins/acVinDRMPlugin/modules/drm/templates/_legalSignature.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/_legalSignature.php
@@ -5,30 +5,30 @@
             <?php echo $legalSignatureForm->renderHiddenFields(); ?>
             <?php echo $legalSignatureForm->renderGlobalErrors(); ?>
             <h2>Activation de votre espace DRM</h2>
-            <br/> 
+            <br/>
             <p>
-                InterLoire met à votre disposition des outils de simplification administrative sur le système d’identification sécurisé : « vinsvaldeloire.pro ». 
+                InterLoire met à votre disposition des outils de simplification administrative sur le système d’identification sécurisé : « vinsvaldeloire.pro ».
             </p>
             <p>
                 Depuis le 1er novembre 2015, la télédéclaration de la DRM ou de la DRA est disponible.
             </p>
-              <br/> 
+              <br/>
             <p>
-                Ce service permet l’enregistrement de la DRM de façon simple et rapide grâce à une interface conviviale. 
+                Ce service permet l’enregistrement de la DRM de façon simple et rapide grâce à une interface conviviale.
             </p>
-          
+
             <p>
                 Le document pdf récapitulatif généré en fin d’enregistrement peut, à votre convenance, être envoyé aux services locaux de la Douane par voie postale ou électronique.
             </p>
-            <br/> 
+            <br/>
             <p>
-                Pour activer votre espace DRM, vous devez prendre connaissance et accepter le contrat d’inscription à la télédéclaration de la DRM. Pour cela, <a href="/data/contrat_service_v1.pdf" style="text-decoration: underline;">cliquez ici</a>.
+                Pour activer votre espace DRM, vous devez prendre connaissance et accepter le contrat d’inscription à la télédéclaration de la DRM. Pour cela, <a href="/data/contrat_service_v2.pdf" style="text-decoration: underline;">cliquez ici</a>.
             </p>
             <br/>
-            <?php echo $legalSignatureForm['terms']->render(array('required' => 'true')); ?><label for="drm_legal_signature_terms">J’accepte le <a href="/data/contrat_service_v1.pdf">contrat d’inscription</a> à la télédéclaration de la DRM.</label>
+            <?php echo $legalSignatureForm['terms']->render(array('required' => 'true')); ?><label for="drm_legal_signature_terms">J’accepte le <a href="/data/contrat_service_v2.pdf">contrat d’inscription</a> à la télédéclaration de la DRM.</label>
             <br/>
             <div class="ligne_btn">
-                <button id="popup_confirm" type="submit" class="btn_validation" style="float: right;" ><span>Activer mon espace</span></button>  
+                <button id="popup_confirm" type="submit" class="btn_validation" style="float: right;" ><span>Activer mon espace</span></button>
             </div>
         </form>
     </div>
