4a609e238b7a19a8e31311fa6c104407a9854795
Vincent LAURENT <vince.laurent@gmail.com>
2017-02-08 16:34:42 +0100

    Fin de l'externalisation de l'app dans le plugin AppPlugin

--- project/apps/ivso/config/ivsoConfiguration.class.php
+++ project/apps/ivso/config/ivsoConfiguration.class.php
@@ -4,9 +4,5 @@ class ivsoConfiguration extends sfApplicationConfiguration
 {
     public function configure()
     {
-        $configCache = $this->getConfigCache();
-        $configCache->registerConfigHandler('config/global.yml', 'sfDefineEnvironmentConfigHandler');
-        include($configCache->checkConfig('config/global.yml'));
-
     }
 }
