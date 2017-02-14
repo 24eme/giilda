a6fa2a0f2841155837ff3693ae85074fa979d809
Tangui Morlier <tangui@tangui.eu.org>
2017-02-14 20:27:37 +0100

    Test par defaut bivc

--- project/test/bootstrap/common.php
+++ project/test/bootstrap/common.php
@@ -3,6 +3,6 @@
 require_once(dirname(__FILE__).'/../../config/ProjectConfiguration.class.php');
 require_once dirname(__FILE__).'/../../lib/vendor/symfony/test/bootstrap/unit.php';
 
-$configuration = ProjectConfiguration::getApplicationConfiguration( 'ivso', 'prod', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration( 'bivc', 'prod', true);
 
 new sfDatabaseManager($configuration);
