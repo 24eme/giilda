8aea10f217da8f5f6e13dd3f2abead5263da95ed
Vincent LAURENT <vince.laurent@gmail.com>
2017-02-17 11:56:58 +0100

    Déblocage visualisation société

--- project/plugins/acVinSocietePlugin/modules/societe/templates/visualisationSuccess.php
+++ project/plugins/acVinSocietePlugin/modules/societe/templates/visualisationSuccess.php
@@ -119,6 +119,7 @@
     <?php endforeach; ?>
 
     <?php foreach ($interlocuteurs as $interlocuteurId => $compte) : ?>
+        <?php if(!$compte): continue; endif; ?> 
             <?php if ($compte->isSocieteContact() || $compte->isEtablissementContact()): ?><?php continue; ?><?php endif; ?>
         <div class="col-xs-4" style="<?php if (isset($etablissement) || (isset($interlocuteur) && $interlocuteur->_id != $compte->_id)): ?>opacity: 0.6<?php endif; ?>">
     <?php include_partial('compte/visualisation', array('compte' => $compte, 'modification' => $modification, 'reduct_rights' => $reduct_rights)); ?>
