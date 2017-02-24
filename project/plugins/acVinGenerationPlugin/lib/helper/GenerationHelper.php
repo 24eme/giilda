949d804dcc84945091c476aec9f5fd1a71b34f27
Mathurin Petit <mathurin.petit@gmail.com>
2017-02-24 16:31:18 +0100

    changement de wording pour les dates de prise en compte de mvt

--- project/plugins/acVinGenerationPlugin/lib/helper/GenerationHelper.php
+++ project/plugins/acVinGenerationPlugin/lib/helper/GenerationHelper.php
@@ -8,7 +8,7 @@ function getLabelForKeyArgument($key)
        case 'operateur_types' : return "Types d'opérateur :";
        case 'date_declaration' : return 'Date de déclaration :';
        case 'date_facturation' : return 'Date de facturation :';
-       case 'date_mouvement' : return 'Date de prise en compte des mouvements :';
+       case 'date_mouvement' : return "Prise en compte des mouvements jusq'au :";
        case 'seuil' : return 'Seuil :';
        default: return "$key :";
    }
