252e265d5da3ecd2a5dd3eadcec9f9370687803a
Vincent LAURENT <vince.laurent@gmail.com>
2017-03-16 11:02:07 +0100

    Les debit et credit négatif sont inversé

--- project/bin/convertExportFacture2CIEL.sh
+++ project/bin/convertExportFacture2CIEL.sh
@@ -1,1 +1,15 @@
-cat | awk -F ";" 'function trunc(len, str) { return sprintf("%-" len "s", sprintf("%." len "s", str)) } { numero_facture = trunc(7, $4); code_journal = trunc(2, $1); date_facturation = trunc(8, gensub(/-/, "", "g", $2)); date_echeance = trunc(8, gensub(/-/, "", "g", $9)); if(!$9) { date_echeance = date_facturation } compte = trunc(8, $6); if($7) { compte = trunc(8, $7) } libelle = trunc(18, "Facture no " numero_facture); montant = trunc(12, $11); sens = trunc(1, $10); ressortissant = trunc(30, $16); libelle_complet = trunc(55, $5); type_ligne = trunc(10, $15); print numero_facture code_journal date_facturation date_echeance trunc(12,"") compte trunc(3, "") libelle trunc(2, "") montant sens trunc(5, "") ressortissant trunc(3, "") libelle_complet trunc(3, "") type_ligne }' | grep -E "^[0-9]+"
+cat | awk -F ";" '
+function trunc(len, str) { return sprintf("%-" len "s", sprintf("%." len "s", str)) }
+{
+    numero_facture = trunc(7, $4);
+    code_journal = trunc(2, $1);
+    date_facturation = trunc(8, gensub(/-/, "", "g", $2));
+    date_echeance = trunc(8, gensub(/-/, "", "g", $9));
+    if(!$9) { date_echeance = date_facturation } compte = trunc(8, $6);
+    if($7) { compte = trunc(8, $7) } libelle = trunc(18, "Facture no " numero_facture);
+    montant = trunc(12, $11); sens = trunc(1, $10);
+    ressortissant = trunc(30, $16);
+    libelle_complet = trunc(55, $5);
+    type_ligne = trunc(10, $15);
+    print numero_facture code_journal date_facturation date_echeance trunc(12,"") compte trunc(3, "") libelle trunc(2, "") montant sens trunc(5, "") ressortissant trunc(3, "") libelle_complet trunc(3, "") type_ligne
+}' | grep -E "^[0-9]+"
--- project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV_bivc.class.php
+++ project/plugins/acVinFacturePlugin/lib/export/ExportFactureCSV_bivc.class.php
@@ -2,4 +2,26 @@
 
 class ExportFactureCSV_bivc extends ExportFactureCSV_ivso {
 
+    public function getMontant($montant, $sens) {
+        if($montant < 0) {
+
+            return $montant * -1;
+        }
+
+        return $montant;
+    }
+
+    public function getSens($montant, $sens) {
+        if($montant < 0 && $sens == "CREDIT") {
+
+            return "DEBIT";
+        }
+
+        if($montant < 0 && $sens == "DEBIT") {
+
+            return "CREDIT";
+        }
+
+        return $sens;
+    }
 }
