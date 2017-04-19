54b7eb7b0bfbd37a134f36e4e77f37755cbcc960
Tangui Morlier <tangui@tangui.eu.org>
2017-04-19 18:32:48 +0200

    Numéro de Fax dans l'entête des documents PDF

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_generique_prelatex.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_generique_prelatex.php
@@ -40,7 +40,8 @@
 \def\NomInterpro{<?php echo sfConfig::get('facture_configuration_facture', array('pdf_nom_interpro'=>''))['pdf_nom_interpro']; ?>}
 \def\InterproAdresse{ <?php echo sfConfig::get('app_configuration_facture')['emetteur_libre']['adresse']; ?>\\
 		       <?php echo sfConfig::get('app_configuration_facture')['emetteur_libre']['code_postal']." ".sfConfig::get('app_configuration_facture_emetteur_libre_ville'); ?> }
-\def\InterproContact{\\<?php echo sfConfig::get('app_configuration_facture')['emetteur_libre']['telephone']; ?>
+\def\InterproContact{\\ Tel: <?php echo sfConfig::get('app_configuration_facture')['emetteur_libre']['telephone'];
+$fax = sfConfig::get('app_configuration_facture')['emetteur_libre']['fax']; echo ($fax) ? ' - Fax :' .$fax : '' ;?>
                                                     \\ Email : <?php echo sfConfig::get('app_configuration_facture')['emetteur_libre']['email']; ?>
 										}
 \def\InterproSIRET{<?php echo sfConfig::get('app_configuration_facture')['infos_interpro']['siret']; ?>}
