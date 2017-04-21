c8d19bed12ccef91d484568bb91033b3e462ca2a
Tangui Morlier <tangui@tangui.eu.org>
2017-04-21 19:38:06 +0200

    ID GENERATION pas statique mais est renseigné par argument

--- project/bin/pdf_ressortissants_sans_facture.sh
+++ project/bin/pdf_ressortissants_sans_facture.sh
@@ -5,10 +5,11 @@
 GENERATION=$1
 
 if ! echo $GENERATION | grep GENERATION  > /dev/null ; then
-  exit 0;
+  echo "GENERATION argument missing"
+  exit 1;
 fi
 
-curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/GENERATION-FACTURE-20170403154806 | sed 's/.*"documents":\[//' | sed 's/].*//' | sed 's/-[0-9]*",*/\n/g' | sed 's/"FACTURE-//' | sort -u > $TMP/factures.list
+curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$GENERATION | sed 's/.*"documents":\[//' | sed 's/].*//' | sed 's/-[0-9]*",*/\n/g' | sed 's/"FACTURE-//' | sort -u > $TMP/factures.list
 php symfony export:societe $SYMFONYTASKOPTIONS | grep ACTIF | grep 'REGION_CVO' | awk -F ';' '{print $1}' societe.csv | grep '^[0-9]' | sed 's/411//' | sort -u > $TMP/societes.list
 
 diff /tmp/societes.list /tmp/factures.list | grep '<' | sed 's/< //' | while read $societeid; do
