502f7a1815947c30793ff23a6f5d449e55bee7df
Vincent LAURENT <vince.laurent@gmail.com>
2017-05-13 20:30:06 +0200

    Nav récupérer à partir du CIVA

--- project/apps/civa/modules/common/templates/_header.php
+++ project/apps/civa/modules/common/templates/_header.php
@@ -6,3 +6,12 @@
 <?php if(!$compte && $sf_request->getAttribute('sf_route')->getRawValue() instanceof InterfaceSocieteRoute): $compte = $sf_request->getAttribute('sf_route')->getSociete()->getMasterCompte()->identifiant; endif; ?>
 
 <?php echo file_get_contents(sfConfig::get('app_url_header')."?compte=".$compte."&isAdmin=".$isAdmin."&compteOrigine=".$compteOrigine); ?>
+
+<div id="main">
+    <?php if($compte): ?>
+    <div id="nav">
+        <?php echo file_get_contents(sfConfig::get('app_url_nav')."?compte=".$compte); ?>
+    </div>
+    <?php else: ?>
+        <div style="height: 20px;"></div>
+    <?php endif; ?>
