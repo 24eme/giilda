f103a780cb631d4fb05c028d3637d465eccf05b6
Vincent LAURENT <vince.laurent@gmail.com>
2017-06-23 18:00:56 +0200

    L'entrée coopérative nécessite aussi une date de replacement

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRMDetail.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRMDetail.class.php
@@ -191,7 +191,8 @@ class DRMDetail extends BaseDRMDetail {
         if(($this->entrees->exist('retourmarchandisesanscvo') && $this->entrees->retourmarchandisesanscvo)
           || ($this->entrees->exist('retourmarchandisetaxees') && $this->entrees->retourmarchandisetaxees)
           || ($this->entrees->exist('retourmarchandisenontaxees') && $this->entrees->retourmarchandisenontaxees)
-          || ($this->entrees->exist('transfertcomptamatierecession') && $this->entrees->transfertcomptamatierecession)) {
+          || ($this->entrees->exist('transfertcomptamatierecession') && $this->entrees->transfertcomptamatierecession)
+          || ($this->entrees->exist('cooperative') && $this->entrees->cooperative)) {
           $this->add('replacement_date',null);
         }else{
           $this->remove('replacement_date');
