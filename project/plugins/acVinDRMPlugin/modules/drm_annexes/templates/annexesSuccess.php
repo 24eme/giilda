7be4b0d6f2952815efa791fb7ae4e10662471982
Mathurin Petit <mathurin.petit@gmail.com>
2017-07-19 19:54:35 +0200

    maj annexes

--- project/plugins/acVinDRMPlugin/modules/drm_annexes/templates/annexesSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm_annexes/templates/annexesSuccess.php
@@ -135,7 +135,7 @@ $paiement_douane_frequence = ($societe->exist('paiement_douane_frequence')) ? $s
                                   <th class="col-xs-1" ></th>
                               </tr>
                             </thead>
-                            <tbody class="drm_non_apurement" id="nonapurement_list">
+                            <tbody class="drm_annexes_stats_europeenes" id="stats_europeenes_list">
                               <tr>
                                 <td class="col-xs-8"><?php echo $annexesForm['statistiques_jus']->renderLabel() ?></td>
                                 <td class="col-xs-3"><?php echo $annexesForm['statistiques_jus']->render() ?><?php echo $annexesForm['statistiques_jus']->renderError() ?></td>
