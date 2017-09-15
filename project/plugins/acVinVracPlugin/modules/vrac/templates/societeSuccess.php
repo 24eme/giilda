f1d8618990f0bd03813a1882274a8792238638ae
Vincent LAURENT <vince.laurent@gmail.com>
2017-09-15 17:04:21 +0200

    Suppression de l'appel $societe->getRepresentant car la méthode n'existe pas

--- project/plugins/acVinVracPlugin/modules/vrac/templates/societeSuccess.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/societeSuccess.php
@@ -79,7 +79,7 @@ use_helper('PointsAides');
         <a class="btn btn-default" href="<?php echo url_for('vrac_history', array('identifiant' => $etablissementPrincipal->identifiant, 'campagne' => ConfigurationClient::getInstance()->getCurrentCampagne(), 'etablissement' => 'tous')); ?>">
             Voir tout l'historique
         </a>
-        <?php if ($societe->isCourtier() || $societe->isNegociant() || $societe->isRepresentant()):
+        <?php if ($societe->isCourtier() || $societe->isNegociant()):
           if ($societe->isNegociant()) {
             $etablissementCreateur = $societe->getNegociant();
           }else{
