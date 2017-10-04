bbe8b0cc6dec2d70a8a40fed69aee1e2b5f60623
Mathurin Petit <mathurin.petit@gmail.com>
2017-10-04 16:10:08 +0200

    ajout d'un InflectorCaseInsensitiv pour matcher des produits écrits en minuscule

--- project/plugins/acVinLibPlugin/lib/util/KeyInflector.class.php
+++ project/plugins/acVinLibPlugin/lib/util/KeyInflector.class.php
@@ -204,4 +204,25 @@ class KeyInflector
 
         return trim($text, '-');
     }
+
+		/**
+		 * Convert any passed string to a url friendly string. Converts 'My first blog post' to 'my-first-blog-post'
+		 *
+		 * @param  string $text  Text to urlize
+		 * @return string $text  Urlized text
+		 */
+		public static function slugifyCaseSensitive($text)
+		{
+				// Remove all non url friendly characters with the unaccent function
+				$text = self::unaccent($text);
+
+				// Remove all none word characters
+				$text = preg_replace('/\W/', ' ', $text);
+
+				// More stripping. Replace spaces with dashes				
+				return trim(preg_replace('/[^A-Z^a-z^0-9^\/]+/', '-',
+													 preg_replace('/([a-z\d])([A-Z])/', '\1_\2',
+													 preg_replace('/([A-Z]+)([A-Z][a-z])/', '\1_\2',
+													 preg_replace('/::/', '/', $text)))), '-');
+		}
 }
