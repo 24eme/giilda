027f4512b0ed7dcdd4bd4d24677c1f8fd0d9f1f1
Tangui Morlier <tangui@tangui.eu.org>
2017-10-13 20:03:55 +0200

    Sécurité pour éviter de supprimer la base

--- project/bin/delete_from_object.sh
+++ project/bin/delete_from_object.sh
@@ -1,5 +1,8 @@
 . bin/config.inc
 
-	
 OBJREV=$(curl -s $1 | sed 's/{"_id":"//' | sed 's/","_rev":"/?rev=/' | sed 's/".*//')
-curl -s -X DELETE http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$OBJREV
+if test "$OBJREV" ; then
+	curl -s -X DELETE http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$OBJREV
+else
+	echo "Document non trouvé. Il faut mettre l'url du document"
+fi
