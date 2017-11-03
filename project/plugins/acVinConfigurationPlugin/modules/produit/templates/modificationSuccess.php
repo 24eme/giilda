6757dfac9821fca3ab84ded4958be0ca40a81b48
Tangui Morlier <tangui@tangui.eu.org>
2017-11-03 18:14:46 +0100

    Meilleure présentation de la configuration (titre, nav, ...)

--- project/plugins/acVinConfigurationPlugin/modules/produit/templates/modificationSuccess.php
+++ project/plugins/acVinConfigurationPlugin/modules/produit/templates/modificationSuccess.php
@@ -2,11 +2,21 @@
     <li>
         <a href="<?php echo url_for("produits") ?>">Produits</a>
     </li>
+    <?php
+    $crum = array();
+    for($n = $form->getObject() ; preg_match('/declaration\/certifications/', $n->getHash()) ; $n = $n->getParent()->getParent()) {
+      $crum[] = array('hash' => $n->getHash(), 'noeud' => $n, 'libelle' => ($n->getLibelle()) ? $n->getLibelle() : 'DEFAUT') ;
+    }
+    $crum = array_reverse($crum);
+    foreach ($crum as $c) {
+      $n = $c['noeud'];
+      echo "<li><a href=\"".url_for('produit_modification', array('noeud' => $n->getTypeNoeud(), 'hash' => $produit->getHashForKey())).'">'.$c['libelle'].'</a></li>';
+    } ?>
     <li class="active">
-        <a href=""><?php echo sprintf("Modification du noeud %s : %s (%s)", $form->getObject()->getTypeNoeud(), $form->getObject()->getLibelle(), $form->getObject()->getKey()) ?></a>
+        <a href="#"><?php echo sprintf("Modification du noeud %s : %s (%s)", $form->getObject()->getTypeNoeud(), $form->getObject()->getLibelle(), $form->getObject()->getKey()) ?></a>
     </li>
 </ol>
 
-<h2>Modification du noeud</h2>
+<h2><?php echo sprintf("Modification du noeud %s : %s (%s)", $form->getObject()->getTypeNoeud(), $form->getObject()->getLibelle(), $form->getObject()->getKey()) ; ?></h2>
 
 <?php include_partial('produit/form', array('form' => $form, 'produit' => $produit)) ?>
