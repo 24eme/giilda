30c97f5b5abacfe3407a7c0de00fd615c66524f1
Tangui Morlier <tangui@tangui.eu.org>
2018-01-17 19:16:22 +0100

    Ajout des statuts suspendu si on ne l'a pas dans le mouvement (genre c'est une SV12)

--- project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
@@ -9,16 +9,23 @@ function(doc) {
          return;
      }
 
-     for(identifiant in doc.mouvements) {
-         if(identifiant == doc.identifiant) {
+     identifiant = doc.identifiant;
+     if(doc.mouvements[identifiant]) {
              for (key in doc.mouvements[identifiant]) {
                  var mouv = doc.mouvements[identifiant][key];
                  pays = "";
-         	    if (mouv.type_hash.match(/^export.*_details$/)) {
-         		          pays = mouv.detail_libelle;
-         	    }
-                 emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, mouv.type_drm, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, mouv.type_drm_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays, mouv.facture]);
+                 if (mouv.type_hash && mouv.type_hash.match(/^export.*_details$/)) {
+                   pays = mouv.detail_libelle;
+                 }
+                 type_drm = "SUSPENDU";
+                 if (mouv.type_drm) {
+                   type_drm = mouv.type_drm;
+                 }
+                 type_drm_libelle = "Suspendu";
+                 if (mouv.type_drm_libelle) {
+                   type_drm_libelle = mouv.type_drm_libelle;
+                 }
+                 emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, type_drm, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, type_drm_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays, mouv.facture]);
              }
-         }
      }
  }
