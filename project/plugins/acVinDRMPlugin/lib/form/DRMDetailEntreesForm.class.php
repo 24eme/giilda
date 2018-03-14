bd51228389acbdbaee5c0a08969d332219383a15
Vincent LAURENT <vince.laurent@gmail.com>
2018-03-14 11:41:55 +0100

    Corrections erreur de syntaxe

--- project/plugins/acVinDRMPlugin/lib/form/DRMDetailEntreesForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/DRMDetailEntreesForm.class.php
@@ -11,7 +11,7 @@ class DRMDetailEntreesForm extends acCouchdbObjectForm {
         foreach ($configurationDetail->getEntreesSorted() as $key => $value) {
             if ($value->readable) {
                 if (!$value->writable || (preg_match($declassementIgp, $certif) && ($key == 'declassement'))
-                    || ($certif == 'AUTRES') && ($key != 'recolte') && ($key != 'revendication') && ($key != 'transfertsrecolte'))
+                    || (($certif == 'AUTRES') && ($key != 'recolte') && ($key != 'revendication') && ($key != 'transfertsrecolte'))
                     || (preg_match('/USAGESINDUSTRIELS/', $appellation) && (!$value->restriction_lies))
 		                ||  (preg_match('/VINSSIG/', $certif) && ($key == 'repli')))
                 {
