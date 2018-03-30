e5ce452c21db0d1d74cccbaf2e3a8506a652f1e4
Tangui Morlier <tangui@tangui.eu.org>
2018-03-30 17:34:24 +0200

    Un peu d'air pour la fin d'une page de facture bien remplie

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_generique.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_generique.php
@@ -53,6 +53,7 @@ foreach ($facture->lignes as $type => $typeLignes) {
             ?>
             ~ & ~ & ~ & ~ &\\
           <?php endfor; ?>
+          ~ & ~ & ~ & ~ &\\
                             \end{tabular}
                           };
             \node[draw=gray, inner sep=-2pt, rounded corners=3pt, line width=2pt, fit=(tab1.north west) (tab1.north east) (tab1.south east) (tab1.south west)] {};
