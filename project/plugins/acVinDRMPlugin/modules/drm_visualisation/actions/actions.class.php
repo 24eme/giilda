a9be879b02a628398188f74ee2dffb68245fb200
Tangui Morlier <tangui@tangui.eu.org>
2018-04-16 12:28:49 +0200

    Gestion des droits pour la réouverture

--- project/plugins/acVinDRMPlugin/modules/drm_visualisation/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm_visualisation/actions/actions.class.php
@@ -13,10 +13,12 @@
  */
 class drm_visualisationActions extends drmGeneriqueActions {
     public function executeReopen(sfWebRequest $request) {
-      $this->redirect403IfIsTeledeclaration();
+      if (!$this->getUser()->hasCredential(myUser::CREDENTIAL_ADMIN)) {
+        $this->redirect403IfIsTeledeclaration();
+      }
       $drm = $this->getRoute()->getDRM();
       $this->redirect403Unless($drm->isTeledeclare());
-      $this->redirect403Unless(!$drm->isNonFactures());
+      $this->redirect403Unless($drm->isNonFactures());
       $drm->valide->date_saisie = null;
       $drm->valide->date_signee = null;
       $drm->save();
@@ -33,7 +35,7 @@ class drm_visualisationActions extends drmGeneriqueActions {
         $this->hide_rectificative = $request->getParameter('hide_rectificative');
         $this->drm_suivante = $this->drm->getSuivante();
         $this->mouvements = DRMMouvementsConsultationView::getInstance()->getMouvementsByEtablissementAndPeriode($this->drm->identifiant, $this->drm->periode);
-        $this->mouvementsByProduit = DRMClient::getInstance()->sortMouvementsForDRM($this->mouvements);      
+        $this->mouvementsByProduit = DRMClient::getInstance()->sortMouvementsForDRM($this->mouvements);
 
         $this->recapCvos = $this->recapCvos();
     }
--- project/plugins/acVinDRMPlugin/modules/drm_visualisation/templates/visualisationSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm_visualisation/templates/visualisationSuccess.php
@@ -106,8 +106,8 @@
       <?php echo getPointAideHtml('drm','visualisation_pdf'); ?>
         <a href="<?php echo url_for('drm_pdf', array('identifiant' => $drm->getIdentifiant(), 'periode_version' => $drm->getPeriodeAndVersion(), 'appellation' => 0)); ?>" class="btn btn-success">Télécharger le PDF</a>
     </div>
-    <?php if ($drm->isTeledeclare() && !$isTeledeclarationMode) : ?>
-      <div class="col-xs-4 text-right">
+    <div class="col-xs-4 text-right">
+    <?php if ($drm->isTeledeclare() && (!$isTeledeclarationMode || ($sf_user->hasCredential(AppUser::CREDENTIAL_ADMIN)))) : ?>
           <?php if ($drm->isNonFactures()): ?>
           <a href="<?php echo url_for('drm_reopen', $drm); ?>" class="btn btn-warning">Reouvrir la DRM</a>
           <?php else: ?>
