3c7ca6c4e6c4774020f2a1556a0022998a97adc4
Vincent LAURENT <vince.laurent@gmail.com>
2018-04-17 15:43:30 +0200

    bash d'intégration continue

---
+++ project/bin/integration_continue.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+. bin/config.inc
+
+if [ "$(echo $COUCHTEST | grep -E _test$)" == "" ]
+then
+    echo "La base $COUCHTEST ne semble pas être une base de test (doit se terminer par \"_test\")"
+    exit;
+fi
+
+APPLICATION=$1
+FORCE=$2
+
+if [ ! $APPLICATION ]
+then
+    echo "Vous devez définir une application"
+    exit;
+fi
+
+git pull -f
+
+BRANCH=$(cat ../.git/HEAD | sed -r 's|^ref: refs/heads/||')
+LASTCOMMIT=$(cat $WORKINGDIR/../.git/refs/heads/$BRANCH)
+DATE=$(date +%Y%m%d%H%M%S)
+
+if [ "$(ls $XMLTESTDIR | grep $LASTCOMMIT | grep $APPLICATION)" != "" ] && [ "$FORCE" = "" ]
+then
+    echo "Test déjà effectué sur le commit $LASTCOMMIT"
+    exit;
+fi
+
+curl -X DELETE $COUCHTEST
+curl -X PUT $COUCHTEST
+
+cd ..
+rm -rf .views/*
+make
+cd -
+
+ls $WORKINGDIR/data/configuration/$APPLICATION | while read jsonFile
+do
+    curl -X POST -d @data/configuration/bivc/$jsonFile  -H "content-type: application/json" $COUCHTEST
+done
+
+php symfony cc
+
+APPLICATION=$APPLICATION NODELETE=1 php symfony test:unit --xml=$XMLTESTDIR/"$DATE"_"$APPLICATION"_"$LASTCOMMIT"_"$BRANCH".xml
