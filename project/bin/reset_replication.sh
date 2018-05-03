bd76cf55477a67e1d93796e36f2bcd9d8ffb4a49
Vincent LAURENT <vince.laurent@gmail.com>
2018-05-03 11:08:14 +0200

    La réplication par doc suffit

--- project/bin/reset_replication.sh
+++ project/bin/reset_replication.sh
@@ -4,7 +4,4 @@
 
 rev=$(curl -s http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC | sed 's/.*rev":"//' | sed 's/".*//' )
 curl -s -X DELETE http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC?rev=$rev  > /dev/null
-#Insure views are replicated as well
-curl -s -X POST -H "Content-Type: application/json" -d '{"target":"'$COUCHBASE'","source":"'http://$COUCHDISTANTHOST:$COUCHPORT/$COUCHBASE'"}' http://$COUCHHOST:$COUCHPORT/_replicate > /dev/null
-#Repost the replication
 curl -s -X PUT -d '{"_id":"$REPLICATIONDOC","target":"'$COUCHBASE'","source":"'http://$COUCHDISTANTHOST:$COUCHPORT/$COUCHBASE'","continuous":true,"create_target":true,"user_ctx": {"roles": ["_admin"]}}' http://$COUCHHOST:$COUCHPORT/_replicator/$REPLICATIONDOC  > /dev/null
