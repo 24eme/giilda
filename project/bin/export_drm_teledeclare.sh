de1e54ae164e65fe4d450cb1da0261b18f5f8648
Vincent LAURENT <vince.laurent@gmail.com>
2018-05-16 17:22:35 +0200

    Export des drms

--- project/bin/export_drm_teledeclare.sh
+++ project/bin/export_drm_teledeclare.sh
@@ -2,6 +2,15 @@
 
 PRODUIT=$1
 EXPORT_PATH=$2
+NUMEROSEQUENCE=0
+COUCHDBSEQFILE=$EXPORT_PATH/.couchdbseq
+CHANGESFILE=$TMP/$(date +%Y%m%d%H%M%S)_export_changes
+LISTDRMFILE=$TMP/$(date +%Y%m%d%H%M%S)_export_drms
+
+if test -f $COUCHDBSEQFILE
+then
+    NUMEROSEQUENCE=$(cat $COUCHDBSEQFILE)
+fi
 
 if ! test "$PRODUIT"; then
     echo "Il manque le nom du produit à filtrer en 1er argument"
@@ -13,24 +22,47 @@ if ! test "$EXPORT_PATH"; then
     exit;
 fi
 
-echo -n > $TMP/drms
+echo -n > $LISTDRMFILE
 
-curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes | grep "\"DRM-" | cut -d "," -f 2 | sed 's/"id":"//' | sed 's/"//' | while read id
+curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?since=$NUMEROSEQUENCE > $CHANGESFILE
+LASTNUMEROSEQUENCE=$(grep "last_seq" $TMP/changes | sed 's/"last_seq"://' | sed 's/}//')
+
+cat $CHANGESFILE | grep "\"DRM-" | cut -d "," -f 2 | sed 's/"id":"//' | sed 's/"//' | while read id
 do
-    curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$id | jq -c '[._id,.teledeclare,.valide.date_signee,.declarant.no_accises]' | sed 's/\[//' | sed 's/\]//' | sed 's/"//g' | grep "true" | sed 's/null//' >> $TMP/drms
+    curl -s http://$COUCHHOST:$COUCHPORT/$COUCHBASE/$id | jq -c '[._id,.teledeclare,.valide.date_signee,.declarant.no_accises]' | sed 's/\[//' | sed 's/\]//' | sed 's/"//g' | grep "true" | sed 's/null//' >> $LISTDRMFILE
 done
 
-cat $TMP/drms| while read ligne
+
+cat $LISTDRMFILE | while read ligne
 do
     ID=$(echo $ligne | cut -d "," -f 1)
     DATE=$(echo $ligne | cut -d "," -f 3 | sed 's/-//g')
     NOACCISES=$(echo $ligne | cut -d "," -f 4)
     PERIODE=$(echo $ID | cut -d "-" -f 3)
-    DRMFILETMP=$TMP/$id$(date +%Y%m%d%H%M%S)
-    php symfony drm:export-csv $ID $SYMFONYTASKOPTIONS | grep $PRODUIT > $DRMFILETMP
-    if test $(cat $DRMFILETMP | grep -E "^CAVE;" | grep $PRODUIT | wc -l | sed -r 's/^0$//'); then
+    DRMFILE=$TMP/$id$(date +%Y%m%d%H%M%S).csv
+    php symfony drm:export-csv $ID $SYMFONYTASKOPTIONS | grep $PRODUIT > $DRMFILE
+    if test $(cat $DRMFILE | grep -E "^CAVE;" | grep $PRODUIT | wc -l | sed -r 's/^0$//'); then
         echo $ID
         mkdir -p $EXPORT_PATH 2> /dev/null
-        cp $DRMFILETMP $EXPORT_PATH/"$DATE"_"$PERIODE"_"$NOACCISES"_"$ID".csv
+        cp $DRMFILE $EXPORT_PATH/"$DATE"_"$PERIODE"_"$NOACCISES"_"$ID".csv
     fi
+    rm $DRMFILE
 done
+
+echo $LASTNUMEROSEQUENCE > $COUCHDBSEQFILE
+rm $CHANGESFILE
+rm $LISTDRMFILE
+echo "<?php
+header(\"Content-Type: text/plain\");
+
+\$files = scandir(dirname(__FILE__));
+rsort(\$files);
+
+foreach(\$files as \$file) {
+	if(!preg_match('/.csv$/', \$file)) {
+		continue;
+	}
+
+	echo \"http://\".\$_SERVER['HTTP_HOST'].\$_SERVER['REQUEST_URI'].\$file.\"\n\";
+}
+" > $EXPORT_PATH/index.php
