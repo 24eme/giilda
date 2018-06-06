66daf3ce7f683b457fd0eab66376a7c875a56224
Mathurin Petit <mathurin.petit@gmail.com>
2018-06-06 15:14:25 +0200

    Enlevements caclulé en live à chaque validate de DRM ou chaque validate de SV12

--- project/plugins/acVinVracPlugin/lib/model/Vrac/Vrac.class.php
+++ project/plugins/acVinVracPlugin/lib/model/Vrac/Vrac.class.php
@@ -535,12 +535,24 @@ class Vrac extends BaseVrac {
         return $this->getDateSaisie();
     }
 
-    public function enleverVolume($vol) {
-        $this->volume_enleve += $vol;
 
-        if ($this->volume_enleve < 0) {
-
-            throw new sfException(sprintf("Suite à un enlevement le volume enleve sur le contrat '%s' est négatif, ce n'est pas normal !", $this->get('_id')));
+    public function updateVolumesEnleves() {
+        $this->volume_enleve = 0;
+        $mvts_drm = DRMMouvementsConsultationView::getInstance()->getMouvementsByEtablissementAndCampagne($this->vendeur_identifiant, $this->campagne);
+        $mvts_sv12 = SV12MouvementsConsultationView::getInstance()->getMouvementsByEtablissementAndCampagne($this->acheteur_identifiant, $this->campagne);
+        foreach ($mvts_drm as $key => $mvt) {
+            $pos = strpos($mvt->produit_hash, $this->produit);
+            if($mvt->type_hash == "vrac_details" && ($pos !== false) && $mvt->detail_identifiant == $this->_id){
+                $volume_enleve = $mvt->volume * -1;
+                $this->volume_enleve += $volume_enleve;
+            }
+        }
+        foreach ($mvts_sv12 as $key => $mvt) {
+            $pos = strpos($mvt->produit_hash, $this->produit);
+            if($pos !== false && $mvt->detail_identifiant == $this->_id){
+                $volume_enleve = $mvt->volume * -1;
+                $this->volume_enleve += $volume_enleve;
+            }
         }
         $seuil_contrat = VracConfiguration::getInstance()->getSoldeSeuil();
         if (strpos($seuil_contrat, '%') !== FALSE) {
@@ -558,6 +570,7 @@ class Vrac extends BaseVrac {
             }
         }
     }
+
 
     public function isSolde() {
         return $this->valide->statut == VracClient::STATUS_CONTRAT_SOLDE;
--- project/test/unit/giilda31DRMVracTest.php
+++ project/test/unit/giilda31DRMVracTest.php
@@ -87,6 +87,8 @@ $t->comment("validation de la DRM et génération des mouvements");
 $drm->validate();
 $drm->save();
 
+$drm->updateVracs();
+
 $mvts_viti = $drm->mouvements->{$drm->identifiant};
 $t->is(count($mvts_viti), 3, $drm->_id." : la validation a généré trois mouvements viti");
 foreach ($mvts_viti as $mvt) {
@@ -181,3 +183,27 @@ foreach($drm_mod->mouvements->{$viti->identifiant} as $k => $mvt) {
   $somme_cvo += $mvt->cvo * $mvt->volume;
 }
 $t->is($somme_cvo, 0, $drm_mod->_id." : Le viti est facturé de toute la cvo");
+
+$drm_mod = DRMClient::getInstance()->find($drm_mod->_id);
+$drm_mod->devalide();
+$drm_mod->save();
+$t->is($drm_mod->isValidee(), false, $drm_mod->_id." : La DRM est réouverte");
+
+$drm_mod = DRMClient::getInstance()->find($drm_mod->_id);
+$vrac_details = $drm_mod->getProduit($produit_hash, 'details')->get('sorties/vrac_details');
+$vrac_details->remove($contrat_key);
+$contrat = DRMESDetailVrac::freeInstance($drm_mod);
+$contrat->identifiant = $vrac->value[VracSoussigneIdentifiantView::VRAC_VIEW_VALUE_ID];
+$contrat->volume = 50;
+$detailVrac = $vrac_details->addDetail($contrat);
+
+$drm_mod->update();
+$drm_mod->validate();
+$drm_mod->save();
+
+$drm_mod->updateVracs();
+
+$t->is(boolval($drm_mod->isValidee()), true, $drm_mod->_id." : La DRM est de nouveau validée");
+
+$vracObj = VracClient::getInstance()->find($vrac->value[VracSoussigneIdentifiantView::VRAC_VIEW_VALUE_ID]);
+$t->is($vracObj->volume_enleve, 150, $vracObj->_id." : Le contrat a pour volume enlevé : 150");
