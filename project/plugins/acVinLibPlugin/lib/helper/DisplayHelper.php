41bb1dd0afab5d1ee5340f2ea34dca77b6c7378f
Tangui Morlier <tangui@tangui.eu.org>
2018-07-31 10:25:44 +0200

    Nouveu logo pour les DS + petit changement dans le cadre d'information

--- project/plugins/acVinLibPlugin/lib/helper/DisplayHelper.php
+++ project/plugins/acVinLibPlugin/lib/helper/DisplayHelper.php
@@ -67,23 +67,22 @@ function display_latex_message_communication($string) {
         }
         echo '\textbf{'.$msg.'} ';
         return;
-            
+
 }
 
 function cut_latex_string($string, $limit) {
     $disp = escape_string_for_latex($string);
-    
+
     $len = strlen($disp);
     if ($len > $limit) {
         $disp = substr($disp, 0, $limit-3).'...';
-    }  
+    }
     return $disp;
 }
-   
+
 function enteteDs($ds,$date_echeance){
     if($ds->getEtablissement()->isNegociant())
-        return 'Cet imprimé doit \^{e}tre obligatoirement rempli \underline{\textbf{avant le '.$date_echeance.'}} au plus\\\\tard par tous les négociants, \textbf{détenant des \underline{stocks de vins d\'appellation}}\\\\ \textbf{\underline{d\'origine}} (revendiqués et/ou agrées) \textbf{et quels que soient leurs lieux}\\\\ \textbf{d\'entreposage} selon la liste proposée ci-après conformément à l\'Accord\\\\ Interprofessionnel d\'InterLoire en vigueur.';
+        return 'Cet imprimé doit \^{e}tre obligatoirement rempli \underline{\textbf{avant le '.$date_echeance.'}} au plus\\\\tard par tous les négociants, \textbf{détenant des \underline{stocks de vins AOP}}\\\\ \textbf{\underline{ou IGP}} (revendiqués et/ou agrées) \textbf{et quels que soient leurs lieux}\\\\ \textbf{d\'entreposage} selon la liste proposée ci-après conformément à l\'Accord\\\\ Interprofessionnel d\'InterLoire en vigueur.';
     else
-        return 'Cet imprimé doit \^{e}tre obligatoirement rempli \textsl{\textbf{avant le '.$date_echeance.'}} au plus tard\\\\par tous les propriétaires, fermiers, métayers, groupements de producteurs\\\\ \textbf{détenant des \textsl{stocks de vins d\'appellation d\'origine}} (revendiqués et/ou agrées)\\\\ \textbf{et quels que soient leurs lieux d\'entreposage} selon la liste proposée ci-après\\\\conformément à l\'Accord Interprofessionnel d\'InterLoire en vigueur.';
+        return 'Cet imprimé doit \^{e}tre obligatoirement rempli \textsl{\textbf{avant le '.$date_echeance.'}} au plus tard\\\\par tous les propriétaires, fermiers, métayers, groupements de producteurs\\\\ \textbf{détenant des \textsl{stocks de vins AOP ou IGP}} (revendiqués et/ou agrées)\\\\ \textbf{et quels que soient leurs lieux d\'entreposage} selon la liste proposée ci-après\\\\conformément à l\'Accord Interprofessionnel d\'InterLoire en vigueur.';
 }
-
