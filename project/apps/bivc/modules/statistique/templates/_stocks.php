3c71a6b5f8b6969bd37d71f755525c42f4fb5586
Tangui Morlier <tangui@tangui.eu.org>
2018-08-28 12:54:29 +0200

    Gestion de la pagination plus générique

--- project/apps/bivc/modules/statistique/templates/_stocks.php
+++ project/apps/bivc/modules/statistique/templates/_stocks.php
@@ -4,7 +4,9 @@ use_helper('BivcStatistique');
 
 $csv = "Appellation;Catégorie;Stock initial;Stock actuel;TOTAL mvt\n";
 foreach ($result['agg_page']['buckets'] as $appellation) {
-	$appellationLibelle = str_replace('AOC', '', $appellation['key']);
+	$appellationKey = str_replace('\\', '', $appellation['key']);
+	$appellationLibelle = preg_replace('/AOC */', '', ConfigurationClient::getCurrent()->get($appellationKey)->getLibelleFormat());
+	$globalLibelle = preg_replace('/AOC */', '', ConfigurationClient::getCurrent()->get($appellationKey)->getAppellation()->getLibelleFormat());
 	$totalStockInitial = (formatNumber($appellation['total_stock_initial']['value']) != 0)? formatNumber($appellation['total_stock_initial']['value']) : null;
 	$totalStockFinal = (formatNumber($appellation['total_stock_final']['value']) != 0)? formatNumber($appellation['total_stock_final']['value']) : null;
 	$totalTotal = (formatNumber($appellation['total_total']['value']) != 0)? formatNumber($appellation['total_total']['value']) : null;
@@ -13,9 +15,9 @@ foreach ($result['agg_page']['buckets'] as $appellation) {
 		$stockInitial = (formatNumber($categorie['stock_initial']['agg_column']['value']) != 0)? formatNumber($categorie['stock_initial']['agg_column']['value']) : null;
 		$stockFinal = (formatNumber($categorie['stock_final']['agg_column']['value']) != 0)? formatNumber($categorie['stock_final']['agg_column']['value']) : null;
 		$totalMvt = (formatNumber($categorie['total']['value']) != 0)? formatNumber($categorie['total']['value']) : null;
-		$csv .= $appellationLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$stockFinal.';'.$totalMvt."\n";
+		$csv .= $globalLibelle.';'.$appellationLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$stockFinal.';'.$totalMvt."\n";
 	}
-	$csv .= $appellationLibelle.';TOTAL;'.$totalStockInitial.';'.$totalStockFinal.';'.$totalTotal."\n";
+	$csv .= $globalLibelle.';'.$appellationLibelle.';TOTAL;'.$totalStockInitial.';'.$totalStockFinal.';'.$totalTotal."\n";
 }
-$csv .= 'TOTAL;TOTAL;'.formatNumber($result['totaux_stock_initial']['value']).';'.formatNumber($result['totaux_stock_final']['value']).';'.formatNumber($result['totaux_total']['value'])."\n";
+$csv .= 'TOTAL;TOTAL;TOTAL;'.formatNumber($result['totaux_stock_initial']['value']).';'.formatNumber($result['totaux_stock_final']['value']).';'.formatNumber($result['totaux_total']['value'])."\n";
 echo $csv;
