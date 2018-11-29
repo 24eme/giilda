49699f79826bb3bb0b379520e995b300d4f4fa3d
mathurin petit <mathurin.petit@gmail.com>
2018-11-29 18:30:01 +0100

    ajout des totaux dans les stats stocks

--- project/apps/ivbd/modules/statistique/templates/_stocks.php
+++ project/apps/ivbd/modules/statistique/templates/_stocks.php
@@ -29,7 +29,7 @@ if ($lastPeriode) {
 		foreach ($lastPeriode as $lastPeriodeKey => $lastPeriodeCaseValue) {
 			$key = sfOutputEscaper::unescape($lastPeriodeKey);
 			$tabKey = explode('/', $lastPeriodeKey);
-			$csv .= $tabKey[0].';'.$tabKey[1].";";			
+			$csv .= $tabKey[0].';'.$tabKey[1].";";
 			foreach ($lastPeriode[$key] as $lastPeriodeKeyKey => $lastPeriodeKeyValue) {
 				$csv .= ";".$lastPeriodeKeyValue;
 				$csv .=  ($lastPeriodeKeyKey == count($lastPeriode[$key]) - 1)? "\n" : ';';
@@ -38,22 +38,36 @@ if ($lastPeriode) {
 	}
 }else{
 	$csv =  "Produit;Catégorie;Stock initial;Somme mouvements;Stock final\n";
+	$tStockInitial = 0;
+	$tStockFinal = 0;
+	$tTotal = 0;
 	foreach ($result['agg_page']['buckets'] as $produitCepage) {
 			if(preg_match("/details(Acquitte)?\/DEFAUT$/",$produitCepage['key'])){
 				$produitCepageKey = str_replace(array('\\', '/details/DEFAUT','/detailsACQUITTE/DEFAUT'), array('','',''), $produitCepage['key']);
 				$produitCepageLibelle = preg_replace('/AOC /', '', ConfigurationClient::getCurrent()->get($produitCepageKey)->getLibelleFormat());
-				$totalStockInitial = (formatNumber($produitCepage['total_stock_initial']['value']) != 0)? formatNumber($produitCepage['total_stock_initial']['value']) : null;
-				$totalStockFinal = (formatNumber($produitCepage['total_stock_final']['value']) != 0)? formatNumber($produitCepage['total_stock_final']['value']) : null;
-				$totalTotal = (formatNumber($produitCepage['total_total']['value']) != 0)? formatNumber($produitCepage['total_total']['value']) : null;
+				$totalStockInitial = 0;
+				$totalStockFinal = 0;
+				$totalTotal = 0;
+
 				foreach ($produitCepage['agg_line']['buckets'] as $categorie) {
 					$categorieLibelle = getFamilleLibelle($categorie['key']);
 					$stockInitial = (formatNumber($categorie['stock_initial']['agg_column']['value']) != 0)? formatNumber($categorie['stock_initial']['agg_column']['value']) : null;
 					$stockFinal = (formatNumber($categorie['stock_final']['agg_column']['value']) != 0)? formatNumber($categorie['stock_final']['agg_column']['value']) : null;
 					$totalMvt = (formatNumber($categorie['total']['value']) != 0)? formatNumber($categorie['total']['value']) : null;
-					$csv .= $produitCepageLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$totalMvt.";".$stockFinal."\n";
+				  $csv .= $produitCepageLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$totalMvt.";".$stockFinal."\n";
+					$totalStockInitial += ($stockInitial)? $stockInitial : 0;
+					$totalTotal += ($totalMvt)? $totalMvt : 0;
+					$totalStockFinal += ($stockFinal)? $stockFinal : 0;
 				}
+				$tStockInitial += $totalStockInitial;
+				$tStockFinal += $totalStockFinal;
+				$tTotal += $totalTotal;
+				$totalStockInitial = nullify($totalStockInitial);
+				$totalStockFinal = nullify($totalStockFinal);
+				$totalTotal = nullify($totalTotal);
+			  $csv .= $produitCepageLibelle.';TOTAL;'.$totalStockInitial.';'.$totalTotal.";".$totalStockFinal."\n";
 			}
-			$csv .= $produitCepageLibelle.';TOTAL;'.$totalStockInitial.';'.$totalTotal.";".$totalStockFinal."\n";
 		}
+		$csv .= 'TOTAL;;'.$tStockInitial.';'.$tTotal.";".$tStockFinal."\n";
 }
 echo $csv;
