9c813422c0522290ac6d52038ec15c4d17442cb7
Gabriel Poma <gpoma@24eme.fr>
2019-01-23 13:51:27 +0100

    refactorisation taille des tags

--- project/plugins/acVinComptePlugin/modules/compte/templates/_tagsVisualisation.php
+++ project/plugins/acVinComptePlugin/modules/compte/templates/_tagsVisualisation.php
@@ -1,16 +1,33 @@
+<?php $btnsize = (!isset($smallBlock)) ? 'btn-sm' : 'btn-xs' ?>
 <div class="row">
     <div class="col-xs-12">
         <strong>Tags :</strong>
+
         <?php foreach ($compte->tags as $type_tag => $selected_tags) : ?>
             <?php foreach ($selected_tags as $t): ?>
+
                 <?php $targs['tags'] = implode(',', array($type_tag . ':' . $t)); ?>
-                <span class="btn-group"><a href="" title="<?php echo ucfirst($type_tag) ?>" class="btn <?php if (!isset($smallBlock)): ?>btn-sm<?php else: ?>btn-xs<?php endif; ?> btn-default active"><?php echo substr(strtoupper($type_tag), 0, 1); ?></a><a class="btn btn-default <?php if (!isset($smallBlock)): ?>btn-sm<?php else: ?>btn-xs<?php endif; ?>" href="<?php echo url_for('compte_search', $targs) ?>"><?php echo str_replace('_', ' ', $t) ?></a>
-                    <?php $targs['tag'] = $t; ?>
-                    <?php $targs['q'] = $compte->identifiant ?>
-                        <?php if ($type_tag == 'manuel'): ?><a class="btn btn-default <?php if (!isset($smallBlock)): ?>btn-sm<?php else: ?>btn-xs<?php endif; ?>" href="<?php echo url_for('compte_removetag', $targs) ?>"><span class="glyphicon glyphicon-trash"></span></a><?php endif; ?>
+                <?php $targs['tag'] = $t; ?>
+                <?php $targs['q'] = $compte->identifiant ?>
+
+                <span class="btn-group">
+                    <a href="" title="<?= ucfirst($type_tag) ?>" class="btn <?= $btnsize ?> btn-default active">
+                        <?= substr(strtoupper($type_tag), 0, 1); ?>
+                    </a>
+                    <a class="btn btn-default <?= $btnsize ?>" href="<?= url_for('compte_search', $targs) ?>">
+                        <?= str_replace('_', ' ', $t) ?>
+                    </a>
+                    <?php if ($type_tag == 'manuel'): ?>
+                        <a class="btn btn-default <?= $btnsize ?>" href="<?= url_for('compte_removetag', $targs) ?>">
+                            <i class="glyphicon glyphicon-trash"></i>
+                        </a>
+                    <?php endif; ?>
                 </span>
             <?php endforeach; ?>
         <?php endforeach; ?>
-        <a class="<?php if (!isset($smallBlock)): ?>btn-sm<?php else: ?>btn-xs<?php endif; ?> btn-default" href="<?php echo url_for('compte_search', array('q' => $compte->identifiant)) ?>"><span class="glyphicon glyphicon-plus"></span></a>
+
+        <a class="<?= $btnsize ?> btn-default" href="<?= url_for('compte_search', array('q' => $compte->identifiant)) ?>">
+            <i class="glyphicon glyphicon-plus"></i>
+        </a>
     </div>
 </div>
