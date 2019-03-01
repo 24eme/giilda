610e119b0b97c4af6a29f996a67b566d30811d5f
mathurin petit <mathurin.petit@gmail.com>
2019-03-01 15:27:14 +0100

    templates pour les statistiques de stocks

--- project/apps/ivbd/modules/statistique/templates/_stocks.php
+++ project/apps/ivbd/modules/statistique/templates/_stocks.php
@@ -2,17 +2,13 @@
 use_helper('Statistique');
 use_helper('IvbdStatistique');
 if ($lastPeriode) {
-	$csv = "Produit;Catégorie;Stock initial;Stock initial N-1;Somme mouvements;Somme mouvements N-1;Stock final;Stock final N-1\n";
+	$csv = "Produit;Stock initial;Stock initial N-1;Somme mouvements;Somme mouvements N-1;Stock final;Stock final N-1\n";
 	$result = $result->getRawValue();
 	$lastPeriode = $lastPeriode->getRawValue();
 	foreach ($result as $key => $values) {
-		$key = sfOutputEscaper::unescape($key);
-		$tabKey = explode('/', $key);
-		$csv .= $tabKey[0].';'.$tabKey[1].";";
+		$key = sfOutputEscaper::unescape($key);	
+		$csv .= $key.';';
 			if (isset($lastPeriode[$key])) {
-					if($tabKey[1] == 'TOTAL'){
-						$lastPeriode[$key] = array_values($lastPeriode[$key]);
-					}
 					foreach ($lastPeriode[$key] as $lastPeriodeCaseKey => $lastPeriodeCaseValue) {
 						$csv .= $values[$lastPeriodeCaseKey].";".$lastPeriode[$key][$lastPeriodeCaseKey];
 						$csv .=  ($lastPeriodeCaseKey == count($lastPeriode[$key]) - 1)? "\n" : ';';
@@ -28,8 +24,7 @@ if ($lastPeriode) {
 	if(count($lastPeriode)){
 		foreach ($lastPeriode as $lastPeriodeKey => $lastPeriodeCaseValue) {
 			$key = sfOutputEscaper::unescape($lastPeriodeKey);
-			$tabKey = explode('/', $lastPeriodeKey);
-			$csv .= $tabKey[0].';'.$tabKey[1].";";
+			$csv .= $key.';';
 			foreach ($lastPeriode[$key] as $lastPeriodeKeyKey => $lastPeriodeKeyValue) {
 				$csv .= ";".$lastPeriodeKeyValue;
 				$csv .=  ($lastPeriodeKeyKey == count($lastPeriode[$key]) - 1)? "\n" : ';';
@@ -37,24 +32,24 @@ if ($lastPeriode) {
 		}
 	}
 }else{
-	$csv =  "Produit;Catégorie;Stock initial;Somme mouvements;Stock final\n";
+	$csv =  "Produit;Stock initial;Somme mouvements;Stock final\n";
 	$tStockInitial = 0;
 	$tStockFinal = 0;
 	$tTotal = 0;
-	foreach ($result['agg_page']['buckets'] as $produitCepage) {
-			if(preg_match("/details(Acquitte)?\/DEFAUT$/",$produitCepage['key'])){
-				$produitCepageKey = str_replace(array('\\', '/details/DEFAUT','/detailsACQUITTE/DEFAUT'), array('','',''), $produitCepage['key']);
-				$produitCepageLibelle = preg_replace('/AOC /', '', ConfigurationClient::getCurrent()->get($produitCepageKey)->getLibelleFormat());
+	foreach ($result['agg_page']['buckets'] as $produitLieu) {
+		$produitLieuKey = $produitLieu['key'];
+		$produitLieuLibelle = preg_replace('/AOC /', '', ConfigurationClient::getCurrent()->get($produitLieuKey)->getLibelleFormat());
+
 				$totalStockInitial = 0;
 				$totalStockFinal = 0;
 				$totalTotal = 0;
 
-				foreach ($produitCepage['agg_line']['buckets'] as $categorie) {
-					$categorieLibelle = getFamilleLibelle($categorie['key']);
-					$stockInitial = (formatNumber($categorie['stock_initial']['agg_column']['value']) != 0)? formatNumber($categorie['stock_initial']['agg_column']['value']) : null;
-					$stockFinal = (formatNumber($categorie['stock_final']['agg_column']['value']) != 0)? formatNumber($categorie['stock_final']['agg_column']['value']) : null;
-					$totalMvt = (formatNumber($categorie['total']['value']) != 0)? formatNumber($categorie['total']['value']) : null;
-				  $csv .= $produitCepageLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$totalMvt.";".$stockFinal."\n";
+				foreach ($produitLieu['agg_line']['buckets'] as $couleur) {
+					$couleurLibelle = getCouleurLibelle($couleur['key']);
+					$stockInitial = (formatNumber($couleur['stock_initial']['agg_column']['value']) != 0)? formatNumber($couleur['stock_initial']['agg_column']['value']) : null;
+					$stockFinal = (formatNumber($couleur['stock_final']['agg_column']['value']) != 0)? formatNumber($couleur['stock_final']['agg_column']['value']) : null;
+					$totalMvt = (formatNumber($couleur['total']['value']) != 0)? formatNumber($couleur['total']['value']) : null;
+				  $csv .= $produitLieuLibelle.' '.$couleurLibelle.';'.$stockInitial.';'.$totalMvt.";".$stockFinal."\n";
 					$totalStockInitial += ($stockInitial)? $stockInitial : 0;
 					$totalTotal += ($totalMvt)? $totalMvt : 0;
 					$totalStockFinal += ($stockFinal)? $stockFinal : 0;
@@ -65,9 +60,9 @@ if ($lastPeriode) {
 				$totalStockInitial = nullify($totalStockInitial);
 				$totalStockFinal = nullify($totalStockFinal);
 				$totalTotal = nullify($totalTotal);
-			  $csv .= $produitCepageLibelle.';TOTAL;'.$totalStockInitial.';'.$totalTotal.";".$totalStockFinal."\n";
-			}
+			  $csv .= "TOTAL ".$produitLieuLibelle.';'.$totalStockInitial.';'.$totalTotal.";".$totalStockFinal."\n";
 		}
-		$csv .= 'TOTAL;appelations;'.$tStockInitial.';'.$tTotal.";".$tStockFinal."\n";
+
+		$csv .= 'TOTAL toutes appellations;'.$tStockInitial.';'.$tTotal.";".$tStockFinal."\n";
 }
 echo $csv;
