06b2a52f130657ed020ecc10b73a41ac5c2e4804
Tangui Morlier <tangui@tangui.eu.org>
2019-03-29 16:52:44 +0100

    Suppression des apps non utilisées

--- project/apps/vinsdeloirecompte/templates/_initMessageAide.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-    url_ajax_msg_aide = 'messageAides';
-</script>
-
-<div id="popup_msg_aide">
-	<p id="texte_popup_msg_aide"></p>
-	<div id="btn_popup_msg_aide"></div>
-</div>
