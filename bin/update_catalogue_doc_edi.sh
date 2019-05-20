3816befe38bf80ef3b26f1d216a5440802d7cb2c
Vincent LAURENT <vince.laurent@gmail.com>
2019-05-20 11:34:37 +0200

    Bash qui centralise la récupération via curl des catalogue de mouvements en csv pour la doc

---
+++ bin/update_catalogue_doc_edi.sh
@@ -0,0 +1,4 @@
+curl https://declaration.ivsopro.com/doc-edi/catalogue_mouvements.csv > doc/logiciels_tiers/ivso/catalogue_mouvements.csv
+curl https://declaration.ivbdpro.fr/doc-edi/catalogue_mouvements.csv > doc/logiciels_tiers/ivbd/catalogue_mouvements.csv
+curl https://declaration.vinsalsace.pro/doc-edi/catalogue_mouvements.csv > doc/logiciels_tiers/civa/catalogue_mouvements.csv
+curl https://declaration.vins-centre-loire.com/doc-edi/catalogue_mouvements.csv > doc/logiciels_tiers/bivc/catalogue_mouvements.csv
