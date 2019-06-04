35df4108e19df35c4bf40d7d9715ffe982247699
mathurin petit <mathurin.petit@gmail.com>
2019-06-04 18:26:30 +0200

    liens callto dans les contacts

--- project/plugins/acVinSocietePlugin/modules/societe/templates/_getInterlocuteurs.php
+++ project/plugins/acVinSocietePlugin/modules/societe/templates/_getInterlocuteurs.php
@@ -28,17 +28,17 @@ if (!is_null($contacts)):
                             <?php endif; ?>
                             <?php if ($contact->telephone_perso): ?>
                                 <li class="tel_perso">
-                                    <a href="tel:<?php echo $contact->telephone_perso; ?>"><?php echo $contact->telephone_perso ?></a>
+                                    <a href="callto:<?php echo str_replace(".",'',$contact->telephone_perso); ?>"><?php echo $contact->telephone_perso ?></a>
                                 </li>
                             <?php endif; ?>
                             <?php if ($contact->telephone_mobile): ?>
                                 <li class="tel_mobile">
-                                    <a href="tel:<?php echo $contact->telephone_mobile; ?>"><?php echo $contact->telephone_mobile ?></a>
+                                    <a href="callto:<?php echo str_replace(".",'',$contact->telephone_mobile); ?>"><?php echo $contact->telephone_mobile ?></a>
                                 </li>
                             <?php endif; ?>
                             <?php if ($contact->telephone_bureau): ?>
                                 <li class="tel">
-                                    <a href="tel:<?php echo $contact->telephone_bureau; ?>"><?php echo $contact->telephone_bureau ?></a>
+                                    <a href="callto:<?php echo str_replace(".",'',$contact->telephone_bureau); ?>"><?php echo $contact->telephone_bureau ?></a>
                                 </li>
                                 <?php endif; ?>
         <?php if ($contact->fax): ?>
@@ -46,7 +46,7 @@ if (!is_null($contacts)):
                     <?php endif; ?>
         <?php if (trim($contact->email)): ?>
                                 <li class="email"><a href="mailto:<?php echo $contact->email; ?>"><?php echo $contact->email; ?></a></li>
-        <?php endif; ?>    
+        <?php endif; ?>
                         </ul>
                     </li>
     <?php endforeach; ?>
