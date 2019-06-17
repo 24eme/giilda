f48156125080c5afe1fd5a06cd7dc5ee1f0a1f6a
mathurin petit <mathurin.petit@gmail.com>
2019-06-17 14:58:33 +0200

    ajout dans l'export Sage du flag facture en prélèvement

--- project/plugins/acVinFacturePlugin/lib/model/views/facture.etablissement.map.view.js
+++ project/plugins/acVinFacturePlugin/lib/model/views/facture.etablissement.map.view.js
@@ -2,6 +2,5 @@ function(doc) {
   if (doc.type != "Facture") {
     return;
   }
-  emit([doc.versement_comptable, doc.identifiant, doc._id], [doc.date_facturation, doc.origines, doc.total_ttc, doc.statut, doc.numero_interloire, doc.total_ht, doc.declarant.nom]);
+  emit([doc.versement_comptable, doc.identifiant, doc._id], [doc.date_facturation, doc.origines, doc.total_ttc, doc.statut, doc.numero_interloire, doc.total_ht, doc.declarant.nom, doc.prelevement_automatique]);
 }
-
