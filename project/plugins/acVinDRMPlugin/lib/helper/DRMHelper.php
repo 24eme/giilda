49d3e8875624d8fe704c8109c24f563ead8cd850
Vincent LAURENT <vince.laurent@gmail.com>
2019-06-26 11:24:20 +0200

    Pas besoin de elseif car ce ne sont que des return

--- project/plugins/acVinDRMPlugin/lib/helper/DRMHelper.php
+++ project/plugins/acVinDRMPlugin/lib/helper/DRMHelper.php
@@ -122,19 +122,32 @@ function getEtatDRMCalendrier($isTeledeclarationMode, $calendrier, $periode, $et
 }
 
 function getTeledeclareeLabelCalendrier($isTeledeclarationMode, $calendrier, $periode, $etablissement = false) {
-    if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode) && isCoherenteDouane($isTeledeclarationMode, $calendrier, $periode))
-          return '(Douane OK)';
-    else if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode) && isTransmiseDouane($isTeledeclarationMode, $calendrier, $periode))
-        return '(Transmise)';
-    else if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode))
-        return  '(Téleclarée)';
-    else if ($isTeledeclarationMode)
-        return '';
-    else {
-        $a = $calendrier->getNumeroArchive($periode, $etablissement);
-        if ($a)
-            return '('.$a.')' ;
+    if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode) && isCoherenteDouane($isTeledeclarationMode, $calendrier, $periode)) {
+
+        return '(Douane OK)';
     }
+    
+    if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode) && isTransmiseDouane($isTeledeclarationMode, $calendrier, $periode)) {
+
+        return '(Transmise)';
+    }
+
+    if (isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode)) {
+
+        return  '(Téleclarée)';
+    }
+
+    if ($isTeledeclarationMode) {
+
+        return '';
+    }
+
+    $a = $calendrier->getNumeroArchive($periode, $etablissement);
+    if (!$a) {
+        return "";
+    }
+
+    return '('.$a.')' ;
 }
 
 function isTeledeclareeCalendrier($isTeledeclarationMode, $calendrier, $periode, $etablissement = false) {
