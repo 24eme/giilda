cd527834ef16e988d85d4696620e83803fce32d5
Jb Lm <lemetayer.jb@gmail.com>
2019-07-01 15:00:19 +0200

    drm negoce / ciel lot 1 famille negociant pur bug

--- project/plugins/acVinEtablissementPlugin/lib/model/Etablissement/EtablissementFamilles.class.php
+++ project/plugins/acVinEtablissementPlugin/lib/model/Etablissement/EtablissementFamilles.class.php
@@ -26,11 +26,12 @@ class EtablissementFamilles
     	self::FAMILLE_NEGOCIANT => "Négociant",
     	self::FAMILLE_COOPERATIVE => "Coopérative",
     	self::FAMILLE_COURTIER => "Courtier",
-		self::FAMILLE_REPRESENTANT => "Representant"
+		self::FAMILLE_REPRESENTANT => "Representant",
+    	self::FAMILLE_NEGOCIANT_PUR => "Négociant Pur",
     );
 
     protected static $type_societe_famille = array(
-        SocieteClient::TYPE_OPERATEUR => array(self::FAMILLE_PRODUCTEUR, self::FAMILLE_NEGOCIANT, self::FAMILLE_COOPERATIVE, self::FAMILLE_REPRESENTANT),
+        SocieteClient::TYPE_OPERATEUR => array(self::FAMILLE_PRODUCTEUR, self::FAMILLE_NEGOCIANT, self::FAMILLE_COOPERATIVE, self::FAMILLE_REPRESENTANT, self::FAMILLE_NEGOCIANT_PUR),
         SocieteClient::TYPE_COURTIER => array(self::FAMILLE_COURTIER),
         SocieteClient::TYPE_AUTRE => array(),
     );
