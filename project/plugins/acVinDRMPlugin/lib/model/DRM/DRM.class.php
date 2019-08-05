d417910b5a20287575a5085bd60f2c3deb88805b
Tangui Morlier <tangui@tangui.eu.org>
2019-08-05 17:42:47 +0200

    Reprise des stock CRD pour les interpros qui reprennent les stocks

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
@@ -226,7 +226,11 @@ class DRM extends BaseDRM implements InterfaceMouvementDocument, InterfaceVersio
 
         foreach($drm->getAllCrds() as $regime => $crds) {
             foreach($crds as $crd) {
-                $this->getOrAdd('crds')->getOrAdd($regime)->getOrAddCrdNode($crd->genre, $crd->couleur, $crd->centilitrage, $crd->detail_libelle, null, true);
+                $stock = null;
+                if (DRMConfiguration::getInstance()->isRepriseStocksChangementCampagne() && $drm->periode == DRMClient::getPeriodePrecedente($this->periode)) {
+                    $stock = $crd->stock_debut;
+                }
+                $this->getOrAdd('crds')->getOrAdd($regime)->getOrAddCrdNode($crd->genre, $crd->couleur, $crd->centilitrage, $crd->detail_libelle, $stock, true);
             }
         }
     }
