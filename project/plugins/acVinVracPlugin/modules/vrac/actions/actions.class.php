45b5f839b21f9b4d50763d9217c5b75db2194bb4
Jb Lm <lemetayer.jb@gmail.com>
2019-09-06 16:14:24 +0200

    correction controle acces onglet par rapport a l'identifiant societe

--- project/plugins/acVinVracPlugin/modules/vrac/actions/actions.class.php
+++ project/plugins/acVinVracPlugin/modules/vrac/actions/actions.class.php
@@ -930,7 +930,7 @@ class vracActions extends sfActions {
 
     private function redirect403IfIsNotTeledeclarationAndNotMe() {
         $this->redirect403IfIsNotTeledeclaration();
-        if ($this->getUser()->getCompte()->identifiant != $this->identifiant) {
+        if ($this->getUser()->getCompte()->id_societe != $this->id_societe) {
             $this->redirect403();
         }
     }
