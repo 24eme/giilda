cf3ee58203d603d1f7332ed9ac99d4a63c77a5e0
Gabriel Poma <gpoma@24eme.fr>
2019-09-13 11:06:58 +0200

    Time limit augmenté pour la verif aussi

--- project/plugins/acVinDRMPlugin/modules/drm_edi/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm_edi/actions/actions.class.php
@@ -23,7 +23,7 @@ class drm_ediActions extends drmGeneriqueActions {
      */
     public function executeVerificationEdi(sfWebRequest $request) {
 
-        set_time_limit(300);
+        set_time_limit(400);
 
         $this->md5 = $request->getParameter('md5');
         $csvFilePath = sfConfig::get('sf_data_dir') . '/upload/' . $this->md5;
