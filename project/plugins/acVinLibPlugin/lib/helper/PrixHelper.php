d1970144db90d1e21e8f4b12e04cad0589a48e2e
mathurin petit <mathurin.petit@gmail.com>
2019-11-07 15:44:40 +0100

    Erreur de TVA depuis 7 ans???

--- project/plugins/acVinLibPlugin/lib/helper/PrixHelper.php
+++ project/plugins/acVinLibPlugin/lib/helper/PrixHelper.php
@@ -1,12 +1,11 @@
 <?php
    function ttc($p) {
-        return $p + $p * 0.196;
+        return $p + $p * 0.2;
     }
-    
-function echoTtc($prixHt) 
+
+function echoTtc($prixHt,$tva = 0.2)
 {
   if (is_null($prixHt))
 		return null;
-  echo sprintf("%01.02f", round(($prixHt + $prixHt * 0.196), 2));
+  echo sprintf("%01.02f", round(($prixHt + $prixHt * $tva), 2));
 }
-    
