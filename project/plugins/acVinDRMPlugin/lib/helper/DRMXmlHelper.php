79359c4492df26bb2f0fb1411a7fc751a7c0ea5b
mathurin petit <mathurin.petit@gmail.com>
2019-12-04 11:01:56 +0100

    xml negoce => il manquait un noeud intermédiaire pour les sorties en paiement droits

--- project/plugins/acVinDRMPlugin/lib/helper/DRMXmlHelper.php
+++ project/plugins/acVinDRMPlugin/lib/helper/DRMXmlHelper.php
@@ -118,8 +118,12 @@ function sortForLot1($tabXml) {
     }
     $xmlSorted['sorties-periode'] = array();
     if (isset($tabXml['sorties-periode']) && count($tabXml['sorties-periode']) > 0) {
-        if (isset($tabXml['sorties-periode']['sorties-avec-paiement-droits']))
-            $xmlSorted['sorties-periode']['sorties-avec-paiement-droits'] = $tabXml['sorties-periode']['sorties-avec-paiement-droits'];
+				if (isset($tabXml['sorties-periode']['sorties-avec-paiement-annee-precedente'])){
+					$xmlSorted['sorties-periode']['sorties-avec-paiement-droits']['sorties-avec-paiement-annee-precedente'] = $tabXml['sorties-periode']['sorties-avec-paiement-annee-precedente'];
+				}
+				if (isset($tabXml['sorties-periode']['sorties-avec-paiement-annee-courante'])){
+					$xmlSorted['sorties-periode']['sorties-avec-paiement-droits']['sorties-avec-paiement-annee-courante'] = $tabXml['sorties-periode']['sorties-avec-paiement-annee-courante'];
+				}
         if (isset($tabXml['sorties-periode']['sorties-sans-paiement-droits'])) {
             if (isset($tabXml['sorties-periode']['sorties-sans-paiement-droits']['sorties-definitives']))
                 $xmlSorted['sorties-periode']['sorties-sans-paiement-droits']['sorties-definitives'] = $tabXml['sorties-periode']['sorties-sans-paiement-droits']['sorties-definitives'];
