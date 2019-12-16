9b19ce4c65ebe6f068d4fa242a28c982db4fdeb3
mathurin petit <mathurin.petit@gmail.com>
2019-12-16 16:44:31 +0100

    syntaxe error => corrected

--- project/plugins/acVinDRMPlugin/lib/form/export_details/DRMDetailExportItemForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/export_details/DRMDetailExportItemForm.class.php
@@ -2,7 +2,10 @@
 
 class DRMDetailExportItemForm extends DRMESDetailsItemForm {
 
+    protected $detail = null;
+
     public function __construct(\acCouchdbJson $object, $options = array(), $CSRFSecret = null) {
+        $this->detail = $object;
         parent::__construct($object, $options, $CSRFSecret);
     }
 
@@ -13,8 +16,8 @@ class DRMDetailExportItemForm extends DRMESDetailsItemForm {
 
     public function getIdentifiantChoices() {
         $country_list = ConfigurationClient::getInstance()->getCountryList();
-        if($object->getDocument()->isNegoce()){
-          $country_list = array_merge(array("AUTRE": "Pays Indéterminé"), $country_list);
+        if($this->detail->getDocument()->isNegoce()){
+          $country_list = array_merge(array("AUTRE" => "Pays Indéterminé"), $country_list);
         }
         return $country_list;
     }
