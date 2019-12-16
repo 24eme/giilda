5d2517215dc5f6dd6ff5749b9403890761338e2b
mathurin petit <mathurin.petit@gmail.com>
2019-12-16 16:11:00 +0100

    Ajout d'un pays 'inderminé' pour les DRM de négoce pur

--- project/plugins/acVinDRMPlugin/lib/form/export_details/DRMDetailExportItemForm.class.php
+++ project/plugins/acVinDRMPlugin/lib/form/export_details/DRMDetailExportItemForm.class.php
@@ -5,15 +5,18 @@ class DRMDetailExportItemForm extends DRMESDetailsItemForm {
     public function __construct(\acCouchdbJson $object, $options = array(), $CSRFSecret = null) {
         parent::__construct($object, $options, $CSRFSecret);
     }
-    
+
     public function getFormName() {
 
         return "drm_detail_export_item";
     }
 
     public function getIdentifiantChoices() {
-
-        return ConfigurationClient::getInstance()->getCountryList();
+        $country_list = ConfigurationClient::getInstance()->getCountryList();
+        if($object->getDocument()->isNegoce()){
+          $country_list = array_merge(array("AUTRE": "Pays Indéterminé"), $country_list);
+        }
+        return $country_list;
     }
 
     public function getPostValidatorClass() {
