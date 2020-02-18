63bdb0ca6e4518cf8387e53174c413ba0afd2341
Gabriel Poma <gpoma@24eme.fr>
2020-02-18 17:13:27 +0100

    Lien vers la documentation
    (Uniquement si admin)

---
+++ project/apps/ivso/modules/common/templates/_navExtra.php
@@ -0,0 +1,5 @@
+<?php if ($sf_user->hasCredential(AppUser::CREDENTIAL_ADMIN)) : ?>
+<li>
+    <a target="_blank" tabindex="-1" href="https://documentation.ivsopro.com/index.php/apps/files/">Documents ↗</a>
+</li>
+<?php endif; ?>
