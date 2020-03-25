a4c87b54dee6e0c2805429d20a799370b1c1700d
Vincent LAURENT <vince.laurent@gmail.com>
2020-03-25 17:02:24 +0100

    On utilise aussi le lien de la conf pour la nav

--- project/apps/ivso/modules/common/templates/_navExtra.php
+++ project/apps/ivso/modules/common/templates/_navExtra.php
@@ -1,3 +1,5 @@
+<?php if(sfConfig::get('app_extra_service_url')): ?>
 <li>
-    <a tabindex="-1" href="https://documentation.ivsopro.com/index.php/apps/files/">Documents ↗</a>
+    <a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url') ?>">Documents ↗</a>
 </li>
+<?php endif; ?>
