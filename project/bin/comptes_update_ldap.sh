e5f84674a22c1fbe8142df671151948fd3024fa0
Tangui Morlier <tangui@tangui.eu.org>
2020-04-02 14:32:21 +0200

    Permet les mises à jours des comptes régulières

--- project/bin/comptes_update_ldap.sh
+++ project/bin/comptes_update_ldap.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 . bin/config.inc
+LOCK="/tmp/compte_update_ldap.lock"
+SEQ="/tmp/compte_update_ldap.seq"
+if test -f $LOCK ; then
+    exit 1
+fi
+touch $LOCK
 
-curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_design/compte/_view/all" | cut -d "," -f 1 | sed 's/{"id":"//' | sed 's/"//' | grep "^COMPTE" > $TMP/comptes_ldap_ids
+curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?since="$(cat $SEQ | sed 's/[^0-9]//g') | grep "COMPTE" > $TMP/comptes_ldap_ids
 
-while read ligne  
+cat $TMP/comptes_ldap_ids | while read ligne
 do
-    echo $ligne
-    php symfony compte:ldap-update $ligne
-done < $TMP/comptes_ldap_ids
+    echo $ligne | awk -F '"' '{print $3}' | sed 's/[^0-9]//g' > $SEQ
+    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | awk -F '"' '{print $6}')
+done
 
+rm $TMP/comptes_ldap_ids $LOCK
