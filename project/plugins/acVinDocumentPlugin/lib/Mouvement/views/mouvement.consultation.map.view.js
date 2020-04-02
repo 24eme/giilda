8689f23f8781c67dcdb13b6a1fb064487bcacf16
Tangui Morlier <tangui@tangui.eu.org>
2020-04-02 22:55:26 +0200

    Ajout de la date de prise en compte du mouvement et la famille de la DRM

--- project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
+++ project/plugins/acVinDocumentPlugin/lib/Mouvement/views/mouvement.consultation.map.view.js
@@ -14,17 +14,18 @@ function(doc) {
             for (key in doc.mouvements[identifiant]) {
                 var mouv = doc.mouvements[identifiant][key];
                 pays = "";
-        	    if (mouv.type_hash.match(/^export.*_details$/)) {
-        		          pays = mouv.detail_libelle;
-        	    }
+                if (mouv.type_hash.match(/^export.*_details$/)) {
+                    pays = mouv.detail_libelle;
+                }
                 var coefficient_facturation = -1;
                 if(mouv.coefficient_facturation) {
                     coefficient_facturation = mouv.coefficient_facturation;
                 }
-
-                if(mouv.volume !== null  || mouv.date_version !== null) {
-                    emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, mouv.type_drm, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, mouv.type_drm_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays, mouv.facture, coefficient_facturation]);
+                familledrm = "PRODUCTEUR"
+                if (doc.declarant.famille) {
+                    familledrm = doc.declarant.famille;
                 }
+                emit([doc.type, identifiant, doc.campagne, doc.periode, doc._id, mouv.produit_hash, mouv.type_drm, mouv.type_hash, mouv.vrac_numero, mouv.detail_identifiant], [doc.declarant.nom, mouv.produit_libelle, mouv.type_drm_libelle, mouv.type_libelle, mouv.volume, mouv.vrac_destinataire, mouv.detail_libelle, mouv.date_version, mouv.version, mouv.cvo, mouv.facturable, doc._id+'/mouvements/'+identifiant+'/'+key, pays, mouv.facture, coefficient_facturation, mouv.date, familledrm]);
             }
         }
     }
