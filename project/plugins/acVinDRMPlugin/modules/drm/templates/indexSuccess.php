d004388a3ae15dc99c4bf5c965b509feabf0308f
moussa <adjimahamat@gmail.com>
2020-04-03 13:19:41 +0200

    DRM controles: Wording

--- project/plugins/acVinDRMPlugin/modules/drm/templates/indexSuccess.php
+++ project/plugins/acVinDRMPlugin/modules/drm/templates/indexSuccess.php
@@ -8,29 +8,38 @@
     </div>
 </div>
 <?php $drm_controles = DRMClient::getDRMControles(); if(!empty($drm_controles)) : ?>
-<div class="row">
-    <h2 style="margin-left: 1em;" >Liste des DRM avec des points des contrôles</h2>
+<div class="row col-xs-12">
+    <h2>Liste des DRM ayant des points d'attention</h2>
     <table class="table table-bordered table-condensed table-striped">
 		<thead>
         	<tr>
-                <th class="col-xs-1">Période</th>
-                <th class="col-xs-2">DRM type</th>
-                <th class="col-xs-5">Établissement</th>
-                <th class="col-xs-2">Status</th>
-                <th class="col-xs-1">Nombre de controles</th>
-                <th class="col-xs-1">Visualisation</th>
+                <th class="text-center col-xs-2">Période (Date de modification)</th>
+                <th class="text-center col-xs-1">Étape</th>
+                <th class="text-center col-xs-5">Établissement</th>
+                <th class="text-center col-xs-2">Controles</th>
+                <th class="text-center col-xs-1">Nombre de controles</th>
+                <th class="text-center col-xs-1">Visualisation</th>
             </tr>
 		</thead>
 		<tbody>
 		<?php foreach ($drm_controles as $identifiant => $drm_controle): ?>
-                <tr>
-                	<td class="text-center" ><a href="<?php echo url_for('drm_visualisation', array('identifiant' => $identifiant, 'periode_version' => $drm_controle->doc['periode'])) ?>"><?php echo $drm_controle->doc["periode"]; ?></a></td>
-                    <td class="text-center"><?php echo $drm_controle->doc["type_creation"]; ?></td>
-                    <td class="text-left"><?php echo $drm_controle->doc["societe"]["raison_sociale"]; ?></td>
-                    <td class="text-center"><?php echo $drm_controle->doc["etape"]; ?></td>
-                    <td class="text-center"><?php echo DRMClient::getNbControlesDRM($drm_controle->doc["controles"]);?></td>
-                    <td class="text-center"><a class="btn btn-sm btn-default" href="<?php echo url_for('drm_visualisation', array('identifiant' => $identifiant, 'periode_version' => $drm_controle->doc['periode'])) ?>">Visualiser</a></td>
-                </tr>
+            <tr>
+            	<td class="text-left" ><a href="<?php echo url_for('drm_visualisation', array('identifiant' => $identifiant, 'periode_version' => $drm_controle->doc['periode'])) ?>"><?php $periode = $drm_controle->doc["periode"]; echo substr($periode,-2)."-".substr($periode, 0, 4); ?><span class="text-muted small"><?php echo count($drm_controle->doc["editeurs"]) ? " (".$drm_controle->doc['editeurs'][0]['date_modification']. ")":null; ?></span></a></td>
+                <td class="text-center"><?php echo $drm_controle->doc["etape"]; ?></td>                    
+                <td class="text-left">
+                    <?php echo $drm_controle->doc["societe"]["raison_sociale"]." ($identifiant)"; ?>
+                    <span class="text-muted small"><?php echo $drm_controle->doc["declarant"]["no_accises"]; ?></span>
+                </td>
+                <td>
+                    <ul>
+                    <?php foreach (array_keys($drm_controle->doc["controles"]) as $key => $controle): ?>
+                        <li><?php echo "$controle"; ?></li>
+                    <?php endforeach; ?>
+                    </ul>
+                </td>
+                <td class="text-center"><?php echo DRMClient::getNbControlesDRM($drm_controle->doc["controles"]);?></td>
+                <td class="text-center"><a class="btn btn-sm btn-default" href="<?php echo url_for('drm_visualisation', array('identifiant' => $identifiant, 'periode_version' => $drm_controle->doc['periode'])) ?>">Visualiser</a></td>
+            </tr>
         <?php endforeach; ?>
         </tbody>
 	</table>
