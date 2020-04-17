dba197b33d74bb6bcf5cdc70b583e3d731a605f1
Vincent LAURENT <vince.laurent@gmail.com>
2020-04-17 17:07:52 +0200

    Si il y a une DRM suivante ou si elle n'est pas master on interdit la réouverture

--- project/plugins/acVinDRMPlugin/modules/drm/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm/actions/actions.class.php
@@ -461,7 +461,7 @@ class drmActions extends drmGeneriqueActions {
             $this->redirect403IfIsTeledeclaration();
         }
         $drm = $this->getRoute()->getDRM();
-        $this->redirect403Unless($drm->isTeledeclareNonFacturee());
+        $this->redirect403Unless($drm->isReouvrable());
 
         $drm = $this->getRoute()->getDRM();
         $drm->devalidate();
