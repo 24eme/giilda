959abea0071c259c13629f288c58598bad11ae6e
mathurin petit <mathurin.petit@gmail.com>
2020-04-20 16:39:09 +0200

    ajout des champs properties necessaire dans un fichier mapping

--- project/plugins/acVinCouchdbToElasticsearchPlugin/lib/task/Couchdb2elBuildMappingTask.class.php
+++ project/plugins/acVinCouchdbToElasticsearchPlugin/lib/task/Couchdb2elBuildMappingTask.class.php
@@ -2,6 +2,12 @@
 
 class Couchdb2elBuildMappingTask extends sfBaseTask {
 
+
+    protected static $defaultProperties = array("seq" => array("type" => "long"),
+                                                                "source" => array("type" => "string", "index" => "not_analyzed"),
+                                                                "id" => array("type" => "string", "index" => "not_analyzed"),
+                                                                "changes" => array("properties" => array("rev" => array("type" => "string", "index" => "not_analyzed"))));
+
     protected function configure() {
         // // add your own arguments here
         $this->addArguments(array(
@@ -18,6 +24,9 @@ class Couchdb2elBuildMappingTask extends sfBaseTask {
         $this->name = 'build-mapping';
         $this->briefDescription = '';
         $this->detailedDescription = <<<EOF
+    [build-mapping|INFO] créer un mapping pour l'indexeur elasticsearch
+    Appel :
+      [php symfony couchdb2el:build-mapping --application="app" |INFO]
 EOF;
     }
 
@@ -26,7 +35,20 @@ EOF;
       $databaseManager = new sfDatabaseManager($this->configuration);
       $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
 
-      echo json_encode(acCouchdbManager::getInstance()->getSchema(),JSON_PRETTY_PRINT);
+      if(!$options['application']){
+        throw new sfException("Il faut une application pour utiliser cette tache : php symfony couchdb2el:build-mapping --application=\"app\"");
+      }
+
+      $mappings = array("mappings" => array());
+      $schemas = acCouchdbManager::getInstance()->getSchema();
+      foreach ($schemas as $schemaName => $schema) {
+        $mappings["mappings"][$schemaName] = array();
+        $mappings["mappings"][$schemaName]["properties"] = self::$defaultProperties;
+        $mappings["mappings"][$schemaName]["doc"] = array();
+        $mappings["mappings"][$schemaName]["doc"]["properties"] = $schema;
+      }
+
+      echo json_encode($mappings,JSON_PRETTY_PRINT );
     }
 
 }
