5b8ef477b63d22c3159369e76ce5dcec81036931
moussa <adjimahamat@gmail.com>
2020-04-23 12:52:01 +0200

    DRM controles:cleancontroles quand plus de points controles

--- project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
+++ project/plugins/acVinDRMPlugin/lib/model/DRM/DRM.class.php
@@ -358,9 +358,10 @@ class DRM extends BaseDRM implements InterfaceMouvementDocument, InterfaceVersio
 
     public function updateControles(){
         $points = new DRMValidation($this, true);
-        if(!$points->hasPoints() && !$this->exist("transmission_douane"))
+	$this->cleanControles();
+	if(!$points->hasPoints() && !$this->exist("transmission_douane")){
             return;
-        $this->cleanControles();
+        }
         if($points->hasPoints()){
             $this->add('controles');
             if($points->hasErreurs()){
