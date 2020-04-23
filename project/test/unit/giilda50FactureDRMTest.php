8dc144defbecbcfc101785b3c101db353b80bb17
Vincent LAURENT <vince.laurent@gmail.com>
2020-04-23 18:08:31 +0200

    Si pas de CVO Nego le test n'était pas a faire

--- project/test/unit/giilda50FactureDRMTest.php
+++ project/test/unit/giilda50FactureDRMTest.php
@@ -237,21 +237,20 @@ $drm->update();
 $drm->validate();
 $drm->save();
 
-$t->is($drm->mouvements->get($nego2->getIdentifiant())->getFirst()->facturable, 1, 'Mouvement du negociant est facturable');
-
 $prixHt = round($details->entrees->recolte / 12 * 2, 2) * $details->getCVOTaux() - round($details->sorties->manquant * $details->getCVOTaux(), 2) - round($details->sorties->destructionperte * $details->getCVOTaux(), 2) - round($vrac_detail->volume * ($details->getCVOTaux() / 2), 2);
 
 $dateFacturation = new DateTime(preg_replace("/([0-9]{4})([0-9]{2})/", '\1-\2-31', $periode));
 $paramFacturation["date_mouvement"] = $dateFacturation->modify("+ 1 month")->format('Y-m-d');
 $facture = FactureClient::getInstance()->createAndSaveFacturesBySociete($societeNego, $paramFacturation);
 
-
 if($hasCVONegociant) {
+    $t->is($drm->mouvements->get($nego2->getIdentifiant())->getFirst()->facturable, 1, 'Mouvement du negociant est facturable');
     $t->ok($facture, "La facture est créée");
     $t->is($facture->total_ht, round($prixHt, 2), "Le total HT est de ".$prixHt." €");
     $t->is($facture->lignes->get($drm->_id)->libelle,DRMClient::getInstance()->getLibelleFromId($drm->_id)." (sur la base des volumes produits)", 'Libellé de la catégorie');
     $t->is(count($facture->lignes->get($drm->_id)->details->toArray(true, false)), 5, "La facture à 5 lignes");
 } else {
+    $t->pass("Aucun test");
     $t->ok(!$facture, "La facture n'est pas créée");
     $t->pass("Rien à facturer");
     $t->pass("Aucun test");
