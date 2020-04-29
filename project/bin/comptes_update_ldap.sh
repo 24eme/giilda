c803d170d601ed3c0aa3565223680eeb3aefe9f5
Tangui Morlier <tangui@tangui.eu.org>
2020-04-29 18:57:10 +0200

    update ldap compatible avec couchdb2

--- project/bin/comptes_update_ldap.sh
+++ project/bin/comptes_update_ldap.sh
@@ -7,14 +7,14 @@ if test -f $LOCK ; then
     exit 1
 fi
 touch $LOCK
-if ! test -s $SEQ; then
-    echo 0 > $SEQ
+if test -s $SEQ; then
+    echo -n "&since="$(cat $SEQ | sed 's/[^0-9a-z_-]//gi') > $SINCESEQ
 fi
 
-curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?feed=continuous&timeout=590000&since="$(cat $SEQ | sed 's/[^0-9]//g') | grep "COMPTE" | while read ligne
+curl -s "http://$COUCHHOST:$COUCHPORT/$COUCHBASE/_changes?feed=continuous&timeout=59000"$SINCESEQ | grep "COMPTE" | grep -v "_design" | while read ligne
 do
-    echo $ligne | awk -F '"' '{print $3}' | sed 's/[^0-9]//g' > $SEQ
-    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | awk -F '"' '{print $6}')
+    echo $ligne | sed 's/.*"seq"://' | sed 's/,.*//' | sed 's/"/g/' > $SEQ
+    php symfony compte:ldap-update $SYMFONYTASKOPTIONS $(echo $ligne | sed 's/.*"id":"//' | sed 's/",.*//' )
 done
 
 rm $LOCK
