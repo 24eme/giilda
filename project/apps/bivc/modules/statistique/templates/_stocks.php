783d89d4ec5f65ccb922a75941c6652c2bcf3d5c
mathurin petit <mathurin.petit@gmail.com>
2020-06-29 19:04:09 +0200

    on remet le pdf comme avant

--- project/apps/bivc/modules/statistique/templates/_stocks.php
+++ project/apps/bivc/modules/statistique/templates/_stocks.php
@@ -1,7 +1,7 @@
 <?php
 use_helper('BivcStatistique');
 
-$csv = "Appellation;Catégorie;Stock initial;Stock actuel;TOTAL mvt\n";
+$csv = "Certification;Appellation;Catégorie;Stock initial;Stock actuel;TOTAL mvt\n";
 foreach ($result['agg_page']['buckets'] as $appellation) {
 	$appellationKey = str_replace('\\', '', $appellation['key']);
 	$appellationLibelle = preg_replace('/AOC */', '', ConfigurationClient::getCurrent()->get($appellationKey)->getLibelleFormat());
@@ -14,9 +14,9 @@ foreach ($result['agg_page']['buckets'] as $appellation) {
 		$stockInitial = (formatNumber($categorie['stock_initial']['agg_column']['value']) != 0)? formatNumber($categorie['stock_initial']['agg_column']['value']) : null;
 		$stockFinal = (formatNumber($categorie['stock_final']['agg_column']['value']) != 0)? formatNumber($categorie['stock_final']['agg_column']['value']) : null;
 		$totalMvt = (formatNumber($categorie['total']['value']) != 0)? formatNumber($categorie['total']['value']) : null;
-		$csv .= $appellationLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$stockFinal.';'.$totalMvt."\n";
+		$csv .= $globalLibelle.';'.$appellationLibelle.';'.$categorieLibelle.';'.$stockInitial.';'.$stockFinal.';'.$totalMvt."\n";
 	}
-	$csv .= 'TOTAL;;'.$totalStockInitial.';'.$totalStockFinal.';'.$totalTotal."\n";
+	$csv .= $globalLibelle.';'.$appellationLibelle.';TOTAL;'.$totalStockInitial.';'.$totalStockFinal.';'.$totalTotal."\n";
 }
 #$csv .= 'TOTAL;TOTAL;TOTAL;'.formatNumber($result['totaux_stock_initial']['value']).';'.formatNumber($result['totaux_stock_final']['value']).';'.formatNumber($result['totaux_total']['value'])."\n";
 echo $csv;
