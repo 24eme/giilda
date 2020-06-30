7d33791d0260e1f2b75c053dc78f972036460104
Vincent LAURENT <vince.laurent@gmail.com>
2020-06-30 12:31:25 +0200

    Formulaire d'admin d'édition des messages

--- project/plugins/acVinConfigurationPlugin/lib/model/Messages/Messages.class.php
+++ project/plugins/acVinConfigurationPlugin/lib/model/Messages/Messages.class.php
@@ -1,3 +1,15 @@
 <?php
 class Messages extends BaseMessages {
+
+    public function getMessages() {
+        $messages = array();
+        foreach($this as $key => $message) {
+            if(in_array($key, array('_rev', '_id', 'type'))) {
+                continue;
+            }
+            $messages[$key] = $message;
+        }
+
+        return $messages;
+    }
 }
