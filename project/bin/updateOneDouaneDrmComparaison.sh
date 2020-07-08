1e7160d05e6fcbd586146c6b41e4aad5c7f1da69
Tangui Morlier <tangui@tangui.eu.org>
2020-07-08 11:03:45 +0200

    Plus de php5

--- project/bin/updateOneDouaneDrmComparaison.sh
+++ project/bin/updateOneDouaneDrmComparaison.sh
@@ -21,12 +21,12 @@ URL_LASTDOC=$(curl -s $CIEL_URL_RETOURXML""$URL_PERIODE"?accise="$NUMEROACCISE"&
 echo "url:"$URL_LASTDOC;
 if test $URL_LASTDOC ; then
     cd $WORKINGDIR;
-    OUT=$(php5 symfony $SYMFONYTASKOPTIONS drm:storeXMLRetour --force-update="1" $URL_LASTDOC)
+    OUT=$(php symfony $SYMFONYTASKOPTIONS drm:storeXMLRetour --force-update="1" $URL_LASTDOC)
 	RET=$?
 	DRM=$(echo $OUT | sed 's/ .*//')
 	echo $OUT
 	if test $RET -eq 0 ; then
-		php5 symfony $SYMFONYTASKOPTIONS drm:compareXMLs $DRM
+		php symfony $SYMFONYTASKOPTIONS drm:compareXMLs $DRM
 	fi
 fi
 
