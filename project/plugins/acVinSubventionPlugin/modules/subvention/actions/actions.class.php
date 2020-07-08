b37b40351e6d6105f93fd633a752e774125d642b
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-08 12:19:11 +0200

    Route et action de création d'une subvention

--- project/plugins/acVinSubventionPlugin/modules/subvention/actions/actions.class.php
+++ project/plugins/acVinSubventionPlugin/modules/subvention/actions/actions.class.php
@@ -3,7 +3,18 @@
 class subventionActions extends sfActions {
 
     public function executeIndex(sfWebRequest $request) {
-      var_dump("ici les subventions"); exit;
+        var_dump("ici les subventions"); exit;
     }
 
+    public function executeCreation(sfWebRequest $request) {
+        $etablissement = $this->getRoute()->getEtablissement();
+
+        $subvention = SubventionClient::getInstance()->createOrFind($etablissement->identifiant, $request->getParameter('operation'));
+        $subvention->save();
+        return $this->redirect('subvention_infos', $subvention);
+    }
+
+    public function executeInfos(sfWebRequest $request) {
+        $this->subvention = $this->getRoute()->getSubvention();
+    }
 }
