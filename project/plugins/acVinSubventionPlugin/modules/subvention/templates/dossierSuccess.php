3410efb1c1f180a9b508e59d1f6a226661015f13
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-10 17:17:08 +0200

    Rail d'étape et unformisation des boutons de navigations

--- project/plugins/acVinSubventionPlugin/modules/subvention/templates/dossierSuccess.php
+++ project/plugins/acVinSubventionPlugin/modules/subvention/templates/dossierSuccess.php
@@ -1,67 +1,72 @@
 <ol class="breadcrumb">
     <li class="active"><a href="<?php echo "#" ?>">Subvention</a></li>
 </ol>
-<h2 class="text-center">Saisie du dossier</h2>
-<h3 class="text-center"><?php echo $subvention->declarant->raison_sociale ?> <small>(<?php echo $subvention->declarant->siret ?>)</small></h3>
-<br/>
-<div class="row">
-    <div class="col-xs-10 col-xs-offset-1">
-        <div class="panel panel-default">
-            <div class="panel-heading"><h2 class="panel-title">Téléchargement de fichier</h2></div>
-            <div class="panel-body">
-              <ul>
-                <li><a href="#">Notice</a></li>
-                  <li><a href="#">Charte Graphique</a></li>
-              </ul>
+
+<section id="principal">
+    <?php include_partial('subvention/etapes', array('subvention' => $subvention)); ?>
+
+    <h2 class="text-center">Saisie du dossier</h2>
+    <h3 class="text-center"><?php echo $subvention->declarant->raison_sociale ?> <small>(<?php echo $subvention->declarant->siret ?>)</small></h3>
+    <br/>
+    <div class="row">
+        <div class="col-xs-10 col-xs-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h2 class="panel-title">Téléchargement de fichier</h2></div>
+                <div class="panel-body">
+                  <ul>
+                    <li><a href="#">Notice</a></li>
+                      <li><a href="#">Charte Graphique</a></li>
+                  </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<br/>
+    <br/>
 
 
-<form class="form-horizontal" role="form" action="<?php echo url_for('subvention_dossier', array('identifiant' => $subvention->identifiant,'operation' => $subvention->operation)) ?>" method="post" enctype="multipart/form-data">
-  <?php echo $form->renderGlobalErrors(); ?>
-  <?php echo $form->renderHiddenFields(); ?>
-
-<div class="row">
-    <div class="col-xs-10 col-xs-offset-1">
-      <h3 >Dossier de subvention <small>(en xls)</small></h3>
-  </div>
-</div>
-<div class="row">
-    <div class="col-xs-8 col-xs-offset-2">
-      <a class="btn btn-default btn-lg col-xs-10" href="#">Télécharger pour compléter</a>
-  </div>
-</div>
-<br/>
-<div class="row">
-
-
-<div class="form-group">
-  <div class="col-xs-8 col-xs-offset-4">
-    <?php echo $form['file']->renderError() ?>
-  </div>
-</div>
-    <div class="col-xs-8 col-xs-offset-2">
-      <label class="btn btn-default btn-lg col-xs-6" for="subvention_dossier_file">
-        <?php echo $form['file']->render(array('style' => 'display:none', 'onchange' => "$('#upload-file-info').html(this.files[0].name)")) ?>
-          <span class='' id="upload-file-info">Choisir un fichier</span>
-      </label>
-      <button type="submit" class="btn btn-default btn-lg btn-upper col-xs-4">Déposer le dossier</button>
-
-
-  </div>
-</div>
-
-
+    <form class="form-horizontal" role="form" action="<?php echo url_for('subvention_dossier', array('identifiant' => $subvention->identifiant,'operation' => $subvention->operation)) ?>" method="post" enctype="multipart/form-data">
+      <?php echo $form->renderGlobalErrors(); ?>
+      <?php echo $form->renderHiddenFields(); ?>
 
     <div class="row">
-        <div class="col-xs-6">
-            <a class="btn btn-default" href="<?php url_for('subvention_infos',$subvention); ?>">Étape précédente</a>
-        </div>
-        <div class="col-xs-6 text-right">
-            <button type="submit" class="btn btn-success">Étape suivante</button>
-        </div>
+        <div class="col-xs-10 col-xs-offset-1">
+          <h3 >Dossier de subvention <small>(en xls)</small></h3>
+      </div>
     </div>
-</form>
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+          <a class="btn btn-default btn-lg col-xs-10" href="#">Télécharger pour compléter</a>
+      </div>
+    </div>
+    <br/>
+    <div class="row">
+
+
+    <div class="form-group">
+      <div class="col-xs-8 col-xs-offset-4">
+        <?php echo $form['file']->renderError() ?>
+      </div>
+    </div>
+        <div class="col-xs-8 col-xs-offset-2">
+          <label class="btn btn-default btn-lg col-xs-6" for="subvention_dossier_file">
+            <?php echo $form['file']->render(array('style' => 'display:none', 'onchange' => "$('#upload-file-info').html(this.files[0].name)")) ?>
+              <span class='' id="upload-file-info">Choisir un fichier</span>
+          </label>
+          <button type="submit" class="btn btn-default btn-lg btn-upper col-xs-4">Déposer le dossier</button>
+
+
+      </div>
+    </div>
+
+
+
+        <div class="row">
+            <div class="col-xs-6">
+                <a class="btn btn-default" tabindex="-1" href="<?php echo url_for('subvention_infos', $subvention); ?>"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;Étape précédente</a>
+            </div>
+            <div class="col-xs-6 text-right">
+                <button type="submit" class="btn btn-success">Étape suivante&nbsp;<span class="glyphicon glyphicon-chevron-right"></button>
+            </div>
+        </div>
+    </form>
+</div>
--- project/plugins/acVinSubventionPlugin/modules/subvention/templates/validationSuccess.php
+++ project/plugins/acVinSubventionPlugin/modules/subvention/templates/validationSuccess.php
@@ -4,69 +4,74 @@
     <li class="active"><a href=""><?php if($subvention->isNew()): ?>Ajouter<?php else: ?>Modifier<?php endif; ?></a></li>
 </ol>
 
+<section id="principal">
+    <?php include_partial('subvention/etapes', array('subvention' => $subvention)); ?>
+
     <h1><strong>Etape 3</strong> - Validation du dossier</h1>
 
-<form class="form-horizontal" role="form" action="<?php echo url_for("subvention_validation", $subvention) ?>" method="post">
-    <?php echo $form->renderHiddenFields(); ?>
-    <?php echo $form->renderGlobalErrors(); ?>
-    
-    <div class="row">
-    	<div class="col-xs-12">
-    		<h3><?php echo $subvention->declarant->raison_sociale ?> (SIRET n°<?php echo $subvention->declarant->siret ?>)</h3>
-			<a href="<?php echo url_for("subvention_pdf", $subvention) ?>" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-file"></span>&nbsp;Vos informations</a>
-		</div>
-	</div>
-    
-    <div class="row">
-    	<div class="col-xs-12">
-    		<h3>Engagements</h3>
-        	<?php 
-            $engagements = sfConfig::get('subvention_configuration_engagements');
-            foreach ($engagements as $key => $libelle):
-               if (!isset($form["engagement_$key"])) continue;
-            ?>
-            <div class="form-group <?php if($form["engagement_$key"]->hasError()): ?>has-error<?php endif; ?>">
-            	<div class="col-xs-12">
-    				<?php echo $form["engagement_$key"]->renderError() ?>
-    			</div>
-				<div class="col-xs-12 checkbox">
-					<label for="validation_<?php echo "engagement_$key" ?>">
-    				<?php echo $form["engagement_$key"]->render() ?>&nbsp;<?php echo $libelle ?>
-    				</label>
-              	</div>
-          	</div>
-          	<?php endforeach; ?>
-		</div>
-    </div>
-    
-    <div class="row">
-    	<div class="col-xs-12">
-    		<h3>Commentaire</h3>
-        	<div class="form-group <?php if($form['commentaire']->hasError()): ?>has-error<?php endif; ?>">
-    			<div class="col-xs-12">
-    				<?php echo $form['commentaire']->renderError() ?>
-    			</div>
-    			<div class="col-xs-10">
-    				<?php echo $form['commentaire']->render(array('class' => 'form-control', 'placeholder' => "Détailler les opérations pour lesquelles vous souhaitez utiliser la subvention")) ?>
-    			</div>
+    <form class="form-horizontal" role="form" action="<?php echo url_for("subvention_validation", $subvention) ?>" method="post">
+        <?php echo $form->renderHiddenFields(); ?>
+        <?php echo $form->renderGlobalErrors(); ?>
+
+        <div class="row">
+        	<div class="col-xs-12">
+        		<h3><?php echo $subvention->declarant->raison_sociale ?> (SIRET n°<?php echo $subvention->declarant->siret ?>)</h3>
+    			<a href="<?php echo url_for("subvention_pdf", $subvention) ?>" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-file"></span>&nbsp;Vos informations</a>
     		</div>
-		</div>
-    </div>
-    
-    <div class="row">
-    	<div class="col-xs-12">
-    		<h3>Dossier joint</h3>
-    		<a href="#" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-file"></span>&nbsp;XLS</a>
     	</div>
-    </div>
-    <br />
-    <br />
-    <div class="row row-margin row-button">
-        <div class="col-xs-6">
-        	<a href="<?php echo url_for('subvention_dossier', $subvention) ?>" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;Etape précédente</a>
+
+        <div class="row">
+        	<div class="col-xs-12">
+        		<h3>Engagements</h3>
+            	<?php
+                $engagements = sfConfig::get('subvention_configuration_engagements');
+                foreach ($engagements as $key => $libelle):
+                   if (!isset($form["engagement_$key"])) continue;
+                ?>
+                <div class="form-group <?php if($form["engagement_$key"]->hasError()): ?>has-error<?php endif; ?>">
+                	<div class="col-xs-12">
+        				<?php echo $form["engagement_$key"]->renderError() ?>
+        			</div>
+    				<div class="col-xs-12 checkbox">
+    					<label for="validation_<?php echo "engagement_$key" ?>">
+        				<?php echo $form["engagement_$key"]->render() ?>&nbsp;<?php echo $libelle ?>
+        				</label>
+                  	</div>
+              	</div>
+              	<?php endforeach; ?>
+    		</div>
         </div>
-        <div class="col-xs-6 text-right">
-            <button type="submit" class="btn btn-success">Valider le dossier&nbsp;<span class="glyphicon glyphicon-ok"></span></button>
+
+        <div class="row">
+        	<div class="col-xs-12">
+        		<h3>Commentaire</h3>
+            	<div class="form-group <?php if($form['commentaire']->hasError()): ?>has-error<?php endif; ?>">
+        			<div class="col-xs-12">
+        				<?php echo $form['commentaire']->renderError() ?>
+        			</div>
+        			<div class="col-xs-10">
+        				<?php echo $form['commentaire']->render(array('class' => 'form-control', 'placeholder' => "Détailler les opérations pour lesquelles vous souhaitez utiliser la subvention")) ?>
+        			</div>
+        		</div>
+    		</div>
         </div>
-    </div>
-</form>
+
+        <div class="row">
+        	<div class="col-xs-12">
+        		<h3>Dossier joint</h3>
+        		<a href="#" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-file"></span>&nbsp;XLS</a>
+        	</div>
+        </div>
+        <br />
+        <br />
+        <div class="row row-margin row-button">
+            <div class="col-xs-6">
+            	<a href="<?php echo url_for('subvention_dossier', $subvention) ?>" tabindex="-1" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;Etape précédente</a>
+            </div>
+            <div class="col-xs-6 text-right">
+                <button type="submit" class="btn btn-success">Valider le dossier&nbsp;<span class="glyphicon glyphicon-ok"></span></button>
+            </div>
+        </div>
+    </form>
+
+</section>
