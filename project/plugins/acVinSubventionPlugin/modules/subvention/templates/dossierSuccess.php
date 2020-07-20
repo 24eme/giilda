aeacc6df380dc690d4245730270631cebfd9301d
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-20 23:07:35 +0200

    Mise en forme de l'étape dossier : étape 1 et étape 2

--- project/plugins/acVinSubventionPlugin/modules/subvention/templates/dossierSuccess.php
+++ project/plugins/acVinSubventionPlugin/modules/subvention/templates/dossierSuccess.php
@@ -7,7 +7,7 @@
 
     <h2>Saisie du dossier</h2>
 
-    <p>Cette consiste à décrire les opérations de communication à mener à l'aide d'un tableur</p>
+    <p>Cette consiste à décrire les opérations de communication à mener à l'aide d'un tableur : </p>
 
     <form class="form-horizontal" role="form" action="<?php echo url_for('subvention_dossier', array('identifiant' => $subvention->identifiant,'operation' => $subvention->operation)) ?>" method="post" enctype="multipart/form-data">
       <?php echo $form->renderGlobalErrors(); ?>
@@ -15,10 +15,13 @@
     <div class="row">
         <div class="col-xs-9">
             <div>
+            <h3>Étape 1</h3>
             <a class="btn btn-default btn-lg" style="width: 100%; text-align: left;" href="<?php echo url_for('subvention_xls', array('identifiant' => $subvention->identifiant,'operation' => $subvention->operation)) ?>"><span class="glyphicon glyphicon-file"></span> <?php if(!$subvention->hasXls()): ?>Télécharger le dossier vierge à compléter<?php else:?> Télécharger mon dossier déposé le <?php echo ($subvention->dossier_date)? (DateTime::createFromFormat("Y-m-d H:i:s",$subvention->dossier_date))->format("d/m/Y à H\hi") : ''; ?><?php endif; ?>
             </a>
             </div>
-            <div style="padding-top: 15px">
+            <hr />
+            <div>
+                <h3>Étape 2</h3>
                 <?php echo $form['file']->renderError() ?>
                 <label class="btn btn-default btn-lg text-left" style="width: 100%; text-align: left;" for="subvention_dossier_file">
                     <?php echo $form['file']->render(array('style' => 'display:none', 'onchange' => "$('#upload-file-info').html(this.files[0].name)")) ?>
@@ -30,7 +33,7 @@
             <?php include_partial('subvention/aide'); ?>
         </div>
     </div>
-    <div style="margin-top: 15px;" class="row">
+    <div style="margin-top: 30px;" class="row">
         <div class="col-xs-6">
             <a class="btn btn-default" tabindex="-1" href="<?php echo url_for('subvention_infos', $subvention); ?>"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;Étape précédente</a>
         </div>
