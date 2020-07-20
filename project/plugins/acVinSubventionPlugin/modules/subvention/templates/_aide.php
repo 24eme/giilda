77335ce77cfcb9cb5c4a29162263df4211819b80
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-20 22:30:24 +0200

    Partial des aides

---
+++ project/plugins/acVinSubventionPlugin/modules/subvention/templates/_aide.php
@@ -0,0 +1,11 @@
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h2 class="panel-title">Aide</h2>
+    </div>
+    <div class="panel-body">
+      <ul>
+        <li><a href="#">Notice</a></li>
+          <li><a href="#">Charte Graphique</a></li>
+      </ul>
+    </div>
+</div>
