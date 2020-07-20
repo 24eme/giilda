8052fd7fbc6898a9d07d216fdd2020d5ccf503ed
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-21 00:08:39 +0200

    Enrrgistrement et remplissage par défaut du formulaire d'engagement

--- project/plugins/acVinSubventionPlugin/lib/form/SubventionEngagementsForm.class.php
+++ project/plugins/acVinSubventionPlugin/lib/form/SubventionEngagementsForm.class.php
@@ -5,6 +5,12 @@ class SubventionEngagementsForm extends acCouchdbForm {
     public function __construct(acCouchdbDocument $doc, $defaults = array(), $options = array(), $CSRFSecret = null) {
         $this->engagements = $doc->getConfiguration()->getEngagements();
         $this->engagementsPrecisions = $doc->getConfiguration()->getEngagementsPrecisions();
+        foreach ($doc->engagements as $key => $value) {
+            $defaults["engagement_$key"] = 1;
+        }
+        foreach ($doc->engagements_precisions as $key => $value) {
+            $defaults["precision_engagement_$key"] = 1;
+        }
         parent::__construct($doc, $defaults, $options, $CSRFSecret);
     }
 
@@ -35,14 +41,22 @@ class SubventionEngagementsForm extends acCouchdbForm {
     }
 
     public function save() {
+        $values = $this->getValues();
         $this->getDocument()->remove('engagements');
         $this->getDocument()->add('engagements');
-        $engagements = sfConfig::get('subvention_configuration_engagements');
-	    foreach ($engagements as $key => $libelle) {
+	    foreach ($this->engagements as $key => $libelle) {
 	        if (isset($values["engagement_$key"]) && $values["engagement_$key"]) {
 	            $this->getDocument()->engagements->add($key, true);
 	        }
 	    }
+        foreach ($this->engagementsPrecisions as $eng => $precisions) {
+           foreach ($precisions as $k => $libelle) {
+               $key = "$eng/$k";
+               if (isset($values["precision_engagement_$key"]) && $values["precision_engagement_$key"]) {
+                   $this->getDocument()->engagements_precisions->add($key, 1);
+               }
+           }
+        }
         $this->getDocument()->save();
     }
 
