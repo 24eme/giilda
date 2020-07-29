5e049f90d0bd8254a4565a8c47de4d13940155ee
Vincent LAURENT <vince.laurent@gmail.com>
2020-07-29 17:51:41 +0200

    Sans le panel

--- project/plugins/acVinSubventionPlugin/modules/subvention/templates/etablissementSuccess.php
+++ project/plugins/acVinSubventionPlugin/modules/subvention/templates/etablissementSuccess.php
@@ -13,39 +13,33 @@
   </div>
 <?php endif; ?>
 
-    <div class="row">
-        <div class="col-xs-6 col-xs-offset-3">
-            <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <h3 class="panel-title">Contrat Relance Viti 
 
-                    <?php if($subvention && $subvention->isApprouve()): ?>
-                        <label class="label label-success pull-right">Approuvé par l'interprofession</label>
-                    <?php elseif($subvention && $subvention->isRefuse()): ?>
-                        <label class="label label-danger pull-right">Réfusé par l'interprofession</label>
-                    <?php elseif($subvention && $subvention->isValide()): ?>
-                        <label class="label label-default pull-right">En attente de validation par l'interprofession</label>
-                    <?php elseif($subvention): ?>
-                        <label class="label label-warning pull-right">En cours de saisie</label>
-                    <?php else: ?>
-                        <label class="label label-info pull-right">Non commencé</label>
-                    <?php endif; ?></h3>
-                  </div>
-                  <div class="panel-body">
-                      Texte explicatif
-                  </div>
-                  <div class="panel-footer">
-                      <?php if($subvention && $subvention->isValide()): ?>
+    <h3>Contrat Relance Viti <?php if($subvention && $subvention->isApprouve()): ?>
+        <small class="pull-right"><span class="label label-success">Approuvé par l'interprofession</span></small>
+    <?php elseif($subvention && $subvention->isRefuse()): ?>
+        <small class="pull-right"><span class="label label-danger">Réfusé par l'interprofession</span></small>
+    <?php elseif($subvention && $subvention->isValide()): ?>
+        <small class="pull-right"><span class="label label-default">En attente de validation par l'interprofession</span></small>
+    <?php elseif($subvention): ?>
+        <small class="pull-right"><span class="label label-warning">En cours de saisie</span></small>
+    <?php else: ?>
+        <small class="pull-right"><span class="label label-info">Non commencé</span></small>
+    <?php endif; ?></h3>
+    <hr style="margin-top: 0;"/>
+                  <p>Texte explicatif</p>
+    <hr />
+                  <div class="row">
+                        <div class="col-xs-4 col-xs-offset-4">
+                        <?php if($subvention && $subvention->isValide()): ?>
                           <a href="<?php echo url_for('subvention_visualisation',array('identifiant' => $etablissement->identifiant, 'operation' => $operation_en_cours)); ?>" class="btn btn-block btn-success">Visualiser la procédure</a>
-                      <?php elseif($subvention): ?>
+                        <?php elseif($subvention): ?>
                           <a href="<?php echo url_for('subvention_infos',array('identifiant' => $etablissement->identifiant, 'operation' => $operation_en_cours)); ?>" class="btn btn-block btn-warning">Continuer la procédure</a>
-                      <?php else: ?>
+                        <?php else: ?>
                           <a href="<?php echo url_for('subvention_creation',array('identifiant' => $etablissement->identifiant, 'operation' => $operation_en_cours)); ?>" class="btn btn-block btn-default">Démarrer la procédure</a>
-                      <?php endif; ?>
-                  </div>
-            </div>
-        </div>
-    </div>
+                        <?php endif; ?>
+                        </div>
+                    </div>
+
 
 
 
