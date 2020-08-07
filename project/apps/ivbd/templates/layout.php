854984b3c6b4e1699fd4380aa88cd792c5c9fd78
Gabriel Poma <gpoma@24eme.fr>
2020-08-07 18:33:38 +0200

    On enlève le message global COVID

--- project/apps/ivbd/templates/layout.php
+++ project/apps/ivbd/templates/layout.php
@@ -1,3 +1,2 @@
 <?php
-$global_message = "<p style='text-align: center;'><span style='background-color:red; color:white; font-weight: bold;'> &nbsp; <a href='https://actus.ivbdpro.fr/wp-content/uploads/2020/04/8-Let-%C3%A0-viti-et-n%C3%A9gociants-du-vignoble-infos-covid-19-version-finale.pdf' style='color: white;'>Informations COVID-19 du 23 avril 2020 (pdf)</a> &nbsp; </span></p>";
 require_once(sfConfig::get('sf_root_dir')."/plugins/AppPlugin/modules/common/templates/layout.php"); ?>
