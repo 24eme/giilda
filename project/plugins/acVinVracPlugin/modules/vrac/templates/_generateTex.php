a1ef4c0bbeabae0b5df356c7157e49c40bc75c01
Tangui Morlier <tangui@tangui.eu.org>
2020-09-22 09:52:46 +0200

    Aout 2019 dans les deux versions de contrats

--- project/plugins/acVinVracPlugin/modules/vrac/templates/_generateTex.php
+++ project/plugins/acVinVracPlugin/modules/vrac/templates/_generateTex.php
@@ -33,7 +33,7 @@
 }
 
 \AddToShipoutPicture{%
-\AtLowerLeftCorner{0.75cm}{-12cm}{\ifodd\c@page\rotatebox{90}{\begin{minipage}{\paperheight} \centering <?php if($vrac->isProduitIGP()): ?>AOÛT 2016<?php else: ?>AOÛT 2018<?php endif; ?>\end{minipage}}\fi}
+\AtLowerLeftCorner{0.75cm}{-12cm}{\ifodd\c@page\rotatebox{90}{\begin{minipage}{\paperheight} \centering AOÛT 2019\end{minipage}}\fi}
 }
 \makeatother
 
