71cceb08a7e1d4aea265dc66b1f7ea9cabf0d4b8
Vincent LAURENT <vince.laurent@gmail.com>
2020-11-26 17:36:33 +0100

    Vue par cvi permet de rechercher un numéro ppm

--- project/plugins/acVinEtablissementPlugin/lib/model/views/etablissement.findByCvi.map.view.js
+++ project/plugins/acVinEtablissementPlugin/lib/model/views/etablissement.findByCvi.map.view.js
@@ -1,6 +1,13 @@
 function(doc) {
   	if (doc.type == "Etablissement") {
-        emit([doc.cvi],[doc.identifiant, doc.nom, doc.siege.commune, doc.region]);
-        emit([doc.no_accises],[doc.identifiant, doc.nom, doc.siege.commune, doc.region]);
+		if(doc.cvi) {
+  			emit([doc.cvi],[doc.identifiant, doc.nom, doc.siege.commune]);
+		}
+		if(doc.no_accises) {
+			emit([doc.no_accises],[doc.identifiant, doc.nom, doc.siege.commune]);
+		}
+		if(doc.ppm) {
+			emit([doc.ppm],[doc.identifiant, doc.nom, doc.siege.commune]);
+		}
  	}
 }
