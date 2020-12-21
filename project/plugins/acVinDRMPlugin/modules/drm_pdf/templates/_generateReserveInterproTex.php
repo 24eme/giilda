8f68616f5280606721b04dce1afb642f9a511d8a
Tangui Morlier <tangui@tangui.eu.org>
2020-12-21 12:15:30 +0100

    Pas de volume commercialisable pour IVBD

--- project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateReserveInterproTex.php
+++ project/plugins/acVinDRMPlugin/modules/drm_pdf/templates/_generateReserveInterproTex.php
@@ -12,19 +12,17 @@ $produits = $drm->getProduitsReserveInterpro();
     \textbf{Réserve interprofessionnelle}
     \end{large}
     \end{center}
-    \begin{tabular}{C{80mm} |C{40mm}|C{40mm}|}
+    \begin{tabular}{C{80mm}|C{40mm}|}
     \rowcolor{lightgray}
     \hline
     \multicolumn{1}{|C{80mm}}{\small{\textbf{Produit}}} &
-    \multicolumn{1}{|C{40mm}}{\small{\textbf{Réserve interprofessionnelle}}} &
-    \multicolumn{1}{|C{40mm}|}{\small{\textbf{Volume commercialisable}}}
+    \multicolumn{1}{|C{40mm}|}{\small{\textbf{Réserve interprofessionnelle}}}
     \\
     \hline
     <?php foreach ($produits as $produit): ?>
 
         \multicolumn{1}{|l}{\small{\textbf{<?php echo $produit->getLibelle(); ?>}}} &
-        \multicolumn{1}{|r}{\small{\textbf{<?php echoFloatWithHl($produit->getRerserveIntepro()); ?>}}} &
-        \multicolumn{1}{|r|}{\small{\textbf{<?php echoFloatWithHl($produit->getVolumeCommercialisable()); ?>}}}
+        \multicolumn{1}{|r|}{\small{\textbf{<?php echoFloatWithHl($produit->getRerserveIntepro()); ?>}}}
         \\
         \hline
 
