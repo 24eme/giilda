d718e3f72aae00a375fb94b9a0adac348b9d4865
Vincent LAURENT <vince.laurent@gmail.com>
2021-01-14 12:51:00 +0100

    Test de la sous génération

--- project/test/unit/giilda49GenerationTest.php
+++ project/test/unit/giilda49GenerationTest.php
@@ -6,3 +6,28 @@ sfContext::createInstance($configuration);
 $t = new lime_test(0);
 
 $t->comment("Création d'une génération");
+$date = "99998877665544";
+
+$doc = GenerationClient::getInstance()->find("GENERATION-TEST-".$date);
+if($doc) { $doc->delete; }
+$doc = GenerationClient::getInstance()->find("GENERATION-TEST-".$date."-TESTSOUSGENERATION");
+if($doc) { $doc->delete; }
+$doc = GenerationClient::getInstance()->find("GENERATION-TEST-".$date."-TESTSOUSGENERATION2");
+if($doc) { $doc->delete; }
+
+$generation = new Generation();
+$generation->date_emission = $date;
+$generation->type_document = "TEST";
+$generation->constructId();
+$generation->save();
+
+$t->is($generation->_id, "GENERATION-TEST-".$date, "id de la génération");
+
+$sousSeneration1 = $generation->getOrCreateSubGeneration("TESTSOUSGENERATION");
+$sousSeneration1->save();
+$sousSeneration2 = $generation->getOrCreateSubGeneration("TESTSOUSGENERATION2");
+$sousSeneration2->save();
+
+$t->is($sousSeneration->_id, $generation->_id."-TESTSOUSGENERATION", "id de la sous génération");
+$t->is($sousSeneration->getMasterGeneration()->id, $generation->_id, "Récupération de la génération maitre");
+$t->is(count($generation->getSubGenerations()), 2, "Récupération des sous générations");
