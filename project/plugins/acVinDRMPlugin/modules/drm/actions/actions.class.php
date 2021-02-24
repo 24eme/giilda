6204f90663bc9b33b1fd5834eb2a0816c661d55d
mathurin petit <mathurin.petit@gmail.com>
2021-02-24 18:45:27 +0100

    pour les retours de DRM, on affiche pas les DRM qui ont des modificatrices plus hautes et le liens d'une modificatrice plus haute renvoi vers cette DRM

--- project/plugins/acVinDRMPlugin/modules/drm/actions/actions.class.php
+++ project/plugins/acVinDRMPlugin/modules/drm/actions/actions.class.php
@@ -27,8 +27,8 @@ class drmActions extends drmGeneriqueActions {
 
     public function executeIndex(sfWebRequest $request) {
       $this->redirect403IfIsTeledeclaration();
-      
-      $res_by_page = 10;        
+
+      $res_by_page = 10;
       $this->page_num = $request->getParameter('page', 1);
 
       $this->current_page = $this->page_num;
@@ -47,11 +47,19 @@ class drmActions extends drmGeneriqueActions {
       $query->setSort([["doc.date_modification" => ["order"=>"desc"]]]);
       $query->setFrom($from);
       $query->setLimit($res_by_page);
-      $this->drm_controles = $index->search($query);
+      $this->drm_controles_search = $index->search($query);
+      $this->nb_results = $this->drm_controles_search->getTotalHits();
 
-      $this->nb_results = $this->drm_controles->getTotalHits();       
-      $this->last_page = ceil($this->nb_results / $res_by_page);        
-      
+      foreach ($this->drm_controles_search as $drm_controle) {
+        if(DRMClient::getInstance()->find($drm_controle->doc['_id'])->isMaster()){
+          $this->drm_controles[$drm_controle->doc['_id']] = $drm_controle;
+        }else{
+          $this->nb_results--;
+        }
+      }
+
+      $this->last_page = ceil($this->nb_results / $res_by_page);
+
       if($this->page_num > $this->last_page){
         return $this->forward404Unless(true);
       }
