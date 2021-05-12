f1c6ad387e33ac762cfef9a36b5380faf68869ec
mathurin petit <mathurin.petit@gmail.com>
2021-05-12 16:28:57 +0200

    dans le template on se prémuni des notice

--- project/apps/ivbd/modules/statistique/templates/_prix.php
+++ project/apps/ivbd/modules/statistique/templates/_prix.php
@@ -1,16 +1,20 @@
 <?php
 use_helper('IvbdStatistique');
-$millesime = preg_replace("/([0-9]+)-([0-9]+)/","$1",ConfigurationClient::getInstance()->getCurrentCampagne());
+$options = $options->getRawValue();
+$configuration = ConfigurationClient::getConfiguration($options['fromDate']);
+
+$millesime = preg_replace("/([0-9]+)-([0-9]+)/","$1",$configuration);
 $csvArray = array();
 $csvProduitCumulVolumes = array();
-
 $csv = 'Type contrat;Produit;Nombre de contrats;Qté en hl;Prix moyen;Volume '.$millesime.'; Cours '.$millesime.';Volume autres millesimes;Cours autres millesimes;Volume tous contrats'."\n";
 foreach ($result['agg_page']['buckets'] as $type_contrats) {
 	$type_contrats_libelle = getConditionnementLibelle(strtoupper($type_contrats['key']));
 	$csvArray[$type_contrats_libelle] = array();
 	foreach ($type_contrats['agg_page']['buckets'] as $produit_contrats_millesimes) {
-		$produitLibelle = getProduitLibelle($produit_contrats_millesimes['key']);
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']] = array();
+
+        $produitLibelle = getProduitLibelle($produit_contrats_millesimes['key'],$configuration);
+
+		$csvArray[$type_contrats_libelle][$produitLibelle] = array();
 
 		$nbContrats = $produit_contrats_millesimes["doc_count"];
 
@@ -29,8 +33,8 @@ foreach ($result['agg_page']['buckets'] as $type_contrats) {
 		$horsMillesimesMoyenne = 0.0;
 
 		foreach ($produit_contrats_millesimes['agg_line']['buckets'] as $contrats_millesimes) {
-			$toutMillesimesVolume += formatNumber($contrats_millesimes['vol_prix']['agg_column']['value'],2);
-			$toutMillesimesPrix += formatNumber($contrats_millesimes['ca']['agg_column']['value'],2);
+			$toutMillesimesVolume += ($contrats_millesimes['vol_prix']['agg_column']['value'])? floatval($contrats_millesimes['vol_prix']['agg_column']['value']) : 0.0;
+			$toutMillesimesPrix += ($contrats_millesimes['ca']['agg_column']['value'])? floatval($contrats_millesimes['ca']['agg_column']['value']) : 0.0;
 			if($contrats_millesimes['key'] == $millesime){
 				$millesimesNb = $contrats_millesimes['doc_count'];
 				$millesimesVolume = formatNumber($contrats_millesimes['vol_prix']['agg_column']['value'],2);
@@ -38,27 +42,27 @@ foreach ($result['agg_page']['buckets'] as $type_contrats) {
 				$millesimesMoyenne  = formatNumber($contrats_millesimes['moyenne']['value'],2);
 			}else{
 				$horsMillesimesNb += $contrats_millesimes['doc_count'];
-				$horsMillesimesVolume += formatNumber($contrats_millesimes['vol_prix']['agg_column']['value'],2);
-				$horsMillesimesPrix += formatNumber($contrats_millesimes['ca']['agg_column']['value'],2);
+				$horsMillesimesVolume += ($contrats_millesimes['vol_prix']['agg_column']['value'])? floatval($contrats_millesimes['vol_prix']['agg_column']['value']) : 0.0;
+				$horsMillesimesPrix += ($contrats_millesimes['ca']['agg_column']['value'])? floatval($contrats_millesimes['ca']['agg_column']['value']) : 0.0 ;
 			}
 		}
 
 		$toutMillesimesMoyenne = ($toutMillesimesVolume)? formatNumber($toutMillesimesPrix / $toutMillesimesVolume / 225,2) : 0.0;
 		$horsMillesimesMoyenne = ($horsMillesimesVolume)? formatNumber($horsMillesimesPrix / $horsMillesimesVolume / 225,2) : 0.0;
-		if(!array_key_exists($produit_contrats_millesimes['key'],$csvProduitCumulVolumes)){
-			$csvProduitCumulVolumes[$produit_contrats_millesimes['key']] = 0.0;
+		if(!array_key_exists($produitLibelle,$csvProduitCumulVolumes)){
+			$csvProduitCumulVolumes[$produitLibelle] = 0.0;
 		}
-		$csvProduitCumulVolumes[$produit_contrats_millesimes['key']] += $toutMillesimesVolume;
+		$csvProduitCumulVolumes[$produitLibelle] += $toutMillesimesVolume;
 
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['type_contrats_libelle'] = $type_contrats_libelle;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['produitLibelle'] = $produitLibelle;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['nbContrats'] = $nbContrats;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['toutMillesimesVolume'] = $toutMillesimesVolume;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['toutMillesimesMoyenne'] = $toutMillesimesMoyenne;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['millesimesVolume'] = $millesimesVolume;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['millesimesMoyenne'] = $millesimesMoyenne;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['horsMillesimesVolume'] = $horsMillesimesVolume;
-		$csvArray[$type_contrats_libelle][$produit_contrats_millesimes['key']]['horsMillesimesMoyenne'] = $horsMillesimesMoyenne;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['type_contrats_libelle'] = $type_contrats_libelle;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['produitLibelle'] = $produitLibelle;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['nbContrats'] = $nbContrats;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['toutMillesimesVolume'] = $toutMillesimesVolume;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['toutMillesimesMoyenne'] = $toutMillesimesMoyenne;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['millesimesVolume'] = $millesimesVolume;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['millesimesMoyenne'] = $millesimesMoyenne;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['horsMillesimesVolume'] = $horsMillesimesVolume;
+		$csvArray[$type_contrats_libelle][$produitLibelle]['horsMillesimesMoyenne'] = $horsMillesimesMoyenne;
 		}
 	}
 
