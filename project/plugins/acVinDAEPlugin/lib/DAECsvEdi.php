81924161e2a971909c91354e54a6c282ba93c7b6
Jb Lm <lemetayer.jb@gmail.com>
2021-06-09 14:52:04 +0200

    back gestion csv dae

--- project/plugins/acVinDAEPlugin/lib/DAECsvEdi.php
+++ project/plugins/acVinDAEPlugin/lib/DAECsvEdi.php
@@ -38,30 +38,7 @@ class DAECsvEdi extends CsvFile {
         $this->buildCountryList();
         $this->buildDeviseList();
 
-        $this->ignore = $ignore_first_if_comment;
-        $this->separator = ';';
-        if (!$file)
-          return ;
-        if (!file_exists($file) && !preg_match('/^http/', $file))
-          throw new Exception("Cannont access $file");
-        $this->file = $file;
-        $handle = fopen($this->file, 'r');
-        if (!$handle) {
-          throw new sfException('unable to open file: '.$this->file);
-        }
-        $buffer = fread($handle, 500);
-        fclose($handle);
-        $buffer = preg_replace('/$[^\n]*\n/', '', $buffer);
-        if (!$buffer) {
-          throw new Exception('invalid csv file; '.$this->file);
-        }
-        $virgule = explode(',', $buffer);
-        $ptvirgule = explode(';', $buffer);
-        $tabulation = explode('\t', $buffer);
-        if (count($virgule) > count($ptvirgule) && count($virgule) > count($tabulation))
-          $this->separator = ',';
-        else if (count($tabulation) > count($ptvirgule))
-          $this->separator = '\t';
+        parent::__construct($file);
     }
 
     public function buildCountryList() {
