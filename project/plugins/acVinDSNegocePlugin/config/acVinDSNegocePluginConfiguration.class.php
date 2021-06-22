33a4982411f387859ff6eb9c8f8eef2844162879
Jb Lm <lemetayer.jb@gmail.com>
2021-06-23 00:38:44 +0200

    suppression de la gestion en objet du routing

--- project/plugins/acVinDSNegocePlugin/config/acVinDSNegocePluginConfiguration.class.php
+++ project/plugins/acVinDSNegocePlugin/config/acVinDSNegocePluginConfiguration.class.php
@@ -2,8 +2,4 @@
 
 class acVinDSNegocePluginConfiguration extends sfPluginConfiguration
 {
-  public function initialize()
-  {
-    $this->dispatcher->connect('routing.load_configuration', array('DSNegoceRouting', 'listenToRoutingLoadConfigurationEvent'));
-  }
 }
