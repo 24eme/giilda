0f04b20af9512985fafd09f0d052c5691846e9f9
Jb Lm <lemetayer.jb@gmail.com>
2021-07-19 23:19:28 +0200

    conf ds

--- project/plugins/acVinDSPlugin/config/acVinDSPluginConfiguration.class.php
+++ project/plugins/acVinDSPlugin/config/acVinDSPluginConfiguration.class.php
@@ -2,4 +2,18 @@
 
 class acVinDSPluginConfiguration extends sfPluginConfiguration
 {
+  public function setup() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            $configCache->registerConfigHandler('config/ds.yml', 'sfDefineEnvironmentConfigHandler', array('prefix' => 'ds_'));
+            $configCache->checkConfig('config/ds.yml');
+        }
+    }
+
+    public function initialize() {
+        if ($this->configuration instanceof sfApplicationConfiguration) {
+            $configCache = $this->configuration->getConfigCache();
+            include($configCache->checkConfig('config/ds.yml'));
+        }
+    }
 }
