20ee34f245b540fc409027c2763e90c76bc65369
Gabriel Poma <gpoma@24eme.fr>
2021-10-05 20:39:29 +0200

    header bivc

--- project/apps/bivc/modules/common/templates/_header.php
+++ project/apps/bivc/modules/common/templates/_header.php
@@ -1,6 +1,18 @@
-<nav class="navbar navbar-default navbar-static-top">
-     <div class="container" style="position: relative;">
-        <img src="/images/logo_site_bivc.png" alt="Logo sancerre" style="height: 46px; position: absolute; left: -44px; padding-top: 4px;" />
-        <?php include_component('common', 'nav'); ?>
+<header id="header">
+  <div class="header-top" style="border-bottom: 1px solid #efefef">
+    <div class="container">
+      <h1 class="logo-site">
+        <a href="/"><img src="/images/logo_site_bivc.png" alt="Logo Sancerre" height="100px"/></a>
+        <span style="font-size: 24px; display: inline-block; vertical-align: middle">
+            Bureau Interprofessionnel des Vins du Centre-Loire
+            <br/><small>Espace déclaratif professionnel</small>
+        </span>
+      </h1>
     </div>
-</nav>
+  </div>
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+            <?php include_component('common', 'nav'); ?>
+      </div>
+  </nav>
+</header>
