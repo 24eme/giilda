9fe727dca3797dcae06965a622de141d34b8bbba
Gabriel Poma <gpoma@24eme.fr>
2021-10-08 12:58:26 +0200

    style bivc header centré

--- project/apps/bivc/modules/common/templates/_header.php
+++ project/apps/bivc/modules/common/templates/_header.php
@@ -1,11 +1,19 @@
 <header id="header">
   <div class="header-top">
     <div class="container">
-      <h1 class="logo-site">
-        <a href="/"><img src="/images/logo_site_bivc.png" alt="Logo Sancerre" height="100px"/></a>
-            Bureau Interprofessionnel des Vins du Centre-Loire</br>
-            Espace déclaratif professionnel
-      </h1>
+      <div class="row">
+        <div class="col-xs-2">
+          <div class="logo-site">
+            <a href="/"><img src="/images/logo_site_bivc.png" alt="Logo Sancerre" height="100px"/></a>
+          </div>
+        </div>
+        <div class="col-xs-8 text-center">
+          <h1>
+              Bureau Interprofessionnel des Vins du Centre-Loire</br>
+              <small>Espace déclaratif professionnel</small>
+          </h1>
+        </div>
+      </div>
     </div>
   </div>
     <nav class="navbar navbar-default navbar-static-top">
