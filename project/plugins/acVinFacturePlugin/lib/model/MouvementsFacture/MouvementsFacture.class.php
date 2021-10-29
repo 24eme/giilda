82edd3c173222f32b829b0c4d7d5a8a1ad680c20
Jb Lm <lemetayer.jb@gmail.com>
2021-10-29 10:35:43 +0200

    correction depart numero d'ordre pour les facturations libre avec des mouvements factures (disabled dans le form)

--- project/plugins/acVinFacturePlugin/lib/model/MouvementsFacture/MouvementsFacture.class.php
+++ project/plugins/acVinFacturePlugin/lib/model/MouvementsFacture/MouvementsFacture.class.php
@@ -78,6 +78,17 @@ class MouvementsFacture extends BaseMouvementsFacture {
         throw new sfException(sprintf('The mouvement %s of the document %s does not exist', $cle_mouvement, $this->document->get('_id')));
     }
 
+    public function getStartIndexForSaisieForm() {
+        $index = 0;
+        foreach($this->getSortedMvts() as $mvt) {
+          if ($mvt->facture && $index < $mvt->vrac_numero) {
+            $index = $mvt->vrac_numero;
+          }
+        }
+        $index++;
+        return $index;
+    }
+
     public function getLastMouvement() {
         $mvts = $this->getSortedMvts();
         if (isset($mvts['999_nouveau_nouveau'])) {
