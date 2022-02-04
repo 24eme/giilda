8f9c8493e83dc7773fb47f086c4be590e88db7a2
Jb Lm <lemetayer.jb@gmail.com>
2022-02-04 18:19:53 +0100

    ajout des constantes manquantes

--- project/plugins/acVinFacturePlugin/lib/model/views/FactureEtablissementView.class.php
+++ project/plugins/acVinFacturePlugin/lib/model/views/FactureEtablissementView.class.php
@@ -11,6 +11,10 @@ class FactureEtablissementView extends acCouchdbView
     const VALUE_TOTAL_TTC = 2;
     const VALUE_STATUT = 3;
     const VALUE_NUMERO_ARCHIVE = 4;
+    const VALUE_NUMERO_IL = 5;
+    const VALUE_TOTAL_HT = 6;
+    const VALUE_DECLARANT = 7;
+    const VALUE_DATE_PAIEMENT = 8;
 
 
     public static function getInstance() {
