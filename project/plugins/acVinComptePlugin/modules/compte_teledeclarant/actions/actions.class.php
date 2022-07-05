4e8dfde08975df16208777615739aea948e92d6a
Tangui Morlier <tangui@tangui.eu.org>
2022-07-05 19:13:11 +0200

    Vérifie l'existance d'un compte

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
@@ -197,8 +197,7 @@ class compte_teledeclarantActions extends sfActions {
         return $this->renderText(file_get_contents($path));
     }
 
-    public function executeViticonnect(sfWebRequest $request)
-    {
+    private function checkApiAccess(sfWebRequest $request) {
         $secret = sfConfig::get('app_viticonnect_secret');;
         $login = $request->getParameter('login');
         $epoch = $request->getParameter('epoch');
@@ -211,7 +210,12 @@ class compte_teledeclarantActions extends sfActions {
             http_response_code(401);
             die("Unauthorized");
         }
+    }
 
+    public function executeViticonnectApi(sfWebRequest $request)
+    {
+        $this->checkApiAccess($request);
+        $login = $request->getParameter('login');
         $compte = acCouchdbManager::getClient('Compte')->retrieveByLogin(strtolower($login));
         if (!$compte) {
             http_response_code(401);
@@ -230,5 +234,19 @@ class compte_teledeclarantActions extends sfActions {
         }
         $this->setLayout(false);
         $this->getResponse()->setHttpHeader('Content-Type', 'text/plain');
+    }
+
+    public function executeViticonnectCheck(sfWebRequest $request)
+    {
+        $this->checkApiAccess($request);
+        $login = $request->getParameter('login');
+        $comptes = EtablissementAllView::getInstance()->findByInterproAndStatut('INTERPRO-declaration', EtablissementClient::STATUT_ACTIF, $login);
+        if(count($comptes) == 1) {
+            echo "Found";
+            exit;
+        }
+        http_response_code(404);
+        die('Not found');
+
     }
 }
