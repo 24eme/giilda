0e2a0878a6a3a9d63e89c3269c7cf1a334070143
Vincent LAURENT <vince.laurent@gmail.com>
2022-07-08 02:34:41 +0200

    Entête csv libellé

--- project/plugins/ImportPlugin/lib/csv/CompteCsvFile.class.php
+++ project/plugins/ImportPlugin/lib/csv/CompteCsvFile.class.php
@@ -186,7 +186,7 @@ class CompteCsvFile extends CsvFile
     }
 
     public static function getCsvHeader() {
-        $csv = "identifiant;nom complet;type;intitule;nom;prénom;adresse;adresse complémentaire;code postal;commune;pays;téléphone bureau;téléphone mobile;téléphone perso;fax;email;commentaire;id société;type société;société raison sociale;société adresse;société adresse complémentaire;société code postal;société commune;société téléphone;société fax;société email;code de création;statut;date creation;date cloture;";
+        $csv = "identifiant;nom complet;type;intitule;nom;prénom;adresse;adresse complémentaire;code postal;commune;pays;téléphone bureau;téléphone mobile;téléphone perso;fax;email;commentaire;société identifiant;société type;société raison sociale;société adresse;société adresse complémentaire;société code postal;société commune;société téléphone;société fax;société email;code de création;statut;date creation;date cloture;";
 
         foreach(SocieteConfiguration::getInstance()->getExtras() as $key => $item) {
             $csv .= $item['nom'].';';
