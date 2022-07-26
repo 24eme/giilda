8afe2eb88845c919efa7bff54662aeef5c0c25c6
Jb Lm <lemetayer.jb@gmail.com>
2022-07-26 10:48:26 +0200

    finalisation des templates pdf relances

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_relances_generique.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_relances_generique.php
@@ -1,16 +1,23 @@
 <?php
 use_helper('Float');
 use_helper('Display');
+use_helper('Date');
 
 include_partial('facture/pdf_generique_prelatex', array('pdf_titre' => 'Relance', 'ressortissant' => $infos));
 include_partial('facture/pdf_relances_generique_entete', array('infos' => $infos));
+if ($infos->nb_relance > 1) {
+  include_partial('facture/pdf_relances_pretexte_r2', array('date' => $infos->date_derniere_relance));
+} else {
+  include_partial('facture/pdf_relances_pretexte_r1');
+}
 ?>
+
 \centering
 \fontsize{8}{10}\selectfont
     \begin{tikzpicture}
 		\node[inner sep=1pt] (tab1){
                         \renewcommand{\arraystretch}{1.2}
-			\begin{tabular}{p{68mm} |p{60mm}|p{24mm}|p{24mm}p{0mm}}
+			\begin{tabular}{p{52mm} |p{52mm}|p{35mm}|p{35mm}p{0mm}}
   			\rowcolor{lightgray}
                         \centering \small{\textbf{N° d'appel}} &
    			            \centering \small{\textbf{Date d'appel}} &
@@ -20,9 +27,9 @@ include_partial('facture/pdf_relances_generique_entete', array('infos' => $infos
   			\hline
                       ~ & ~ & ~ & ~ &\\
 
-<?php foreach ($factures as $facture): ?>
-    \multicolumn{1}{r|}{<?php echo $facture[12] ?>} &
-    \multicolumn{1}{r|}{<?php echo $facture[13] ?>} &
+<?php $papillon = new stdClass(); $papillon->montant_ttc = 0; foreach ($factures as $facture): $papillon->montant_ttc += $facture[15]; ?>
+    \multicolumn{1}{c|}{<?php echo $facture[12] ?>} &
+    \multicolumn{1}{c|}{<?php echo format_date($facture[13],'dd/MM/yyyy'); ?>} &
     \multicolumn{1}{r|}{<?php echoArialFloat($facture[14]) ?>} &
     \multicolumn{1}{r}{<?php echoArialFloat($facture[15]) ?>}
     \\
@@ -31,8 +38,19 @@ include_partial('facture/pdf_relances_generique_entete', array('infos' => $infos
   };
     \node[draw=gray, inner sep=-2pt, rounded corners=3pt, line width=2pt, fit=(tab1.north west) (tab1.north east) (tab1.south east) (tab1.south west)] {};
 \end{tikzpicture}
+\begin{flushright}
+\textbf{TOTAL DU : <?php echoArialFloat($papillon->montant_ttc) ?>~\texteuro{}}
+\end{flushright}
+
+
+
 <?php
+if ($infos->nb_relance > 1) {
+  include_partial('facture/pdf_relances_posttexte_r2');
+} else {
+  include_partial('facture/pdf_relances_posttexte_r1');
+}
 include_partial('facture/pdf_relances_generique_reglement');
-//include_partial('facture/pdf_relances_generique_echeances', array('facture' => $facture));
+include_partial('facture/pdf_relances_generique_echeances', array('papillon' => $papillon));
 ?>
 \end{document}
--- project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_relances_generique_echeances.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_pdf_relances_generique_echeances.php
@@ -2,8 +2,8 @@
 use_helper('Float');
 use_helper('Date');
 $chequesOrdre = FactureConfiguration::getInstance()->getOrdreCheques();
-$echeances = $facture->getEcheancesPapillon();
 ?>
+
 \begin{center}
 
 \begin{minipage}[b]{1\textwidth}
@@ -14,41 +14,21 @@ $echeances = $facture->getEcheancesPapillon();
 	\multicolumn{3}{>{\columncolor[rgb]{0.8,0.8,0.8}}c}{\centering \small{\textbf{Références de facturation}}} \\
 
         \CutlnPapillonEntete
-        <?php if($facture->getNbPaiementsAutomatique()): ?>
-          &
-     \centering \fontsize{7}{8}\selectfont Conformément à votre demande, le montant de cette facture sera prélevé \\ ~ &
-
-      \centering \small{Echéance} &
-      \centering \small{Client~/~Facture} &
-      \multicolumn{1}{c}{\small{Montant TTC}} \\
-
-                  \centering \small{~} &
-                  \centering \fontsize{7}{8}\selectfont sur votre compte \textbf{<?php echo $facture->getSociete()->getMandatSepa()->getBanqueNom() ?>} n° \textbf{<?php echo $facture->getSociete()->getMandatSepa()->getNumeroCompte() ?>} le \textbf{<?php echo format_date($facture->paiements[0]->date,'dd/MM/yyyy'); ?>} &
-
-                  \centering \small{\textbf{<?php echo format_date($facture->date_echeance,'dd/MM/yyyy'); ?>}} &
-                  \centering \small{\FactureRefCodeComptableClient~/~\FactureNum} &
-                  \multicolumn{1}{r}{\small{\textbf{<?php echo echoArialFloat($facture->total_ttc); ?>~\texteuro{}}}}  \\
-
-
-        <?php else: ?>
-        <?php $nb = count($echeances) ; foreach ($echeances as $key => $papillon) : ?>
         &
    \centering \fontsize{7}{8}\selectfont Par chèque à l'ordre : <?php echo ($chequesOrdre)? $chequesOrdre : "Ordre chèque"; ?> \\ ~ &
 
     \centering \small{Echéance} &
-    \centering \small{Client~/~Facture} &
-    \multicolumn{1}{c}{\small{Montant TTC}} \\
+    \centering \small{Client~/~Relance} &
+    \multicolumn{1}{c}{\small{Cotisation due}} \\
 
                 \centering \small{~} &
                 \centering \fontsize{7}{8}\selectfont Par virement bancaire : \InterproBANQUE \\  \textbf{BIC~:}~\InterproBIC~\textbf{IBAN~:}~\InterproIBAN &
 
-                \centering \small{\textbf{<?php echo format_date($papillon->echeance_date,'dd/MM/yyyy'); ?>}} &
+                \centering \small{\textbf{A reception}} &
                 \centering \small{\FactureRefCodeComptableClient~/~\FactureNum} &
-                \multicolumn{1}{r}{\small{\textbf{<?php echo echoArialFloat($papillon->montant_ttc); ?>~\texteuro{}}}}  \\
+                \multicolumn{1}{r}{\small{\textbf{<?php echoArialFloat($papillon->montant_ttc); ?>~\texteuro{}}}}  \\
 
 
-        <?php endforeach; ?>
-      <?php endif; ?>
       \CutlnPapillon
 \end{tabular}
 \end{minipage}
