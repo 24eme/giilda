efe176da52b824a588e67345f432582572bccc89
Jb Lm <lemetayer.jb@gmail.com>
2022-09-20 17:17:27 +0200

    ajout de la region dans les keys de la vue generation

--- project/plugins/acVinGenerationPlugin/lib/model/views/generation.history.map.view.js
+++ project/plugins/acVinGenerationPlugin/lib/model/views/generation.history.map.view.js
@@ -2,5 +2,6 @@ function(doc) {
     if (doc.type != "Generation") {
         return;
     }
-    emit([doc.type_document, doc.date_emission, doc.identifiant], [doc.nb_documents, doc.documents, doc.somme, doc.statut, doc.libelle]);
+    var region = (doc.arguments && doc.arguments.region)? doc.arguments.region : null;
+    emit([doc.type_document, region, doc.date_emission, doc.identifiant], [doc.nb_documents, doc.documents, doc.somme, doc.statut, doc.libelle]);
 }
