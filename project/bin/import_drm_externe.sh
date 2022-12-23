afb6decd5d4147c2e20d739dcefa822d01c29024
Tangui Morlier <tangui@tangui.eu.org>
2022-12-23 15:02:08 +0100

    Message si pb de FTP + évite que les DRM soient chargées plusieurs fois via le clean

--- project/bin/import_drm_externe.sh
+++ project/bin/import_drm_externe.sh
@@ -7,7 +7,7 @@ mkdir -p $DRMEXTERNEIMPORTDIR 2> /dev/null
 #En cas de problème avec la clé publique lftp, voir http://tutos.tangui.eu.org/2021/02/23/lftp-host-key-verification-failed/
 for u in "${GETDRMEXTERNECMD[@]}"
 do
-    eval $u
+    eval $u > /dev/null || echo "Erreur dans la récupération FTP des DRM"
 done
 
 touch -d "7 day ago" /tmp/import_drm_externe.$$.file
@@ -16,8 +16,8 @@ do
     file=$(basename $path)
     PERIODE=$(echo -n $file | cut -d "_" -f 2)
     IDENTIFIANT=$(echo -n $file | cut -d "_" -f 3)
-    cat $DRMEXTERNEIMPORTDIR/$file | grep -v ";dont_revendique;" | grep -E "^(ANNEXE|CRD|CAVE)" | grep -v ';acquitté;' > $DRMEXTERNEIMPORTDIR/$file".cleaned.csv"
-    php symfony drm:edi-import $DRMEXTERNEIMPORTDIR/$file".cleaned.csv" $PERIODE $IDENTIFIANT $SYMFONYTASKOPTIONS --trace | grep -v 'DEBUG:';
+    cat $DRMEXTERNEIMPORTDIR/$file | grep -v ";dont_revendique;" | grep -E "^(ANNEXE|CRD|CAVE)" | grep -v ';acquitté;' > $DRMEXTERNEIMPORTDIR/$file".cleaned"
+    php symfony drm:edi-import $DRMEXTERNEIMPORTDIR/$file".cleaned" $PERIODE $IDENTIFIANT $SYMFONYTASKOPTIONS --trace | grep -v 'DEBUG:';
 done
 rm /tmp/import_drm_externe.$$.file
 touch $DRMEXTERNEIMPORTDIR/last_update
