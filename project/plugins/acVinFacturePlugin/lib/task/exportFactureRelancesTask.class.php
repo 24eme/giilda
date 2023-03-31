859e75dac58125a6f6a0e7050c44217198edeb9c
Jb Lm <lemetayer.jb@gmail.com>
2023-03-31 14:57:10 +0200

    ajout d'une option de filtrage interpo

--- project/plugins/acVinFacturePlugin/lib/task/exportFactureRelancesTask.class.php
+++ project/plugins/acVinFacturePlugin/lib/task/exportFactureRelancesTask.class.php
@@ -11,6 +11,7 @@ class exportFactureRelancesTask extends sfBaseTask
             new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'default'),
             new sfCommandOption('factureid', null, sfCommandOption::PARAMETER_OPTIONAL, 'Export a specific Facture', ''),
             new sfCommandOption('entete', null, sfCommandOption::PARAMETER_REQUIRED, "Ligne d'entête", true),
+            new sfCommandOption('interpro', null, sfCommandOption::PARAMETER_OPTIONAL, 'Interpro'),
         ));
 
         $this->namespace        = 'export';
@@ -44,6 +45,9 @@ EOF;
             if (!$facture||($facture->getNumberToRelance() === false)) {
                 return;
             }
+            if ($options["interpro"] && $facture->getOrAdd('interpro') != $options["interpro"]) {
+                return;
+            }
             $export = new ExportFactureRelanceCSV($facture, false);
             echo $export->export();
             return ;
@@ -53,6 +57,9 @@ EOF;
           $facture = FactureClient::getInstance()->find($vfacture->id);
           if(!$facture||($facture->getNumberToRelance() === false)) {
               continue;
+          }
+          if ($options["interpro"] && $facture->getOrAdd('interpro') != $options["interpro"]) {
+              return;
           }
           $export = new ExportFactureRelanceCSV($facture, false);
           echo $export->export();
