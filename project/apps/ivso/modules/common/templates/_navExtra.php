fd401437f91047a254a4da9e06db2e24e92e0555
Tangui Morlier <tangui@tangui.eu.org>
2023-07-24 19:38:57 +0200

    Ajout de la possibilité d'un lien météo

--- project/apps/ivso/modules/common/templates/_navExtra.php
+++ project/apps/ivso/modules/common/templates/_navExtra.php
@@ -1,5 +1,6 @@
-<?php if(sfConfig::get('app_extra_service_url')): ?>
-<li>
-    <a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url') ?>">Documents ↗</a>
-</li>
+<?php if(sfConfig::get('app_extra_service_url_documents')): ?>
+    <li><a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url')['documents'] ?>">Docs ↗</a></li>
+<?php endif; ?>
+<?php if(sfConfig::get('app_extra_service_url_meteo')): ?>
+    <li><a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url_meteo'); ?>">Météo ↗</a></li>
 <?php endif; ?>
