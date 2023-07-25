2afba31f7cc81d6f6c9eb3e580f471d685d65759
Tangui Morlier <tangui@tangui.eu.org>
2023-07-25 09:50:59 +0200

    Météo qui s'ouvre dans une autre fenetre

--- project/apps/ivso/modules/common/templates/_navExtra.php
+++ project/apps/ivso/modules/common/templates/_navExtra.php
@@ -2,5 +2,5 @@
     <li><a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url')['documents'] ?>">Docs ↗</a></li>
 <?php endif; ?>
 <?php if(sfConfig::get('app_extra_service_url_meteo')): ?>
-    <li><a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url_meteo'); ?>">Météo ↗</a></li>
+    <li><a tabindex="-1" target="_blank" href="<?php echo sfConfig::get('app_extra_service_url_meteo'); ?>">Météo ↗</a></li>
 <?php endif; ?>
