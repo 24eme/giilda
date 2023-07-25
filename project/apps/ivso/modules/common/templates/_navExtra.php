14ffc62ec65ca83145c99cf541419b0124d8025e
Tangui Morlier <tangui@tangui.eu.org>
2023-07-25 10:21:14 +0200

    Permet l'usage du target=blank

--- project/apps/ivso/modules/common/templates/_navExtra.php
+++ project/apps/ivso/modules/common/templates/_navExtra.php
@@ -2,5 +2,5 @@
     <li><a tabindex="-1" href="<?php echo sfConfig::get('app_extra_service_url')['documents'] ?>">Docs <small>↗</small></a></li>
 <?php endif; ?>
 <?php if(sfConfig::get('app_extra_service_url_meteo')): ?>
-    <li><a tabindex="-1" target="_blank" href="<?php echo sfConfig::get('app_extra_service_url_meteo'); ?>">Météo <small>↗</small></a></li>
+    <li><a tabindex="-1" target="_blank" class="external" href="<?php echo sfConfig::get('app_extra_service_url_meteo'); ?>">Météo <small>↗</small></a></li>
 <?php endif; ?>
