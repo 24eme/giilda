a72271cff3716b773d94e4b3cd05086268642cfc
Jb Lm <lemetayer.jb@gmail.com>
2023-10-26 17:14:33 +0200

    facturation par mail en utilisant un partial pour le contenu du mail

---
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_email.php
@@ -0,0 +1,15 @@
+<?php
+$interproNom = $facture->getConfiguration()->getNomInterproTeledeclaration();
+if (!$interproNom) {
+    $interproNom = sfConfig::get('app_teledeclaration_interpro');
+}
+?>
+Bonjour,
+
+Une nouvelle facture émise par <?php echo $interproNom ?> est disponible.
+
+Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>
+
+Bien cordialement,
+
+<?php echo $interproNom ?>
--- project/plugins/acVinGenerationPlugin/lib/GenerationFactureMail.class.php
+++ project/plugins/acVinGenerationPlugin/lib/GenerationFactureMail.class.php
@@ -2,8 +2,8 @@
 
 class GenerationFactureMail extends GenerationAbstract {
 
-    public function getEmailBody() {
-        return sfContext::getInstance()->getController()->getAction('facture', 'main')->getPartial('facture/email');
+    public function getEmailBody($facture) {
+        return sfContext::getInstance()->getController()->getAction('facture', 'main')->getPartial('facture/email', ['facture' => $facture]);
     }
 
     public function generateMailForADocumentId($id) {
@@ -27,15 +27,7 @@ class GenerationFactureMail extends GenerationAbstract {
          ->setFrom($interproEmail)
          ->setTo($facture->getSociete()->getEmailCompta())
          ->setSubject("Facture n°".$facture->getNumeroInterpro()." - ".$interproNom)
-         ->setBody("Bonjour,
-
-Une nouvelle facture émise par ".$interproNom." est disponible.
-
-Vous pouvez la télécharger directement en cliquant sur le lien : ".ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($id)), true)."
-
-Bien cordialement,
-
-".$interproNom);
+         ->setBody($this->getEmailBody($facture));
 
         return $message;
     }
