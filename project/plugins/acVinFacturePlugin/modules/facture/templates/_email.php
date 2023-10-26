312b0180d7ca56bc6fe611407675c1a83f7f9a18
Jb Lm <lemetayer.jb@gmail.com>
2023-10-26 18:02:35 +0200

    ajout d'un point pour prise en compte de l'espace du dessous

--- project/plugins/acVinFacturePlugin/modules/facture/templates/_email.php
+++ project/plugins/acVinFacturePlugin/modules/facture/templates/_email.php
@@ -8,7 +8,7 @@ Bonjour,
 
 Une nouvelle facture émise par <?php echo $interproNom ?> est disponible.
 
-Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>
+Vous pouvez la télécharger directement en cliquant sur le lien : <?php echo ProjectConfiguration::getAppRouting()->generate('facture_pdf_auth', array('id' => $facture->_id, 'auth' => FactureClient::generateAuthKey($facture->_id)), true); ?>.
 
 Bien cordialement,
 
