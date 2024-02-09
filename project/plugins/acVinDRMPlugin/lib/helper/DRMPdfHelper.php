5805215bccb341491ef89d0680d12d0d0eeebc73
Tanguy Le Faucheur <tale-fau@24eme.fr>
2024-02-09 15:04:10 +0100

    Suppression fonction helper inutile

--- project/plugins/acVinDRMPlugin/lib/helper/DRMPdfHelper.php
+++ project/plugins/acVinDRMPlugin/lib/helper/DRMPdfHelper.php
@@ -4,42 +4,6 @@ function getCheckBoxe($b) {
     return ($b) ? '\squareChecked' : '$\square$';
 }
 
-function getActualRegime($regimes_array, $regime_crd) {
-    foreach ($regimes_array as $regime_crd_key => $long_libelle) {
-        if ($regime_crd_key == $regime_crd && str_contains($long_libelle, '+')) {
-            $split_values = explode('+', $long_libelle);
-
-            $key_value_pairs = array_map(function($split) use ($regimes_array) {
-                return [in_array($split, $regimes_array) => $split];
-            }, $split_values);
-
-            $detect_regime_array = array_combine(array_keys($key_value_pairs), array_column($key_value_pairs, 0));
-            foreach ($detect_regime_array as $index => $value) {
-                $detect_regime_array[$index] = trim($value);
-            }
-
-        }
-        elseif ($regime_crd_key == $regime_crd) {
-            $detect_regime_array[in_array($long_libelle, $regimes_array)] = $long_libelle;
-        }
-    }
-    $ret = array(EtablissementClient::REGIME_CRD_PERSONNALISE, EtablissementClient::REGIME_CRD_COLLECTIF_ACQUITTE, EtablissementClient::REGIME_CRD_COLLECTIF_SUSPENDU);
-    foreach ($ret as $index => $libelle) {
-        $libelle_corresp = EtablissementClient::$regimes_crds_libelles[$libelle];
-        foreach ($detect_regime_array as $key => $regime) {
-            if (strcmp($libelle_corresp, $regime) == 0) {
-                $ret[$index] = [$libelle_corresp => true];
-                break;
-            } else {
-                $ret[$index] = [$libelle_corresp => false];
-            }
-        }
-    }
-
-//reussir a changer la valeur des key ou alors ajouter des valeurs en + (true/false) a chq libelle
-    return $ret;
-}
-
 function getInfosInterpro() {
     return sfConfig::get('app_teledeclaration_contact_drm');
 }
