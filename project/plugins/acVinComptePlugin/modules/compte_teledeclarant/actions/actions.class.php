99ebd67ca801e3a660d10b22c66e09ade036c582
Vincent LAURENT <vince.laurent@gmail.com>
2024-02-16 14:20:16 +0100

    Encore une coquille

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
@@ -283,7 +283,7 @@ class compte_teledeclarantActions extends sfActions {
             $this->entities['raison_sociale'][] = htmlspecialchars($compte->getSociete()->raison_sociale, ENT_XML1, 'UTF-8');
             $this->entities['siret'][] = str_replace(' ', '', $compte->getSociete()->siret);
             $this->entities['tva'][] = str_replace(' ', '', $compte->getSociete()->no_tva_intracommunautaire);
-            $this->entities['adresse'][] = htmlspecialchars($e->etablissement->getAdresse(), ENT_XML1, 'UTF-8');
+            $this->entities['adresse'][] = htmlspecialchars($compte->getSociete()->getAdresse(), ENT_XML1, 'UTF-8');
             $this->entities['adresse_complementaire'][] = htmlspecialchars($compte->getSociete()->getAdresseComplementaire(), ENT_XML1, 'UTF-8');
             $this->entities['code_postal'][] = $compte->getSociete()->getCodePostal();
             $this->entities['commmune'][] = htmlspecialchars($compte->getSociete()->getCommune(), ENT_XML1, 'UTF-8');
