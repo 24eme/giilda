1c9e566aa05febc389456304a54650171a1d0939
Vincent LAURENT <vince.laurent@gmail.com>
2024-03-15 09:30:36 +0100

    Revert "Blog mandat sepa dans la facturation"
    This reverts commit b471de30aa1856f4b96095685b70f286e0dcde9f.

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/actions/actions.class.php
@@ -137,6 +137,8 @@ class compte_teledeclarantActions extends sfActions {
         }
 
         $this->societe = $this->compte->getSociete();
+        $this->mandatSepa = MandatSepaClient::getInstance()->findLastBySociete($this->societe);
+
 
         $this->form = new CompteTeledeclarantForm($this->compte);
 
--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationSuccess.php
@@ -181,6 +181,76 @@ if($compte->getSociete()->isTransaction()){
 </div>
 </div>
 
+<?php if (MandatSepaConfiguration::getInstance()->isActive()): ?>
+<div class="row">
+  <div class="col-xs-12">
+      <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="titre_principal">Coordonnées bancaires</h4>
+          </div>
+          <div class="panel-body">
+
+          <div class="col-xs-12">
+            <h4>Vos coordonnées bancaires : </h4>
+          </div>
+          <?php if ($mandatSepa): ?>
+            <div class="col-xs-8">
+              <div class="row">
+                <div class="col-xs-6 text-right">
+                    <label>IBAN :</label>
+                </div>
+                <div class="col-xs-6 text-left">
+                    <?php echo $mandatSepa->debiteur->iban; ?>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-6 text-right">
+                    <label>BIC :</label>
+                </div>
+                <div class="col-xs-6 text-left">
+                    <?php echo $mandatSepa->debiteur->bic; ?>
+                </div>
+              </div>
+              <div class="row">&nbsp;</div>
+              <div class="row">
+                <div class="col-xs-6 text-right">
+                    <label>Mandat de prélèvement SEPA :</label>
+                </div>
+                <div class="col-xs-6 text-left">
+                    <a href="<?php echo url_for('mandatsepa_pdf', $mandatSepa) ?>" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-file"></span>&nbsp;Télécharger le document</a>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xs-6 text-right">
+                    <label>Statut :</label>
+                </div>
+                <div class="col-xs-6 text-left<?php if(!$mandatSepa->is_signe): ?> text-danger<?php endif; ?>">
+                    <?php echo $mandatSepa->getStatut(); ?>
+                </div>
+              </div>
+            </div>
+            <?php if (!$mandatSepa->is_telecharge && false): ?>
+            <?php include_partial('mandatsepa/popupIncitationSignatureMandat', array('mandatSepa' => $mandatSepa)); ?>
+            <?php endif; ?>
+          <?php else: ?>
+            <div class="col-xs-8">
+              <div class="row">
+                <div class="col-xs-6 text-right"></div>
+                <div class="col-xs-6 text-left">
+                  <p>Vous n'avez pas saisi de coordonnées bancaires</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12">
+                  <a href="<?php echo url_for('compte_teledeclarant_coordonnees_bancaires') ?>" class=" btn btn-warning modifier" style="cursor: pointer; float: right;">Saisir vos coordonnées bancaires</a>
+            </div>
+          <?php endif; ?>
+          </div>
+    </div>
+  </div>
+</div>
+<?php endif; ?>
+
 <div class="col-xs-12">
   <a href="<?php echo url_for('common_homepage'); ?>" class=" btn btn-default " alt="Retour" style="cursor: pointer;">Retour</a>
 </div>
