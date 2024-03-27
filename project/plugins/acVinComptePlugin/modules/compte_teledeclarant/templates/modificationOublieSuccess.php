19eaaa270112c962e29fbf9bb64bd585c893e8b6
Vincent LAURENT <vince.laurent@gmail.com>
2024-03-27 02:00:56 +0100

    Mise en forme html/css de toutes les étapes de mot de passe oublié

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationOublieSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationOublieSuccess.php
@@ -1,34 +1,34 @@
 <form action="" class="form-horizontal" method="post" id="principal">
-<div class="row">
-  <div class="col-xs-10 col-xs-offset-1">
+  <div class="row">
+    <div class="col-xs-10 col-xs-offset-1">
       <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4 class="titre_principal">Modification de votre mot de passe</h4>
-          </div>
-    <div class="panel-body">
-                <p style="margin-bottom: 20px;" class="intro">Merci d'indiquer un nouveau mot de passe :</p>
-                <?php echo $form->renderHiddenFields(); ?>
-                <?php echo $form->renderGlobalErrors(); ?>
-                <?php echo $form['mdp1']->renderError() ?>
-                <div class="form-group">
-                   <?php echo $form['mdp1']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
-                   <div class="col-sm-5">
-                     <?php echo $form['mdp1']->render() ?>
-                   </div>
-                </div>
-                <?php echo $form['mdp2']->renderError() ?>
-                <div class="form-group">
-                   <?php echo $form['mdp2']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
-                   <div class="col-sm-5">
-                     <?php echo $form['mdp2']->render() ?>
-                   </div>
-                </div>
-
-                <div class="col-xs-12 text-right">
-                     <button class="btn btn-primary" type="submit">Valider</button>
-                </div>
-            </div>
+        <div class="panel-heading">
+          <h4 class="titre_principal">Modification de votre mot de passe</h4>
         </div>
-    </div>
+        <div class="panel-body">
+          <p style="margin-bottom: 30px;">Merci d'indiquer un nouveau mot de passe.</p>
+          <?php echo $form->renderHiddenFields(); ?>
+          <?php echo $form->renderGlobalErrors(); ?>
+          <?php echo $form['mdp1']->renderError() ?>
+          <div class="form-group">
+            <?php echo $form['mdp1']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+            <div class="col-sm-5">
+              <?php echo $form['mdp1']->render(['autofocus' => 'autofocus']) ?>
+              </div>
+            </div>
+            <?php echo $form['mdp2']->renderError() ?>
+            <div class="form-group">
+              <?php echo $form['mdp2']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+              <div class="col-sm-5">
+                <?php echo $form['mdp2']->render() ?>
+                </div>
+              </div>
+
+              <div class="text-right" style="margin-top: 30px;">
+                <button class="btn btn-primary" type="submit">Valider</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-</form>
+    </form>
--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/motDePasseOublieSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/motDePasseOublieSuccess.php
@@ -1,40 +1,27 @@
-<!-- #principal -->
-<div id="principal" class="clearfix mdp_oublie">
-<div class="row">
-  <div class="col-xs-12">
+<form action="" class="form-horizontal" method="post" id="principal">
+  <div class="row">
+    <div class="col-xs-10 col-xs-offset-1">
       <div class="panel panel-default">
-          <div class="panel-heading">
-    <form action="" class="ui-tabs" method="post">
-        <h4 class="titre_principal">Mon compte - Mot de passe oublié</h4>
-</div>
-  <div class="panel panel-body">
-        <p class="titre_section">Afin de récuperer votre mot de passe veuillez renseigner votre identifiant.</p>
-        <br/>
-        <div id="nouvelle_declaration" class="row">
-            <?php echo $form->renderHiddenFields(); ?>
-              <div class="ligne_form col-xs-12">
-            <?php echo $form->renderGlobalErrors(); ?>
-</div>
-            <div class="ligne_form col-xs-12">
-                <?php echo $form['login']->renderError() ?>
-              </div>
-              <div class="ligne_form col-xs-12">
-                <div class="row">
-                    <div class="col-xs-4 text-right">
-                        <?php echo $form['login']->renderLabel() ?>
-                    </div>
-                    <div class="col-xs-8 text-left">
-                      <?php echo $form['login']->render() ?>
-                  </div>
-                </div>
-            <div class="col-xs-12 text-right">
-              <button class="btn btn-warning" type="submit">Valider</button>
-            </div>
+        <div class="panel-heading">
+          <h4 class="titre_principal">Mon compte - Mot de passe oublié</h4>
         </div>
-    </form>
-  </div>
+        <div class="panel-body">
+          <p style="margin-bottom: 30px;">Afin de récuperer votre mot de passe veuillez renseigner votre identifiant.</p>
+          <?php echo $form->renderHiddenFields(); ?>
+          <?php echo $form->renderGlobalErrors(); ?>
+          <?php echo $form['login']->renderError() ?>
+          <div class="form-group">
+            <?php echo $form['login']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+            <div class="col-sm-5">
+              <?php echo $form['login']->render(['autofocus' => 'autofocus']) ?>
+            </div>
+          </div>
+
+          <div class="text-right" style="margin-top: 30px;">
+            <button class="btn btn-primary" type="submit">Valider</button>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-</div>
-</div>
-</div>
+  </div>
+</form>
