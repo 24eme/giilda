cd3ea7ac20b35033c5d7af075e320d3a098c6744
Vincent LAURENT <vince.laurent@gmail.com>
2024-03-27 15:26:06 +0100

    Mise en forme css de la page mon compte

--- project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationSuccess.php
+++ project/plugins/acVinComptePlugin/modules/compte_teledeclarant/templates/modificationSuccess.php
@@ -7,7 +7,6 @@ if($compte->getSociete()->isTransaction()){
 }
 
 ?>
-<div id="principal" >
   <div class="row">
     <div class="col-xs-12">
         <div class="panel panel-default">
@@ -15,159 +14,128 @@ if($compte->getSociete()->isTransaction()){
               <h4 class="titre_principal">Mon compte <span style="opacity: 0.5;" class="text-muted pull-right"><?php echo $compte->nom_a_afficher ?></span></h4>
             </div>
             <div class="panel-body">
-            <?php if($compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_NOUVEAU): ?>
+            <?php if ($sf_user->hasFlash('maj')) : ?>
+                <p class="alert alert-success"><?php echo $sf_user->getFlash('maj'); ?></p>
+            <?php endif; ?>
+            <?php if($sf_user->isAdmin() && $compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_NOUVEAU): ?>
            <div class="alert alert-warning">Ce compte n'a pas encore été créé par le télédéclarant : <a href="<?php echo url_for('societe_visualisation', $compte->getSociete()) ?>">Voir la société</a></div>
-         <?php elseif($compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_OUBLIE): ?>
-           <div class="alert alert-info">Ce compte en est cours de mot de passe oublié : <a href="<?php echo url_for('societe_visualisation', $compte->getSociete()) ?>">Voir la société</a></div>
-         <?php elseif($compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_INACTIF): ?>
+         <?php elseif($sf_user->isAdmin() && $compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_OUBLIE): ?>
+           <div class="alert alert-info">Ce compte est en procédure de mot de passe oublié : <a href="<?php echo url_for('societe_visualisation', $compte->getSociete()) ?>">Voir la société</a></div>
+         <?php elseif($sf_user->isAdmin() && $compte->getStatutTeledeclarant() == CompteClient::STATUT_TELEDECLARANT_INACTIF): ?>
            <div class="alert alert-danger">Ce compte est suspendu : <a href="<?php echo url_for('societe_visualisation', $compte->getSociete()) ?>">Voir la société</a></div>
          <?php endif; ?>
 
          <?php if(isset($form)): ?>
 
-        <div id="modification_compte" class="col-xs-12">
-        	<p>Sur cette page, si besoin, vous pouvez redéfinir votre mot de passe ou modifier vos informations de contact.</p>
-            <div class="presentation row" <?php if ($form->hasErrors()) echo ' style="display:none;"'; ?> >
-              <div class="col-xs-12">
-                <h4>Vos identifiants de connexion : </h4>
+        <div id="modification_compte">
+        	<p style="margin-bottom: 30px;">Sur cette page, si besoin, vous pouvez redéfinir votre mot de passe ou modifier vos informations de contact.</p>
+          <div class="presentation form-horizontal" <?php if ($form->hasErrors()) echo ' style="display:none;"'; ?> >
 
-                <?php if ($sf_user->hasFlash('maj')) : ?>
-                    <p class="alert alert-success"><?php echo $sf_user->getFlash('maj'); ?></p>
-                <?php endif; ?>
+            <h4>Vos identifiants de connexion : </h4>
+
+              <div class="form-group">
+                <label class="col-sm-4 control-label">Login :</label>
+                <div class="col-sm-4">
+                  <p class="form-control-static"><?php echo $compte->login; ?></p>
+                </div>
               </div>
 
-
-                <div class="col-xs-8" >
-                  <div class="row">
-                    <div class="col-xs-6 text-right">
-                        <label>Login :</label>
-                    </div>
-                    <div class="col-xs-6 text-left">
-                        <?php echo $compte->login; ?>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-6 text-right">
-                        <label>Mot de passe :</label>
-                    </div>
-                    <div class="col-xs-6 text-left">
-                        ******
-                    </div>
-                  </div>
+              <div class="form-group">
+                <label class="col-sm-4 control-label">Mot de passe :</label>
+                <div class="col-sm-4">
+                  <p class="form-control-static">******</p>
                 </div>
+              </div>
 
-                <div class="col-xs-12">
-                	<h4>Vos informations de contact : </h4>
-              	</div>
+              <div class="form-group"><label class="col-sm-4 control-label"></label><div class="col-sm-4"><p class="form-control-static"></p></div></div>
 
+            	<h4>Vos informations de contact : </h4>
 
-                <div class="col-xs-8" >
-                  <div class="row">
-                    <div class="col-xs-6 text-right">
-                        <label>Email :</label>
-                    </div>
-                    <div class="col-xs-6 text-left">
-                        <?php echo $email_teledecl; ?>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-6 text-right">
-                        <label>Téléphone :</label>
-                    </div>
-                    <div class="col-xs-6 text-left">
-                        <?php echo ($etablissementPrincipal) ? $etablissementPrincipal->telephone_bureau : $compte->telephone_bureau; ?>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-xs-6 text-right">
-                        <label>Mobile :</label>
-                    </div>
-                    <div class="col-xs-6 text-left">
-                        <?php echo ($etablissementPrincipal) ? $etablissementPrincipal->telephone_mobile : $compte->telephone_mobile; ?>
-                    </div>
-                  </div>
+              <div class="form-group">
+                <label class="col-sm-4 control-label">Email :</label>
+                <div class="col-sm-4">
+                  <p class="form-control-static"><?php echo $email_teledecl; ?></p>
                 </div>
+              </div>
 
-                <div class="col-xs-12">&nbsp;<br/><br/></div>
-                <div class="col-xs-12">
-                      <a href="#" class=" btn btn-warning modifier" style="cursor: pointer; float: right;">Modifier les informations</a>
+              <div class="form-group">
+                <label class="col-sm-4 control-label">Téléphone :</label>
+                <div class="col-sm-4">
+                  <p class="form-control-static"><?php echo ($etablissementPrincipal) ? $etablissementPrincipal->telephone_bureau : $compte->telephone_bureau; ?></p>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label class="col-sm-4 control-label">Mobile :</label>
+                <div class="col-sm-4">
+                  <p class="form-control-static"><?php echo ($etablissementPrincipal) ? $etablissementPrincipal->telephone_mobile : $compte->telephone_mobile; ?></p>
+                </div>
+              </div>
+
+                <div>
+                    <a href="<?php echo url_for('common_homepage'); ?>" class=" btn btn-default " alt="Retour" style="cursor: pointer;">Retour</a>
+                      <a href="#" class=" btn btn-default modifier" style="cursor: pointer; float: right;">Modifier les informations</a>
                 </div>
             </div>
-            <div class="modification clearfix"<?php if (!$form->hasErrors()) echo ' style="display:none;"'; ?>>
-              <div class="col-xs-12">
-                <h4>Modification de vos identifiants de connexion :</h4>
-              </div>
+            <div class="modification"<?php if (!$form->hasErrors()) echo ' style="display:none;"'; ?>>
 
-                <form method="post" action="">
+                <form method="post" class="form-horizontal" action="">
                     <?php echo $form->renderHiddenFields(); ?>
                     <?php echo $form->renderGlobalErrors(); ?>
 
-                    <div class="col-xs-8" >
-                      <div class="row">
-                        <div class="col-xs-6 text-right">
-                            <label>Login :</label>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                    	<?php echo $compte->login; ?>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-12"><?php echo $form['mdp1']->renderError() ?></div>
-                        <div class="col-xs-6 text-right">
-                            <?php echo $form['mdp1']->renderLabel() ?>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                              <?php echo $form['mdp1']->render() ?>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-12"><?php echo $form['mdp2']->renderError() ?></div>
-                        <div class="col-xs-6 text-right">
-                            <?php echo $form['mdp2']->renderLabel() ?>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                              <?php echo $form['mdp2']->render() ?>
-                        </div>
+                    <h4>Modification de vos identifiants de connexion :</h4>
+                    <div class="form-group">
+                      <label class="col-sm-4 control-label">Login :</label>
+                      <div class="col-sm-4">
+                        <p class="form-control-static"><?php echo $compte->login; ?></p>
                       </div>
                     </div>
 
-                    <div class="col-xs-12">
-		            	<h4>Modification de vos informations de contact :</h4>
-		            </div>
-
-                    <div class="col-xs-8" >
-                      <div class="row">
-                        <div class="col-xs-12"><?php echo $form['email']->renderError() ?></div>
-                        <div class="col-xs-6 text-right">
-                            <?php echo $form['email']->renderLabel() ?>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                              <?php echo $form['email']->render() ?>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-12"><?php echo $form['telephone_bureau']->renderError() ?></div>
-                        <div class="col-xs-6 text-right">
-                            <?php echo $form['telephone_bureau']->renderLabel() ?>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                              <?php echo $form['telephone_bureau']->render() ?>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="col-xs-12"><?php echo $form['telephone_mobile']->renderError() ?></div>
-                        <div class="col-xs-6 text-right">
-                            <?php echo $form['telephone_mobile']->renderLabel() ?>
-                        </div>
-                        <div class="col-xs-6 text-left">
-                              <?php echo $form['telephone_mobile']->render() ?>
-                        </div>
+                    <?php echo $form['mdp1']->renderError() ?>
+                    <div class="form-group">
+                      <?php echo $form['mdp1']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+                      <div class="col-sm-4">
+                        <?php echo $form['mdp1']->render() ?>
                       </div>
                     </div>
 
-                    <div class="col-xs-12">
-                        <a class="btn btn-danger annuler" style="float: left;" href="#" >Annuler</a>
-                        <button type="submit" class=" btn btn-success btn_valider modifier pull-right" style="cursor: pointer;" >Valider</button>
+                    <?php echo $form['mdp2']->renderError() ?>
+                    <div class="form-group">
+                      <?php echo $form['mdp2']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+                      <div class="col-sm-4">
+                        <?php echo $form['mdp2']->render() ?>
+                      </div>
+                    </div>
+
+		            	  <h4>Modification de vos informations de contact :</h4>
+
+                    <?php echo $form['email']->renderError() ?>
+                    <div class="form-group">
+                      <?php echo $form['email']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+                      <div class="col-sm-4">
+                        <?php echo $form['email']->render() ?>
+                      </div>
+                    </div>
+
+                    <?php echo $form['telephone_bureau']->renderError() ?>
+                    <div class="form-group">
+                      <?php echo $form['telephone_bureau']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+                      <div class="col-sm-4">
+                        <?php echo $form['telephone_bureau']->render() ?>
+                      </div>
+                    </div>
+
+                    <?php echo $form['telephone_mobile']->renderError() ?>
+                    <div class="form-group">
+                      <?php echo $form['telephone_mobile']->renderLabel(null, ['class' => 'col-sm-4 control-label']) ?>
+                      <div class="col-sm-4">
+                        <?php echo $form['telephone_mobile']->render() ?>
+                      </div>
+                    </div>
+
+                    <div>
+                        <a class="btn btn-default annuler" style="float: left;" href="#" >Annuler</a>
+                        <button type="submit" class=" btn btn-primary btn_valider modifier pull-right" style="cursor: pointer;" >Valider</button>
                     </div>
 
                 </form>
@@ -179,10 +147,4 @@ if($compte->getSociete()->isTransaction()){
 </div>
 </div>
 </div>
-</div>
-
-<div class="col-xs-12">
-  <a href="<?php echo url_for('common_homepage'); ?>" class=" btn btn-default " alt="Retour" style="cursor: pointer;">Retour</a>
-</div>
-
 </div>
