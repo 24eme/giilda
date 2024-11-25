961d0f85836b987d8f2add199fc0bb85548322da
Tangui Morlier <tangui@tangui.eu.org>
2024-11-25 12:42:00 +0100

    Copier/Coller plus large pour le mot de passe oublié

--- project/plugins/acVinComptePlugin/modules/compte/templates/_visualisationLogin.php
+++ project/plugins/acVinComptePlugin/modules/compte/templates/_visualisationLogin.php
@@ -1,7 +1,7 @@
 <?php $route = ($sf_request->getAttribute('sf_route')) ? $sf_request->getAttribute('sf_route')->getRawValue() : null; ?>
 <div class="row">
 <div style="margin-bottom: 5px;" class="col-xs-1  text-muted">Login&nbsp;:</div>
-<div style="margin-bottom: 5px;" class="col-xs-3">
+<div style="margin-bottom: 5px;" class="col-xs-2">
 <?php echo $compte->getLogin(); ?>
 <?php if ($sf_user->hasCredential(myUser::CREDENTIAL_ADMIN) && $compte && !$sf_user->isUsurpationCompte() && $compte->getLogin()) : ?>
             <a style="text-decoration: none; color:gray;" href="<?php echo url_for('auth_usurpation', array('identifiant' => $compte->identifiant)) ?>" title="Connexion mode déclarant"><span class="glyphicon glyphicon-cloud-upload"></span></a>
@@ -11,12 +11,16 @@
 <?php if (preg_match('/{TEXT}(.*)/', $compte_login->mot_de_passe, $m)) : ?>
 <div class="col-xs-2"> &nbsp; </div>
 <div style="margin-bottom: 5px;" class="col-xs-3 text-muted">Code de création&nbsp;:</div>
-<div style="margin-bottom: 5px;" class="col-xs-3"><?php echo $m[1]; ?></div>
+<div style="margin-bottom: 5px;" class="col-xs-4"><?php echo $m[1]; ?></div>
 <?php elseif (preg_match('/{OUBLIE}(.*)/',  $compte_login->mot_de_passe, $m)) : ?>
-    <div style="margin-bottom: 5px;" class="col-xs-8"><span class="text-muted">Mot de passe oublié&nbsp;:</span> <input id="input-share-link" type="text" size=35 value="<?php echo "https://".$_SERVER['HTTP_HOST'].url_for("compte_teledeclarant_mot_de_passe_oublie_login", array("login" =>  $compte_login->identifiant, "mdp" => $m[1])); ?>" disabled="disabled"/> <a href="#" onclick="navigator.clipboard.writeText(document.getElementById('input-share-link').value); console.log([this, this.getElementsByTagName('i')]); const icon_message = this.getElementsByTagName('i')[0]; icon_message.classList.remove('glyphicon-duplicate');icon_message.classList.add('glyphicon-ok');setTimeout(function() { icon_message.classList.remove('glyphicon-ok');icon_message.classList.add('glyphicon-duplicate');}, 750);return false;"><i class="glyphicon glyphicon-duplicate"></i></a></div>
+    <div onclick="navigator.clipboard.writeText(document.getElementById('input-share-link').value); const icon_message = this.getElementsByTagName('i')[0]; icon_message.classList.remove('glyphicon-duplicate');icon_message.classList.add('glyphicon-ok');setTimeout(function() { icon_message.classList.remove('glyphicon-ok');icon_message.classList.add('glyphicon-duplicate');}, 750);return false;" style="margin-bottom: 5px;" class="col-xs-8">
+      <span class="text-muted">Mot de passe oublié&nbsp;:</span>
+      <input id="input-share-link" type="text" size=35 value="<?php echo "https://".$_SERVER['HTTP_HOST'].url_for("compte_teledeclarant_mot_de_passe_oublie_login", array("login" =>  $compte_login->identifiant, "mdp" => $m[1])); ?>"/>
+      <a><i class="glyphicon glyphicon-duplicate"></i></a>
+    </div>
 <?php else: ?>
 <div class="col-xs-2"> &nbsp; </div>
-<div style="margin-bottom: 5px;" class="col-xs-6 text-muted">Mot de passe déjà créé</div>
+<div style="margin-bottom: 5px;" class="col-xs-7 text-muted">Mot de passe déjà créé</div>
 <?php endif; ?>
 </div>
 <?php if ($compte->getTeledeclarationEmail()) : ?>
