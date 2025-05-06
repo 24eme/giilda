35bc635e3d7cbab6d4ced41ac05750cf39ffee72
Tangui Morlier <tangui@tangui.eu.org>
2025-05-06 18:24:51 +0200

    Transmisssion Required

--- project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
+++ project/plugins/acVinDRMPlugin/modules/drm_validation/templates/_signature_popup.php
@@ -21,7 +21,7 @@
                       <div class="checkbox">
                           <?php echo $validationForm['transmission_ciel']->renderError(); ?>
                           <label>
-                              <input id="drm_transmission_ciel_visible" type="checkbox"  value="1" checked="checked" />
+                              <input id="drm_transmission_ciel_visible" type="checkbox"  value="1" checked="checked" required="required"/>
                               <strong>Transmission pour préremplissage de votre DRM electronique sur le portail douane.gouv.fr</strong>
                           </label>
                       </div>
