00fae42f7c22424e49fb9955906695c5691d3208
Vincent LAURENT <vince.laurent@gmail.com>
2025-07-25 19:57:27 +0200

    igpsudouest: Recherche des informations d'une société à sa création à partir du siret, amélioration de la mise en forme du process de création et calculAuto du numéro de TVA

--- project/plugins/acVinSocietePlugin/modules/societe/templates/creationSocieteSuccess.php
+++ project/plugins/acVinSocietePlugin/modules/societe/templates/creationSocieteSuccess.php
@@ -1,38 +1,31 @@
+<ol class="breadcrumb">
+    <li><a href="<?php echo url_for('societe') ?>">Contacts</a></li>
+    <li class="active"><a href="">Création d'une société</a></li>
+</ol>
 
-<!-- #principal -->
-<section id="principal">
-    <ol class="breadcrumb">
-        <li><a href="<?php echo url_for('societe') ?>">Contacts</a></li>
-        <li class="active"><a href="">Création d'une société</a></li>
-
-    </ol>
-    <!-- #contenu_etape -->
-    <section id="contenu_etape">
-        <h2>Création d'une société</h2>
-        <form class="form-horizontal" action="<?php echo url_for('societe_creation'); ?>" method="post">
-            <div id="recherche_societe" class="col-md-8 panel panel-default">
+<form class="form-horizontal" action="<?php echo url_for('societe_creation'); ?>" method="post">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div id="recherche_societe" class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Création d'une société</h3>
+                </div>
                 <div class="panel-body">
                     <div class="form-group<?php if ($form['raison_sociale']->hasError()): ?> has-error<?php endif; ?>">
                         <?php echo $form['raison_sociale']->renderError(); ?>
-                        <?php echo $form['raison_sociale']->renderLabel(null, array("class" => "col-xs-6 control-label")); ?>
-                        <div class="col-xs-6"><?php echo $form['raison_sociale']->render(array("class" => "form-control first-focus", "autofocus" => "autofocus")); ?></div>
+                        <?php echo $form['raison_sociale']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+                        <div class="col-xs-8"><?php echo $form['raison_sociale']->render(array("class" => "form-control first-focus", "autofocus" => "autofocus")); ?></div>
                     </div>
-                    <div class="text-right col-xs-12"><button id="btn_rechercher" type="submit" class="btn btn-success">Créer</button></div>
+                    <div class="form-group<?php if ($form['siret']->hasError()): ?> has-error<?php endif; ?>">
+                        <?php echo $form['siret']->renderError(); ?>
+                        <?php echo $form['siret']->renderLabel(null, array("class" => "col-xs-4 control-label")); ?>
+                        <div class="col-xs-4"><?php echo $form['siret']->render(array("class" => "form-control first-focus")); ?></div>
+                        <div class="col-xs-offset-4 col-xs-8">
+                        <p class="help-block ">Facultatif, permet de rechercher automatiquement les informations depuis le site de l'INSEE</p>
+                        </div>
+                    </div>
+                    <div class="text-right"><button id="btn_rechercher" type="submit" class="btn btn-primary">Créer la société</button></div>
                 </div>
-        </form>
-    </section>
-</section>
-<?php
-slot('colButtons');
-?>
-<div id="action" class="bloc_col">
-    <h2>Action</h2>
-    <div class="contenu">
-        <div class="btnRetourAccueil">
-            <a href="<?php echo url_for('societe'); ?>" class="btn_majeur btn_acces"><span>Retour à l'accueil</span></a>
-        </div>
+            </div>
     </div>
-</div>
-<?php
-end_slot();
-?>
+</form>
