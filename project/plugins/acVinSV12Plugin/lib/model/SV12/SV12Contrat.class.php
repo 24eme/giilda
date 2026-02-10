e0e1ea8633531e5b63542707f58a3af500697978
JB LM <lemetayer.jb@gmail.com>
2026-02-10 18:41:49 +0100

    evite les warning pour les produits sans cvo

--- project/plugins/acVinSV12Plugin/lib/model/SV12/SV12Contrat.class.php
+++ project/plugins/acVinSV12Plugin/lib/model/SV12/SV12Contrat.class.php
@@ -199,7 +199,7 @@ class SV12Contrat extends BaseSV12Contrat {
     }
 
     public function getTauxCvo() {
-        $this->cvo = $this->getDroitCVO()->taux*1.0;
+        $this->cvo = ($this->getDroitCVO())? $this->getDroitCVO()->taux*1.0 : 0;
         return $this->cvo;
     }
 
