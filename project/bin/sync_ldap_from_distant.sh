756b18a321f6a940a95491b05f97c92fb7d89a3a
chachathecool <char.var@posteo.com>
2026-07-27 13:01:42 +0200

    Fix: supprimer les doubles guillemets dans les variables

--- project/bin/sync_ldap_from_distant.sh
+++ project/bin/sync_ldap_from_distant.sh
@@ -4,9 +4,9 @@ cd $(dirname $0)/..
 
 . bin/config.inc
 
-admindn=$(grep [^e]dn: config/app.yml | sed 's/.*dn: *//')
+admindn=$(grep [^e]dn: config/app.yml | sed 's/.*dn: *//' | sed 's/"//g')
 adminpass=$(grep -A 5  ldap config/app.yml | grep pass:  | sed 's/.*pass: *//' | sed 's/"//g' )
-basedc=$(grep dc: config/app.yml | sed 's/.*dc: *//')
+basedc=$(grep dc: config/app.yml | sed 's/.*dc: *//' | sed 's/"//g')
 
 ldapsearch -x -H "ldap://"$COUCHDISTANTHOST -D $admindn -w"$adminpass" -b $basedc  > /tmp/ldap.diff
 tail -n "+"$(grep -n "cn: monitor" /tmp/ldap.diff  | awk -F ':' '{print $1 + 1}' | tail -n 1) /tmp/ldap.diff  | ldapadd -x -D $admindn -w"$adminpass"
